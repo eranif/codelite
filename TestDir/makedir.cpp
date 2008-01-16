b711d98c512c2d72348bd28384de95116b2bd34d
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2008-01-16 16:39:06 +0000

    - added new utility for creating directories instead of relying on the system utils
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@939 9da81c78-c036-0410-9e1f-a2b0375e4b5a

---
+++ TestDir/makedir.cpp
@@ -0,0 +1,35 @@
+#include "wx/filefn.h"
+#include "wx/dir.h"
+#include <wx/init.h>	//wxInitializer
+#include <wx/string.h>	//wxString
+#include "wx/cmdline.h"
+
+static const wxCmdLineEntryDesc cmdLineDesc[] = 
+{
+	{wxCMD_LINE_PARAM,  NULL, NULL, wxT("input file"), wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_MULTIPLE },
+	{wxCMD_LINE_NONE }
+};
+
+
+int main(int argc, char **argv) 
+{
+	//Initialize the wxWidgets library
+	wxInitializer initializer;
+	
+	//parse the input
+	wxCmdLineParser parser;
+	parser.SetDesc(cmdLineDesc);
+	parser.SetCmdLine(argc, argv);
+	if (parser.Parse() != 0) {
+		return -1;
+	}
+
+	for (size_t i=0; i< parser.GetParamCount(); i++) {
+		wxString argument = parser.GetParam(i);
+		if( !wxDir::Exists(argument) ){
+			wxMkdir(argument, 0777);
+		}
+	}
+
+	return 0;
+}

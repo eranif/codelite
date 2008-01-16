548e682539b7b74325e2c22790988c751b8bdd69
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2008-01-16 21:30:05 +0000

    - Dirpicker control startup dir is now set according to the value in the text control
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@942 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- Plugin/dirpicker.cpp
+++ Plugin/dirpicker.cpp
@@ -1,20 +1,23 @@
+#include "wx/dir.h"
+#include "wx/filename.h"
 #include "dirpicker.h"
 #include "wx/sizer.h"
 #include "macros.h"
+#include "wx/log.h"
 #include <wx/dirdlg.h>
 
 BEGIN_EVENT_TABLE(DirPicker, wxPanel)
-EVT_TEXT(wxID_ANY, DirPicker::OnText)
+	EVT_TEXT(wxID_ANY, DirPicker::OnText)
 END_EVENT_TABLE()
 
 DirPicker::DirPicker(wxWindow *parent, wxWindowID id, const wxString &buttonCaption, const wxString &defaultPos, const wxString &message, const wxPoint& pos, const wxSize& size, long style)
-: wxPanel(parent, id, pos, size, wxTAB_TRAVERSAL | wxNO_BORDER)
-, m_path(NULL)
-, m_combo(NULL)
-, m_buttonCaption(buttonCaption)
-, m_dlgCaption(message)
-, m_style(style)
-, m_defaultPos(defaultPos)
+		: wxPanel(parent, id, pos, size, wxTAB_TRAVERSAL | wxNO_BORDER)
+		, m_path(NULL)
+		, m_combo(NULL)
+		, m_buttonCaption(buttonCaption)
+		, m_dlgCaption(message)
+		, m_style(style)
+		, m_defaultPos(defaultPos)
 {
 	CreateControls();
 	ConnectEvents();
@@ -34,10 +37,10 @@ void DirPicker::CreateControls()
 	wxBoxSizer *mainSizer = new wxBoxSizer(wxHORIZONTAL);
 	SetSizer(mainSizer);
 
-	if(m_style & wxDP_USE_TEXTCTRL){
+	if (m_style & wxDP_USE_TEXTCTRL) {
 		m_path = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize);
 		mainSizer->Add(m_path, 1, wxLEFT | wxTOP | wxBOTTOM, 0);
-	}else{
+	} else {
 		m_combo = new wxComboBox(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize);
 		mainSizer->Add(m_combo, 1, wxLEFT | wxTOP | wxBOTTOM, 0);
 	}
@@ -57,14 +60,27 @@ void DirPicker::OnButtonClicked(wxCommandEvent &event)
 {
 	wxUnusedVar(event);
 
-	if(m_defaultPos.IsEmpty())
-	{
+	//no default position was provided by the user
+	//try to take it from the text control
+	wxString work_dir = GetPath();
+	m_defaultPos.Empty();
+	if (work_dir.IsEmpty() == false) {
+		if( wxDir::Exists(work_dir) ){
+			wxFileName fn(work_dir, wxEmptyString);
+			wxLogMessage(work_dir + wxT(" exists"));
+			fn.MakeAbsolute();
+			m_defaultPos = fn.GetFullPath();
+		}
+	}
+
+	if (m_defaultPos.IsEmpty()) {
 		m_defaultPos = wxGetCwd();
 	}
 
+	wxLogMessage(wxT("setting working dir to : ") + m_defaultPos );
+	
 	wxDirDialog *dlg = new wxDirDialog(this, m_dlgCaption, m_defaultPos);
-	if(dlg->ShowModal() == wxID_OK)
-	{
+	if (dlg->ShowModal() == wxID_OK) {
 		// Get the dirname
 		wxString path = dlg->GetPath();
 		SetPath(path);
@@ -74,12 +90,12 @@ void DirPicker::OnButtonClicked(wxCommandEvent &event)
 
 wxString DirPicker::GetPath()
 {
-	if(m_style & wxDP_USE_TEXTCTRL)
+	if (m_style & wxDP_USE_TEXTCTRL)
 		return m_path->GetValue();
-	else{
+	else {
 		wxString path(m_combo->GetValue());
 		//if the path is not whitin the combobox list, add it
-		if(m_combo->FindString(path) == wxNOT_FOUND){
+		if (m_combo->FindString(path) == wxNOT_FOUND) {
 			SetPath(path);
 		}
 		return path;
@@ -88,17 +104,13 @@ wxString DirPicker::GetPath()
 
 void DirPicker::SetPath(const wxString &path)
 {
-	if(m_style & wxDP_USE_TEXTCTRL)
+	if (m_style & wxDP_USE_TEXTCTRL)
 		m_path->SetValue(path);
-	else
-	{
+	else {
 		int where = m_combo->FindString(path);
-		if(where != wxNOT_FOUND)
-		{
+		if (where != wxNOT_FOUND) {
 			m_combo->SetSelection(where);
-		}
-		else
-		{
+		} else {
 			where = m_combo->Append(path);
 			m_combo->SetSelection(where);
 		}

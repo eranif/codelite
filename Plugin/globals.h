da69f5d4232d1b40e4ea4dac17de8fb8978af562
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2008-03-24 19:08:33 +0000

    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@1206 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- Plugin/globals.h
+++ Plugin/globals.h
@@ -5,16 +5,16 @@
 #include "wx/arrstr.h"
 class wxListCtrl;
 
-extern void SendCmdEvent(int eventId, void *clientData = NULL);
-extern void PostCmdEvent(int eventId, void *clientData = NULL);
-extern void SetColumnText (wxListCtrl *list, long indx, long column, const wxString &rText, int imgId = wxNOT_FOUND );
-extern wxString GetColumnText(wxListCtrl *list, long index, long column);
-extern long AppendListCtrlRow(wxListCtrl *list);
-extern bool ReadFileWithConversion(const wxString &fileName, wxString &content);
-extern bool RemoveDirectory(const wxString &path);
-extern bool IsValidCppIndetifier(const wxString &id);
-extern bool IsValidCppFile(const wxString &id);
-extern wxString ExpandAllVariables(const wxString &expression, const wxString &projectName, const wxString &fileName);
+void SendCmdEvent(int eventId, void *clientData = NULL);
+void PostCmdEvent(int eventId, void *clientData = NULL);
+void SetColumnText (wxListCtrl *list, long indx, long column, const wxString &rText, int imgId = wxNOT_FOUND );
+wxString GetColumnText(wxListCtrl *list, long index, long column);
+long AppendListCtrlRow(wxListCtrl *list);
+bool ReadFileWithConversion(const wxString &fileName, wxString &content);
+bool RemoveDirectory(const wxString &path);
+bool IsValidCppIndetifier(const wxString &id);
+bool IsValidCppFile(const wxString &id);
+wxString ExpandAllVariables(const wxString &expression, const wxString &projectName, const wxString &fileName);
 
 #endif //GLOBALS_H
 

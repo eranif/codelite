a39eb0c9cdafc29dc7fff921e916a92f7a9f74d4
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2008-03-26 18:55:33 +0000

    added update.bat script that copies the runtime file to C:\Program Files\CodeLite
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@1231 9da81c78-c036-0410-9e1f-a2b0375e4b5a

---
+++ Runtime/update.bat
@@ -0,0 +1,7 @@
+@echo off
+copy debuggers\*.dll "C:\Program Files\CodeLite\debuggers\"
+copy plugins\*.dll "C:\Program Files\CodeLite\plugins\"
+copy rc\*.xrc "C:\Program Files\CodeLite\rc\"
+copy rc\*.xrc "C:\Program Files\CodeLite\rc\"
+copy templates\* "C:\Program Files\CodeLite\templates\"
+copy CodeLite.exe "C:\Program Files\CodeLite\"

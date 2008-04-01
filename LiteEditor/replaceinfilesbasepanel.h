2ff8c1647da16e44eef4af26d3ad71cd4de8bed6
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2008-04-01 05:30:45 +0000

    * reduced flicking on linux when seelcting same tab again * improved the layout of the 'Replace Results' tab so more sapce is available for results
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@1288 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- LiteEditor/replaceinfilesbasepanel.h
+++ LiteEditor/replaceinfilesbasepanel.h
@@ -14,11 +14,10 @@
 #include <wx/font.h>
 #include <wx/colour.h>
 #include <wx/settings.h>
+#include <wx/listbox.h>
+#include <wx/sizer.h>
 #include <wx/textctrl.h>
 #include <wx/button.h>
-#include <wx/sizer.h>
-#include <wx/statline.h>
-#include <wx/listbox.h>
 #include <wx/panel.h>
 
 ///////////////////////////////////////////////////////////////////////////
@@ -32,21 +31,20 @@ class ReplaceInFilesBasePanel : public wxPanel
 	private:
 	
 	protected:
+		wxStaticText* m_staticText8;
+		wxListBox* m_listBox1;
 		wxStaticText* m_staticText4;
 		wxTextCtrl* m_textCtrlReplaceWith;
 		wxButton* m_buttonReplaceAll;
 		wxButton* m_buttonClear;
-		wxStaticLine* m_staticline2;
-		wxStaticText* m_staticText8;
-		wxListBox* m_listBox1;
 		
 		// Virtual event handlers, overide them in your derived class
+		virtual void OnItemSelected( wxCommandEvent& event ){ event.Skip(); }
+		virtual void OnItemDClicked( wxCommandEvent& event ){ event.Skip(); }
 		virtual void OnReplaceAll( wxCommandEvent& event ){ event.Skip(); }
 		virtual void OnReplaceAllUI( wxUpdateUIEvent& event ){ event.Skip(); }
 		virtual void OnClearResults( wxCommandEvent& event ){ event.Skip(); }
 		virtual void OnClearResultsUI( wxUpdateUIEvent& event ){ event.Skip(); }
-		virtual void OnItemSelected( wxCommandEvent& event ){ event.Skip(); }
-		virtual void OnItemDClicked( wxCommandEvent& event ){ event.Skip(); }
 		
 	
 	public:

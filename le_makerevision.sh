ef8c9f963b3ec82322dd34e33fca66bbb4e800c6
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2008-04-06 09:50:03 +0000

    "
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@1340 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- le_makerevision.sh
+++ le_makerevision.sh
@@ -2,20 +2,19 @@
 file_exist=`test -f LiteEditor/svninfo.cpp`
 file_exist=$?
 
-cur_rev=`svn info | grep Revision | awk '{print $2;}'`
-if [ $file_exist -eq 1 ] 
+if [ $file_exist -eq 1 ]
 then
-	## file does not exist, create an empty file
-	touch LiteEditor/svninfo.cpp
+       ## file does not exist, create an empty file
+       touch LiteEditor/svninfo.cpp
 fi
-src_rev=`cat LiteEditor/svninfo.cpp | awk '{print $4;}' | sed s/\"//g | sed s/\;//g`
 
-if [ "$src_rev" != "$cur_rev" ]
-then
-	## generate the svninfo file again
-	\cp /dev/null LiteEditor/svninfo.cpp
-	echo char *SvnRevision = \"$cur_rev\"";" >> LiteEditor/svninfo.cpp
-	echo "" >> LiteEditor/svninfo.cpp
-	echo Generating svninfo file...
-fi
+cur_rev=`svn info | grep Revision | awk '{print $2;}'`
+
+rm -fr LiteEditor/svninfo.cpp
+
+## generate the svninfo file 
+echo "#include <wx/string.h>" >> LiteEditor/svninfo.cpp
+printf "const wxChar * SvnRevision = wxT(\"%s\");\n" ${cur_rev} >> LiteEditor/svninfo.cpp
+echo "" >> LiteEditor/svninfo.cpp
+echo Generating svninfo file...
 

52644ba47e877f599930f27141183caeb1bca6a3
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2008-04-23 22:20:43 +0000

    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@1430 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- LiteEditor/generic_context.cpp
+++ LiteEditor/generic_context.cpp
@@ -3,8 +3,8 @@
 #include "editor_config.h"
 #include "cl_editor.h"
 
-ContextGeneric::ContextGeneric(LEditor *container, const wxString &name) 
-: ContextBase(container)
+ContextGeneric::ContextGeneric(LEditor *container, const wxString &name)
+		: ContextBase(container)
 {
 	//-----------------------------------------------
 	// Load laguage settings from configuration file
@@ -18,36 +18,37 @@ ContextGeneric::~ContextGeneric()
 {
 }
 
-ContextBase *ContextGeneric::NewInstance(LEditor *container){
+ContextBase *ContextGeneric::NewInstance(LEditor *container)
+{
 	return new ContextGeneric(container, GetName());
 }
 
-// Dont implement this function, maybe derived child will want 
+// Dont implement this function, maybe derived child will want
 // to do something with it
 void ContextGeneric::AutoIndent(const wxChar &nChar)
 {
 	ContextBase::AutoIndent(nChar);
 }
 
-// Dont implement this function, maybe derived child will want 
+// Dont implement this function, maybe derived child will want
 // to do something with it
 void ContextGeneric::CodeComplete()
 {
 }
 
-// Dont implement this function, maybe derived child will want 
+// Dont implement this function, maybe derived child will want
 // to do something with it
 void ContextGeneric::CompleteWord()
 {
 }
 
-// Dont implement this function, maybe derived child will want 
+// Dont implement this function, maybe derived child will want
 // to do something with it
 void ContextGeneric::GotoDefinition()
 {
 }
 
-// Dont implement this function, maybe derived child will want 
+// Dont implement this function, maybe derived child will want
 // to do something with it
 void ContextGeneric::GotoPreviousDefintion()
 {
@@ -59,32 +60,36 @@ void ContextGeneric::ApplySettings()
 	std::list<StyleProperty> styles;
 	LexerConfPtr lexPtr;
 	// Read the configuration file
-	if(EditorConfigST::Get()->IsOk()){
+	if (EditorConfigST::Get()->IsOk()) {
 		lexPtr = EditorConfigST::Get()->GetLexer(m_name);
 	}
 
 	// Update the control
 	LEditor &rCtrl = GetCtrl();
-	rCtrl.SetLexer(lexPtr->GetLexerId());
 
 	wxString keyWords = lexPtr->GetKeyWords();
 	keyWords.Replace(wxT("\n"), wxT(" "));
 	keyWords.Replace(wxT("\r"), wxT(" "));
-	rCtrl.SetKeyWords(0, keyWords);
-	rCtrl.StyleClearAll();
+
+	for (int i = 0; i <= wxSCI_KEYWORDSET_MAX; ++i) {
+		rCtrl.SetKeyWords(i, keyWords);
+	}
 	
+	rCtrl.StyleClearAll();
+	rCtrl.SetStyleBits(rCtrl.GetStyleBitsNeeded());
+	rCtrl.SetLexer(lexPtr->GetLexerId());
+
 	styles = lexPtr->GetProperties();
 	std::list<StyleProperty>::iterator iter = styles.begin();
-	for(; iter != styles.end(); iter++)
-	{
+	for (; iter != styles.end(); iter++) {
 		StyleProperty st = (*iter);
 		int size = st.GetFontSize();
 		wxString face = st.GetFaceName();
 		bool bold = st.IsBold();
-		
+
 		wxFont font(size, wxFONTFAMILY_TELETYPE, wxNORMAL, bold ? wxBOLD : wxNORMAL, false, face);
 
-		if(st.GetId() == 0){ //default
+		if (st.GetId() == 0) { //default
 			rCtrl.StyleSetFont(wxSCI_STYLE_DEFAULT, font);
 			rCtrl.StyleSetSize(wxSCI_STYLE_DEFAULT, (*iter).GetFontSize());
 			rCtrl.StyleSetForeground(wxSCI_STYLE_DEFAULT, (*iter).GetFgColour());

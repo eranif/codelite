076896608b506c59dd7fdf43e9f4554848fd402c
rjpcomputing <rjpcomputing@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2008-04-30 19:35:34 +0000

    Removed duplicate line from the update.bat.
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@1474 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- Runtime/update.bat
+++ Runtime/update.bat
@@ -2,7 +2,6 @@
 copy debuggers\*.dll "C:\Program Files\CodeLite\debuggers\"
 copy plugins\*.dll "C:\Program Files\CodeLite\plugins\"
 copy rc\*.xrc "C:\Program Files\CodeLite\rc\"
-copy rc\*.xrc "C:\Program Files\CodeLite\rc\"
 copy templates\* "C:\Program Files\CodeLite\templates\"
 copy images\* "C:\Program Files\CodeLite\images\"
 copy astyle.sample "C:\Program Files\CodeLite\"

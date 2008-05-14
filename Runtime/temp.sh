f49c8a960ab1469e02381751acd3c4577104aa12
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2008-05-14 16:43:17 +0000

    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@1536 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- Runtime/temp.sh
+++ Runtime/temp.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
-exe_list="CodeLiteUR "
+## Get list of files to work on
+exe_name="CodeLiteUR"
+exe_list=$exe_name `ls ./plugins/*.so ./debuggers/*.so`
+
+## Loop over the files, and update the path of the wx library
 for file in $exe_list
 do
 	orig_path=`otool -L $file  | grep libwx | awk '{print $1;}'`
-	new_path=`otool -L $file  | grep libwx | awk '{print $1;}' | xargs basename`
-	echo install_name_tool -change $orig_path $new_path $file
-	install_name_tool -change $orig_path @executable_path/$new_path $file
+	
+	for path in $orig_path
+	do
+		new_path=`echo $path | xargs basename`
+		echo install_name_tool -change $orig_path @executable_path/$new_path $file
+		## install_name_tool -change $orig_path @executable_path/$new_path $file
+	done
 done
 

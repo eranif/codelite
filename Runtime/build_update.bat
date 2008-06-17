551ecebad13b1507e6f9ca67804a2506665de663
rjpcomputing <rjpcomputing@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2008-06-17 20:21:17 +0000

    Updated the *update.bat files to be much simpler and more dynamic. Now using 'xcopy'.
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@1699 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- Runtime/build_update.bat
+++ Runtime/build_update.bat
@@ -2,57 +2,23 @@
 ::Cleanup if there is an old update.
 if exist update rmdir /S /Q update
 
-::Make the directory tree
-if not exist update\debuggers mkdir update\debuggers
-if not exist update\plugins mkdir update\plugins
-if not exist update\rc mkdir update\rc
-if not exist update\templates\gizmos mkdir update\templates\gizmos
-if not exist update\templates\projects\dynamic-library mkdir update\templates\projects\dynamic-library
-if not exist update\templates\projects\dynamic-library-wx-enabled mkdir update\templates\projects\dynamic-library-wx-enabled
-if not exist update\templates\projects\executable mkdir update\templates\projects\executable
-if not exist update\templates\projects\premake-project mkdir update\templates\projects\premake-project
-if not exist update\templates\projects\static-library mkdir update\templates\projects\static-library
-if not exist update\templates\projects\static-library-wx-enabled mkdir update\templates\projects\static-library-wx-enabled
-if not exist update\templates\projects\vc-dynamic-library mkdir update\templates\projects\vc-dynamic-library
-if not exist update\templates\projects\vc-executable mkdir update\templates\projects\vc-executable
-if not exist update\templates\projects\vc-static-lib mkdir update\templates\projects\vc-static-lib
-if not exist update\images mkdir update\images
-if not exist update\lexers\Default mkdir update\lexers\Default
-if not exist update\lexers\BlackTheme mkdir update\lexers\BlackTheme
-if not exist update\bin mkdir update\bin
-if not exist update\sdk mkdir update\sdk
-if not exist update\sdk\lib mkdir update\sdk\lib
-if not exist update\sdk\include mkdir update\sdk\include
-if not exist update\sdk\include\UnitTest++ mkdir update\sdk\include\UnitTest++
-if not exist update\sdk\include\UnitTest++\win32 mkdir update\sdk\include\UnitTest++\win32
-
-:: Copy the files
-copy debuggers\*.dll "update\debuggers\"
-copy plugins\*.dll "update\plugins\"
-copy rc\*.xrc "update\rc\"
-copy templates\* "update\templates\"
-copy templates\gizmos\* "update\templates\gizmos\"
-copy templates\projects\* "update\templates\projects\"
-copy templates\projects\dynamic-library\* "update\templates\projects\dynamic-library\"
-copy templates\projects\dynamic-library-wx-enabled\* "update\templates\projects\dynamic-library-wx-enabled\"
-copy templates\projects\executable\* "update\templates\projects\executable\"
-copy templates\projects\premake-project\* "update\templates\projects\premake-project\"
-copy templates\projects\static-library\* "update\templates\projects\static-library\"
-copy templates\projects\static-library-wx-enabled\* "update\templates\projects\static-library-wx-enabled\"
-copy templates\projects\vc-dynamic-library\* "update\templates\projects\vc-dynamic-library\"
-copy templates\projects\vc-executable\* "update\templates\projects\vc-executable\"
-copy templates\projects\vc-static-lib\* "update\templates\projects\vc-static-lib\"
-copy images\* "update\images\"
-copy astyle.sample "update\"
-copy *.html "update\"
-copy config\*.default "update\config\"
-copy lexers\Default\*.xml "update\lexers\Default\"
-copy lexers\BlackTheme\*.xml "update\lexers\BlackTheme\"
-copy CodeLite.exe "update\"
-copy bin\*.exe "update\bin\"
-copy "sdk\lib\win32\*.a" "update\sdk\lib\"
-copy "sdk\include\UnitTest++\*.h" "update\sdk\include\UnitTest++\"
-copy "sdk\include\UnitTest++\win32\*.h" "update\sdk\include\UnitTest++\win32\"
-copy wxmsw28u_gcc*.dll "update\"
-copy ..\sdk\curl\lib\libcurl-4.dll "update\"
-copy bin\premakesinglebuild.lua "update\bin\"
+:: Copy the folders
+xcopy bin\*.exe "update\bin\" /E /I /H /Y /EXCLUDE:excludes
+xcopy config\*.default "update\config\" /E /I /H /Y /EXCLUDE:excludes
+xcopy debuggers\*.dll "update\debuggers\" /E /I /H /Y /EXCLUDE:excludes
+xcopy images\* "update\images\" /E /I /H /Y /EXCLUDE:excludes
+xcopy lexers\*.xml "update\lexers\" /E /I /H /Y /EXCLUDE:excludes
+xcopy plugins\*.dll "update\plugins\" /E /I /H /Y /EXCLUDE:excludes
+xcopy rc\*.xrc "update\rc\" /E /I /H /Y /EXCLUDE:excludes
+xcopy templates\* "update\templates\" /E /I /H /Y /EXCLUDE:excludes
+xcopy *.html "update\" /H /Y /EXCLUDE:excludes
+:: Copy the UnitTest++ files
+xcopy sdk\lib\win32\*.a "update\sdk\lib\" /Y
+xcopy sdk\include\UnitTest++\*.h "update\sdk\include\UnitTest++\" /Y
+xcopy sdk\include\UnitTest++\win32\*.h "update\sdk\include\UnitTest++\win32\" /Y
+:: Copy the misc files
+xcopy astyle.sample "update\" /Y
+xcopy CodeLite.exe "update\" /Y
+xcopy wxmsw28u_gcc*.dll "update\" /Y
+xcopy ..\sdk\curl\lib\libcurl-4.dll "update\" /Y
+xcopy bin\premakesinglebuild.lua "update\bin\" /Y
--- Runtime/update.bat
+++ Runtime/update.bat
@@ -1,17 +1,15 @@
 @echo off
-copy debuggers\*.dll "C:\Program Files\CodeLite\debuggers\"
-copy plugins\*.dll "C:\Program Files\CodeLite\plugins\"
-copy rc\*.xrc "C:\Program Files\CodeLite\rc\"
-copy templates\* "C:\Program Files\CodeLite\templates\"
-copy templates\gizmos\* "C:\Program Files\CodeLite\templates\gizmos\"
-copy templates\projects\* "C:\Program Files\CodeLite\templates\projects\"
-copy images\* "C:\Program Files\CodeLite\images\"
-copy astyle.sample "C:\Program Files\CodeLite\"
-copy *.html "C:\Program Files\CodeLite\"
-copy config\*.default "C:\Program Files\CodeLite\config\"
-copy lexers\Default\*.xml "C:\Program Files\CodeLite\lexers\Default\"
-copy lexers\BlackTheme\*.xml "C:\Program Files\CodeLite\lexers\BlackTheme\"
-copy ..\sdk\curl\lib\libcurl-4.dll "C:\Program Files\CodeLite\"
-copy ..\sdk\curl\lib\libcurl-4.dll .
-copy CodeLite.exe "C:\Program Files\CodeLite\"
-copy bin\*.exe "C:\Program Files\CodeLite\bin\"
+xcopy bin\*.exe "C:\Program Files\CodeLite\bin\" /E /I /H /Y /EXCLUDE:excludes
+xcopy config\*.default "C:\Program Files\CodeLite\config\" /E /I /H /Y /EXCLUDE:excludes
+xcopy debuggers\*.dll "C:\Program Files\CodeLite\debuggers\" /E /I /H /Y /EXCLUDE:excludes
+xcopy images\* "C:\Program Files\CodeLite\images\" /E /I /H /Y /EXCLUDE:excludes
+xcopy lexers\*.xml "C:\Program Files\CodeLite\lexers\Default\" /E /I /H /Y /EXCLUDE:excludes
+xcopy plugins\*.dll "C:\Program Files\CodeLite\plugins\" /E /I /H /Y /EXCLUDE:excludes
+xcopy rc\*.xrc "C:\Program Files\CodeLite\rc\" /E /I /H /Y /EXCLUDE:excludes
+xcopy templates\ "C:\Program Files\CodeLite\templates\" /E /I /H /Y /EXCLUDE:excludes
+xcopy *.html "C:\Program Files\CodeLite\" /H /Y /EXCLUDE:excludes
+:: Copy the misc files
+xcopy ..\sdk\curl\lib\libcurl-4.dll "C:\Program Files\CodeLite\" /Y
+xcopy ..\sdk\curl\lib\libcurl-4.dll . /Y
+xcopy astyle.sample "C:\Program Files\CodeLite\" /Y
+xcopy CodeLite.exe "C:\Program Files\CodeLite\" /Y

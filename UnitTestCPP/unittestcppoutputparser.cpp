68dfbde56edabe7eb06fb71c81dabbbf485140a7
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2008-06-19 16:06:55 +0000

    fixed unittest++ parsing on Mac
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@1709 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- UnitTestCPP/unittestcppoutputparser.cpp
+++ UnitTestCPP/unittestcppoutputparser.cpp
@@ -76,19 +76,32 @@ void UnitTestCppOutputParser::Parse(TestSummary &summary)
 		}
 
 		// test for error line
-		//main.cpp(82): error: Failure in Test4: false
+		
 		if (line.Contains(wxT(": error: Failure"))) {
 			// increase the error count
 			
 			int where = line.Find(wxT(": error: Failure"));
 			if ( where != wxNOT_FOUND ) {
 				ErrorLineInfo info;
+#if defined (__WXGTK__) || defined (__WXMSW__)				
+				// for some reason the output for Mac is different from the one
+				// on Windows & GTK
+				// main.cpp(82): error: Failure in Test4: false
 				info.file = line.BeforeFirst(wxT('('));
 				line = line.AfterFirst(wxT('('));
 				
 				info.line = line.BeforeFirst(wxT(')'));
 				info.description = line.AfterFirst(wxT(')'));
+#else	
+				// Mac output is:
+				// main.cpp:82: error: Failure in Test4: false
+				info.description = line.Mid((size_t)where);
 				
+				// remove the description
+				line = line.Mid(0, (size_t)where);
+				info.line = line.AfterLast(wxT(':'));
+				info.file = line.BeforeLast(wxT(':'));
+#endif
 				summary.errorLines.push_back(info);
 				summary.errorCount++;
 			}

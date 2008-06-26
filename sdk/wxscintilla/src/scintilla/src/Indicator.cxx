00c52ed321ec8a2cff0c163eec2a57ee9af5df4f
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2008-06-26 19:47:18 +0000

    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@1754 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- sdk/wxscintilla/src/scintilla/src/Indicator.cxx
+++ sdk/wxscintilla/src/scintilla/src/Indicator.cxx
@@ -73,8 +73,8 @@ void Indicator::Draw(Surface *surface, const PRectangle &rc, const PRectangle &r
 		rcBox.left = rc.left;
 		rcBox.right = rc.right;
 		
-//		surface->RoundedRectangle(rc, fore.allocated, fore.allocated);
-		surface->AlphaRectangle(rcBox, 2, fore.allocated, 70, fore.allocated, 100, 0);
+		surface->RoundedRectangle(rcBox, fore.allocated, fore.allocated);
+//		surface->AlphaRectangle(rcBox, 2, fore.allocated, 70, fore.allocated, 100, 0);
 	} else {	// Either INDIC_PLAIN or unknown
 		surface->MoveTo(rc.left, ymid);
 		surface->LineTo(rc.right, ymid);

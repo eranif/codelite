5fb5e7d3ef816b76c51b0c3a31830e760bc92fac
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2008-07-01 13:28:01 +0000

    - Generated Setters/Getters generates proper code for pointers members
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@1787 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- LiteEditor/setters_getters_dlg.cpp
+++ LiteEditor/setters_getters_dlg.cpp
@@ -1,39 +1,39 @@
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 //
-// copyright            : (C) 2008 by Eran Ifrah                            
-// file name            : setters_getters_dlg.cpp              
-//                                                                          
+// copyright            : (C) 2008 by Eran Ifrah
+// file name            : setters_getters_dlg.cpp
+//
 // -------------------------------------------------------------------------
-// A                                                                        
-//              _____           _      _     _ _                            
-//             /  __ \         | |    | |   (_) |                           
-//             | /  \/ ___   __| | ___| |    _| |_ ___                      
-//             | |    / _ \ / _  |/ _ \ |   | | __/ _ )                     
-//             | \__/\ (_) | (_| |  __/ |___| | ||  __/                     
-//              \____/\___/ \__,_|\___\_____/_|\__\___|                     
-//                                                                          
-//                                                  F i l e                 
-//                                                                          
-//    This program is free software; you can redistribute it and/or modify  
-//    it under the terms of the GNU General Public License as published by  
-//    the Free Software Foundation; either version 2 of the License, or     
-//    (at your option) any later version.                                   
-//                                                                          
+// A
+//              _____           _      _     _ _
+//             /  __ \         | |    | |   (_) |
+//             | /  \/ ___   __| | ___| |    _| |_ ___
+//             | |    / _ \ / _  |/ _ \ |   | | __/ _ )
+//             | \__/\ (_) | (_| |  __/ |___| | ||  __/
+//              \____/\___/ \__,_|\___\_____/_|\__\___|
+//
+//                                                  F i l e
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
- #include "precompiled_header.h"
+#include "precompiled_header.h"
 #include "setters_getters_dlg.h"
 #include "macros.h"
 #include "language.h"
 #include "wx/tokenzr.h"
 
 SettersGettersDlg::SettersGettersDlg(wxWindow* parent)
-: SettersGettersBaseDlg(parent)
+		: SettersGettersBaseDlg(parent)
 {
 	ConnectCheckBox(m_checkStartWithUppercase, SettersGettersDlg::OnCheckStartWithUpperCase);
 	ConnectCheckList(m_checkListMembers, SettersGettersDlg::OnCheckStartWithUpperCase);
-} 
+}
 
 void SettersGettersDlg::Init(const std::vector<TagEntryPtr> &tags, const wxFileName &file, int lineno)
 {
@@ -43,25 +43,24 @@ void SettersGettersDlg::Init(const std::vector<TagEntryPtr> &tags, const wxFileN
 	m_members = tags;
 
 	wxArrayString members;
-	for(size_t i=0; i<tags.size() ; i++){
+	for (size_t i=0; i<tags.size() ; i++) {
 		members.Add(tags.at(i)->GetName() + wxT(" : [Getter]"));
 		members.Add(tags.at(i)->GetName() + wxT(" : [Setter]"));
 
 		m_tagsMap[tags.at(i)->GetName() + wxT(" : [Getter]")] = tags.at(i);
 		m_tagsMap[tags.at(i)->GetName() + wxT(" : [Setter]")] = tags.at(i);
 	}
-	
+
 	//append all members to the check list
 	m_checkListMembers->Clear();
 	m_checkListMembers->Append(members);
-	
+
 	//uncheck all items
-	for(unsigned int i=0; i<m_checkListMembers->GetCount(); i++)
-	{
+	for (unsigned int i=0; i<m_checkListMembers->GetCount(); i++) {
 		m_checkListMembers->Check(i, false);
 	}
-	
-	if(tags.empty() == false){
+
+	if (tags.empty() == false) {
 		m_textClassName->SetValue(tags.at(0)->GetParent());
 	}
 
@@ -83,13 +82,13 @@ wxString SettersGettersDlg::GenerateFunctions()
 {
 	wxString code;
 	GenerateSetters(code);
-	if(code.IsEmpty() == false){
+	if (code.IsEmpty() == false) {
 		code.Prepend(wxT("//Setters\n"));
 		code << wxT("\n\n");
 	}
 	wxString settersCode;
 	GenerateGetters(settersCode);
-	if(settersCode.IsEmpty() == false){
+	if (settersCode.IsEmpty() == false) {
 		code << wxT("//Getters\n");
 		code << settersCode << wxT("\n");
 	}
@@ -98,17 +97,16 @@ wxString SettersGettersDlg::GenerateFunctions()
 
 void SettersGettersDlg::GenerateGetters(wxString &code)
 {
-	for(size_t i=0; i<m_checkListMembers->GetCount(); i++)
-	{
+	for (size_t i=0; i<m_checkListMembers->GetCount(); i++) {
 		TagEntryPtr tag;
-		if(m_checkListMembers->IsChecked((unsigned int)i)){
+		if (m_checkListMembers->IsChecked((unsigned int)i)) {
 			wxString item = m_checkListMembers->GetString((unsigned int)i);
 			//get the tag for this
 			std::map<wxString, TagEntryPtr>::iterator iter = m_tagsMap.find(item);
-			if(iter != m_tagsMap.end()){
+			if (iter != m_tagsMap.end()) {
 				tag = iter->second;
 				//geenerate function for this tag
-				if(item.EndsWith(wxT("[Getter]"))){
+				if (item.EndsWith(wxT("[Getter]"))) {
 					code << GenerateGetter(tag) << wxT("\n");
 				}
 			}
@@ -118,17 +116,16 @@ void SettersGettersDlg::GenerateGetters(wxString &code)
 
 void SettersGettersDlg::GenerateSetters(wxString &code)
 {
-	for(size_t i=0; i<m_checkListMembers->GetCount(); i++)
-	{
+	for (size_t i=0; i<m_checkListMembers->GetCount(); i++) {
 		TagEntryPtr tag;
-		if(m_checkListMembers->IsChecked((unsigned int)i)){
+		if (m_checkListMembers->IsChecked((unsigned int)i)) {
 			wxString item = m_checkListMembers->GetString((unsigned int)i);
 			//get the tag for this
 			std::map<wxString, TagEntryPtr>::iterator iter = m_tagsMap.find(item);
-			if(iter != m_tagsMap.end()){
+			if (iter != m_tagsMap.end()) {
 				tag = iter->second;
 				//geenerate function for this tag
-				if(item.EndsWith(wxT("[Setter]"))){
+				if (item.EndsWith(wxT("[Setter]"))) {
 					code << GenerateSetter(tag) << wxT("\n");;
 				}
 			}
@@ -141,34 +138,40 @@ wxString SettersGettersDlg::GenerateSetter(TagEntryPtr tag)
 	bool startWithUpper  = m_checkStartWithUppercase->IsChecked();
 
 	Variable var;
-	if(LanguageST::Get()->VariableFromPattern(tag->GetPattern(), tag->GetName(), var))
-	{
+	if (LanguageST::Get()->VariableFromPattern(tag->GetPattern(), tag->GetName(), var)) {
 		wxString func;
 		wxString scope = _U(var.m_typeScope.c_str());
 		func << wxT("void ");
-		if(startWithUpper){
+		if (startWithUpper) {
 			func << wxT("Set");
-		}else{
+		} else {
 			func << wxT("set");
 		}
 		wxString name = _U(var.m_name.c_str());
 		FormatName(name);
-		func << name << wxT("(const ");
+		
+		if(!var.m_isPtr){
+			func << name << wxT("(const ");
+		}else{
+			func << name << wxT("(");
+		}
 
-		if(!scope.IsEmpty() && !(scope == wxT("<global>"))){
+		if (!scope.IsEmpty() && !(scope == wxT("<global>"))) {
 			func << scope << wxT("::");
 		}
 
-		func << _U(var.m_type.c_str()) 
-			 << _U(var.m_templateDecl.c_str()) 
-			 << _U(var.m_starAmp.c_str())
-			 << wxT("& ");
-		
+		func << _U(var.m_type.c_str()) << _U(var.m_templateDecl.c_str()) << _U(var.m_starAmp.c_str());
+		if(!var.m_isPtr){
+			func << wxT("& ");
+		}else{
+			func << wxT(" ");
+		}
+
 		wxString tmpName = _U(var.m_name.c_str());
 		tmpName.StartsWith(wxT("m_"), &tmpName);
 
 		func << tmpName << wxT(") {this->") << _U(var.m_name.c_str()) << wxT(" = ") << tmpName << wxT(";}");
-		return func;			 
+		return func;
 	}
 	return wxEmptyString;
 }
@@ -178,29 +181,40 @@ wxString SettersGettersDlg::GenerateGetter(TagEntryPtr tag)
 	bool startWithUpper  = m_checkStartWithUppercase->IsChecked();
 
 	Variable var;
-	if(LanguageST::Get()->VariableFromPattern(tag->GetPattern(), tag->GetName(), var))
-	{
+	if (LanguageST::Get()->VariableFromPattern(tag->GetPattern(), tag->GetName(), var)) {
 		wxString func;
 		wxString scope = _U(var.m_typeScope.c_str());
-		func << wxT("const ");
-		if(!scope.IsEmpty() && !(scope == wxT("<global>"))){
-			func << scope 
-				 << wxT("::");
+		if (!var.m_isPtr) {
+			func << wxT("const ");
+			if (!scope.IsEmpty() && !(scope == wxT("<global>"))) {
+				func << scope
+				<< wxT("::");
+			}
+			func << _U(var.m_type.c_str()) << _U(var.m_templateDecl.c_str()) << _U(var.m_starAmp.c_str()) << wxT("& ");
+		} else {
+			// generate different code for pointer
+			if (!scope.IsEmpty() && !(scope == wxT("<global>"))) {
+				func << scope
+				<< wxT("::");
+			}
+			func << _U(var.m_type.c_str()) << _U(var.m_templateDecl.c_str()) << _U(var.m_starAmp.c_str()) << wxT(" ");
 		}
-		func << _U(var.m_type.c_str()) 
-			 << _U(var.m_templateDecl.c_str()) 
-			 << _U(var.m_starAmp.c_str())
-			 << wxT("& ");
-		if(startWithUpper){
+
+		if (startWithUpper) {
 			func << wxT("Get");
-		}else{
+		} else {
 			func << wxT("get");
 		}
 
 		wxString name = _U(var.m_name.c_str());
 		FormatName(name);
-		func << name << wxT("() const {return ") << _U(var.m_name.c_str()) << wxT(";}");
-		return func;			 
+		
+		if(!var.m_isPtr){
+			func << name << wxT("() const {return ") << _U(var.m_name.c_str()) << wxT(";}");
+		} else {
+			func << name << wxT("() {return ") << _U(var.m_name.c_str()) << wxT(";}");
+		}
+		return func;
 	}
 	return wxEmptyString;
 }
@@ -210,7 +224,7 @@ void SettersGettersDlg::FormatName(wxString &name)
 	name.StartsWith(wxT("m_"), &name);
 	wxStringTokenizer tkz(name, wxT("_"));
 	name.Clear();
-	while(tkz.HasMoreTokens()){
+	while (tkz.HasMoreTokens()) {
 		wxString token = tkz.NextToken();
 		wxString pre = token.Mid(0, 1);
 		token.Remove(0, 1);
@@ -227,7 +241,7 @@ void SettersGettersDlg::UpdatePreview()
 	m_code = GenerateFunctions();
 	m_textPreview->SetReadOnly(false);
 	//remove previous preview
-	if(m_textPreview->CanUndo()) m_textPreview->Undo();
+	if (m_textPreview->CanUndo()) m_textPreview->Undo();
 	m_textPreview->BeginUndoAction();
 	m_textPreview->InsertTextWithIndentation(m_code, m_lineno);
 	m_textPreview->EndUndoAction();
@@ -237,7 +251,7 @@ void SettersGettersDlg::UpdatePreview()
 void SettersGettersDlg::OnCheckAll(wxCommandEvent &e)
 {
 	wxUnusedVar(e);
-	for(unsigned int i=0; i< (unsigned int)m_checkListMembers->GetCount(); i++){
+	for (unsigned int i=0; i< (unsigned int)m_checkListMembers->GetCount(); i++) {
 		m_checkListMembers->Check(i);
 	}
 	UpdatePreview();
@@ -246,7 +260,7 @@ void SettersGettersDlg::OnCheckAll(wxCommandEvent &e)
 void SettersGettersDlg::OnUncheckAll(wxCommandEvent &e)
 {
 	wxUnusedVar(e);
-	for(unsigned int i=0; i< (unsigned int)m_checkListMembers->GetCount(); i++){
+	for (unsigned int i=0; i< (unsigned int)m_checkListMembers->GetCount(); i++) {
 		m_checkListMembers->Check(i, false);
 	}
 	UpdatePreview();

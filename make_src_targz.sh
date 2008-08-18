60a9eda0d72cf9b1a5c650a3403d2765714a0827
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2008-08-18 23:22:23 +0000

    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@2002 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- make_src_targz.sh
+++ make_src_targz.sh
@@ -1,11 +1,11 @@
 # export the current directory 
 cur_rev=`svn info | grep Revision | awk '{print $2;}'`
 curdir=`pwd`
-rm -fr /tmp/codelite-src-${cur_rev}/
+rm -fr /tmp/codelite-${cur_rev}/
 
-svn export . /tmp/codelite-src-${cur_rev}/
-cp ./LiteEditor/svninfo.cpp /tmp/codelite-src-${cur_rev}/LiteEditor/
+svn export . /tmp/codelite-1.0.${cur_rev}/
+cp ./LiteEditor/svninfo.cpp /tmp/codelite-1.0.${cur_rev}/LiteEditor/
 echo "Creating tar-ball"
 cd /tmp/
-tar cvfz ${curdir}/codelite-1.0.${cur_rev}-src.tar.gz codelite-src-${cur_rev}/*
+tar cvfz ${curdir}/codelite-1.0.${cur_rev}.tar.gz codelite-1.0.${cur_rev}/*
 

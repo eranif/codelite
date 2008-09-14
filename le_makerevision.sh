26078632443961c36a9cfa249ad135903d02a57e
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2008-09-14 08:46:35 +0000

    reverted back changes to le_makerevision, and configure script since it stopped to work
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@2137 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- le_makerevision.sh
+++ le_makerevision.sh
@@ -13,7 +13,7 @@ fi
 # not under svn, but we do have the svninfo file, do nothing 
 if [ $has_file -eq 1 ] && [ $has_svn -eq 0 ]; then
        ## file already exists, and we can't update it
-       exit 0
+       return 0
 fi
 
 # under svn

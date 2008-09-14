8e3d72f7c724c7bd5515fd462788c7e7f133307e
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2008-09-14 13:42:59 +0000

    - Applied patch to make the le_makerevision.sh work properly when svn isn't available (David Hart)
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@2140 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- le_makerevision.sh
+++ le_makerevision.sh
@@ -8,25 +8,17 @@ fi
 
 if (test -d ".svn"); then
 	has_svn=1
-fi
-
-# not under svn, but we do have the svninfo file, do nothing 
-if [ $has_file -eq 1 ] && [ $has_svn -eq 0 ]; then
-       ## file already exists, and we can't update it
-       return 0
-fi
-
-# under svn
-rm -fr LiteEditor/svninfo.cpp
-if [ $has_svn -eq 1 ]; then
 	cur_rev=`svn info | grep Revision | awk '{print $2;}'`
 else
 	cur_rev=""
 fi
 
-## generate the svninfo file 
-echo "#include <wx/string.h>" >> LiteEditor/svninfo.cpp
-printf "const wxChar * SvnRevision = wxT(\"%s\");\n" ${cur_rev} >> LiteEditor/svninfo.cpp
-echo "" >> LiteEditor/svninfo.cpp
-echo Generating svninfo file...
+## generate the svninfo file
+## we always do this if svn is available, but even if it isn't, create a 'blank' file if none exists
+if [ $has_svn -eq 1 ] || [ $has_file -eq 0 ]; then
+	echo "#include <wx/string.h>" > LiteEditor/svninfo.cpp
+	printf "const wxChar * SvnRevision = wxT(\"%s\");\n" ${cur_rev} >> LiteEditor/svninfo.cpp
+	echo "" >> LiteEditor/svninfo.cpp
+	echo "Generating svninfo file..."
+fi
 

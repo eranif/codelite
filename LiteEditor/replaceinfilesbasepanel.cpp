3c6dfd449afe744d9781198dff9eeb22fc4c0af3
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2008-09-14 10:09:39 +0000

    - Replace Results panel: moved the 'replace with' and clear button to be above the find results
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@2138 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- LiteEditor/replaceinfilesbasepanel.cpp
+++ LiteEditor/replaceinfilesbasepanel.cpp
@@ -1,28 +1,4 @@
-//////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////
-//
-// copyright            : (C) 2008 by Eran Ifrah                            
-// file name            : replaceinfilesbasepanel.cpp              
-//                                                                          
-// -------------------------------------------------------------------------
-// A                                                                        
-//              _____           _      _     _ _                            
-//             /  __ \         | |    | |   (_) |                           
-//             | /  \/ ___   __| | ___| |    _| |_ ___                      
-//             | |    / _ \ / _  |/ _ \ |   | | __/ _ )                     
-//             | \__/\ (_) | (_| |  __/ |___| | ||  __/                     
-//              \____/\___/ \__,_|\___\_____/_|\__\___|                     
-//                                                                          
-//                                                  F i l e                 
-//                                                                          
-//    This program is free software; you can redistribute it and/or modify  
-//    it under the terms of the GNU General Public License as published by  
-//    the Free Software Foundation; either version 2 of the License, or     
-//    (at your option) any later version.                                   
-//                                                                          
-//////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////
- ///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
 // C++ code generated with wxFormBuilder (version Mar 19 2008)
 // http://www.wxformbuilder.org/
 //
@@ -42,14 +18,6 @@ ReplaceInFilesBasePanel::ReplaceInFilesBasePanel( wxWindow* parent, wxWindowID i
 	m_staticText8->Wrap( -1 );
 	bSizer1->Add( m_staticText8, 0, wxRIGHT|wxLEFT, 5 );
 	
-	wxBoxSizer* bSizer4;
-	bSizer4 = new wxBoxSizer( wxHORIZONTAL );
-	
-	m_listBox1 = new wxListBox( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0, NULL, wxLB_SINGLE ); 
-	bSizer4->Add( m_listBox1, 1, wxEXPAND|wxTOP|wxRIGHT|wxLEFT, 5 );
-	
-	bSizer1->Add( bSizer4, 1, wxEXPAND, 5 );
-	
 	wxBoxSizer* bSizer15;
 	bSizer15 = new wxBoxSizer( wxHORIZONTAL );
 	
@@ -66,27 +34,35 @@ ReplaceInFilesBasePanel::ReplaceInFilesBasePanel( wxWindow* parent, wxWindowID i
 	m_buttonClear = new wxButton( this, wxID_ANY, wxT("&Clear"), wxDefaultPosition, wxDefaultSize, 0 );
 	bSizer15->Add( m_buttonClear, 0, wxRIGHT|wxLEFT, 5 );
 	
-	bSizer1->Add( bSizer15, 0, wxEXPAND, 5 );
+	bSizer1->Add( bSizer15, 0, wxEXPAND|wxTOP|wxBOTTOM, 5 );
+	
+	wxBoxSizer* bSizer4;
+	bSizer4 = new wxBoxSizer( wxHORIZONTAL );
+	
+	m_listBox1 = new wxListBox( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0, NULL, wxLB_SINGLE ); 
+	bSizer4->Add( m_listBox1, 1, wxEXPAND|wxTOP|wxRIGHT|wxLEFT, 5 );
+	
+	bSizer1->Add( bSizer4, 1, wxEXPAND, 5 );
 	
 	this->SetSizer( bSizer1 );
 	this->Layout();
 	
 	// Connect Events
-	m_listBox1->Connect( wxEVT_COMMAND_LISTBOX_SELECTED, wxCommandEventHandler( ReplaceInFilesBasePanel::OnItemSelected ), NULL, this );
-	m_listBox1->Connect( wxEVT_COMMAND_LISTBOX_DOUBLECLICKED, wxCommandEventHandler( ReplaceInFilesBasePanel::OnItemDClicked ), NULL, this );
 	m_buttonReplaceAll->Connect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( ReplaceInFilesBasePanel::OnReplaceAll ), NULL, this );
 	m_buttonReplaceAll->Connect( wxEVT_UPDATE_UI, wxUpdateUIEventHandler( ReplaceInFilesBasePanel::OnReplaceAllUI ), NULL, this );
 	m_buttonClear->Connect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( ReplaceInFilesBasePanel::OnClearResults ), NULL, this );
 	m_buttonClear->Connect( wxEVT_UPDATE_UI, wxUpdateUIEventHandler( ReplaceInFilesBasePanel::OnClearResultsUI ), NULL, this );
+	m_listBox1->Connect( wxEVT_COMMAND_LISTBOX_SELECTED, wxCommandEventHandler( ReplaceInFilesBasePanel::OnItemSelected ), NULL, this );
+	m_listBox1->Connect( wxEVT_COMMAND_LISTBOX_DOUBLECLICKED, wxCommandEventHandler( ReplaceInFilesBasePanel::OnItemDClicked ), NULL, this );
 }
 
 ReplaceInFilesBasePanel::~ReplaceInFilesBasePanel()
 {
 	// Disconnect Events
-	m_listBox1->Disconnect( wxEVT_COMMAND_LISTBOX_SELECTED, wxCommandEventHandler( ReplaceInFilesBasePanel::OnItemSelected ), NULL, this );
-	m_listBox1->Disconnect( wxEVT_COMMAND_LISTBOX_DOUBLECLICKED, wxCommandEventHandler( ReplaceInFilesBasePanel::OnItemDClicked ), NULL, this );
 	m_buttonReplaceAll->Disconnect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( ReplaceInFilesBasePanel::OnReplaceAll ), NULL, this );
 	m_buttonReplaceAll->Disconnect( wxEVT_UPDATE_UI, wxUpdateUIEventHandler( ReplaceInFilesBasePanel::OnReplaceAllUI ), NULL, this );
 	m_buttonClear->Disconnect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( ReplaceInFilesBasePanel::OnClearResults ), NULL, this );
 	m_buttonClear->Disconnect( wxEVT_UPDATE_UI, wxUpdateUIEventHandler( ReplaceInFilesBasePanel::OnClearResultsUI ), NULL, this );
+	m_listBox1->Disconnect( wxEVT_COMMAND_LISTBOX_SELECTED, wxCommandEventHandler( ReplaceInFilesBasePanel::OnItemSelected ), NULL, this );
+	m_listBox1->Disconnect( wxEVT_COMMAND_LISTBOX_DOUBLECLICKED, wxCommandEventHandler( ReplaceInFilesBasePanel::OnItemDClicked ), NULL, this );
 }
--- LiteEditor/replaceinfilesbasepanel.h
+++ LiteEditor/replaceinfilesbasepanel.h
@@ -1,28 +1,4 @@
-//////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////
-//
-// copyright            : (C) 2008 by Eran Ifrah                            
-// file name            : replaceinfilesbasepanel.h              
-//                                                                          
-// -------------------------------------------------------------------------
-// A                                                                        
-//              _____           _      _     _ _                            
-//             /  __ \         | |    | |   (_) |                           
-//             | /  \/ ___   __| | ___| |    _| |_ ___                      
-//             | |    / _ \ / _  |/ _ \ |   | | __/ _ )                     
-//             | \__/\ (_) | (_| |  __/ |___| | ||  __/                     
-//              \____/\___/ \__,_|\___\_____/_|\__\___|                     
-//                                                                          
-//                                                  F i l e                 
-//                                                                          
-//    This program is free software; you can redistribute it and/or modify  
-//    it under the terms of the GNU General Public License as published by  
-//    the Free Software Foundation; either version 2 of the License, or     
-//    (at your option) any later version.                                   
-//                                                                          
-//////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////
- ///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
 // C++ code generated with wxFormBuilder (version Mar 19 2008)
 // http://www.wxformbuilder.org/
 //
@@ -38,10 +14,10 @@
 #include <wx/font.h>
 #include <wx/colour.h>
 #include <wx/settings.h>
-#include <wx/listbox.h>
-#include <wx/sizer.h>
 #include <wx/textctrl.h>
 #include <wx/button.h>
+#include <wx/sizer.h>
+#include <wx/listbox.h>
 #include <wx/panel.h>
 
 ///////////////////////////////////////////////////////////////////////////
@@ -56,19 +32,19 @@ class ReplaceInFilesBasePanel : public wxPanel
 	
 	protected:
 		wxStaticText* m_staticText8;
-		wxListBox* m_listBox1;
 		wxStaticText* m_staticText4;
 		wxTextCtrl* m_textCtrlReplaceWith;
 		wxButton* m_buttonReplaceAll;
 		wxButton* m_buttonClear;
+		wxListBox* m_listBox1;
 		
 		// Virtual event handlers, overide them in your derived class
-		virtual void OnItemSelected( wxCommandEvent& event ){ event.Skip(); }
-		virtual void OnItemDClicked( wxCommandEvent& event ){ event.Skip(); }
 		virtual void OnReplaceAll( wxCommandEvent& event ){ event.Skip(); }
 		virtual void OnReplaceAllUI( wxUpdateUIEvent& event ){ event.Skip(); }
 		virtual void OnClearResults( wxCommandEvent& event ){ event.Skip(); }
 		virtual void OnClearResultsUI( wxUpdateUIEvent& event ){ event.Skip(); }
+		virtual void OnItemSelected( wxCommandEvent& event ){ event.Skip(); }
+		virtual void OnItemDClicked( wxCommandEvent& event ){ event.Skip(); }
 		
 	
 	public:

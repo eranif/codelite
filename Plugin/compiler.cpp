8f276791e64f6c221bc55962887d046c10f2c197
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2008-10-17 20:03:54 +0000

    - Fixed build g++ compiler error regex to ignore compiler output in form of: /home/user/Projects/DevIO/devicecomm/ResponseFilter.cpp:25: instantiated from here, otherwise it was considered and marked as error (patch by RJP Computing)
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@2302 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- Plugin/compiler.cpp
+++ Plugin/compiler.cpp
@@ -100,7 +100,7 @@ Compiler::Compiler(wxXmlNode *node)
 		m_switches[wxT("Object")] = wxT("-o ");
 		m_switches[wxT("ArchiveOutput")] = wxT(" ");
 		m_objectSuffix = wxT(".o");
-		m_errorPattern = wxT("(^[a-zA-Z:]{0,2}[a-zA-Z\\.0-9_/\\+\\-]+ *)(:)([0-9]+)(:)([ eor]*)");
+		m_errorPattern = wxT("(^[a-zA-Z:]{0,2}[a-zA-Z\\.0-9_/\\+\\-]+ *)(:)([0-9]+)(:)( *[eor]+:?)");
 		m_errorFileNameIndex = wxT("1");
 		m_errorLineNubmerIndex = wxT("3");
 		m_warningPattern = wxT("(^[a-zA-Z:]{0,2}[a-zA-Z\\.0-9_/\\+\\-]+ *)(:)([0-9]+ *)(:)([0-9:]*)( warning:)");

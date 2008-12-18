18761f159d625de46a738ebdee74f399bc2bca65
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2008-12-18 14:58:06 +0000

    "
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@2580 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- Plugin/macros.h
+++ Plugin/macros.h
@@ -1,25 +1,25 @@
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 //
-// copyright            : (C) 2008 by Eran Ifrah                            
-// file name            : macros.h              
-//                                                                          
+// copyright            : (C) 2008 by Eran Ifrah
+// file name            : macros.h
+//
 // -------------------------------------------------------------------------
-// A                                                                        
-//              _____           _      _     _ _                            
-//             /  __ \         | |    | |   (_) |                           
-//             | /  \/ ___   __| | ___| |    _| |_ ___                      
-//             | |    / _ \ / _  |/ _ \ |   | | __/ _ )                     
-//             | \__/\ (_) | (_| |  __/ |___| | ||  __/                     
-//              \____/\___/ \__,_|\___\_____/_|\__\___|                     
-//                                                                          
-//                                                  F i l e                 
-//                                                                          
-//    This program is free software; you can redistribute it and/or modify  
-//    it under the terms of the GNU General Public License as published by  
-//    the Free Software Foundation; either version 2 of the License, or     
-//    (at your option) any later version.                                   
-//                                                                          
+// A
+//              _____           _      _     _ _
+//             /  __ \         | |    | |   (_) |
+//             | /  \/ ___   __| | ___| |    _| |_ ___
+//             | |    / _ \ / _  |/ _ \ |   | | __/ _ )
+//             | \__/\ (_) | (_| |  __/ |___| | ||  __/
+//              \____/\___/ \__,_|\___\_____/_|\__\___|
+//
+//                                                  F i l e
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
  #ifndef MACROS_H
@@ -84,7 +84,7 @@ const wxString clCMD_EDIT = wxT("<Edit...>");
 const wxString clCMD_DELETE = wxT("<Delete...>");
 
 // constant message
-const wxString BUILD_START_MSG      = wxT("----------Building: \n");
+const wxString BUILD_START_MSG      = wxT("----------Build Started--------\n");
 const wxString BUILD_END_MSG        = wxT("----------Build Ended----------\n");
 const wxString BUILD_PROJECT_PREFIX = wxT("----------Building project:[ ");
 const wxString CLEAN_PROJECT_PREFIX = wxT("----------Cleaning project:[ ");

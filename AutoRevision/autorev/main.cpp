5ffebb2e0ef4e5b84b977c0b922ed7e4b0ac721d
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2008-12-26 14:15:15 +0000

    - applied patch to allow autorev to work for non ASCII users by using svn info --xml (jfouche)
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@2626 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- AutoRevision/autorev/main.cpp
+++ AutoRevision/autorev/main.cpp
@@ -4,59 +4,48 @@
 #ifdef _WIN32
 #include "io.h"
 #endif
-#include <string>
 
+static const char* REVISION_PATTERN = "revision=\"";
 
 int main(int argc, char **argv){
-	
+
 	if(argc < 2){
 		printf("usage: autorev <directory>\n");
 		return -1;
 	}
-	
+
 	//execute the svn executable
 	chdir(argv[1]);
-	FILE *fp = popen("svn info", "r");
+	FILE *fp = popen("svn info --xml", "r");
 	if(!fp){
 		printf("failed to execute svn\n");
 	}
-	
+
 	char line[256];
-	bool found(false);
-	while(fgets(line, sizeof(line), fp)){
-		if(strstr(line, "Revision: ")){
-			found = true;
+	char* posRevision = NULL;
+	while(fgets(line, sizeof(line), fp) || posRevision){
+		posRevision = strstr(line, REVISION_PATTERN);
+		if(posRevision){
 			break;
 		}
 		memset(line, 0, sizeof(line));
 	}
-	
+
 	//create the output file
 	FILE *of = fopen("svninfo.cpp", "w+");
 	if(!of){
 		perror("failed to create output file");
 		return -1;
 	}
-	
+
 	fprintf(of, "#include <wx/string.h>\n");
 	fprintf(of, "const wxChar* SvnRevision = wxT(\"");
-	if(found){
+	if(posRevision){
 		//we got the revision number
-		int counter(0);
-		char *tok = strtok(line, ":");
-		while(tok){
-			if(counter == 1){
-				std::string token = tok;
-				static std::string trimString(" \r\n\t\v");
-				token.erase(0, token.find_first_not_of(trimString)); 
-				token.erase(token.find_last_not_of(trimString)+1);
-				fprintf(of, "%s", token.c_str());
-				break;
-			}else{
-				tok = strtok(NULL, ":");
-			}
-			counter++;
-		}
+		posRevision += strlen(REVISION_PATTERN);
+		char * posRevisionEnd = strstr(posRevision, "\"");
+		*posRevisionEnd = 0;
+		fputs(posRevision, of);
 	}
 	fprintf(of, "\");\n");
 	pclose(fp);

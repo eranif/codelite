a0acd803742128edbbb55e1b1daedf5ff94bee36
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2009-01-14 18:26:38 +0000

    - updated configure script for sqlite3 in codelite_indexer
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@2705 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- sdk/codelite_indexer/libctags/libctags.h
+++ sdk/codelite_indexer/libctags/libctags.h
@@ -1,13 +1,22 @@
 #ifndef __LIBCTAGS_H__
 #define __LIBCTAGS_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /** allow loading the libctags using dlsymbol & dlopen **/
 typedef char* (*CTAGS_MAKE_TAGS_FUNC)(const char*, const char*);
 typedef void  (*CTAGS_SHUDOWN_FUNC)();
 typedef void (*CTAGS_FREE_FUNC)(char *);
 
 /** standard incudes **/
-extern char *ctags_make_tags (const char *cmd, const char *infile);
-extern void ctags_shutdown();
-extern void ctags_free(char *ptr);
+char *ctags_make_tags (const char *cmd, const char *infile);
+void ctags_shutdown();
+void ctags_free(char *ptr);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif // __LIBCTAGS_H__

28aeabbb5f30389acf25c7b21aeea09bf8a2e4a9
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2009-02-15 15:10:52 +0000

    - added missing files
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@2765 9da81c78-c036-0410-9e1f-a2b0375e4b5a

---
+++ Plugin/build_config_common.cpp
@@ -0,0 +1,156 @@
+#include "build_config_common.h"
+#include "globals.h"
+#include "xmlutils.h"
+#include <wx/tokenzr.h>
+
+BuildConfigCommon::BuildConfigCommon(wxXmlNode* node, wxString confType)
+: m_confType(confType)
+{
+	if (node) {
+		// read the compile options
+		wxXmlNode *compile = XmlUtils::FindFirstByTagName(node, wxT("Compiler"));
+		if (compile) {
+			m_compileOptions = XmlUtils::ReadString(compile, wxT("Options"));
+			wxXmlNode *child = compile->GetChildren();
+			while (child) {
+				if (child->GetName() == wxT("IncludePath")) {
+					m_includePath.Add(XmlUtils::ReadString(child, wxT("Value")));
+				} else if (child->GetName() == wxT("Preprocessor")) {
+					m_preprocessor.Add(XmlUtils::ReadString(child, wxT("Value")));
+				}
+				child = child->GetNext();
+			}
+		}
+
+		// read the linker options
+		wxXmlNode *linker = XmlUtils::FindFirstByTagName(node, wxT("Linker"));
+		if (linker) {
+			m_linkOptions = XmlUtils::ReadString(linker, wxT("Options"));
+			wxXmlNode *child = linker->GetChildren();
+			while (child) {
+				if (child->GetName() == wxT("Library")) {
+					m_libs.Add(XmlUtils::ReadString(child, wxT("Value")));
+				} else if (child->GetName() == wxT("LibraryPath")) {
+					m_libPath.Add(XmlUtils::ReadString(child, wxT("Value")));
+				}
+				child = child->GetNext();
+			}
+		}
+
+		// read the resource compile options
+		wxXmlNode *resCmp = XmlUtils::FindFirstByTagName(node, wxT("ResourceCompiler"));
+		if (resCmp) {
+			m_resCompileOptions = XmlUtils::ReadString(resCmp, wxT("Options"));
+			wxXmlNode *child = resCmp->GetChildren();
+			while (child) {
+				if (child->GetName() == wxT("IncludePath")) {
+					m_resCmpIncludePath << XmlUtils::ReadString(child, wxT("Value")) << wxT(";");
+				}
+				child = child->GetNext();
+			}
+		}
+	}
+	else {
+		m_includePath.Add(wxT("."));
+		m_libPath.Add(wxT("."));
+	}
+}
+
+BuildConfigCommon::~BuildConfigCommon()
+{
+	
+}
+
+wxXmlNode *BuildConfigCommon::ToXml() const
+{
+	wxXmlNode *node = new wxXmlNode(NULL, wxXML_ELEMENT_NODE, m_confType);
+	
+	//create the compile node
+	wxXmlNode *compile = new wxXmlNode(NULL, wxXML_ELEMENT_NODE, wxT("Compiler"));
+	compile->AddProperty(wxT("Options"), m_compileOptions);
+	node->AddChild(compile);
+
+	size_t i=0;
+	for (i=0; i<m_includePath.GetCount(); i++) {
+		wxXmlNode *option = new wxXmlNode(NULL, wxXML_ELEMENT_NODE, wxT("IncludePath"));
+		option->AddProperty(wxT("Value"), m_includePath.Item(i));
+		compile->AddChild(option);
+	}
+
+	for (i=0; i<m_preprocessor.GetCount(); i++) {
+		wxXmlNode *prep = new wxXmlNode(NULL, wxXML_ELEMENT_NODE, wxT("Preprocessor"));
+		prep->AddProperty(wxT("Value"), m_preprocessor.Item(i));
+		compile->AddChild(prep);
+	}
+
+	//add the link node
+	wxXmlNode *link = new wxXmlNode(NULL, wxXML_ELEMENT_NODE, wxT("Linker"));
+	link->AddProperty(wxT("Options"), m_linkOptions);
+	node->AddChild(link);
+
+	for (i=0; i<m_libPath.GetCount(); i++) {
+		wxXmlNode *option = new wxXmlNode(NULL, wxXML_ELEMENT_NODE, wxT("LibraryPath"));
+		option->AddProperty(wxT("Value"), m_libPath.Item(i));
+		link->AddChild(option);
+	}
+
+	for (i=0; i<m_libs.GetCount(); i++) {
+		wxXmlNode *option = new wxXmlNode(NULL, wxXML_ELEMENT_NODE, wxT("Library"));
+		option->AddProperty(wxT("Value"), m_libs.Item(i));
+		link->AddChild(option);
+	}
+	
+	//add the resource compiler node
+	wxXmlNode *resCmp = new wxXmlNode(NULL, wxXML_ELEMENT_NODE, wxT("ResourceCompiler"));
+	resCmp->AddProperty(wxT("Options"), m_resCompileOptions);
+	node->AddChild(resCmp);
+
+	wxStringTokenizer tok(m_resCmpIncludePath, wxT(";"));
+	while (tok.HasMoreTokens()) {
+		wxXmlNode *option = new wxXmlNode(NULL, wxXML_ELEMENT_NODE, wxT("IncludePath"));
+		option->AddProperty(wxT("Value"),tok.NextToken());
+		resCmp->AddChild(option);
+	}
+	return node;
+}
+
+void BuildConfigCommon::SetPreprocessor(const wxString &pre)
+{
+	FillFromSmiColonString(m_preprocessor, pre);
+}
+
+void BuildConfigCommon::SetIncludePath(const wxString &path)
+{
+	FillFromSmiColonString(m_includePath, path);
+}
+
+void BuildConfigCommon::SetLibraries(const wxString &libs)
+{
+	FillFromSmiColonString(m_libs, libs);
+}
+
+void BuildConfigCommon::SetLibPath(const wxString &paths)
+{
+	FillFromSmiColonString(m_libPath, paths);
+}
+
+wxString BuildConfigCommon::GetLibPath() const
+{
+	return ArrayToSmiColonString(m_libPath);
+}
+
+wxString BuildConfigCommon::GetLibraries() const
+{
+	return ArrayToSmiColonString(m_libs);
+}
+
+wxString BuildConfigCommon::GetIncludePath() const
+{
+	return ArrayToSmiColonString(m_includePath);
+}
+
+wxString BuildConfigCommon::GetPreprocessor() const
+{
+	return ArrayToSmiColonString(m_preprocessor);
+}
+
---
+++ Plugin/build_config_common.h
@@ -0,0 +1,120 @@
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//
+// copyright            : (C) 2008 by Eran Ifrah
+// file name            : build_config.h
+//
+// -------------------------------------------------------------------------
+// A
+//              _____           _      _     _ _
+//             /  __ \         | |    | |   (_) |
+//             | /  \/ ___   __| | ___| |    _| |_ ___
+//             | |    / _ \ / _  |/ _ \ |   | | __/ _ )
+//             | \__/\ (_) | (_| |  __/ |___| | ||  __/
+//              \____/\___/ \__,_|\___\_____/_|\__\___|
+//
+//                                                  F i l e
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+#ifndef BUILD_CONFIGURATION_COMMON_H
+#define BUILD_CONFIGURATION_COMMON_H
+
+#include "configuration_object.h"
+#include <wx/arrstr.h>
+#include <wx/string.h>
+
+#ifndef WXDLLIMPEXP_LE_SDK
+#ifdef WXMAKINGDLL_LE_SDK
+#    define WXDLLIMPEXP_LE_SDK WXEXPORT
+#elif defined(WXUSINGDLL_LE_SDK)
+#    define WXDLLIMPEXP_LE_SDK WXIMPORT
+#else /* not making nor using FNB as DLL */
+#    define WXDLLIMPEXP_LE_SDK
+#endif // WXMAKINGDLL_LE_SDK
+#endif
+
+class WXDLLIMPEXP_LE_SDK BuildConfigCommon : public ConfObject
+{
+	wxArrayString m_includePath;
+	wxString m_compileOptions;
+	wxString m_linkOptions;
+	wxArrayString m_libs;
+	wxArrayString m_libPath;
+	wxArrayString m_preprocessor;
+	wxString m_resCompileOptions;
+	wxString m_resCmpIncludePath;
+	
+	wxString m_confType; // xml node name 
+
+public:
+	BuildConfigCommon(wxXmlNode* node, wxString confType = wxT("Configuration"));
+	virtual ~BuildConfigCommon();
+	wxXmlNode *ToXml() const;
+	BuildConfigCommon *Clone() const;
+
+	//--------------------------------
+	// Setters / Getters
+	//--------------------------------
+	
+	wxString GetPreprocessor() const;
+	void GetPreprocessor(wxArrayString &arr) {
+		arr = m_preprocessor;
+	}
+	void SetPreprocessor(const wxString &prepr);
+
+	const wxString &GetCompileOptions() const {
+		return m_compileOptions;
+	}
+	void SetCompileOptions(const wxString &options) {
+		m_compileOptions = options;
+	}
+
+	const wxString &GetLinkOptions() const {
+		return m_linkOptions;
+	}
+	void SetLinkOptions(const wxString &options) {
+		m_linkOptions = options;
+	}
+
+	wxString GetIncludePath() const;
+	void SetIncludePath(const wxString &path);
+	void SetIncludePath(const wxArrayString &paths) {
+		m_includePath = paths;
+	}
+
+	wxString GetLibraries() const;
+	void SetLibraries(const wxString &libs);
+	void SetLibraries(const wxArrayString &libs) {
+		m_libs = libs;
+	}
+
+	wxString GetLibPath() const;
+	void SetLibPath(const wxString &path);
+	void SetLibPath(const wxArrayString &libPaths) {
+		m_libPath = libPaths;
+	}
+
+	const wxString& GetResCmpIncludePath() const {
+		return m_resCmpIncludePath;
+	}
+	void SetResCmpIncludePath(const wxString &path) {
+		m_resCmpIncludePath = path;
+	}
+	
+	const wxString &GetResCompileOptions() const {
+		return m_resCompileOptions;
+	}
+	void SetResCmpOptions(const wxString &options) {
+		m_resCompileOptions = options;
+	}
+};
+
+typedef SmartPtr<BuildConfigCommon> BuildConfigCommonPtr;
+
+#endif // BUILD_CONFIGURATION_COMMON_H

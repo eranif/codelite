9fc6c2380d2516ce3d6ebd3bb340bc5c33f58778
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2009-09-04 12:41:29 +0000

    - Compiler annotation will no longer include duplicate lines
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@2931 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- LiteEditor/buidltab.cpp
+++ LiteEditor/buidltab.cpp
@@ -338,7 +338,7 @@ void BuildTab::MarkEditor ( LEditor *editor )
 			wxMemoryBuffer style_bytes;
 			int      line_number = i->second.linenum;
 			wxString tip = GetBuildToolTip(editor->GetFileName().GetFullPath(), line_number, style_bytes);
-			
+
 			editor->AnnotationSetText (line_number, tip);
 			editor->AnnotationSetStyles(line_number, style_bytes );
 
@@ -550,38 +550,39 @@ wxString BuildTab::GetBuildToolTip(const wxString& fileName, int lineno, wxMemor
             }
 
 			wxString tmpTip (wxT(" ") + text.Trim(false).Trim() + wxT("\n"));
-			
+
 #if defined(__WXGTK__) || defined (__WXMAC__)
 			// Remove any non ascii characters from the tip
 			wxString asciiTip;
-			
+
 			for(size_t at=0; at<tmpTip.Length(); at++) {
 				if( isprint((char)tmpTip.GetChar(at) ) || tmpTip.GetChar(at) == wxT('\n') ) {
 					asciiTip.Append( tmpTip.GetChar(at) );
 				}
 			}
-			
+
 			tmpTip = asciiTip;
 			tmpTip.Replace(wxT("\r"), wxT(""));
 			tmpTip.Replace(wxT("\t"), wxT(" "));
 #endif
-
-			for(size_t j=0; j<tmpTip.Length(); j++) {
-				if( i->second.linecolor == wxSCI_LEX_GCC_WARNING ) {
-					styleBits.AppendByte((char)eAnnotationStyleWarning);
-				} else {
-					styleBits.AppendByte((char)eAnnotationStyleError);
+			if( tip.Contains(tmpTip) == false ) {
+				for(size_t j=0; j<tmpTip.Length(); j++) {
+					if( i->second.linecolor == wxSCI_LEX_GCC_WARNING ) {
+						styleBits.AppendByte((char)eAnnotationStyleWarning);
+					} else {
+						styleBits.AppendByte((char)eAnnotationStyleError);
+					}
 				}
+				tip << tmpTip;
 			}
-			tip << tmpTip;
 
         }
     }
-	
+
 	if(tip.IsEmpty() == false) {
 		tip.RemoveLast();
 		styleBits.SetDataLen( styleBits.GetDataLen()-1 );
 	}
-	
+
 	return tip ;
 }

728bdc43fa581dd66c627c1b2620753866183984
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2009-10-23 11:22:52 +0000

    - Increased internal version to 2.0.1 to deploy new settings of the tags pre-processors
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@3239 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- LiteEditor/app.cpp
+++ LiteEditor/app.cpp
@@ -269,7 +269,7 @@ bool App::OnInit()
 #endif
 
 	// Update codelite revision and Version
-	EditorConfig::Init( SvnRevision, wxT("2.0.0") );
+	EditorConfig::Init( SvnRevision, wxT("2.0.1") );
 
 	wxString curdir = wxGetCwd();
 	::wxSetWorkingDirectory(homeDir);

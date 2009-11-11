f4bef4adcb335cd4bc514fde5f278cf5f777fd97
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2009-11-11 17:52:51 +0000

    - Renamed the 'Settings -> Editor' menu entry to 'Settings -> Global Editor Preferences' (dialog title was modified as well)
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@3335 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- LiteEditor/options_base_dlg2.h
+++ LiteEditor/options_base_dlg2.h
@@ -25,29 +25,29 @@
 ///////////////////////////////////////////////////////////////////////////////
 /// Class OptionsBaseDlg2
 ///////////////////////////////////////////////////////////////////////////////
-class OptionsBaseDlg2 : public wxDialog 
+class OptionsBaseDlg2 : public wxDialog
 {
 	private:
-	
+
 	protected:
 		wxTreebook * m_treeBook;
 		wxStaticLine* m_staticline1;
 		wxButton* m_okButton;
 		wxButton* m_cancelButton;
 		wxButton* m_applyButton;
-		
+
 		// Virtual event handlers, overide them in your derived class
 		virtual void OnActivate( wxActivateEvent& event ){ event.Skip(); }
 		virtual void OnInitDialog( wxInitDialogEvent& event ){ event.Skip(); }
 		virtual void OnButtonOK( wxCommandEvent& event ){ event.Skip(); }
 		virtual void OnButtonCancel( wxCommandEvent& event ){ event.Skip(); }
 		virtual void OnButtonApply( wxCommandEvent& event ){ event.Skip(); }
-		
-	
+
+
 	public:
-		OptionsBaseDlg2( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxT("Editor Settings"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 640,480 ), long style = wxDEFAULT_DIALOG_STYLE|wxMAXIMIZE_BOX|wxRESIZE_BORDER );
+		OptionsBaseDlg2( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxT("Global Editor Preferences"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 640,480 ), long style = wxDEFAULT_DIALOG_STYLE|wxMAXIMIZE_BOX|wxRESIZE_BORDER );
 		~OptionsBaseDlg2();
-	
+
 };
 
 #endif //__options_base_dlg2__

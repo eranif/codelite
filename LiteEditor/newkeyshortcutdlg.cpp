7f264e04d092080d8fcbb7fdc523ef128d17beff
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2009-11-15 17:54:13 +0000

    - Removed limitation on 'keyboard shortcuts' it can now accepts all the keys on the keyboard (BUG#2897477)
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@3355 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- LiteEditor/newkeyshortcutdlg.cpp
+++ LiteEditor/newkeyshortcutdlg.cpp
@@ -1,25 +1,25 @@
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 //
-// copyright            : (C) 2008 by Eran Ifrah                            
-// file name            : newkeyshortcutdlg.cpp              
-//                                                                          
+// copyright            : (C) 2008 by Eran Ifrah
+// file name            : newkeyshortcutdlg.cpp
+//
 // -------------------------------------------------------------------------
-// A                                                                        
-//              _____           _      _     _ _                            
-//             /  __ \         | |    | |   (_) |                           
-//             | /  \/ ___   __| | ___| |    _| |_ ___                      
-//             | |    / _ \ / _  |/ _ \ |   | | __/ _ )                     
-//             | \__/\ (_) | (_| |  __/ |___| | ||  __/                     
-//              \____/\___/ \__,_|\___\_____/_|\__\___|                     
-//                                                                          
-//                                                  F i l e                 
-//                                                                          
-//    This program is free software; you can redistribute it and/or modify  
-//    it under the terms of the GNU General Public License as published by  
-//    the Free Software Foundation; either version 2 of the License, or     
-//    (at your option) any later version.                                   
-//                                                                          
+// A
+//              _____           _      _     _ _
+//             /  __ \         | |    | |   (_) |
+//             | /  \/ ___   __| | ___| |    _| |_ ___
+//             | |    / _ \ / _  |/ _ \ |   | | __/ _ )
+//             | \__/\ (_) | (_| |  __/ |___| | ||  __/
+//              \____/\___/ \__,_|\___\_____/_|\__\___|
+//
+//                                                  F i l e
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
  #include "newkeyshortcutdlg.h"
@@ -153,8 +153,8 @@ wxString NewKeyShortcutDlg::ToString(wxKeyEvent &e)
 		if ( n == WXSIZEOF(wxKeyNames) ) {
 			// must be a simple key
 			if (
-			    isascii(code) &&
-			    wxIsalnum(code) ) {
+			    isascii(code) /*&&
+			    wxIsalnum(code)*/ ) {
 				text << (wxChar)code;
 			} else {
 				return wxEmptyString;

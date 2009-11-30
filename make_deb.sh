fe3fce8f46cbb6f9a1c985757307185dc81d8671
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2009-11-30 05:40:45 +0000

    - Updated ./make_deb script to generate fakeroot this will avoid having .svn files inside the .deb package
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@3426 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- make_deb.sh
+++ make_deb.sh
@@ -1,11 +1,10 @@
+#!/bin/sh
 ################################################################################
 ## This file is part of CodeLite IDE and is released
 ## under the terms of the GNU General Public License as published by
 ##    the Free Software Foundation; either version 2 of the License, or
 ##    (at your option) any later version.
 ################################################################################
-
-#!/bin/sh
 
 
 ## Copy files to the fakeroot directory structure
@@ -49,32 +48,44 @@ Options:
 EOF
 }
 
-## Making control file
-generateControlFile()
+## Making control and postinst files
+generateDebianFiles()
 {
-    rm -fr fakeroot/DEBIAN/control
-    echo "Package: codelite" >> fakeroot/DEBIAN/control
-    echo "Version: 2.0.${cur_rev}" >> fakeroot/DEBIAN/control
-    echo "Section: devel " >> fakeroot/DEBIAN/control
-    echo "Priority: optional " >> fakeroot/DEBIAN/control
-    echo "Architecture: ${arch} " >> fakeroot/DEBIAN/control
-    echo "Essential: no" >> fakeroot/DEBIAN/control
-    echo "Depends: libwxgtk2.8-0, libgtk2.0-0" >> fakeroot/DEBIAN/control
-    echo "Installed-Size: ${inst_size} " >> fakeroot/DEBIAN/control
-    echo "Maintainer: Eran Ifrah [eran.ifrah@gmail.com]" >> fakeroot/DEBIAN/control
-    echo "Description: CodeLite is a powerful open-source, cross platform code editor for the C/C++ programming languages (regularly tested on Windows XP sp2, Ubuntu 8.04 Hardy Heron, Fedora Core 9 and Debian lenny/sid). " >> fakeroot/DEBIAN/control
-    echo "		 CodeLite is distributed under the terms of the GPL license." >> fakeroot/DEBIAN/control
-    echo "		 CodeLite uses a sophisticated, yet intuitive interface which allows user easily to " >> fakeroot/DEBIAN/control
-    echo "		 create and build complex projects. " >> fakeroot/DEBIAN/control
-    echo ""  >> fakeroot/DEBIAN/control
+    cat > fakeroot/DEBIAN/control <<EOF
+Package: codelite
+Version: 2.0.${cur_rev}
+Section: devel
+Priority: optional
+Architecture: ${arch}
+Essential: no
+Depends: libwxgtk2.8-0, libgtk2.0-0
+Installed-Size: ${inst_size}
+Maintainer: Eran Ifrah [eran.ifrah@gmail.com]
+Description: CodeLite is a powerful open-source, cross platform code editor for the C/C++ programming languages (regularly tested on Windows XP sp2, Ubuntu 8.04 Hardy Heron, Fedora Core 9 and Debian lenny/sid).
+		 CodeLite is distributed under the terms of the GPL license.
+		 CodeLite uses a sophisticated, yet intuitive interface which allows user easily to
+		 create and build complex projects.
+
+EOF
+    cat > fakeroot/DEBIAN/postinst <<EOF
+#!/bin/sh
+echo "Running postinst step..."
+rm -rf \${HOME}/.codelite/config/liteeditor.xml
+## Menu updating
+if [ "\$1" = "configure" ] && [ -x "\`which update-menus 2>/dev/null\`" ]; then
+    update-menus
+fi
+echo "Done"
+EOF
+    chmod 0755 fakeroot/DEBIAN/postinst
 }
 
 ## Making destop file
 generateDesktopFile()
 {
     rm -fr fakeroot/usr/share/applications/codelite.desktop
-	cp -fr Runtime/codelite.desktop fakeroot/usr/share/applications/codelite.desktop
-	chmod +x fakeroot/usr/share/applications/codelite.desktop
+    cp -fr Runtime/codelite.desktop fakeroot/usr/share/applications/codelite.desktop
+    chmod +x fakeroot/usr/share/applications/codelite.desktop
 }
 
 ## Making menu entry for WMs
@@ -87,14 +98,14 @@ generateMenuFile()
     echo "" >> fakeroot/usr/share/menu/codelite
 }
 
-
-## clear previous packaged files
-\rm -fr fakeroot/usr/
-
 ## Parse command line options
 parseCommandLine $1
 
 EXE_NAME=codelite
+
+## clear previous packaged files
+rm -fr fakeroot
+mkdir -p fakeroot/DEBIAN/
 mkdir -p fakeroot/${PREFIX}/bin/
 
 cp ./Runtime/${EXE_NAME} fakeroot/${PREFIX}/bin/codelite
@@ -121,55 +132,55 @@ mkdir -p fakeroot/${PREFIX}/lib/codelite/resources
 mkdir -p fakeroot/usr/share/applications/
 mkdir -p fakeroot/usr/share/menu/
 
-\cp -pr Runtime/config/build_settings.xml.default fakeroot/${PREFIX}/share/codelite/config/build_settings.xml.default
-\cp -pr Runtime/config/plugins.xml.default fakeroot/${PREFIX}/share/codelite/config/plugins.xml.default
-\cp -pr Runtime/config/debuggers.xml.gtk fakeroot/${PREFIX}/share/codelite/config/debuggers.xml.default
-\cp -pr Runtime/config/accelerators.conf.default fakeroot/${PREFIX}/share/codelite/config/accelerators.conf.default
-\cp -pr Runtime/config/codelite.xml.default.gtk fakeroot/${PREFIX}/share/codelite/config/codelite.xml.default
-\cp -pr Runtime/rc/menu.xrc fakeroot/${PREFIX}/share/codelite/rc/
-\cp -pfr Runtime/templates/* fakeroot/${PREFIX}/share/codelite/templates/
-\cp -pfr Runtime/lexers/* fakeroot/${PREFIX}/share/codelite/lexers/
-#\cp -pr static-symbols.tags fakeroot/${PREFIX}/share/codelite/
-\cp -pr lib/Debugger.so fakeroot/${PREFIX}/lib/codelite/debuggers/
-\cp -pr lib/CodeFormatter.so fakeroot/${PREFIX}/lib/codelite/
-\cp -pr lib/Copyright.so fakeroot/${PREFIX}/lib/codelite/
-\cp -pr lib/Subversion.so fakeroot/${PREFIX}/lib/codelite/
-\cp -pr lib/Gizmos.so fakeroot/${PREFIX}/lib/codelite/
-\cp -pr lib/cscope.so fakeroot/${PREFIX}/lib/codelite/
-\cp -pr lib/UnitTestCPP.so fakeroot/${PREFIX}/lib/codelite/
-\cp -pr lib/ExternalTools.so fakeroot/${PREFIX}/lib/codelite/
-\cp -pr lib/ContinuousBuild.so fakeroot/${PREFIX}/lib/codelite/
-#\cp -pr lib/SymbolView.so fakeroot/${PREFIX}/lib/codelite/
-\cp -pr lib/SnipWiz.so fakeroot/${PREFIX}/lib/codelite/
-\cp -pr lib/wxformbuilder.so fakeroot/${PREFIX}/lib/codelite/
-\cp -pr lib/abbreviation.so fakeroot/${PREFIX}/lib/codelite/
-\cp -pr lib/QmakePlugin.so fakeroot/${PREFIX}/lib/codelite/
-\cp -pr lib/CppCheck.so fakeroot/${PREFIX}/lib/codelite/
-\cp -pr lib/libwxscintillau.so fakeroot/${PREFIX}/lib/codelite/
-\cp -pr lib/libpluginu.so fakeroot/${PREFIX}/lib/codelite/
-\cp -pr lib/libcodeliteu.so fakeroot/${PREFIX}/lib/codelite/
-\cp -pr lib/libwxsqlite3u.so fakeroot/${PREFIX}/lib/codelite/
-\cp -pr lib/libwxpropgridu.so fakeroot/${PREFIX}/lib/codelite/
-\cp -pr Runtime/plugins/resources/*.* fakeroot/${PREFIX}/lib/codelite/resources/
-\cp -pr Runtime/*.html fakeroot/${PREFIX}/share/codelite/
-\cp -pr Runtime/astyle.sample fakeroot/${PREFIX}/share/codelite/
-\cp -pr Runtime/src/* fakeroot/${PREFIX}/share/codelite/src/
-\cp -pr Runtime/images/*.png fakeroot/${PREFIX}/share/codelite/images/
+cp -pr Runtime/config/build_settings.xml.default fakeroot/${PREFIX}/share/codelite/config/build_settings.xml.default
+cp -pr Runtime/config/plugins.xml.default fakeroot/${PREFIX}/share/codelite/config/plugins.xml.default
+cp -pr Runtime/config/debuggers.xml.gtk fakeroot/${PREFIX}/share/codelite/config/debuggers.xml.default
+cp -pr Runtime/config/accelerators.conf.default fakeroot/${PREFIX}/share/codelite/config/accelerators.conf.default
+cp -pr Runtime/config/codelite.xml.default.gtk fakeroot/${PREFIX}/share/codelite/config/codelite.xml.default
+cp -pr Runtime/rc/menu.xrc fakeroot/${PREFIX}/share/codelite/rc/
+cp -pfr Runtime/templates/* fakeroot/${PREFIX}/share/codelite/templates/
+cp -pfr Runtime/lexers/* fakeroot/${PREFIX}/share/codelite/lexers/
+#cp -pr static-symbols.tags fakeroot/${PREFIX}/share/codelite/
+cp -pr lib/Debugger.so fakeroot/${PREFIX}/lib/codelite/debuggers/
+cp -pr lib/CodeFormatter.so fakeroot/${PREFIX}/lib/codelite/
+cp -pr lib/Copyright.so fakeroot/${PREFIX}/lib/codelite/
+cp -pr lib/Subversion.so fakeroot/${PREFIX}/lib/codelite/
+cp -pr lib/Gizmos.so fakeroot/${PREFIX}/lib/codelite/
+cp -pr lib/cscope.so fakeroot/${PREFIX}/lib/codelite/
+cp -pr lib/UnitTestCPP.so fakeroot/${PREFIX}/lib/codelite/
+cp -pr lib/ExternalTools.so fakeroot/${PREFIX}/lib/codelite/
+cp -pr lib/ContinuousBuild.so fakeroot/${PREFIX}/lib/codelite/
+#cp -pr lib/SymbolView.so fakeroot/${PREFIX}/lib/codelite/
+cp -pr lib/SnipWiz.so fakeroot/${PREFIX}/lib/codelite/
+cp -pr lib/wxformbuilder.so fakeroot/${PREFIX}/lib/codelite/
+cp -pr lib/abbreviation.so fakeroot/${PREFIX}/lib/codelite/
+cp -pr lib/QmakePlugin.so fakeroot/${PREFIX}/lib/codelite/
+cp -pr lib/CppCheck.so fakeroot/${PREFIX}/lib/codelite/
+cp -pr lib/libwxscintillau.so fakeroot/${PREFIX}/lib/codelite/
+cp -pr lib/libpluginu.so fakeroot/${PREFIX}/lib/codelite/
+cp -pr lib/libcodeliteu.so fakeroot/${PREFIX}/lib/codelite/
+cp -pr lib/libwxsqlite3u.so fakeroot/${PREFIX}/lib/codelite/
+cp -pr lib/libwxpropgridu.so fakeroot/${PREFIX}/lib/codelite/
+cp -pr Runtime/plugins/resources/*.* fakeroot/${PREFIX}/lib/codelite/resources/
+cp -pr Runtime/*.html fakeroot/${PREFIX}/share/codelite/
+cp -pr Runtime/astyle.sample fakeroot/${PREFIX}/share/codelite/
+cp -pr Runtime/src/* fakeroot/${PREFIX}/share/codelite/src/
+cp -pr Runtime/images/*.png fakeroot/${PREFIX}/share/codelite/images/
 
-\cp -pr AUTHORS fakeroot/${PREFIX}/share/codelite/
-\cp -pr COPYING fakeroot/${PREFIX}/share/codelite/
+cp -pr AUTHORS fakeroot/${PREFIX}/share/codelite/
+cp -pr COPYING fakeroot/${PREFIX}/share/codelite/
 
 chmod 0644 fakeroot/${PREFIX}/lib/codelite/debuggers/*.so
 chmod 0644 fakeroot/${PREFIX}/lib/codelite/*.so
 
 ## Clean all .svn files
-find fakeroot/usr/ -name ".svn" | xargs rm -fr
+find fakeroot -name ".svn" | xargs rm -fr
 
 ## Get installed size
 inst_size=`du -s fakeroot/usr/ | awk -F" " '{ print $1 }'`
 
-## Make debain control file
-generateControlFile
+## Make debain files
+generateDebianFiles
 
 ## Make desktop file
 generateDesktopFile

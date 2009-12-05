558c6c23ebc839b84eecf22e860b0d7e8bf6af79
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2009-12-05 14:24:03 +0000

    - Added some VS macros for code-completion
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@3440 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- LiteEditor/app.cpp
+++ LiteEditor/app.cpp
@@ -340,7 +340,7 @@ bool App::OnInit()
 #endif
 
 	// Update codelite revision and Version
-	EditorConfig::Init( SvnRevision, wxT("2.0.1") );
+	EditorConfig::Init( SvnRevision, wxT("2.0.2") );
 
 	wxString curdir = wxGetCwd();
 	::wxSetWorkingDirectory(homeDir);

b5c22781564e76a4cadb209431a3722fcedc53a8
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2009-12-28 06:29:03 +0000

    - updated packaging scripts to the correct version pattern
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@3579 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- InnoSetup/make_packages.bat
+++ InnoSetup/make_packages.bat
@@ -4,13 +4,13 @@
 IF "%1"=="" GOTO USAGE
 
 :: Installer name for IDE only
-SET CODELITE_INSTALLER=codelite-2.0.%1
+SET CODELITE_INSTALLER=codelite-2.1.0%1
 
 :: Installer name for IDE + MinGW
-SET CODELITE_INSTALLER_MINGW=codelite-2.0.%1-mingw4.4.0
+SET CODELITE_INSTALLER_MINGW=codelite-2.1.0%1-mingw4.4.0
 
 :: Installer name for IDE + MinGW + WX2.8.10
-SET CODELITE_INSTALLER_MINGW_WX=codelite-2.0.%1-mingw4.4.0-wx2.8.10
+SET CODELITE_INSTALLER_MINGW_WX=codelite-2.1.0%1-mingw4.4.0-wx2.8.10
 
 "%ProgramFiles%\Inno Setup 5\iscc" lite_editor.iss /F%CODELITE_INSTALLER%
 

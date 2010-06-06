a9036d561ef5ceffb38a7fd206176a6df5572438
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2010-06-06 12:43:09 +0000

    Adjusted various packaging scripts for the coming minor release (mainly bug fixes release)
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@4073 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- InnoSetup/make_packages.bat
+++ InnoSetup/make_packages.bat
@@ -4,13 +4,13 @@
 IF "%1"=="" GOTO USAGE
 
 :: Installer name for IDE only
-SET CODELITE_INSTALLER=codelite-2.5.2.%1
+SET CODELITE_INSTALLER=codelite-2.5.3.%1
 
 :: Installer name for IDE + MinGW
-SET CODELITE_INSTALLER_MINGW=codelite-2.5.2.%1-mingw4.4.1
+SET CODELITE_INSTALLER_MINGW=codelite-2.5.3.%1-mingw4.4.1
 
 :: Installer name for IDE + MinGW + WX2.8.10
-SET CODELITE_INSTALLER_MINGW_WX=codelite-2.5.2.%1-mingw4.4.1-wx2.8.10
+SET CODELITE_INSTALLER_MINGW_WX=codelite-2.5.3.%1-mingw4.4.1-wx2.8.10
 
 "%ProgramFiles%\Inno Setup 5\iscc" lite_editor.iss /F%CODELITE_INSTALLER%
 

dc7a53d3dcdef0885298db96f719b0eeda824409
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2010-07-20 19:03:59 +0000

    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@4188 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- LiteEditor/app.cpp
+++ LiteEditor/app.cpp
@@ -63,7 +63,7 @@
 // Define the version string for this codelite
 //////////////////////////////////////////////
 extern wxChar *SvnRevision;
-wxString CODELITE_VERSION_STR = wxString::Format(wxT("v2.6.%s"), SvnRevision);
+wxString CODELITE_VERSION_STR = wxString::Format(wxT("v2.6.0.%s"), SvnRevision);
 
 #if defined(__WXMAC__)||defined(__WXGTK__)
 #include <signal.h> // sigprocmask

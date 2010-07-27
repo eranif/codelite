d13113c8264ffd2ab85cc2c4bf39f7cad493f07f
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2010-07-27 17:42:58 +0000

    Added new tests case to test 'using namespace' statements
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@4218 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- CodeCompletionsTests/CCTest/cctest.cpp
+++ CodeCompletionsTests/CCTest/cctest.cpp
@@ -23,6 +23,14 @@ wxString LoadFile(const wxString &filename)
 // Code Completion test cases
 /////////////////////////////////////////////////////////////////////////////
 
+TEST_FUNC(testUsingNamespace)
+{
+	std::vector<TagEntryPtr> tags;
+	TagsManagerST::Get()->AutoCompleteCandidates(wxFileName(wxT("../tests/using_namespace.h")), 3, wxT("v.at(0)."), LoadFile(wxT("../tests/using_namespace.h")), tags);
+	CHECK_SIZE(tags.size(), 197);
+	return true;
+}
+
 TEST_FUNC(testInnerClassInheritance)
 {
 	std::vector<TagEntryPtr> tags;
---
+++ CodeCompletionsTests/CCTest/tests/using_namespace.h
@@ -0,0 +1,3 @@
+using namespace std;
+
+vector<string> v;

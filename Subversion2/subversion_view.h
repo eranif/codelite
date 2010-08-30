8ad471fb55e72931d9782981ed7ac0a3f419368a
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2010-08-30 15:11:59 +0000

    Added copyrights block to various codelite sources
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@4358 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- Subversion2/subversion_view.h
+++ Subversion2/subversion_view.h
@@ -1,3 +1,28 @@
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//
+// copyright            : (C) 2009 by Eran Ifrah
+// file name            : subversion_view.h
+//
+// -------------------------------------------------------------------------
+// A
+//              _____           _      _     _ _
+//             /  __ \         | |    | |   (_) |
+//             | /  \/ ___   __| | ___| |    _| |_ ___
+//             | |    / _ \ / _  |/ _ \ |   | | __/ _ )
+//             | \__/\ (_) | (_| |  __/ |___| | ||  __/
+//              \____/\___/ \__,_|\___\_____/_|\__\___|
+//
+//                                                  F i l e
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
 #ifndef __subversion_page__
 #define __subversion_page__
 

64aee4cf22045d348f78c2df8279826473033fc8
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2010-10-30 18:49:17 +0000

    - Updated codelite to 2.8.0.X as prepration for next release
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@4530 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- LiteEditor/app.cpp
+++ LiteEditor/app.cpp
@@ -64,7 +64,7 @@
 // Define the version string for this codelite
 //////////////////////////////////////////////
 extern wxChar *SvnRevision;
-wxString CODELITE_VERSION_STR = wxString::Format(wxT("v2.7.0.%s"), SvnRevision);
+wxString CODELITE_VERSION_STR = wxString::Format(wxT("v2.8.0.%s"), SvnRevision);
 
 #if defined(__WXMAC__)||defined(__WXGTK__)
 #include <signal.h> // sigprocmask

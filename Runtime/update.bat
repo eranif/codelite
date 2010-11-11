b6cea7188649eac5b45f617d3b08a420880051d3
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2010-11-11 21:26:09 +0000

    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@4583 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- Runtime/update.bat
+++ Runtime/update.bat
@@ -1,31 +1,31 @@
 @echo off
 
-xcopy config\*.default "%ProgramFiles%\CodeLite\config\" /E /I /H /Y /EXCLUDE:excludes
-xcopy config\build_settings.xml.default.win "%ProgramFiles%\CodeLite\config\build_settings.xml.default" /E /I /H /Y /EXCLUDE:excludes
-xcopy debuggers\*.dll "%ProgramFiles%\CodeLite\debuggers\" /E /I /H /Y /EXCLUDE:excludes
-xcopy images\* "%ProgramFiles%\CodeLite\images\" /E /I /H /Y /EXCLUDE:excludes
-xcopy lexers\*.xml "%ProgramFiles%\CodeLite\lexers\" /E /I /H /Y /EXCLUDE:excludes
-xcopy plugins\*.dll "%ProgramFiles%\CodeLite\plugins\" /E /I /H /Y /EXCLUDE:excludes
-xcopy lib*.dll "%ProgramFiles%\CodeLite\" /E /I /H /Y /EXCLUDE:excludes
-xcopy plugins\resources\*.* "%ProgramFiles%\CodeLite\plugins\resources\" /E /I /H /Y /EXCLUDE:excludes
-xcopy rc\*.xrc "%ProgramFiles%\CodeLite\rc\" /E /I /H /Y /EXCLUDE:excludes
-xcopy templates\ "%ProgramFiles%\CodeLite\templates\" /E /I /H /Y /EXCLUDE:excludes
-xcopy templates\formbuilder\* "%ProgramFiles%\CodeLite\templates\formbuilder\" /E /I /H /Y /EXCLUDE:excludes
-xcopy templates\projects\* "%ProgramFiles%\CodeLite\templates\projects\" /E /I /H /Y /EXCLUDE:excludes
-xcopy templates\gizmos\* "%ProgramFiles%\CodeLite\templates\gizmos\" /E /I /H /Y /EXCLUDE:excludes
-xcopy templates\qmake\* "%ProgramFiles%\CodeLite\templates\qmake\" /E /I /H /Y /EXCLUDE:excludes
-xcopy *.html "%ProgramFiles%\CodeLite\" /H /Y /EXCLUDE:excludes
-xcopy codelite-icons.zip "%ProgramFiles%\CodeLite\" /H /Y /EXCLUDE:excludes
+xcopy config\*.default "%ProgramFiles(x86)%\CodeLite\config\" /E /I /H /Y /EXCLUDE:excludes
+xcopy config\build_settings.xml.default.win "%ProgramFiles(x86)%\CodeLite\config\build_settings.xml.default" /E /I /H /Y /EXCLUDE:excludes
+xcopy debuggers\*.dll "%ProgramFiles(x86)%\CodeLite\debuggers\" /E /I /H /Y /EXCLUDE:excludes
+xcopy images\* "%ProgramFiles(x86)%\CodeLite\images\" /E /I /H /Y /EXCLUDE:excludes
+xcopy lexers\*.xml "%ProgramFiles(x86)%\CodeLite\lexers\" /E /I /H /Y /EXCLUDE:excludes
+xcopy plugins\*.dll "%ProgramFiles(x86)%\CodeLite\plugins\" /E /I /H /Y /EXCLUDE:excludes
+xcopy lib*.dll "%ProgramFiles(x86)%\CodeLite\" /E /I /H /Y /EXCLUDE:excludes
+xcopy plugins\resources\*.* "%ProgramFiles(x86)%\CodeLite\plugins\resources\" /E /I /H /Y /EXCLUDE:excludes
+xcopy rc\*.xrc "%ProgramFiles(x86)%\CodeLite\rc\" /E /I /H /Y /EXCLUDE:excludes
+xcopy templates\ "%ProgramFiles(x86)%\CodeLite\templates\" /E /I /H /Y /EXCLUDE:excludes
+xcopy templates\formbuilder\* "%ProgramFiles(x86)%\CodeLite\templates\formbuilder\" /E /I /H /Y /EXCLUDE:excludes
+xcopy templates\projects\* "%ProgramFiles(x86)%\CodeLite\templates\projects\" /E /I /H /Y /EXCLUDE:excludes
+xcopy templates\gizmos\* "%ProgramFiles(x86)%\CodeLite\templates\gizmos\" /E /I /H /Y /EXCLUDE:excludes
+xcopy templates\qmake\* "%ProgramFiles(x86)%\CodeLite\templates\qmake\" /E /I /H /Y /EXCLUDE:excludes
+xcopy *.html "%ProgramFiles(x86)%\CodeLite\" /H /Y /EXCLUDE:excludes
+xcopy codelite-icons.zip "%ProgramFiles(x86)%\CodeLite\" /H /Y /EXCLUDE:excludes
 
 :: Copy the misc files
-copy codelite_indexer.exe "%ProgramFiles%\CodeLite\" /Y
-copy codelite_cppcheck.exe "%ProgramFiles%\CodeLite\" /Y
-copy codelite_launcher.exe "%ProgramFiles%\CodeLite\" /Y
-copy makedir.exe "%ProgramFiles%\CodeLite\" /Y
-copy le_exec.exe "%ProgramFiles%\CodeLite\" /Y
-copy CodeLite.exe "%ProgramFiles%\CodeLite\" /Y
-copy patch.exe "%ProgramFiles%\CodeLite\" /Y
-copy rm.exe "%ProgramFiles%\CodeLite\" /Y
-copy astyle.sample "%ProgramFiles%\CodeLite\" /Y
-copy ..\sdk\wxconfig\wx-config.exe "%ProgramFiles%\CodeLite\" /Y
+copy codelite_indexer.exe "%ProgramFiles(x86)%\CodeLite\" /Y
+copy codelite_cppcheck.exe "%ProgramFiles(x86)%\CodeLite\" /Y
+copy codelite_launcher.exe "%ProgramFiles(x86)%\CodeLite\" /Y
+copy makedir.exe "%ProgramFiles(x86)%\CodeLite\" /Y
+copy le_exec.exe "%ProgramFiles(x86)%\CodeLite\" /Y
+copy CodeLite.exe "%ProgramFiles(x86)%\CodeLite\" /Y
+copy patch.exe "%ProgramFiles(x86)%\CodeLite\" /Y
+copy rm.exe "%ProgramFiles(x86)%\CodeLite\" /Y
+copy astyle.sample "%ProgramFiles(x86)%\CodeLite\" /Y
+copy ..\sdk\wxconfig\wx-config.exe "%ProgramFiles(x86)%\CodeLite\" /Y
 

eranif/codelite

commit aca68222b3e4528c743966256311ec8f1ce23752

git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@3326 9da81c78-c036-0410-9e1f-a2b0375e4b5a

sdk/codelite_indexer/codelite_index_main.cpp
@@ -22,11 +22,19 @@ int main(int argc, char **argv)
 	bool check_parent(false);
 	long parent_pid (0);
 	if(argc < 2){
-		printf("Usage: %s <string> [--pid]\n", argv[0]);
+		printf("Usage: %s <string> [--pid]\n",    argv[0]);
+		printf("Usage: %s --batch <file_list> <output file>\n", argv[0]);
 		printf("   <string> - a unique string that identifies this indexer from other instances               \n");
 		printf("   --pid    - when set, <string> is handled as process number and the indexer will            \n");
+		printf("   --batch  - when set, batch parsing is done using list of files set in file_list argument   \n");
 		printf("              check if this process alive. Incase it is down, the indexer will go down as well\n");
 		return 1;
+	}
+
+	if ( argc == 4 && strcmp( argv[1], "--batch") == 0 ) {
+		// Batch mode
+		ctags_batch_parse(argv[2], argv[3]);
+		return 0;
 	}
 
 	if ( argc == 3 && strcmp( argv[2], "--pid") == 0 ) {

CodeLite/processreaderthread.h
@@ -22,8 +22,8 @@
 //
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
-#ifndef WORKER_THREAD_H
-#define WORKER_THREAD_H
+#ifndef _ProcessReaderThread_H_
+#define _ProcessReaderThread_H_
 
 #include <deque>
 #include "wx/thread.h"
@@ -122,4 +122,4 @@ public:
 extern const wxEventType wxEVT_PROC_DATA_READ;
 extern const wxEventType wxEVT_PROC_TERMINATED;
 
-#endif // WORKER_THREAD_H
+#endif // _ProcessReaderThread_H_

LiteEditor/fileview.cpp
@@ -212,7 +212,7 @@ void FileViewTree::BuildTree()
 		Thaw();
 
 		//set selection to first item
-		SelectItem( root, false );
+		SelectItem( root, HasFlag(wxTR_MULTIPLE) ? false : true );
 	}
 }
 

sdk/codelite_indexer/codelite_indexer.project
@@ -120,7 +120,6 @@
   </VirtualDirectory>
   <Dependencies Name="Debug_Unix"/>
   <Dependencies Name="Release_Unix"/>
-  <Dependencies Name="Debug_Win"/>
   <Settings Type="Executable">
     <Configuration Name="Debug_Unix" CompilerType="gnu g++" DebuggerType="GNU gdb debugger" Type="Executable" BuildCmpWithGlobalSettings="append" BuildLnkWithGlobalSettings="append" BuildResWithGlobalSettings="append">
       <Compiler Options="-g" Required="yes">
@@ -132,23 +131,23 @@
       <ResourceCompiler Options="" Required="no"/>
       <General OutputFile="$(IntermediateDirectory)/$(ProjectName)" IntermediateDirectory="./Debug" Command="./$(ProjectName)" CommandArguments="" WorkingDirectory="$(IntermediateDirectory)" PauseExecWhenProcTerminates="yes"/>
       <Debugger IsRemote="no" RemoteHostName="" RemoteHostPort="" DebuggerPath="">
-        <PostConnectCommands/>
-        <StartupCommands/>
+        <PostConnectCommands></PostConnectCommands>
+        <StartupCommands></StartupCommands>
       </Debugger>
       <PreBuild/>
       <PostBuild/>
       <CustomBuild Enabled="no">
-        <CleanCommand/>
-        <BuildCommand/>
-        <PreprocessFileCommand/>
-        <SingleFileCommand/>
-        <MakefileGenerationCommand/>
+        <CleanCommand></CleanCommand>
+        <BuildCommand></BuildCommand>
+        <PreprocessFileCommand></PreprocessFileCommand>
+        <SingleFileCommand></SingleFileCommand>
+        <MakefileGenerationCommand></MakefileGenerationCommand>
         <ThirdPartyToolName>None</ThirdPartyToolName>
-        <WorkingDirectory/>
+        <WorkingDirectory></WorkingDirectory>
       </CustomBuild>
       <AdditionalRules>
-        <CustomPostBuild/>
-        <CustomPreBuild/>
+        <CustomPostBuild></CustomPostBuild>
+        <CustomPreBuild></CustomPreBuild>
       </AdditionalRules>
     </Configuration>
     <Configuration Name="Debug_Win" CompilerType="gnu g++" DebuggerType="GNU gdb debugger" Type="Executable" BuildCmpWithGlobalSettings="append" BuildLnkWithGlobalSettings="append" BuildResWithGlobalSettings="append">
@@ -160,25 +159,27 @@
       </Compiler>
       <Linker Options="" Required="yes"/>
       <ResourceCompiler Options="" Required="no"/>
-      <General OutputFile="$(IntermediateDirectory)/$(ProjectName)" IntermediateDirectory="./Debug" Command="./$(ProjectName)" CommandArguments="3356 --pid" WorkingDirectory="$(IntermediateDirectory)" PauseExecWhenProcTerminates="yes"/>
+      <General OutputFile="$(IntermediateDirectory)/$(ProjectName)" IntermediateDirectory="./Debug" Command="C:\Development\C++\codelite\CL_trunk\sdk\codelite_indexer\Debug\$(ProjectName)" CommandArguments="--batch C:/Development/C++/linux-source-2.6.31/cscope_file.list" WorkingDirectory="C:/Development/C++/linux-source-2.6.31/" PauseExecWhenProcTerminates="yes"/>
       <Debugger IsRemote="no" RemoteHostName="" RemoteHostPort="" DebuggerPath="">
-        <PostConnectCommands/>
-        <StartupCommands/>
+        <PostConnectCommands></PostConnectCommands>
+        <StartupCommands></StartupCommands>
       </Debugger>
       <PreBuild/>
       <PostBuild/>
       <CustomBuild Enabled="no">
-        <CleanCommand/>
-        <BuildCommand/>
-        <PreprocessFileCommand/>
-        <SingleFileCommand/>
-        <MakefileGenerationCommand/>
+        <CleanCommand></CleanCommand>
+        <BuildCommand></BuildCommand>
+        <PreprocessFileCommand></PreprocessFileCommand>
+        <SingleFileCommand></SingleFileCommand>
+        <MakefileGenerationCommand></MakefileGenerationCommand>
         <ThirdPartyToolName>None</ThirdPartyToolName>
-        <WorkingDirectory/>
+        <WorkingDirectory></WorkingDirectory>
       </CustomBuild>
       <AdditionalRules>
-        <CustomPostBuild/>
-        <CustomPreBuild/>
+        <CustomPostBuild></CustomPostBuild>
+        <CustomPreBuild>
+
+</CustomPreBuild>
       </AdditionalRules>
     </Configuration>
     <Configuration Name="Release_Unix" CompilerType="gnu g++" DebuggerType="GNU gdb debugger" Type="Executable" BuildCmpWithGlobalSettings="append" BuildLnkWithGlobalSettings="append" BuildResWithGlobalSettings="append">
@@ -193,23 +194,23 @@
       <ResourceCompiler Options="" Required="no"/>
       <General OutputFile="$(IntermediateDirectory)/$(ProjectName)" IntermediateDirectory="./Release" Command="./$(ProjectName)" CommandArguments="" WorkingDirectory="$(IntermediateDirectory)" PauseExecWhenProcTerminates="yes"/>
       <Debugger IsRemote="no" RemoteHostName="" RemoteHostPort="" DebuggerPath="">
-        <PostConnectCommands/>
-        <StartupCommands/>
+        <PostConnectCommands></PostConnectCommands>
+        <StartupCommands></StartupCommands>
       </Debugger>
       <PreBuild/>
       <PostBuild/>
       <CustomBuild Enabled="no">
-        <CleanCommand/>
-        <BuildCommand/>
-        <PreprocessFileCommand/>
-        <SingleFileCommand/>
-        <MakefileGenerationCommand/>
+        <CleanCommand></CleanCommand>
+        <BuildCommand></BuildCommand>
+        <PreprocessFileCommand></PreprocessFileCommand>
+        <SingleFileCommand></SingleFileCommand>
+        <MakefileGenerationCommand></MakefileGenerationCommand>
         <ThirdPartyToolName>None</ThirdPartyToolName>
-        <WorkingDirectory/>
+        <WorkingDirectory></WorkingDirectory>
       </CustomBuild>
       <AdditionalRules>
-        <CustomPostBuild/>
-        <CustomPreBuild/>
+        <CustomPostBuild></CustomPostBuild>
+        <CustomPreBuild></CustomPreBuild>
       </AdditionalRules>
     </Configuration>
     <Configuration Name="Release_Win" CompilerType="gnu g++" DebuggerType="GNU gdb debugger" Type="Executable" BuildCmpWithGlobalSettings="append" BuildLnkWithGlobalSettings="append" BuildResWithGlobalSettings="append">
@@ -220,25 +221,27 @@
       </Compiler>
       <Linker Options="-O2;-s" Required="yes"/>
       <ResourceCompiler Options="" Required="no"/>
-      <General OutputFile="$(IntermediateDirectory)/codelite_indexer" IntermediateDirectory="./Release" Command="./$(ProjectName)" CommandArguments="" WorkingDirectory="$(IntermediateDirectory)" PauseExecWhenProcTerminates="yes"/>
+      <General OutputFile="$(IntermediateDirectory)/codelite_indexer" IntermediateDirectory="./Release" Command="C:\Development\C++\codelite\CL_trunk\sdk\codelite_indexer\Release\$(ProjectName)" CommandArguments="--batch C:/Development/C++/linux-source-2.6.31/cscope_file.list" WorkingDirectory="C:/Development/C++/linux-source-2.6.31/" PauseExecWhenProcTerminates="yes"/>
       <Debugger IsRemote="no" RemoteHostName="" RemoteHostPort="" DebuggerPath="">
-        <PostConnectCommands/>
-        <StartupCommands/>
+        <PostConnectCommands></PostConnectCommands>
+        <StartupCommands></StartupCommands>
       </Debugger>
       <PreBuild/>
       <PostBuild/>
       <CustomBuild Enabled="no">
-        <CleanCommand/>
-        <BuildCommand/>
-        <PreprocessFileCommand/>
-        <SingleFileCommand/>
-        <MakefileGenerationCommand/>
+        <CleanCommand></CleanCommand>
+        <BuildCommand></BuildCommand>
+        <PreprocessFileCommand></PreprocessFileCommand>
+        <SingleFileCommand></SingleFileCommand>
+        <MakefileGenerationCommand></MakefileGenerationCommand>
         <ThirdPartyToolName>None</ThirdPartyToolName>
-        <WorkingDirectory/>
+        <WorkingDirectory></WorkingDirectory>
       </CustomBuild>
       <AdditionalRules>
-        <CustomPostBuild/>
-        <CustomPreBuild/>
+        <CustomPostBuild></CustomPostBuild>
+        <CustomPreBuild>
+
+</CustomPreBuild>
       </AdditionalRules>
     </Configuration>
     <GlobalSettings>
@@ -251,5 +254,6 @@
       <ResourceCompiler Options=""/>
     </GlobalSettings>
   </Settings>
+  <Dependencies Name="Debug_Win"/>
   <Dependencies Name="Release_Win"/>
 </CodeLite_Project>

sdk/codelite_indexer/libctags/libctags.h
@@ -14,7 +14,7 @@ typedef void (*CTAGS_FREE_FUNC)(char *);
 char *ctags_make_tags (const char *cmd, const char *infile);
 void ctags_shutdown();
 void ctags_free(char *ptr);
-
+void ctags_batch_parse(const char* filelist, const char * outputfile);
 #ifdef __cplusplus
 }
 #endif

sdk/codelite_indexer/libctags/main.c
@@ -568,7 +568,9 @@ extern char *ctags_make_tags (const char *cmd, const char *infile)
 	makeTags (args);
 	cArgDelete(args);
 
-	stringListClear(Option.ignore);
+	if ( Option.ignore ) {
+		stringListClear(Option.ignore);
+	}
 
 	/* open the tags file, read it and convert it into char* */
 	file_name = (char*)malloc(strlen(TagFile.directory) + 6);
@@ -698,19 +700,49 @@ extern void ctags_free(char *ptr)
 	}
 }
 
-#if 0
-int main(int argc, char **argv)
+extern void ctags_batch_parse(const char* filelist, const char * outputfile)
 {
-	int i = 0;
-	for (i=0; i<10000; i++) {
-		char *tags = ctags_make_tags("--excmd=pattern --sort=no --fields=aKmSsnit --c-kinds=+p --C++-kinds=+p  -IwxT,_T", "test.h");
-		printf("%s\n\n\n\n\n", tags);
-		free(tags);
+	/* Open the filelist and load list of files */
+	char *  line         = (char*)0;
+	FILE *  fp           = NULL;
+	list_t *l            = NULL;
+	int counter          = 0;
+
+	char *file_content = load_file(filelist);
+
+	if( !file_content ) return;
+
+	/* Open output file */
+	fp = fopen(outputfile, "w+a");
+	if ( !fp ) return;
+
+	/* read the string as line by line */
+	line = strtok(file_content, "\r\n");
+
+	list_init( &l );
+
+	while (line) {
+		if(strlen(line)){
+			list_append(l, (void*)strdup(line));
+		}
+		line = strtok((char*)0, "\n");
 	}
 
+	list_node_t *n = l->head;
+	while( n ) {
+		counter++;
+		char *tags = (char*)0;
+		tags = ctags_make_tags("--excmd=pattern --sort=no --fields=aKmSsnit --c-kinds=+p --C++-kinds=+p ", (char*)n->data);
+		free(n->data);
+		if ( tags ) {
+			fprintf(fp, "%s", tags);
+			free(tags);
+		}
+		n = n->next;
+	}
+	list_destroy( l );
+	free ( file_content );
 	ctags_shutdown();
-	return 0;
 }
-#endif
 
 /* vi:set tabstop=4 shiftwidth=4: */

commit 37560b0852b3fb53dae265b172a9b1abe9059e40

Corrected a 'usage' error introduced by r3326
git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@4775 9da81c78-c036-0410-9e1f-a2b0375e4b5a

sdk/codelite_indexer/codelite_index_main.cpp
@@ -35,8 +35,8 @@ int main(int argc, char **argv)
 		printf("Usage: %s --batch <file_list> <output file>\n", argv[0]);
 		printf("   <string> - a unique string that identifies this indexer from other instances               \n");
 		printf("   --pid    - when set, <string> is handled as process number and the indexer will            \n");
+		printf("              check if this process alive. If it is down, the indexer will go down as well\n");
 		printf("   --batch  - when set, batch parsing is done using list of files set in file_list argument   \n");
-		printf("              check if this process alive. Incase it is down, the indexer will go down as well\n");
 		return 1;
 	}
 
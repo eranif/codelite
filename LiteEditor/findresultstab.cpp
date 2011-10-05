eranif/codelite

commit 8ed2c0f93b4cbd5b4a5feefb8375623240a3f969

Applied with thanks a patch from esminis that fixes:  Bug 3153847: line additions/deletions will no longer break opening FindInFiles results  Bug 3302858: alterations in a line containing multiple matches will no longer break opening the next FindInFiles hit
git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@5083 9da81c78-c036-0410-9e1f-a2b0375e4b5a

LiteEditor/findresultstab.cpp
@@ -712,34 +712,50 @@ void FindResultsTab::PrevMatch()
 void FindResultsTab::DoOpenSearchResult(const SearchResult &result, wxScintilla *sci, int markerLine)
 {
 	if (!result.GetFileName().IsEmpty()) {
-		LEditor *editor = clMainFrame::Get()->GetMainBook()->OpenFile(result.GetFileName(), wxEmptyString, result.GetLineNumber()-1);
-		if (editor && result.GetColumn() >= 0 && result.GetLen() >= 0) {
+		LEditor *editor = clMainFrame::Get()->GetMainBook()->OpenFile(result.GetFileName());
+		if (editor && result.GetLen() >= 0) {
+			// Update the destination position if there have been subsequent changes in the editor
+			int position = result.GetPosition();			
+			const std::vector<int>* changes = editor->GetChanges();
+			unsigned int changesTotal = changes->size();
+			int changePosition = 0;
+			int changeLength = 0;
+			int resultLength = result.GetLen();
+			bool removed = false;
+			for (unsigned int i = 0; i < changesTotal; i += 2) {
+				changePosition = changes->at(i);
+				changeLength = changes->at(i + 1);
+				if ((changeLength < 0) && (changePosition - changeLength > position) &&
+											(changePosition < position + resultLength)) {
+					removed = true;
+					break;
+				} else if (changePosition <= position) {
+					position += changeLength;
+				}
+			}
+			if (!removed) {
+				editor->SetCaretAt(position);
+				editor->EnsureCaretVisible();
+				editor->SetSelection(position, position + resultLength);
 
-			int offset = editor->PositionFromLine(result.GetLineNumber()-1) + result.GetColumn();
+	#ifdef __WXGTK__
+				editor->ScrollToColumn(0);
+	#endif
 
-			editor->SetCaretAt(offset);
-			editor->GotoLine(result.GetLineNumber()-1);
-			editor->EnsureCaretVisible();
-			editor->SetSelection(offset, offset + result.GetLen());
+				if ( sci ) {
+					// remove the previous marker and add the new one
+					sci->MarkerDeleteAll( 7 );
+					sci->MarkerAdd(markerLine, 7 );
 
-#ifdef __WXGTK__
-			editor->ScrollToColumn(0);
-#endif
-
-			if ( sci ) {
-				// remove the previous marker and add the new one
-				sci->MarkerDeleteAll( 7 );
-				sci->MarkerAdd(markerLine, 7 );
-
-				// make the marked line visible
-				int pos = sci->PositionFromLine(markerLine);
-				sci->SetCurrentPos     (pos);
-				sci->SetSelectionStart (pos);
-				sci->SetSelectionEnd   (pos);
-				sci->EnsureCaretVisible(   );
-#ifdef __WXGTK__
-				sci->ScrollToColumn(0);
-#endif
+					// make the marked line visible
+					sci->SetCurrentPos     (position);
+					sci->SetSelectionStart (position);
+					sci->SetSelectionEnd   (position);
+					sci->EnsureCaretVisible();
+	#ifdef __WXGTK__
+					sci->ScrollToColumn(0);
+	#endif
+				}
 			}
 		}
 	}

LiteEditor/cl_editor.cpp
@@ -3574,11 +3574,27 @@ void LEditor::OnChange(wxScintillaEvent& event)
 
 	if (event.GetModificationType() & wxSCI_MOD_INSERTTEXT || event.GetModificationType() & wxSCI_MOD_DELETETEXT) {
 
+		// Cache details of the number of lines added/removed
+		// This is used to 'update' any affected FindInFiles result. See bug 3153847
+		if (event.GetModificationType() & wxSCI_PERFORMED_UNDO) {
+			m_changes.pop_back();
+			m_changes.pop_back();
+		} else {
+			m_changes.push_back(event.GetPosition());
+			m_changes.push_back(event.GetLength() * (event.GetModificationType() & wxSCI_MOD_DELETETEXT ? -1 : 1));
+		}
+
 		int numlines(event.GetLinesAdded());
-		if ( numlines && GetReloadingFile() == false) {
-			// a line was added / removed from the document, synchronized between the breakpoints on this editor
-			// and the breakpoint manager
-			UpdateBreakpoints();
+
+		if ( numlines) {
+			if (GetReloadingFile() == false) {
+				// a line was added to or removed from the document, so synchronize the breakpoints on this editor
+				// and the breakpoint manager
+				UpdateBreakpoints();
+			} else {
+				// The file has been reloaded, so the cached line-changes are no longer relevant
+				m_changes.clear();
+			}
 		}
 
 		// ignore this event incase we are in the middle of file reloading

LiteEditor/cl_editor.h
@@ -114,6 +114,7 @@ class LEditor : public wxScintilla, public IEditor
 	wxStopWatch                                 m_watch;
 	ContextBasePtr                              m_context;
 	wxMenu *                                    m_rightClickMenu;
+	std::vector<int>                            m_changes;
 	std::vector<wxMenuItem*>                    m_dynItems;
 	std::vector<BPtoMarker>                     m_BPstoMarkers;
 	static FindReplaceDialog *                  m_findReplaceDlg;
@@ -637,6 +638,13 @@ public:
 
 	wxString GetEolString();
 	void HighlightWord(StringHighlightOutput *highlightOutput);
+
+	/**
+	 * Get the vector of line-number changes. Used for FindInFiles
+	 */
+	const std::vector<int>* GetChanges() {
+		return &m_changes;
+	}
 
 private:
 	void FillBPtoMarkerArray();

Plugin/search_thread.cpp
@@ -265,13 +265,14 @@ void SearchThread::DoSearchFile(const wxString &fileName, const SearchData *data
 		CppWordScanner scanner("", fileData.mb_str().data(), 0);
 		states = scanner.states();
 	}
-
+	int lineOffset = 0;
 	if ( data->IsRegularExpression() ) {
 		// regular expression search
 		while (tkz.HasMoreTokens()) {
 			// Read the next line
 			wxString line = tkz.NextToken();
-			DoSearchLineRE(line, lineNumber, fileName, data, states);
+			DoSearchLineRE(line, lineNumber, lineOffset, fileName, data, states);
+			lineOffset += line.Length() + 1;
 			lineNumber++;
 		}
 	} else {
@@ -280,7 +281,8 @@ void SearchThread::DoSearchFile(const wxString &fileName, const SearchData *data
 
 			// Read the next line
 			wxString line = tkz.NextToken();
-			DoSearchLine(line, lineNumber, fileName, data, states);
+			DoSearchLine(line, lineNumber, lineOffset, fileName, data, states);
+			lineOffset += line.Length() + 1;
 			lineNumber++;
 		}
 	}
@@ -288,7 +290,7 @@ void SearchThread::DoSearchFile(const wxString &fileName, const SearchData *data
 	if ( m_results.empty() == false )
 		SendEvent(wxEVT_SEARCH_THREAD_MATCHFOUND, data->GetOwner());
 }
-void SearchThread::DoSearchLineRE(const wxString &line, const int lineNum, const wxString &fileName, const SearchData *data, TextStatesPtr statesPtr)
+void SearchThread::DoSearchLineRE(const wxString &line, const int lineNum, const int lineOffset, const wxString &fileName, const SearchData *data, TextStatesPtr statesPtr)
 {
 	wxRegEx &re = GetRegex(data->GetFindString(), data->IsMatchCase());
 	size_t col = 0;
@@ -306,6 +308,7 @@ void SearchThread::DoSearchLineRE(const wxString &line, const int lineNum, const
 			iCorrectedCol = UTF8Length(line.c_str(), col);
 			iCorrectedLen = UTF8Length(line.c_str(), col+len) - iCorrectedCol;
 			SearchResult result;
+			result.SetPosition(lineOffset + col);
 			result.SetColumnInChars((int)col);
 			result.SetColumn(iCorrectedCol);
 			result.SetLineNumber(lineNum);
@@ -369,7 +372,7 @@ void SearchThread::DoSearchLineRE(const wxString &line, const int lineNum, const
 	}
 }
 
-void SearchThread::DoSearchLine(const wxString &line, const int lineNum, const wxString &fileName, const SearchData *data, TextStatesPtr statesPtr)
+void SearchThread::DoSearchLine(const wxString &line, const int lineNum, const int lineOffset, const wxString &fileName, const SearchData *data, TextStatesPtr statesPtr)
 {
 	wxString findString = data->GetFindString();
 	wxString modLine = line;
@@ -422,6 +425,7 @@ void SearchThread::DoSearchLine(const wxString &line, const int lineNum, const w
 			iCorrectedCol = UTF8Length(line.c_str(), col);
 			iCorrectedLen = UTF8Length(findString.c_str(), findString.Length());
 			SearchResult result;
+			result.SetPosition(lineOffset + col);
 			result.SetColumnInChars(col);
 			result.SetColumn(iCorrectedCol);
 			result.SetLineNumber(lineNum);

Plugin/search_thread.h
@@ -228,6 +228,7 @@ public:
 //------------------------------------------
 class WXDLLIMPEXP_SDK SearchResult : public wxObject {
 	wxString m_pattern;
+	int      m_position;
 	int      m_lineNumber;
 	int      m_column;
 	wxString m_fileName;
@@ -252,7 +253,7 @@ public:
 	SearchResult& operator=(const SearchResult &rhs) {
 		if (this == &rhs)
 			return *this;
-
+		m_position      = rhs.m_position;
 		m_column        = rhs.m_column;
 		m_lineNumber    = rhs.m_lineNumber;
 		m_pattern       = rhs.m_pattern.c_str();
@@ -281,6 +282,9 @@ public:
 	void SetPattern(const wxString &pat) {
 		m_pattern = pat.c_str();
 	}
+	void SetPosition(const int &position) {
+		m_position = position;
+	}
 	void SetLineNumber(const int &line) {
 		m_lineNumber = line;
 	}
@@ -291,6 +295,9 @@ public:
 		m_fileName = fileName.c_str();
 	}
 
+	const int& GetPosition() const {
+		return m_position;
+	}
 	const int& GetLineNumber() const {
 		return m_lineNumber;
 	}
@@ -506,10 +513,10 @@ private:
 	void DoSearchFile(const wxString &fileName, const SearchData *data);
 
 	// Perform search on a line
-	void DoSearchLine(const wxString &line, const int lineNum, const wxString &fileName, const SearchData *data, TextStatesPtr statesPtr);
+	void DoSearchLine(const wxString &line, const int lineNum, const int lineOffset, const wxString &fileName, const SearchData *data, TextStatesPtr statesPtr);
 
 	// Perform search on a line using regular expression
-	void DoSearchLineRE(const wxString &line, const int lineNum, const wxString &fileName, const SearchData *data, TextStatesPtr statesPtr);
+	void DoSearchLineRE(const wxString &line, const int lineNum, const int lineOffset, const wxString &fileName, const SearchData *data, TextStatesPtr statesPtr);
 
 	// Send an event to the notified window
 	void SendEvent(wxEventType type, wxEvtHandler *owner);

commit 09b15ea72604a106ddaeeefeb1dde62a1af7603d

Fix a bug introduced by r5083: when going to a FindInFiles result, the line wouldn't be made visible on the screen if that editor was not already opened.
git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@5094 9da81c78-c036-0410-9e1f-a2b0375e4b5a

LiteEditor/findresultstab.cpp
@@ -735,6 +735,8 @@ void FindResultsTab::DoOpenSearchResult(const SearchResult &result, wxScintilla 
 			}
 			if (!removed) {
 				editor->SetCaretAt(position);
+				// The next line is necessary as otherwise EnsureCaretVisible() fails when the file wasn't previously open :/
+				editor->GotoLine(editor->LineFromPosition(position)-1);
 				editor->EnsureCaretVisible();
 				editor->SetSelection(position, position + resultLength);
 
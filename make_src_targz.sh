726d7249687cfc28ca686c224284c0a82bee1f86
dghart <dghart@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2011-11-25 22:14:26 +0000

    Improved the source-archive creation script. It now creates one suitable for *nix, another for MSWin
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@5312 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- make_src_targz.sh
+++ make_src_targz.sh
@@ -9,15 +9,21 @@
 
 cur_rev=`svn info | grep Revision | awk '{print $2;}'`
 curdir=`pwd`
-codelite_ver="codelite-3.0.0"
-rm -fr /tmp/codelite-${cur_rev}/
+codelite_ver="codelite-3.5.0"
+
+rm -fr /tmp/${codelite_ver}.${cur_rev}/
 
 svn export . /tmp/${codelite_ver}.${cur_rev}/
 
 ## set correct permissions
 
 cp ./LiteEditor/svninfo.cpp /tmp/${codelite_ver}.${cur_rev}/LiteEditor/
-echo "Creating tar-ball"
+echo "Creating tarballs"
 cd /tmp/
 rm -fr /tmp/${codelite_ver}.${cur_rev}/sdk/curl
-tar cvfz ${curdir}/${codelite_ver}.${cur_rev}.tar.gz ${codelite_ver}.${cur_rev}/* --exclude ${codelite_ver}.${cur_rev}/Runtime/clang++.exe
+
+# First for *nix
+tar cvfz ${curdir}/${codelite_ver}.${cur_rev}.tar.gz ${codelite_ver}.${cur_rev}/* --exclude *.exe --exclude *.dll
+
+# Then for MSWin
+tar cv --lzma -f ${curdir}/${codelite_ver}.${cur_rev}.tar.7z ${codelite_ver}.${cur_rev}/* --exclude *.so

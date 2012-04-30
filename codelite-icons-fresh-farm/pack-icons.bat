9882670ec6722a4e2e738500f8b601bae146a103
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2012-04-30 21:05:32 +0000

    Added new icon set to codelite
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@5493 9da81c78-c036-0410-9e1f-a2b0375e4b5a

---
+++ codelite-icons-fresh-farm/pack-icons.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+set CURRENT_DUR=%CD%
+set PATH=%PATH%;%ProgramFiles(x86)%\7-Zip;C:\Program Files\7-Zip
+rmdir /S /Q ..\tmp-1
+svn export . ..\tmp-1 --force
+cd ..\tmp
+7z a -tzip codelite-icons-fresh-farm.zip *
+copy codelite-icons-fresh-farm.zip ..\Runtime
+cd %CURRENT_DUR%
+rmdir /S /Q ..\tmp-1

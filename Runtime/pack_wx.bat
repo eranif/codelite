2c48241193f3c76c60217a5dbb3f78abfc59d32d
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2012-05-26 09:03:06 +0000

    Updated packaging scripts
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@5559 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- Runtime/pack_wx.bat
+++ Runtime/pack_wx.bat
@@ -1,5 +1,5 @@
 @echo off
 if not exist wxWidgets-2.9.4 mkdir wxWidgets-2.9.4
-xcopy C:\wx-trunk\lib\gcc_dll\* "wxWidgets-2.9.4\lib\gcc_dll\" /E /I /H /Y /EXCLUDE:wx_excludes
-xcopy C:\wx-trunk\include\* "wxWidgets-2.9.4\include\" /E /I /H /Y /EXCLUDE:wx_excludes
-xcopy C:\wx-trunk\build\msw\config.gcc "wxWidgets-2.9.4\build\msw\config.gcc" /Y /EXCLUDE:wx_excludes
+xcopy C:\src\wx-trunk\lib\gcc_dll\* "wxWidgets-2.9.4\lib\gcc_dll\" /E /I /H /Y /EXCLUDE:wx_excludes
+xcopy C:\src\wx-trunk\include\* "wxWidgets-2.9.4\include\" /E /I /H /Y /EXCLUDE:wx_excludes
+xcopy C:\src\wx-trunk\build\msw\config.gcc "wxWidgets-2.9.4\build\msw\config.gcc" /Y /EXCLUDE:wx_excludes

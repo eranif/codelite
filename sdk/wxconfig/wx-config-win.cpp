b623862f51716a2ed181b59d06037ff079644796
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2012-08-30 16:37:03 +0000

    Make wx-config to honor --libs <comma-list> in the middle of the command line argument
    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@5773 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- sdk/wxconfig/wx-config-win.cpp
+++ sdk/wxconfig/wx-config-win.cpp
@@ -326,9 +326,15 @@ public:
             std::string line;
             line = argv[i];
             split(line);
+			
+			if( line == "--libs" ) {
+				std::string libsList;
+				if( (i + 1) < argc ) {
+					libsList = argv[i+1];
+				}
+				parseLibs(libsList);
+			}
         }
-        parseLibs(argv[argc-1]);
-
         return true;
     }
 

ddba9a4f9bd683d2c60e8b97439dc623627e15cb
eranif <eranif@9da81c78-c036-0410-9e1f-a2b0375e4b5a>
2013-01-26 15:35:20 +0000

    git-svn-id: https://codelite.svn.sourceforge.net/svnroot/codelite/trunk@6213 9da81c78-c036-0410-9e1f-a2b0375e4b5a

--- InnoSetup/make_packages.bat
+++ InnoSetup/make_packages.bat
@@ -7,7 +7,7 @@ IF "%1"=="win7" GOTO WIN7
 
 :: Preare a development installer
 SET CODELITE_INSTALLER=codelite-devel
-"D:\software%\Inno Setup 5\iscc" lite_editor.iss /F%CODELITE_INSTALLER%
+"D:\software\Inno Setup 5\iscc" lite_editor.iss /F%CODELITE_INSTALLER%
 
 GOTO END
 
@@ -15,7 +15,7 @@ GOTO END
 
 :: Preare a development installer for Windows 7
 SET CODELITE_INSTALLER=codelite-devel
-"D:\software%\Inno Setup 5\iscc" lite_editor_win7.iss /F%CODELITE_INSTALLER%
+"D:\software\Inno Setup 5\iscc" lite_editor_win7.iss /F%CODELITE_INSTALLER%
 
 GOTO END
 
@@ -32,11 +32,11 @@ SET CODELITE_INSTALLER_MINGW=codelite-5.0.%2-mingw4.7.1
 :: Installer name for IDE + MinGW + WX2.9.4
 SET CODELITE_INSTALLER_MINGW_WX=codelite-5.0.%2-mingw4.7.1-wx2.9.4
 
-"D:\software%\Inno Setup 5\iscc" lite_editor.iss /F%CODELITE_INSTALLER%
+"D:\software\Inno Setup 5\iscc" lite_editor.iss /F%CODELITE_INSTALLER%
 
-"D:\software%\Inno Setup 5\iscc" codelite_mingw.iss /F%CODELITE_INSTALLER_MINGW%
+"D:\software\Inno Setup 5\iscc" codelite_mingw.iss /F%CODELITE_INSTALLER_MINGW%
 
-"D:\software%\Inno Setup 5\iscc" codelite_mingw_wx.iss /F%CODELITE_INSTALLER_MINGW_WX%
+"D:\software\Inno Setup 5\iscc" codelite_mingw_wx.iss /F%CODELITE_INSTALLER_MINGW_WX%
 
 GOTO END
 

147311e007ff62ab56590402bf205f8a2729dc0f
dghart <david@4Pane.co.uk>
2013-02-01 17:07:05 +0000

    Better fix for git-revision.sh
    for values of 'better' that include 1) working, and 2) not asserting

--- git-revision.sh
+++ git-revision.sh
@@ -10,9 +10,9 @@ echo "#ifndef CL_GIT_REVISION" > LiteEditor/autoversion.cpp
 echo "#define CL_GIT_REVISION" >> LiteEditor/autoversion.cpp
 echo "" >> LiteEditor/autoversion.cpp
 
-#echo "#include \"wx/chartype.h\"" >> LiteEditor/autoversion.cpp
+echo "#include \"wx/string.h\"" >> LiteEditor/autoversion.cpp
 
-echo "const char* clGitRevision = \"${GIT_VERSION}\";" >> LiteEditor/autoversion.cpp
+echo "const wxChar* clGitRevision = wxT(\"${GIT_VERSION}\");" >> LiteEditor/autoversion.cpp
 echo "#endif" >> LiteEditor/autoversion.cpp
 echo "" >> LiteEditor/autoversion.cpp
 

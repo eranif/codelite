1468d43b4f358b8e651339d0e8c8d350c6a4183a
eran <eran.ifrah@gmail.com>
2013-02-07 09:16:12 +0200

    codelite-clang: when parsing a file and creating a TU, make sure that the flag 'CXTranslationUnit_DetailedPreprocessingRecord' is also passed to clang_parseTranslationUnit

--- codelite_clang/main.cpp
+++ codelite_clang/main.cpp
@@ -12,15 +12,15 @@ void usage(const char* program)
     printf("Usage: %s {parse|parse-macros|code-complete} <file> [output-folder] <compiler options>\n", program);
     printf("    parse <source-file> <output-folder> <compiler-agruments> and compile it into an AST\n");
     printf("    parse-macros <source-file> <output-folder> <compiler-arguments> parse if needed and print all macros found into stdout\n");
-	printf("    code-complete <source-file> <AST-file> <line:col>\n");
+    printf("    code-complete <source-file> <AST-file> <line:col>\n");
     exit (0);
 }
 
 int main( int argc, char** argv )
 {
 #ifdef _WIN32
-	// No windows crash dialogs
-	SetErrorMode(SEM_FAILCRITICALERRORS|SEM_NOGPFAULTERRORBOX|SEM_NOOPENFILEERRORBOX);
+    // No windows crash dialogs
+    SetErrorMode(SEM_FAILCRITICALERRORS|SEM_NOGPFAULTERRORBOX|SEM_NOOPENFILEERRORBOX);
 #endif
 
     wxInitialize();
@@ -28,13 +28,13 @@ int main( int argc, char** argv )
         usage(argv[0]);
     }
     argv++; // Skip the program name
-    
+
     const char* command = argv[0];
     argv++;
-    
+
     const char* file    = argv[0];
     argv++;
-    
+
     Clang clang(file, command, argc - 3, argv);
     if(!clang.IsOK()) {
         usage(argv[0]);

2bcfa6ee4fcc973346f57bb68caec8a9b97fb654
eran <eran.ifrah@gmail.com>
2013-02-23 12:28:01 +0200

    Fixed: https://sourceforge.net/p/codelite/bugs/875/

--- git/gitentry.cpp
+++ git/gitentry.cpp
@@ -26,6 +26,8 @@ GitEntry::GitEntry()
 #endif
     , m_gitDiffDlgSashPos(0)
     , m_gitConsoleSashPos(0)
+    , m_colourTrackedFile(*wxGREEN)
+    , m_colourDiffFile(*wxRED)
 {
 }
 
@@ -36,21 +38,36 @@ GitEntry::~GitEntry()
 void GitEntry::FromJSON(const JSONElement& json)
 {
     m_entries           = json.namedObject("m_entries").toStringMap();
-    m_colourTrackedFile = json.namedObject("m_colourTrackedFile").toString();
-    m_colourDiffFile    = json.namedObject("m_colourDiffFile").toString();
+    wxString track, diff;
+    track               = json.namedObject("m_colourTrackedFile").toString();
+    diff                = json.namedObject("m_colourDiffFile").toString();
     m_pathGIT           = json.namedObject("m_pathGIT").toString();
     m_pathGITK          = json.namedObject("m_pathGITK").toString();
     m_flags             = json.namedObject("m_flags").toSize_t(m_flags);
     m_gitDiffDlgSashPos = json.namedObject("m_gitDiffDlgSashPos").toInt(m_gitDiffDlgSashPos);
     m_gitConsoleSashPos = json.namedObject("m_gitConsoleSashPos").toInt(m_gitConsoleSashPos);
+    
+    // override the colour only if it is a valid colour
+    if ( !track.IsEmpty() ) {
+        m_colourTrackedFile = track;
+    }
+    if ( !diff.IsEmpty() ) {
+        m_colourDiffFile = diff;
+    }
 }
 
 JSONElement GitEntry::ToJSON() const
 {
     JSONElement json = JSONElement::createObject(GetName());
     json.addProperty("m_entries",           m_entries);
-    json.addProperty("m_colourTrackedFile", m_colourTrackedFile.GetAsString(wxC2S_HTML_SYNTAX));
-    json.addProperty("m_colourDiffFile",    m_colourDiffFile.GetAsString(wxC2S_HTML_SYNTAX));
+    if ( m_colourTrackedFile.IsOk() ) {
+        json.addProperty("m_colourTrackedFile", m_colourTrackedFile.GetAsString(wxC2S_HTML_SYNTAX));
+    }
+    
+    if ( m_colourDiffFile.IsOk() ) {
+        json.addProperty("m_colourDiffFile",    m_colourDiffFile.GetAsString(wxC2S_HTML_SYNTAX));
+    }
+    
     json.addProperty("m_pathGIT",           m_pathGIT);
     json.addProperty("m_pathGITK",          m_pathGITK);
     json.addProperty("m_flags",             m_flags);

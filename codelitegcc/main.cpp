f0bfd7c27a7660e3a031c914b1f1f5a5973a5f07
Eran <eran.ifrah@gmail.com>
2013-04-29 18:03:12 +0300

    Simplified codelitegcc source code (it should now be able to handle file names with empty spaces in their path)

--- codelitegcc/main.cpp
+++ codelitegcc/main.cpp
@@ -13,7 +13,7 @@ typedef std::vector<std::string> StringVec_t;
 
 std::vector<std::string> extract_file_name( const std::string& line );
 char * normalize_path(const char * src, size_t src_len);
-
+bool is_source_file(const std::string& filename, std::string &fixed_file_name);
 void * Memrchr(const void *buf, int c, size_t num);
 
 #ifndef _WIN32
@@ -33,13 +33,19 @@ int main(int argc, char **argv)
     if ( argc < 2 ) {
         return -1;
     }
-
+    
+    StringVec_t file_names;
     const char *pdb = getenv("CL_COMPILATION_DB");
     std::string commandline;
     for ( int i=1; i<argc; ++i ) {
         // Wrap all arguments with spaces with double quotes
         std::string arg = argv[i];
-
+        std::string file_name;
+        
+        if ( is_source_file( arg, file_name ) ) {
+            file_names.push_back( file_name );
+        }
+        
         // re-escape double quotes if needed
         size_t pos = arg.find('"');
         while ( pos != std::string::npos ) {
@@ -57,7 +63,6 @@ int main(int argc, char **argv)
     }
 
     if ( pdb ) {
-        StringVec_t file_names = extract_file_name(commandline);
         for(size_t i=0; i<file_names.size(); ++i) {
 #if __DEBUG
             printf("filename: %s\n", file_names.at(i).c_str());
@@ -79,84 +84,41 @@ int main(int argc, char **argv)
     return exitCode;
 }
 
-size_t search_file_extension(const std::string &line, const std::string& ext)
-{
-    // note about searching the extension:
-    // the 'line' passed to this function will *always* have a trailing space (see the 'main' function)
-    // this fact makes the search easier for us:
-    // always search for '{ext} ' or '{ext}"' this will avoid false matching like:
-    // {source-name}.cpp.o
-
-    StringVec_t opts;
-    opts.push_back(ext + " ");
-    opts.push_back(ext + '"');
-
-    for(size_t i=0; i<opts.size(); ++i) {
-        size_t where = line.find(opts.at(i));
-        if ( where != std::string::npos ) {
-            return where;
-        }
-    }
-    return std::string::npos;
-}
-
-/**
- * @brief split an input string using 'delim'
- */
-StringVec_t& split_string(const std::string &s, char delim, StringVec_t &elems)
-{
-    std::stringstream ss(s);
-    std::string item;
-    while (std::getline(ss, item, delim)) {
-        elems.push_back(item);
-    }
-    return elems;
-}
-
 bool ends_with(const std::string &s, const std::string& e)
 {
     size_t where = s.rfind(e);
     return ( where == std::string::npos ? false : ((s.length() - where) == e.length()) );
 }
 
-StringVec_t extract_file_name(const std::string& line)
+bool is_source_file(const std::string& filename, std::string &fixed_file_name)
 {
-    StringVec_t extensions, tokens;
+    StringVec_t extensions;
     extensions.push_back(".cpp");
     extensions.push_back(".cxx");
     extensions.push_back(".cc");
     extensions.push_back(".c");
-
-    std::string tmp_line = line;
-    /// replace all tabs with spaces
-    std::replace(tmp_line.begin(), tmp_line.end(), '\t', ' ');
-
-    split_string(line, ' ', tokens);
-    StringVec_t files;
-    for(size_t i=0; i<tokens.size(); ++i) {
-        for(size_t n=0; n<extensions.size(); ++n) {
-            if ( ends_with(tokens.at(i), extensions.at(n)) ) {
-                std::string file_name = tokens.at(i);
+    
+    for(size_t n=0; n<extensions.size(); ++n) {
+        if ( ends_with(filename, extensions.at(n)) ) {
+            fixed_file_name = filename;
 
 #ifdef _WIN32
-                std::replace(file_name.begin(), file_name.end(), '/', '\\');
+            std::replace(fixed_file_name.begin(), fixed_file_name.end(), '/', '\\');
 #endif
-                char* ret = normalize_path(file_name.c_str(), file_name.length());
-                file_name = ret;
-                free(ret);
+            char* ret = normalize_path(fixed_file_name.c_str(), fixed_file_name.length());
+            fixed_file_name = ret;
+            free(ret);
 
-                // rtrim
-                file_name.erase(0, file_name.find_first_not_of("\t\r\v\n\" "));
+            // rtrim
+            fixed_file_name.erase(0, fixed_file_name.find_first_not_of("\t\r\v\n\" "));
 
-                // ltrim
-                file_name.erase(file_name.find_last_not_of("\t\r\v\n\" ")+1);
-
-                files.push_back( file_name );
-                break;
-            }
+            // ltrim
+            fixed_file_name.erase(fixed_file_name.find_last_not_of("\t\r\v\n\" ")+1);
+            return true;
         }
     }
-    return files;
+    fixed_file_name.clear();
+    return false;
 }
 
 char * normalize_path(const char * src, size_t src_len)

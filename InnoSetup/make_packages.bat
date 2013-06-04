7cee14b087fa983065407cd436107d13a9c0b902
eran <eran.ifrah@gmail.com>
2013-06-04 20:50:10 +0300

    Added new installer script for wxWidgets make_packages.bat script now only packs codelite+mingw

--- InnoSetup/make_packages.bat
+++ InnoSetup/make_packages.bat
@@ -3,7 +3,6 @@
 :: Check command arguments
 IF "%1"=="/?" GOTO USAGE
 IF "%1"=="all" GOTO ALL
-IF "%1"=="win7" GOTO WIN7
 
 :: Preare a development installer
 SET CODELITE_INSTALLER=codelite-devel
@@ -11,32 +10,14 @@ SET CODELITE_INSTALLER=codelite-devel
 
 GOTO END
 
-:WIN7
-
-:: Preare a development installer for Windows 7
-SET CODELITE_INSTALLER=codelite-devel
-"D:\software\Inno Setup 5\iscc" lite_editor_win7.iss /F%CODELITE_INSTALLER%
-
-GOTO END
-
 :ALL
 
 IF "%2"=="" GOTO USAGE
 
-:: Installer name for IDE only
-SET CODELITE_INSTALLER=codelite-5.1.%2
-
 :: Installer name for IDE + MinGW
-SET CODELITE_INSTALLER_MINGW=codelite-5.1.%2-mingw4.7.1
-
-:: Installer name for IDE + MinGW + WX2.9.4
-SET CODELITE_INSTALLER_MINGW_WX=codelite-5.1.%2-mingw4.7.1-wx2.9.4
-
-"D:\software\Inno Setup 5\iscc" lite_editor.iss /F%CODELITE_INSTALLER%
+SET CODELITE_INSTALLER_MINGW=codelite-5.2.%2-mingw4.7.1
 
 "D:\software\Inno Setup 5\iscc" codelite_mingw.iss /F%CODELITE_INSTALLER_MINGW%
-
-"D:\software\Inno Setup 5\iscc" codelite_mingw_wx.iss /F%CODELITE_INSTALLER_MINGW_WX%
 
 GOTO END
 

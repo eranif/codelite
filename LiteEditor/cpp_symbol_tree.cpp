2d880d5f2bfd2784baf744feef2e926f27af133a
eran <eran.ifrah@gmail.com>
2013-08-26 10:26:36 +0300

    Call EnsureVisible after selecting an item

--- LiteEditor/cpp_symbol_tree.cpp
+++ LiteEditor/cpp_symbol_tree.cpp
@@ -44,26 +44,26 @@ const wxEventType wxEVT_CMD_CPP_SYMBOL_ITEM_SELECTED = wxNewEventType();
 //----------------------------------------------------------------
 wxImageList* CreateSymbolTreeImages()
 {
-	wxImageList *images = new wxImageList(16, 16, true);
+    wxImageList *images = new wxImageList(16, 16, true);
 
-	BitmapLoader *bmpLoader = PluginManager::Get()->GetStdIcons();
-	images->Add(bmpLoader->LoadBitmap(wxT("mime/16/cpp")));               // 0
-	images->Add(bmpLoader->LoadBitmap(wxT("cc/16/namespace")));           // 1
-	images->Add(bmpLoader->LoadBitmap(wxT("cc/16/globals")));             // 2
-	images->Add(bmpLoader->LoadBitmap(wxT("cc/16/class")));               // 3
-	images->Add(bmpLoader->LoadBitmap(wxT("cc/16/struct")));              // 4
-	images->Add(bmpLoader->LoadBitmap(wxT("cc/16/function_public")));     // 5
-	images->Add(bmpLoader->LoadBitmap(wxT("cc/16/function_protected")));  // 6
-	images->Add(bmpLoader->LoadBitmap(wxT("cc/16/function_private")));    // 7
-	images->Add(bmpLoader->LoadBitmap(wxT("cc/16/member_public")));       // 8
-	images->Add(bmpLoader->LoadBitmap(wxT("cc/16/member_protected")));    // 9
-	images->Add(bmpLoader->LoadBitmap(wxT("cc/16/member_private")));      // 10
-	images->Add(bmpLoader->LoadBitmap(wxT("cc/16/typedef")));             // 11
-	images->Add(bmpLoader->LoadBitmap(wxT("cc/16/macro")));               // 12
-	images->Add(bmpLoader->LoadBitmap(wxT("cc/16/enum")));                // 13
-	images->Add(bmpLoader->LoadBitmap(wxT("cc/16/enumerator")));          // 14
-	images->Add(bmpLoader->LoadBitmap(wxT("mime/16/cpp")));               // 15
-	return images;
+    BitmapLoader *bmpLoader = PluginManager::Get()->GetStdIcons();
+    images->Add(bmpLoader->LoadBitmap(wxT("mime/16/cpp")));               // 0
+    images->Add(bmpLoader->LoadBitmap(wxT("cc/16/namespace")));           // 1
+    images->Add(bmpLoader->LoadBitmap(wxT("cc/16/globals")));             // 2
+    images->Add(bmpLoader->LoadBitmap(wxT("cc/16/class")));               // 3
+    images->Add(bmpLoader->LoadBitmap(wxT("cc/16/struct")));              // 4
+    images->Add(bmpLoader->LoadBitmap(wxT("cc/16/function_public")));     // 5
+    images->Add(bmpLoader->LoadBitmap(wxT("cc/16/function_protected")));  // 6
+    images->Add(bmpLoader->LoadBitmap(wxT("cc/16/function_private")));    // 7
+    images->Add(bmpLoader->LoadBitmap(wxT("cc/16/member_public")));       // 8
+    images->Add(bmpLoader->LoadBitmap(wxT("cc/16/member_protected")));    // 9
+    images->Add(bmpLoader->LoadBitmap(wxT("cc/16/member_private")));      // 10
+    images->Add(bmpLoader->LoadBitmap(wxT("cc/16/typedef")));             // 11
+    images->Add(bmpLoader->LoadBitmap(wxT("cc/16/macro")));               // 12
+    images->Add(bmpLoader->LoadBitmap(wxT("cc/16/enum")));                // 13
+    images->Add(bmpLoader->LoadBitmap(wxT("cc/16/enumerator")));          // 14
+    images->Add(bmpLoader->LoadBitmap(wxT("mime/16/cpp")));               // 15
+    return images;
 }
 
 CppSymbolTree::CppSymbolTree()
@@ -71,148 +71,149 @@ CppSymbolTree::CppSymbolTree()
 }
 
 CppSymbolTree::CppSymbolTree(wxWindow *parent, const wxWindowID id, const wxPoint &pos, const wxSize &size, long style)
-		: SymbolTree(parent, id, pos, size, style)
+    : SymbolTree(parent, id, pos, size, style)
 {
-	Connect(GetId(), wxEVT_COMMAND_TREE_ITEM_RIGHT_CLICK, wxTreeEventHandler(CppSymbolTree::OnMouseRightUp));
-	Connect(GetId(), wxEVT_LEFT_DCLICK, wxMouseEventHandler(CppSymbolTree::OnMouseDblClick));
-	Connect(GetId(), wxEVT_COMMAND_TREE_KEY_DOWN, wxTreeEventHandler(CppSymbolTree::OnItemActivated));
-	MSWSetNativeTheme(this);
+    Connect(GetId(), wxEVT_COMMAND_TREE_ITEM_RIGHT_CLICK, wxTreeEventHandler(CppSymbolTree::OnMouseRightUp));
+    Connect(GetId(), wxEVT_LEFT_DCLICK, wxMouseEventHandler(CppSymbolTree::OnMouseDblClick));
+    Connect(GetId(), wxEVT_COMMAND_TREE_KEY_DOWN, wxTreeEventHandler(CppSymbolTree::OnItemActivated));
+    MSWSetNativeTheme(this);
 }
 
 void CppSymbolTree::OnMouseRightUp(wxTreeEvent &event)
 {
-	wxTreeItemId item = event.GetItem();
-	if (item.IsOk()) {
-		SelectItem(item, true);
-	}
+    wxTreeItemId item = event.GetItem();
+    if (item.IsOk()) {
+        SelectItem(item, true);
+    }
 }
 
 void CppSymbolTree::OnMouseDblClick(wxMouseEvent& event)
 {
-	//-----------------------------------------------------
-	// We override the doubleclick on item event
-	// to demonstrate how to access the tag information
-	// stored in the symbol tree
-	//-----------------------------------------------------
+    //-----------------------------------------------------
+    // We override the doubleclick on item event
+    // to demonstrate how to access the tag information
+    // stored in the symbol tree
+    //-----------------------------------------------------
 
-	wxTreeItemId treeItem = GetSelection();
-	if (!treeItem) {
-		event.Skip();
-		return;
-	}
+    wxTreeItemId treeItem = GetSelection();
+    if (!treeItem) {
+        event.Skip();
+        return;
+    }
 
-	// Make sure the double click was done on an actual item
-	int flags = wxTREE_HITTEST_ONITEMLABEL;
-	if (HitTest(event.GetPosition(), flags) != treeItem) {
-		event.Skip();
-		return;
-	}
+    // Make sure the double click was done on an actual item
+    int flags = wxTREE_HITTEST_ONITEMLABEL;
+    if (HitTest(event.GetPosition(), flags) != treeItem) {
+        event.Skip();
+        return;
+    }
 
-	DoItemActivated(treeItem, event, true);
+    DoItemActivated(treeItem, event, true);
 }
 
 bool CppSymbolTree::ActivateSelectedItem()
 {
-	wxTreeItemId item = GetSelection();
-	wxTreeEvent dummy;
-	return DoItemActivated(item, dummy, true);
+    wxTreeItemId item = GetSelection();
+    wxTreeEvent dummy;
+    return DoItemActivated(item, dummy, true);
 }
 
 bool CppSymbolTree::DoItemActivated(wxTreeItemId item, wxEvent &event, bool notify)
 {
-	//-----------------------------------------------------
-	// Each tree items, keeps a private user data that
-	// holds the key for searching the its corresponding
-	// node in the m_tree data structure
-	//-----------------------------------------------------
-	if (item.IsOk() == false)
-		return false;
+    //-----------------------------------------------------
+    // Each tree items, keeps a private user data that
+    // holds the key for searching the its corresponding
+    // node in the m_tree data structure
+    //-----------------------------------------------------
+    if (item.IsOk() == false)
+        return false;
 
-	MyTreeItemData* itemData = static_cast<MyTreeItemData*>(GetItemData(item));
-	if ( !itemData ) {
-		event.Skip();
-		return false;
-	}
+    MyTreeItemData* itemData = static_cast<MyTreeItemData*>(GetItemData(item));
+    if ( !itemData ) {
+        event.Skip();
+        return false;
+    }
 
-	wxString filename = itemData->GetFileName();
-	wxString project = ManagerST::Get()->GetProjectNameByFile(filename);
-	wxString pattern = itemData->GetPattern();
+    wxString filename = itemData->GetFileName();
+    wxString project = ManagerST::Get()->GetProjectNameByFile(filename);
+    wxString pattern = itemData->GetPattern();
     int      lineno  = itemData->GetLine();
 
-	// Open the file and set the cursor to line number
-	if(clMainFrame::Get()->GetMainBook()->OpenFile(filename, project, lineno-1)) {
-		// get the editor, and search for the pattern in the file
-		LEditor *editor = clMainFrame::Get()->GetMainBook()->GetActiveEditor();
-		if (editor) {
-			FindAndSelect(editor, pattern, GetItemText(item));
-		}
-	}
+    // Open the file and set the cursor to line number
+    if(clMainFrame::Get()->GetMainBook()->OpenFile(filename, project, lineno-1)) {
+        // get the editor, and search for the pattern in the file
+        LEditor *editor = clMainFrame::Get()->GetMainBook()->GetActiveEditor();
+        if (editor) {
+            FindAndSelect(editor, pattern, GetItemText(item));
+        }
+    }
 
-	// post an event that an item was activated
-	if ( notify ) {
-		wxCommandEvent e(wxEVT_CMD_CPP_SYMBOL_ITEM_SELECTED);
-		e.SetEventObject(this);
-		wxPostEvent(GetEventHandler(), e);
-	}
-	return true;
+    // post an event that an item was activated
+    if ( notify ) {
+        wxCommandEvent e(wxEVT_CMD_CPP_SYMBOL_ITEM_SELECTED);
+        e.SetEventObject(this);
+        wxPostEvent(GetEventHandler(), e);
+    }
+    return true;
 }
 
 void CppSymbolTree::OnItemActivated(wxTreeEvent &event)
 {
-	if (event.GetKeyCode() == WXK_RETURN) {
-		wxTreeItemId item = GetSelection();
-		DoItemActivated(item, event, true);
-	} else {
-		event.Skip();
-	}
+    if (event.GetKeyCode() == WXK_RETURN) {
+        wxTreeItemId item = GetSelection();
+        DoItemActivated(item, event, true);
+    } else {
+        event.Skip();
+    }
 }
 
 void CppSymbolTree::AdvanceSelection(bool forward)
 {
-	wxTreeItemId item = GetSelection();
-	if (!item.IsOk()) {
-		return;
-	}
+    wxTreeItemId item = GetSelection();
+    if (!item.IsOk()) {
+        return;
+    }
 
-	wxTreeItemId nextItem;
-	if (forward) {
-		//Item is visible, scroll to it to make sure GetNextVisible() wont
-		//fail
-		ScrollTo(item);
-		nextItem = GetNextVisible(item);
-	} else {
-		nextItem = TryGetPrevItem(item);
-	}
+    wxTreeItemId nextItem;
+    if (forward) {
+        //Item is visible, scroll to it to make sure GetNextVisible() wont
+        //fail
+        ScrollTo(item);
+        nextItem = GetNextVisible(item);
+    } else {
+        nextItem = TryGetPrevItem(item);
+    }
 
-	if (nextItem.IsOk()) {
-		SelectItem(nextItem);
-	}
+    if (nextItem.IsOk()) {
+        SelectItem(nextItem);
+        EnsureVisible(nextItem);
+    }
 }
 
 wxTreeItemId CppSymbolTree::TryGetPrevItem(wxTreeItemId item)
 {
-	wxCHECK_MSG( item.IsOk(), wxTreeItemId(), wxT("invalid tree item") );
+    wxCHECK_MSG( item.IsOk(), wxTreeItemId(), wxT("invalid tree item") );
 
-	// find out the starting point
-	wxTreeItemId prevItem = GetPrevSibling(item);
-	if ( !prevItem.IsOk() ) {
-		prevItem = GetItemParent(item);
-	}
+    // find out the starting point
+    wxTreeItemId prevItem = GetPrevSibling(item);
+    if ( !prevItem.IsOk() ) {
+        prevItem = GetItemParent(item);
+    }
 
-	// from there we must be able to navigate until this item
-	while ( prevItem.IsOk() ) {
-		ScrollTo(prevItem);
-		const wxTreeItemId nextItem = GetNextVisible(prevItem);
-		if ( !nextItem.IsOk() || nextItem == item )
-			return prevItem;
+    // from there we must be able to navigate until this item
+    while ( prevItem.IsOk() ) {
+        ScrollTo(prevItem);
+        const wxTreeItemId nextItem = GetNextVisible(prevItem);
+        if ( !nextItem.IsOk() || nextItem == item )
+            return prevItem;
 
-		prevItem = nextItem;
-	}
+        prevItem = nextItem;
+    }
 
-	return wxTreeItemId();
+    return wxTreeItemId();
 }
 
 void CppSymbolTree::FindAndSelect(LEditor* editor, wxString& pattern, const wxString& name)
 {
-	editor->FindAndSelect(pattern, name);
+    editor->FindAndSelect(pattern, name);
 }
--- LiteEditor/cpp_symbol_tree.h
+++ LiteEditor/cpp_symbol_tree.h
@@ -1,28 +1,28 @@
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 //
-// copyright            : (C) 2008 by Eran Ifrah                            
-// file name            : cpp_symbol_tree.h              
-//                                                                          
+// copyright            : (C) 2008 by Eran Ifrah
+// file name            : cpp_symbol_tree.h
+//
 // -------------------------------------------------------------------------
-// A                                                                        
-//              _____           _      _     _ _                            
-//             /  __ \         | |    | |   (_) |                           
-//             | /  \/ ___   __| | ___| |    _| |_ ___                      
-//             | |    / _ \ / _  |/ _ \ |   | | __/ _ )                     
-//             | \__/\ (_) | (_| |  __/ |___| | ||  __/                     
-//              \____/\___/ \__,_|\___\_____/_|\__\___|                     
-//                                                                          
-//                                                  F i l e                 
-//                                                                          
-//    This program is free software; you can redistribute it and/or modify  
-//    it under the terms of the GNU General Public License as published by  
-//    the Free Software Foundation; either version 2 of the License, or     
-//    (at your option) any later version.                                   
-//                                                                          
+// A
+//              _____           _      _     _ _
+//             /  __ \         | |    | |   (_) |
+//             | /  \/ ___   __| | ___| |    _| |_ ___
+//             | |    / _ \ / _  |/ _ \ |   | | __/ _ )
+//             | \__/\ (_) | (_| |  __/ |___| | ||  __/
+//              \____/\___/ \__,_|\___\_____/_|\__\___|
+//
+//                                                  F i l e
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
- #ifndef LITEEDITOR_CPP_SYMBOL_TREE_H
+#ifndef LITEEDITOR_CPP_SYMBOL_TREE_H
 #define LITEEDITOR_CPP_SYMBOL_TREE_H
 
 #include "symbol_tree.h"
@@ -33,40 +33,40 @@ extern const wxEventType wxEVT_CMD_CPP_SYMBOL_ITEM_SELECTED;
 /// This class represents the GUI tree for the C++ symbols
 class CppSymbolTree : public SymbolTree
 {
-	std::stack<wxTreeItemId> m_itemsStack;
-	
+    std::stack<wxTreeItemId> m_itemsStack;
+
 public:
 
-	CppSymbolTree();
-	
-	/// Nothing special here, just call our parent constructor
-	CppSymbolTree(wxWindow *parent, const wxWindowID id, const wxPoint &pos = wxDefaultPosition, const wxSize &size = wxDefaultSize, long style = wxTR_HIDE_ROOT | wxTR_HAS_BUTTONS);
-		
+    CppSymbolTree();
 
-	/// destructor
-	virtual ~CppSymbolTree()
-	{};
+    /// Nothing special here, just call our parent constructor
+    CppSymbolTree(wxWindow *parent, const wxWindowID id, const wxPoint &pos = wxDefaultPosition, const wxSize &size = wxDefaultSize, long style = wxTR_HIDE_ROOT | wxTR_HAS_BUTTONS);
 
-	//activate the selected item.
-	//If there is no selection, retun false,
-	//else try to open the file associated with the 
-	//selected item
-	//return true on successfull operation false otherwise
-	bool ActivateSelectedItem();
-	
-	//advance the selection to next item (if possible)
-	void AdvanceSelection(bool forward = true);
+
+    /// destructor
+    virtual ~CppSymbolTree()
+    {};
+
+    //activate the selected item.
+    //If there is no selection, retun false,
+    //else try to open the file associated with the
+    //selected item
+    //return true on successfull operation false otherwise
+    bool ActivateSelectedItem();
+
+    //advance the selection to next item (if possible)
+    void AdvanceSelection(bool forward = true);
 
 protected:
-	virtual void OnMouseDblClick(wxMouseEvent& event);
-	virtual void OnMouseRightUp(wxTreeEvent& event);
-	virtual void OnItemActivated(wxTreeEvent &event);
-	bool DoItemActivated(wxTreeItemId item, wxEvent &event, bool notify);
-	void FindAndSelect(LEditor *editor, wxString &pattern, const wxString &name);
-	
-	wxTreeItemId TryGetPrevItem(wxTreeItemId item);
+    virtual void OnMouseDblClick(wxMouseEvent& event);
+    virtual void OnMouseRightUp(wxTreeEvent& event);
+    virtual void OnItemActivated(wxTreeEvent &event);
+    bool DoItemActivated(wxTreeItemId item, wxEvent &event, bool notify);
+    void FindAndSelect(LEditor *editor, wxString &pattern, const wxString &name);
 
-	DECLARE_DYNAMIC_CLASS(CppSymbolTree)
+    wxTreeItemId TryGetPrevItem(wxTreeItemId item);
+
+    DECLARE_DYNAMIC_CLASS(CppSymbolTree)
 };
 
 #endif // LITEEDITOR_CPP_SYMBOL_TREE_H

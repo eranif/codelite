8882d3ce783d3747bd28e5eda231d0cdc6182bd0
eran <eran.ifrah@gmail.com>
2013-09-10 09:19:25 +0300

    Improved the "New Plugin Wizard" dialog

---
+++ Gizmos/PluginWizard.cpp
@@ -0,0 +1,60 @@
+#include "PluginWizard.h"
+#include "globals.h"
+
+PluginWizard::PluginWizard(wxWindow* parent)
+    : PluginWizardBase(parent)
+{
+}
+
+PluginWizard::~PluginWizard()
+{
+}
+
+bool PluginWizard::Run(NewPluginData& pd)
+{
+    bool res = RunWizard( GetFirstPage() );
+    if ( res ) {
+        pd.SetCodelitePath( m_dirPickerCodeliteDir->GetPath() );
+        pd.SetPluginDescription( m_textCtrlDescription->GetValue() );
+        pd.SetPluginName( m_textCtrlName->GetValue() );
+        pd.SetProjectPath( m_textCtrlPreview->GetValue() );
+    }
+    return res;
+}
+
+void PluginWizard::OnFinish(wxWizardEvent& event)
+{
+    event.Skip();
+}
+
+void PluginWizard::OnPageChanging(wxWizardEvent& event)
+{
+    if ( event.GetDirection() && event.GetPage() == m_pages.at(0)) {
+        wxString pluginName = m_textCtrlName->GetValue();
+        pluginName.Trim();
+        if ( pluginName.IsEmpty() || !::IsValidCppIndetifier(pluginName) ) {
+            ::wxMessageBox(_("Invalid plugin name"), "codelite");
+            event.Veto();
+            return;
+        }
+    } else if ( event.GetDirection() && event.GetPage() == m_pages.at(1)) {
+        if ( !wxDir::Exists( m_dirPickerCodeliteDir->GetPath() ) ) {
+            ::wxMessageBox(_("codelite folder does not exists"), "codelite");
+            event.Veto();
+            return;
+        }
+
+        if ( !wxDir::Exists( m_dirPickerPluginPath->GetPath() ) ) {
+            ::wxMessageBox(_("The selected plugin folder does not exist"), "codelite");
+            event.Veto();
+            return;
+        }
+    }
+}
+void PluginWizard::OnProjectPathChanged(wxFileDirPickerEvent& event)
+{
+    wxFileName project(event.GetPath(), m_textCtrlName->GetValue());
+    project.SetExt("project");
+    project.AppendDir(m_textCtrlName->GetValue());
+    m_textCtrlPreview->ChangeValue( project.GetFullPath() );
+}
---
+++ Gizmos/PluginWizard.h
@@ -0,0 +1,19 @@
+#ifndef PLUGINWIZARD_H
+#define PLUGINWIZARD_H
+
+#include "gizmos_base.h"
+#include "newplugindata.h"
+
+class PluginWizard : public PluginWizardBase
+{
+public:
+    PluginWizard(wxWindow* parent);
+    virtual ~PluginWizard();
+
+    bool Run(NewPluginData& pd);
+protected:
+    virtual void OnProjectPathChanged(wxFileDirPickerEvent& event);
+    virtual void OnFinish(wxWizardEvent& event);
+    virtual void OnPageChanging(wxWizardEvent& event);
+};
+#endif // PLUGINWIZARD_H

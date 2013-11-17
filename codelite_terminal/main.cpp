f532ab960ff39e8ca55ef57e314e99ee7e760779
eran <eran.ifrah@gmail.com>
2013-11-17 21:28:47 +0200

    Make codelite-terminal a foreground applicaiton on OSX

--- codelite_terminal/main.cpp
+++ codelite_terminal/main.cpp
@@ -8,6 +8,10 @@
 #include <wx/log.h>
 #include <wx/stdpaths.h>
 
+#ifdef __WXMAC__
+#   include <ApplicationServices/ApplicationServices.h>
+#endif
+
 // Define the MainApp
 class MainApp : public wxApp
 {
@@ -23,33 +27,39 @@ public:
             wxFileName::Mkdir(wxStandardPaths::Get().GetUserDataDir(), wxS_DIR_DEFAULT, wxPATH_MKDIR_FULL);
             ::wxMkdir(wxStandardPaths::Get().GetUserDataDir());
         }
-        
+
         parser.AddOption("c", "command", "command line to execute",                       wxCMD_LINE_VAL_STRING);
         parser.AddOption("t", "title",   "set the console title",                         wxCMD_LINE_VAL_STRING);
         parser.AddSwitch("e", "exit",    "exit when execution of command terminates",     wxCMD_LINE_PARAM_OPTIONAL);
         parser.AddSwitch("w", "wait",    "wait for any key to be pressed before exiting", wxCMD_LINE_PARAM_OPTIONAL);
-        
+
         if ( !DoParseCommandLine( parser ) ) {
             return false;
         }
-        
+
         // Add the common image handlers
         wxImage::AddHandler( new wxPNGHandler );
         wxImage::AddHandler( new wxJPEGHandler );
-        
+
         TerminalOptions options;
         wxString commandToRun, title;
         parser.Found("c", &commandToRun);
         if ( parser.Found("t", &title) ) {
             options.SetTitle( title );
         }
-        
+
         options.EnableFlag( TerminalOptions::kExitWhenInfiriorTerminates,   parser.Found("e") );
         options.EnableFlag( TerminalOptions::kPauseBeforeExit,              parser.Found("w") );
         options.SetCommand( commandToRun );
 
         MainFrame *mainFrame = new MainFrame(NULL, options);
         SetTopWindow(mainFrame);
+
+#ifdef __WXMAC__
+        ProcessSerialNumber PSN;
+        GetCurrentProcess(&PSN);
+        TransformProcessType(&PSN,kProcessTransformToForegroundApplication);
+#endif
         return GetTopWindow()->Show();
     }
 

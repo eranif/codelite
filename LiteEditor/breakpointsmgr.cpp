2c540ad6903f8af7022ba72344c558477c021ccb
dghart <david@4Pane.co.uk>
2013-12-23 17:41:51 +0000

    Make it possible again to add watchpoints
    A few months ago a bug was fixed. That revealed a hidden bug that prevented watchpoints being processed correctly.

--- LiteEditor/breakpointsmgr.cpp
+++ LiteEditor/breakpointsmgr.cpp
@@ -59,7 +59,8 @@ bool BreakptMgr::AddBreakpointByLineno(const wxString& file, const int lineno, c
 
 bool BreakptMgr::AddBreakpoint(const BreakpointInfo &bp)
 {
-    if(bp.file.IsEmpty() && bp.function_name.IsEmpty() && bp.memory_address.IsEmpty() && bp.lineno == wxNOT_FOUND) {
+    if (bp.bp_type != BP_type_watchpt &&
+        bp.file.IsEmpty() && bp.function_name.IsEmpty() && bp.memory_address.IsEmpty() && bp.lineno == wxNOT_FOUND) {
         // no function nor file? no memory address?
         // do nothing then
         return true;

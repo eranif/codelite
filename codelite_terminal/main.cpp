5bf67bb6b8c22b647d73f3dbf84beaee782f0291
Eran <eran.ifrah@gmail.com>
2013-12-28 11:24:54 +0200

    codelite-terminal: changed the command line arguments accepted by codelite-terminal, also allow it to accept arguments with spaces and double quotes

--- codelite_terminal/main.cpp
+++ codelite_terminal/main.cpp
@@ -7,6 +7,7 @@
 #include <wx/dir.h>
 #include <wx/log.h>
 #include <wx/stdpaths.h>
+#include "commandlineparser.h"
 
 #ifdef __WXMAC__
 #   include <ApplicationServices/ApplicationServices.h>
@@ -19,23 +20,31 @@ public:
     MainApp() {}
     virtual ~MainApp() {}
 
+    void PrintUsage() {
+        wxPrintf("%s [-t <title>] [-e] [-w] [-d <working directory>] -- <command to execute>\n", wxApp::argv[0]);
+        wxPrintf("-t | --title             Set the console title\n");
+        wxPrintf("-e | --exit              Exit when execution of command terminates\n");
+        wxPrintf("-w | --wait              Wait for any key to be pressed before exiting\n");
+        wxPrintf("-d | --working-directory Set the working directory\n");
+        wxPrintf("\n");
+        exit(1);
+    }
+
     virtual bool OnInit() {
+
         SetAppName("codelite-terminal");
-        wxCmdLineParser parser(wxApp::argc, wxApp::argv);
+
+        CommandLineParser parser(wxApp::argc, wxApp::argv);
+        parser.AddOption("t", "title", CommandLineParser::kOptionWithValue|CommandLineParser::kOptional);
+        parser.AddOption("d", "working-directory", CommandLineParser::kOptionWithValue|CommandLineParser::kOptional);
+        parser.AddOption("e", "exit"); // optional, no value
+        parser.AddOption("w", "wait"); // optional, no value
+        parser.Parse();
+
         {
             wxLogNull noLog;
             wxFileName::Mkdir(wxStandardPaths::Get().GetUserDataDir(), wxS_DIR_DEFAULT, wxPATH_MKDIR_FULL);
             ::wxMkdir(wxStandardPaths::Get().GetUserDataDir());
-        }
-
-        parser.AddOption("c", "command",            "Command line to execute",                       wxCMD_LINE_VAL_STRING);
-        parser.AddOption("t", "title",              "Set the console title",                         wxCMD_LINE_VAL_STRING);
-        parser.AddSwitch("e", "exit",               "Exit when execution of command terminates",     wxCMD_LINE_PARAM_OPTIONAL);
-        parser.AddSwitch("w", "wait",               "Wait for any key to be pressed before exiting", wxCMD_LINE_PARAM_OPTIONAL);
-        parser.AddOption("d", "working-directory",  "Set the working directory",                     wxCMD_LINE_VAL_STRING);
-
-        if ( !DoParseCommandLine( parser ) ) {
-            return false;
         }
 
         // Add the common image handlers
@@ -44,18 +53,19 @@ public:
 
         TerminalOptions options;
         wxString commandToRun, title, workingDirectory;
-        parser.Found("c", &commandToRun);
-        parser.Found("d", &workingDirectory);
-        if ( parser.Found("t", &title) ) {
-            options.SetTitle( title );
+        commandToRun = parser.GetCommand();
+        workingDirectory = parser.GetArg("d", "working-directory");
+
+        if ( parser.HasOption("t", "title") ) {
+            options.SetTitle( parser.GetArg("t", "title") );
         }
-        
+
         if ( !workingDirectory.IsEmpty() ) {
             ::wxSetWorkingDirectory( workingDirectory );
         }
-        
-        options.EnableFlag( TerminalOptions::kExitWhenInfiriorTerminates,   parser.Found("e") );
-        options.EnableFlag( TerminalOptions::kPauseBeforeExit,              parser.Found("w") );
+
+        options.EnableFlag( TerminalOptions::kExitWhenInfiriorTerminates,   parser.HasOption("e", "exit") );
+        options.EnableFlag( TerminalOptions::kPauseBeforeExit,              parser.HasOption("w", "wait") );
         options.SetCommand( commandToRun );
 
         MainFrame *mainFrame = new MainFrame(NULL, options);
@@ -67,11 +77,6 @@ public:
         TransformProcessType(&PSN,kProcessTransformToForegroundApplication);
 #endif
         return GetTopWindow()->Show();
-    }
-
-    bool DoParseCommandLine(wxCmdLineParser& parser) {
-        parser.Parse(false);
-        return true;
     }
 };
 

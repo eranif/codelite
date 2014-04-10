ee0048a399081da6822d6624856b369d5479ae48
Eran <eran.ifrah@gmail.com>
2014-04-10 08:33:01 +0300

    Nothing really important..

--- LLDBDebugger/LLDBProtocol/LLDBConnector.cpp
+++ LLDBDebugger/LLDBProtocol/LLDBConnector.cpp
@@ -12,5 +12,9 @@ bool LLDBConnector::ConnectToDebugger()
 {
     m_socket.SetIp("127.0.0.1");
     m_socket.SetPort(LLDB_PORT);
-    return m_socket.Connect();
+    if ( !m_socket.Connect() )
+        return false;
+    
+    // Start the lldb event thread
+    
 }

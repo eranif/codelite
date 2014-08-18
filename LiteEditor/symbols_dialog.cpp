b9224d41b775d72d590623e85e9590d5887e449c
Eran <eran.ifrah@gmail.com>
2014-08-18 12:21:30 +0300

    Removed the obsolete initialization of m_project member which caused a performance issues when selecting items in the 'Resolve Ambiguity' dialog

--- LiteEditor/symbols_dialog.cpp
+++ LiteEditor/symbols_dialog.cpp
@@ -1,180 +1,173 @@
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 //
-// copyright            : (C) 2008 by Eran Ifrah                            
-// file name            : symbols_dialog.cpp              
-//                                                                          
+// copyright            : (C) 2008 by Eran Ifrah
+// file name            : symbols_dialog.cpp
+//
 // -------------------------------------------------------------------------
-// A                                                                        
-//              _____           _      _     _ _                            
-//             /  __ \         | |    | |   (_) |                           
-//             | /  \/ ___   __| | ___| |    _| |_ ___                      
-//             | |    / _ \ / _  |/ _ \ |   | | __/ _ )                     
-//             | \__/\ (_) | (_| |  __/ |___| | ||  __/                     
-//              \____/\___/ \__,_|\___\_____/_|\__\___|                     
-//                                                                          
-//                                                  F i l e                 
-//                                                                          
-//    This program is free software; you can redistribute it and/or modify  
-//    it under the terms of the GNU General Public License as published by  
-//    the Free Software Foundation; either version 2 of the License, or     
-//    (at your option) any later version.                                   
-//                                                                          
+// A
+//              _____           _      _     _ _
+//             /  __ \         | |    | |   (_) |
+//             | /  \/ ___   __| | ___| |    _| |_ ___
+//             | |    / _ \ / _  |/ _ \ |   | | __/ _ )
+//             | \__/\ (_) | (_| |  __/ |___| | ||  __/
+//              \____/\___/ \__,_|\___\_____/_|\__\___|
+//
+//                                                  F i l e
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
- #include "symbols_dialog.h"
+#include "symbols_dialog.h"
 #include "manager.h"
 #include "macros.h"
 #include "globals.h"
 #include "windowattrmanager.h"
 
 BEGIN_EVENT_TABLE(SymbolsDialog, SymbolsDialogBase)
-	EVT_CHAR_HOOK(SymbolsDialog::OnCharHook)
+EVT_CHAR_HOOK(SymbolsDialog::OnCharHook)
 END_EVENT_TABLE()
 
-SymbolsDialog::SymbolsDialog( wxWindow* parent )
-		: SymbolsDialogBase( parent )
-		, m_line(wxNOT_FOUND)
-		, m_file(wxEmptyString)
-		, m_selectedItem(wxNOT_FOUND)
+SymbolsDialog::SymbolsDialog(wxWindow* parent)
+    : SymbolsDialogBase(parent)
+    , m_line(wxNOT_FOUND)
+    , m_file(wxEmptyString)
+    , m_selectedItem(wxNOT_FOUND)
 {
-	// Initialise the list control
-	m_results->InsertColumn(0, _("Symbol"));
-	m_results->InsertColumn(1, _("Kind"));
-	m_results->InsertColumn(2, _("File"));
-	m_results->InsertColumn(3, _("Line"));
-	m_results->InsertColumn(4, _("Pattern"));
-	
-	m_results->Connect(wxEVT_COMMAND_LIST_ITEM_DESELECTED, wxListEventHandler(SymbolsDialog::OnItemDeselected), NULL, this);
-	Centre();
-	WindowAttrManager::Load(this, wxT("SymbolsDialog"), NULL);
+    // Initialise the list control
+    m_results->InsertColumn(0, _("Symbol"));
+    m_results->InsertColumn(1, _("Kind"));
+    m_results->InsertColumn(2, _("File"));
+    m_results->InsertColumn(3, _("Line"));
+    m_results->InsertColumn(4, _("Pattern"));
+
+    m_results->Connect(
+        wxEVT_COMMAND_LIST_ITEM_DESELECTED, wxListEventHandler(SymbolsDialog::OnItemDeselected), NULL, this);
+    Centre();
+    WindowAttrManager::Load(this, wxT("SymbolsDialog"), NULL);
 }
 
-SymbolsDialog::~SymbolsDialog()
-{
-	WindowAttrManager::Save(this, wxT("SymbolsDialog"), NULL);
-}
+SymbolsDialog::~SymbolsDialog() { WindowAttrManager::Save(this, wxT("SymbolsDialog"), NULL); }
 
-void SymbolsDialog::AddSymbol(const TagEntryPtr &tag, bool sel)
+void SymbolsDialog::AddSymbol(const TagEntryPtr& tag, bool sel)
 {
-	//-------------------------------------------------------
-	// Populate the columns
-	//-------------------------------------------------------
+    //-------------------------------------------------------
+    // Populate the columns
+    //-------------------------------------------------------
 
-	wxString line;
-	line << tag->GetLine();
-	
-	long index = AppendListCtrlRow(m_results);
-	SetColumnText(m_results, index, 0, tag->GetFullDisplayName());
-	SetColumnText(m_results, index, 1, tag->GetKind());
-	SetColumnText(m_results, index, 2, tag->GetFile());
-	SetColumnText(m_results, index, 3, line);
-	SetColumnText(m_results, index, 4, tag->GetPattern());
+    wxString line;
+    line << tag->GetLine();
+
+    long index = AppendListCtrlRow(m_results);
+    SetColumnText(m_results, index, 0, tag->GetFullDisplayName());
+    SetColumnText(m_results, index, 1, tag->GetKind());
+    SetColumnText(m_results, index, 2, tag->GetFile());
+    SetColumnText(m_results, index, 3, line);
+    SetColumnText(m_results, index, 4, tag->GetPattern());
 
     // list ctrl can reorder items, so use returned index to insert tag
-    m_tags.insert(m_tags.begin()+index, tag);
+    m_tags.insert(m_tags.begin() + index, tag);
 }
 
-void SymbolsDialog::AddSymbols(const std::vector<TagEntryPtr> &tags, size_t sel)
+void SymbolsDialog::AddSymbols(const std::vector<TagEntryPtr>& tags, size_t sel)
 {
-	wxUnusedVar(sel);
-	for (size_t i=0; i<tags.size(); i++) {
-		AddSymbol(tags[i], false);
-	}
-	m_results->SetColumnWidth(0, wxLIST_AUTOSIZE);
-	m_results->SetColumnWidth(1, wxLIST_AUTOSIZE);
-	m_results->SetColumnWidth(2, wxLIST_AUTOSIZE);
-	m_results->SetColumnWidth(3, wxLIST_AUTOSIZE);
-	m_results->SetColumnWidth(4, wxLIST_AUTOSIZE);
-	
-	m_results->SetFocus();
-	if (tags.empty() == false) {
-		m_results->SetItemState(0, wxLIST_STATE_SELECTED, wxLIST_STATE_SELECTED);
-		m_results->SetItemState(0, wxLIST_STATE_FOCUSED, wxLIST_STATE_FOCUSED);
-		m_tag = m_tags[m_selectedItem = 0];
-	}
+    wxUnusedVar(sel);
+    for(size_t i = 0; i < tags.size(); i++) {
+        AddSymbol(tags[i], false);
+    }
+    m_results->SetColumnWidth(0, wxLIST_AUTOSIZE);
+    m_results->SetColumnWidth(1, wxLIST_AUTOSIZE);
+    m_results->SetColumnWidth(2, wxLIST_AUTOSIZE);
+    m_results->SetColumnWidth(3, wxLIST_AUTOSIZE);
+    m_results->SetColumnWidth(4, wxLIST_AUTOSIZE);
+
+    m_results->SetFocus();
+    if(tags.empty() == false) {
+        m_results->SetItemState(0, wxLIST_STATE_SELECTED, wxLIST_STATE_SELECTED);
+        m_results->SetItemState(0, wxLIST_STATE_FOCUSED, wxLIST_STATE_FOCUSED);
+        m_tag = m_tags[m_selectedItem = 0];
+    }
 }
 
-void SymbolsDialog::UpdateFileAndLine(wxListEvent &event)
+void SymbolsDialog::UpdateFileAndLine(wxListEvent& event)
 {
-	wxString line_number;
-	m_file = GetColumnText(m_results, event.m_itemIndex, 2);
-	line_number = GetColumnText(m_results, event.m_itemIndex, 3);
-	m_pattern = GetColumnText(m_results, event.m_itemIndex, 4);
-	line_number.ToLong( &m_line );
-	m_project = ManagerST::Get()->GetProjectNameByFile(m_file);
+    wxString line_number;
+    m_file = GetColumnText(m_results, event.m_itemIndex, 2);
+    line_number = GetColumnText(m_results, event.m_itemIndex, 3);
+    m_pattern = GetColumnText(m_results, event.m_itemIndex, 4);
+    line_number.ToLong(&m_line);
 }
 
-void SymbolsDialog::OnItemSelected(wxListEvent &event)
+void SymbolsDialog::OnItemSelected(wxListEvent& event)
 {
-	UpdateFileAndLine(event);
-	m_tag = m_tags[m_selectedItem = event.m_itemIndex];
+    UpdateFileAndLine(event);
+    m_tag = m_tags[m_selectedItem = event.m_itemIndex];
 }
 
-void SymbolsDialog::OnItemActivated(wxListEvent &event)
+void SymbolsDialog::OnItemActivated(wxListEvent& event)
 {
-	UpdateFileAndLine(event);
-	EndModal(wxID_OK);
+    UpdateFileAndLine(event);
+    EndModal(wxID_OK);
 }
 
-
-void SymbolsDialog::OnButtonCancel(wxCommandEvent &event)
+void SymbolsDialog::OnButtonCancel(wxCommandEvent& event)
 {
-	wxUnusedVar(event);
-	EndModal(wxID_CANCEL);
+    wxUnusedVar(event);
+    EndModal(wxID_CANCEL);
 }
 
-void SymbolsDialog::OnButtonOK(wxCommandEvent &event)
+void SymbolsDialog::OnButtonOK(wxCommandEvent& event)
 {
-	wxUnusedVar(event);
-	EndModal(wxID_OK);
+    wxUnusedVar(event);
+    EndModal(wxID_OK);
 }
 
-void SymbolsDialog::OnItemDeselected(wxListEvent &event)
+void SymbolsDialog::OnItemDeselected(wxListEvent& event)
 {
-	wxUnusedVar(event);
-	m_selectedItem = wxNOT_FOUND;
+    wxUnusedVar(event);
+    m_selectedItem = wxNOT_FOUND;
     m_tag = NULL;
 }
 
-void SymbolsDialog::OnCharHook(wxKeyEvent &event)
+void SymbolsDialog::OnCharHook(wxKeyEvent& event)
 {
-	if (event.GetKeyCode() == WXK_DOWN) {
+    if(event.GetKeyCode() == WXK_DOWN) {
 
-		if (m_selectedItem == wxNOT_FOUND && m_results->GetItemCount() > 0) {
-			m_tag = m_tags[m_selectedItem = 0];
-		}
+        if(m_selectedItem == wxNOT_FOUND && m_results->GetItemCount() > 0) {
+            m_tag = m_tags[m_selectedItem = 0];
+        }
 
-		if (m_selectedItem == wxNOT_FOUND)
-			return;
+        if(m_selectedItem == wxNOT_FOUND) return;
 
-		if (m_results->GetItemCount() > m_selectedItem + 1) {
-			m_results->SetItemState(m_selectedItem, wxLIST_STATE_SELECTED, wxLIST_STATE_DROPHILITED);
-			m_tag = m_tags[++m_selectedItem];
-            
-			m_results->SetItemState(m_selectedItem, wxLIST_STATE_SELECTED, wxLIST_STATE_SELECTED);
-			m_results->EnsureVisible(m_selectedItem);
-			return;
-		}
-	} else if ( event.GetKeyCode() == WXK_UP) {
-		if (m_selectedItem == wxNOT_FOUND && m_results->GetItemCount() > 0) {
-			m_tag = m_tags[m_selectedItem = 0];
-		}
+        if(m_results->GetItemCount() > m_selectedItem + 1) {
+            m_results->SetItemState(m_selectedItem, wxLIST_STATE_SELECTED, wxLIST_STATE_DROPHILITED);
+            m_tag = m_tags[++m_selectedItem];
 
-		if (m_selectedItem == wxNOT_FOUND)
-			return;
+            m_results->SetItemState(m_selectedItem, wxLIST_STATE_SELECTED, wxLIST_STATE_SELECTED);
+            m_results->EnsureVisible(m_selectedItem);
+            return;
+        }
+    } else if(event.GetKeyCode() == WXK_UP) {
+        if(m_selectedItem == wxNOT_FOUND && m_results->GetItemCount() > 0) {
+            m_tag = m_tags[m_selectedItem = 0];
+        }
 
-		//select the previous one if we can
-		if ((m_selectedItem - 1) >= 0) {
-			//we can select the next one
-			m_results->SetItemState(m_selectedItem, wxLIST_STATE_SELECTED, wxLIST_STATE_DROPHILITED);
-			m_tag = m_tags[--m_selectedItem];
-			
-			m_results->SetItemState(m_selectedItem, wxLIST_STATE_SELECTED, wxLIST_STATE_SELECTED);
-			m_results->EnsureVisible(m_selectedItem);
-		}
-		return;
-	}
-	event.Skip();
+        if(m_selectedItem == wxNOT_FOUND) return;
+
+        // select the previous one if we can
+        if((m_selectedItem - 1) >= 0) {
+            // we can select the next one
+            m_results->SetItemState(m_selectedItem, wxLIST_STATE_SELECTED, wxLIST_STATE_DROPHILITED);
+            m_tag = m_tags[--m_selectedItem];
+
+            m_results->SetItemState(m_selectedItem, wxLIST_STATE_SELECTED, wxLIST_STATE_SELECTED);
+            m_results->EnsureVisible(m_selectedItem);
+        }
+        return;
+    }
+    event.Skip();
 }
-
--- LiteEditor/symbols_dialog.h
+++ LiteEditor/symbols_dialog.h
@@ -1,28 +1,28 @@
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 //
-// copyright            : (C) 2008 by Eran Ifrah                            
-// file name            : symbols_dialog.h              
-//                                                                          
+// copyright            : (C) 2008 by Eran Ifrah
+// file name            : symbols_dialog.h
+//
 // -------------------------------------------------------------------------
-// A                                                                        
-//              _____           _      _     _ _                            
-//             /  __ \         | |    | |   (_) |                           
-//             | /  \/ ___   __| | ___| |    _| |_ ___                      
-//             | |    / _ \ / _  |/ _ \ |   | | __/ _ )                     
-//             | \__/\ (_) | (_| |  __/ |___| | ||  __/                     
-//              \____/\___/ \__,_|\___\_____/_|\__\___|                     
-//                                                                          
-//                                                  F i l e                 
-//                                                                          
-//    This program is free software; you can redistribute it and/or modify  
-//    it under the terms of the GNU General Public License as published by  
-//    the Free Software Foundation; either version 2 of the License, or     
-//    (at your option) any later version.                                   
-//                                                                          
+// A
+//              _____           _      _     _ _
+//             /  __ \         | |    | |   (_) |
+//             | /  \/ ___   __| | ___| |    _| |_ ___
+//             | |    / _ \ / _  |/ _ \ |   | | __/ _ )
+//             | \__/\ (_) | (_| |  __/ |___| | ||  __/
+//              \____/\___/ \__,_|\___\_____/_|\__\___|
+//
+//                                                  F i l e
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
- #ifndef __symbols_dialog__
+#ifndef __symbols_dialog__
 #define __symbols_dialog__
 
 /**
@@ -40,57 +40,54 @@ class SymbolsDialog : public SymbolsDialogBase
 {
     std::vector<TagEntryPtr> m_tags;
     TagEntryPtr m_tag;
-	long m_line;
-	wxString m_file;
-	wxString m_project;
-	wxString m_pattern;
-	long m_selectedItem;
-	
+    long m_line;
+    wxString m_file;
+    wxString m_pattern;
+    long m_selectedItem;
+
 public:
-	/** Constructor */
-	SymbolsDialog( wxWindow* parent );
-	virtual ~SymbolsDialog();
-	
-	/**
-	 * Add symbol to the dialog - this should be called before calling the dialog
-	 * ShowModal() method
-	 * \param tag tag to add
-	 * \param sel when set to true, this item will be selected
-	 */
-	void AddSymbol(const TagEntryPtr &tag, bool sel); 
+    /** Constructor */
+    SymbolsDialog(wxWindow* parent);
+    virtual ~SymbolsDialog();
 
-	/**
-	 * A wrapper around the AddSymbol methos that allows
-	 * adding a vector of tags
-	 * \param tags 
-	 * \param sel zero based index to select
-	 */
-	void AddSymbols(const std::vector<TagEntryPtr> &tags, size_t sel);
+    /**
+     * Add symbol to the dialog - this should be called before calling the dialog
+     * ShowModal() method
+     * \param tag tag to add
+     * \param sel when set to true, this item will be selected
+     */
+    void AddSymbol(const TagEntryPtr& tag, bool sel);
 
+    /**
+     * A wrapper around the AddSymbol methos that allows
+     * adding a vector of tags
+     * \param tags
+     * \param sel zero based index to select
+     */
+    void AddSymbols(const std::vector<TagEntryPtr>& tags, size_t sel);
 
-	//---------------------------------------------
-	// Getters
-	//---------------------------------------------
-	const wxString &GetFile() const { return m_file; }
-	long GetLine() const { return m_line; }
-	const wxString &GetProject() const { return m_project; }
-	const wxString &GetPattern() const { return m_pattern; }
+    //---------------------------------------------
+    // Getters
+    //---------------------------------------------
+    const wxString& GetFile() const { return m_file; }
+    long GetLine() const { return m_line; }
+    const wxString& GetPattern() const { return m_pattern; }
     TagEntryPtr GetTag() { return m_tag; }
-	
-private:
-	void UpdateFileAndLine(wxListEvent &event);
 
-	//-----------------------------------------------
-	// Event handlers
-	//-----------------------------------------------
-	void OnItemSelected(wxListEvent &event);
-	void OnItemActivated(wxListEvent &event);
-	void OnButtonCancel(wxCommandEvent &event);
-	void OnButtonOK(wxCommandEvent &event);
-	void OnCharHook(wxKeyEvent &event);
-	void OnItemDeselected(wxListEvent &event);
-	
-	DECLARE_EVENT_TABLE()
+private:
+    void UpdateFileAndLine(wxListEvent& event);
+
+    //-----------------------------------------------
+    // Event handlers
+    //-----------------------------------------------
+    void OnItemSelected(wxListEvent& event);
+    void OnItemActivated(wxListEvent& event);
+    void OnButtonCancel(wxCommandEvent& event);
+    void OnButtonOK(wxCommandEvent& event);
+    void OnCharHook(wxKeyEvent& event);
+    void OnItemDeselected(wxListEvent& event);
+
+    DECLARE_EVENT_TABLE()
 };
 
 #endif // __symbols_dialog__

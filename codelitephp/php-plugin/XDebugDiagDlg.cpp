b40fe2a031516dd56b29b87de09fca9c44f0c679
Eran <eran.ifrah@gmail.com>
2014-10-11 16:48:30 +0300

    XDebug diagnostic dialog should now remmeber its pos/size

--- codelitephp/php-plugin/XDebugDiagDlg.cpp
+++ codelitephp/php-plugin/XDebugDiagDlg.cpp
@@ -1,12 +1,15 @@
 #include "XDebugDiagDlg.h"
+#include "windowattrmanager.h"
 
 XDebugDiagDlg::XDebugDiagDlg(wxWindow* parent)
     : XDebugDiagDlgBase(parent)
 {
+    WindowAttrManager::Load(this, "XDebugDiagDlg");
 }
 
 XDebugDiagDlg::~XDebugDiagDlg()
 {
+    WindowAttrManager::Save(this, "XDebugDiagDlg");
 }
 
 void XDebugDiagDlg::Load(const wxString& html)

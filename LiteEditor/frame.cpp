7ed77aab0aae3cd6ace6b0666366aa1436b0c57a
Eran Ifrah <eran.ifrah@gmail.com>
2014-11-27 09:16:02 -0800

    Enable 'Full Screen' buttons at the top right of codelite app

--- LiteEditor/frame.cpp
+++ LiteEditor/frame.cpp
@@ -968,6 +968,10 @@ void clMainFrame::CreateGUIControls(void)
     SetIcon(icon);
 #endif
 
+#if defined(__WXOSX__) && wxCHECK_VERSION(3, 1, 0)
+    EnableFullScreenView();
+#endif
+
     // tell wxAuiManager to manage this frame
     m_mgr.SetManagedWindow(this);
     m_mgr.SetArtProvider(new clAuiDockArt(PluginManager::Get()));

c26099378a6bfd57711c218ef5e3de8185cd2153
Eran <eran.ifrah@gmail.com>
2015-02-23 19:14:55 +0200

    Added superglobals.php file to provide basic code completion for PHP super globals

---
+++ codelitephp/resources/cc/superglobals.php
@@ -0,0 +1,12 @@
+<?php
+
+$_COOKIE = array();
+$_ENV = array();
+$_FILES = array();
+$_GET = array();
+$_POST = array();
+$_REQUEST = array();
+$_SERVER = array();
+$_SESSION = array();
+$GLOBALS = array();
+

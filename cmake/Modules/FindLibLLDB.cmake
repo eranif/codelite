82fc74b68c7f213cbcdbb55eb1f99aa8add1c01d
Eran <eran.ifrah@gmail.com>
2015-03-03 19:52:24 +0200

    Fixed detection of LLDB on Ubuntu 14.10

--- cmake/Modules/FindLibLLDB.cmake
+++ cmake/Modules/FindLibLLDB.cmake
@@ -3,7 +3,12 @@ if (UNIX)
         ## Locate the official packages
         find_library(LIBLLDB_T
                      NAMES liblldb.so liblldb.so.1
-                     HINTS  /usr/lib /usr/local/lib ${CMAKE_INSTALL_LIBDIR})
+                     HINTS  
+                     	/usr/lib 
+			/usr/local/lib 
+			/usr/lib/llvm-3.6/lib
+			/usr/lib/llvm-3.4/lib
+			${CMAKE_INSTALL_LIBDIR})
 
         find_path(LIBLLDB_INCLUDE_T NAMES lldb/API/SBDebugger.h
                   HINTS 

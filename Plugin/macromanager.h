e014649b9bd9bd93f32a88e8fc00af9cb1820005
eran <eran.ifrah@gmail.com>
2015-04-21 15:22:59 +0300

    indentation fix

--- Plugin/macromanager.h
+++ Plugin/macromanager.h
@@ -33,54 +33,60 @@ class IManager;
 class WXDLLIMPEXP_SDK MacroManager
 {
 public:
-	static MacroManager* Instance();
+    static MacroManager* Instance();
 
 private:
-	MacroManager();
-	virtual ~MacroManager();
+    MacroManager();
+    virtual ~MacroManager();
 
 public:
-	/*
-	 * The following macro will be expanded into their real values:
-	 * $(ProjectPath)
-	 * $(WorkspacePath)
-	 * $(ProjectName)
-	 * $(IntermediateDirectory)
-	 * $(ConfigurationName)
-	 * $(OutDir)
-	 * $(ProjectFiles)
-	 * $(ProjectFiles)
-	 * $(ProjectFilesAbs)
-	 * $(CurrentFileName)
-	 * $(CurrentFilePath)
-	 * $(CurrentFileExt)
-	 * $(CurrentFileFullPath)
-	 * $(User)
-	 * $(Date)
-	 * $(CodeLitePath)
-	 * $(CurrentSelection)
-	 * $(ProjectOutputFile)
-	 */
-	wxString Expand(const wxString &expression, IManager *manager, const wxString &project, const wxString &confToBuild = wxEmptyString);
-	
-	
-	/**
-	 * @brief search for variableName and replace all its occurance with 'replaceWith'
-	 * This function supports the following formats:
-	 * $variableName
-	 * ${variableName}
-	 * $(variableName)
-	 * %variableName%
-	 */
-	wxString Replace(const wxString &inString, const wxString &variableName, const wxString &replaceWith, bool bIgnoreCase = false);
-	
-	/**
-	 * @brief search for variable in the inString.
-	 * @param inString [input]
-	 * @param name [output] name only
-	 * @param fullname [output] includes the $ / %%
-	 */
-	bool FindVariable(const wxString &inString, wxString &name, wxString &fullname);
+    /*
+     * The following macro will be expanded into their real values:
+     * $(ProjectPath)
+     * $(WorkspacePath)
+     * $(ProjectName)
+     * $(IntermediateDirectory)
+     * $(ConfigurationName)
+     * $(OutDir)
+     * $(ProjectFiles)
+     * $(ProjectFiles)
+     * $(ProjectFilesAbs)
+     * $(CurrentFileName)
+     * $(CurrentFilePath)
+     * $(CurrentFileExt)
+     * $(CurrentFileFullPath)
+     * $(User)
+     * $(Date)
+     * $(CodeLitePath)
+     * $(CurrentSelection)
+     * $(ProjectOutputFile)
+     * $(Selection)
+     */
+    wxString Expand(const wxString& expression,
+                    IManager* manager,
+                    const wxString& project,
+                    const wxString& confToBuild = wxEmptyString);
+
+    /**
+     * @brief search for variableName and replace all its occurance with 'replaceWith'
+     * This function supports the following formats:
+     * $variableName
+     * ${variableName}
+     * $(variableName)
+     * %variableName%
+     */
+    wxString Replace(const wxString& inString,
+                     const wxString& variableName,
+                     const wxString& replaceWith,
+                     bool bIgnoreCase = false);
+
+    /**
+     * @brief search for variable in the inString.
+     * @param inString [input]
+     * @param name [output] name only
+     * @param fullname [output] includes the $ / %%
+     */
+    bool FindVariable(const wxString& inString, wxString& name, wxString& fullname);
 };
 
 #endif // MACROMANAGER_H

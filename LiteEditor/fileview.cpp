d1ca2c9c602ed3e716628b030944be0e554b96f4
Eran <eran.ifrah@gmail.com>
2015-05-02 09:16:11 +0300

    Updated 'dark' icons theme

--- LiteEditor/fileview.cpp
+++ LiteEditor/fileview.cpp
@@ -160,8 +160,7 @@ void FileViewTree::OnBuildInProgress(wxUpdateUIEvent& event) { event.Enable(!Man
 FileViewTree::FileViewTree(wxWindow* parent, const wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
 {
     Create(parent, id, pos, size, style);
-    MSWSetNativeTheme(this);
-    //    SetBackgroundColour("rgb(230, 230, 230)");
+    //MSWSetNativeTheme(this);
 
     // Initialise images map
     BitmapLoader* bmpLoader = PluginManager::Get()->GetStdIcons();

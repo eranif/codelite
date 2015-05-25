9b7454bcf1d3c3e97fe198a8e3fd3193b2f009a6
Eran Ifrah <eran@debian-jessie.debian-jessie>
2015-05-25 02:29:23 -0400

    Fixed build error on Linux

--- Plugin/Notebook.h
+++ Plugin/Notebook.h
@@ -13,12 +13,16 @@
 #undef WXDLLIMPEXP_SDK
 #endif
 
+#ifdef __WXMSW__
 #ifdef WXMAKINGDLL_SDK
 #define WXDLLIMPEXP_SDK __declspec(dllexport)
 #elif defined(WXUSINGDLL_SDK)
 #define WXDLLIMPEXP_SDK __declspec(dllimport)
 #else // not making nor using DLL
 #define WXDLLIMPEXP_SDK
+#endif
+#else
+#define WXDLLIMPEXP_SDK 
 #endif
 
 enum NotebookStyle {

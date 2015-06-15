d6420aa4f947c1fd657903132438f5eacb34883f
Eran <eran.ifrah@gmail.com>
2015-06-15 09:09:41 +0300

    Find in files dialog: set a tooltip to the "..." (3 dots) button

--- Plugin/dirpicker.cpp
+++ Plugin/dirpicker.cpp
@@ -22,7 +22,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
- #include "wx/dir.h"
+#include "wx/dir.h"
 #include "wx/filename.h"
 #include "dirpicker.h"
 #include "wx/sizer.h"
@@ -31,144 +31,146 @@
 #include <wx/dirdlg.h>
 
 BEGIN_EVENT_TABLE(DirPicker, wxPanel)
-	EVT_TEXT(wxID_ANY, DirPicker::OnText)
+EVT_TEXT(wxID_ANY, DirPicker::OnText)
 END_EVENT_TABLE()
 
-DirPicker::DirPicker(wxWindow *parent, wxWindowID id, const wxString &buttonCaption, const wxString &defaultPos, const wxString &message, const wxPoint& pos, const wxSize& size, long style)
-		: wxPanel(parent, id, pos, size, wxTAB_TRAVERSAL | wxNO_BORDER)
-		, m_path(NULL)
-		, m_combo(NULL)
-		, m_buttonCaption(buttonCaption)
-		, m_dlgCaption(message)
-		, m_style(style)
-		, m_defaultPos(defaultPos)
+DirPicker::DirPicker(wxWindow* parent,
+                     wxWindowID id,
+                     const wxString& buttonCaption,
+                     const wxString& defaultPos,
+                     const wxString& message,
+                     const wxPoint& pos,
+                     const wxSize& size,
+                     long style)
+    : wxPanel(parent, id, pos, size, wxTAB_TRAVERSAL | wxNO_BORDER)
+    , m_path(NULL)
+    , m_combo(NULL)
+    , m_buttonCaption(buttonCaption)
+    , m_dlgCaption(message)
+    , m_style(style)
+    , m_defaultPos(defaultPos)
 {
-	CreateControls();
-	ConnectEvents();
+    CreateControls();
+    ConnectEvents();
 }
 
-DirPicker::~DirPicker()
-{
-}
+DirPicker::~DirPicker() {}
 
-void DirPicker::OnText(wxCommandEvent &event)
-{
-	GetParent()->GetEventHandler()->ProcessEvent(event);
-}
+void DirPicker::OnText(wxCommandEvent& event) { GetParent()->GetEventHandler()->ProcessEvent(event); }
 
 void DirPicker::CreateControls()
 {
-	wxBoxSizer *mainSizer = new wxBoxSizer(wxHORIZONTAL);
-	SetSizer(mainSizer);
+    wxBoxSizer* mainSizer = new wxBoxSizer(wxHORIZONTAL);
+    SetSizer(mainSizer);
 
-	size_t flags = wxRIGHT | wxTOP | wxBOTTOM | wxEXPAND;
+    size_t flags = wxRIGHT | wxTOP | wxBOTTOM | wxEXPAND;
 
-	if (m_style & wxDP_USE_TEXTCTRL) {
-		m_path = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize);
-		mainSizer->Add(m_path, 1, flags, 5);
-	} else {
-		m_combo = new wxComboBox(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize);
-		mainSizer->Add(m_combo, 1, flags , 5);
-	}
+    if(m_style & wxDP_USE_TEXTCTRL) {
+        m_path = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize);
+        mainSizer->Add(m_path, 1, flags, 5);
+    } else {
+        m_combo = new wxComboBox(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize);
+        mainSizer->Add(m_combo, 1, flags, 5);
+    }
 
-	m_button = new wxButton(this, wxID_ANY, m_buttonCaption, wxDefaultPosition, wxDefaultSize, wxBU_EXACTFIT);
-	mainSizer->Add(m_button, 0, wxEXPAND | wxALL, 5);
+    m_button = new wxButton(this, wxID_ANY, m_buttonCaption, wxDefaultPosition, wxDefaultSize, wxBU_EXACTFIT);
+    m_button->SetToolTip(_("Browse for folder..."));
+    mainSizer->Add(m_button, 0, wxEXPAND | wxALL, 5);
 
-	Layout();
+    Layout();
 }
 
-void DirPicker::ConnectEvents()
+void DirPicker::ConnectEvents() { ConnectButton(m_button, DirPicker::OnButtonClicked); }
+
+void DirPicker::OnButtonClicked(wxCommandEvent& event)
 {
-	ConnectButton(m_button, DirPicker::OnButtonClicked);
-}
+    wxUnusedVar(event);
 
-void DirPicker::OnButtonClicked(wxCommandEvent &event)
-{
-	wxUnusedVar(event);
+    // no default position was provided by the user
+    // try to take it from the text control
+    wxString work_dir = GetPath();
+    m_defaultPos.Empty();
+    if(work_dir.IsEmpty() == false) {
+        if(wxDir::Exists(work_dir)) {
+            wxFileName fn(work_dir, wxEmptyString);
+            wxLogMessage(work_dir + wxT(" exists"));
+            fn.MakeAbsolute();
+            m_defaultPos = fn.GetFullPath();
+        }
+    }
 
-	//no default position was provided by the user
-	//try to take it from the text control
-	wxString work_dir = GetPath();
-	m_defaultPos.Empty();
-	if (work_dir.IsEmpty() == false) {
-		if( wxDir::Exists(work_dir) ){
-			wxFileName fn(work_dir, wxEmptyString);
-			wxLogMessage(work_dir + wxT(" exists"));
-			fn.MakeAbsolute();
-			m_defaultPos = fn.GetFullPath();
-		}
-	}
+    if(m_defaultPos.IsEmpty()) {
+        m_defaultPos = wxGetCwd();
+    }
 
-	if (m_defaultPos.IsEmpty()) {
-		m_defaultPos = wxGetCwd();
-	}
+    wxLogMessage(wxT("setting working dir to : ") + m_defaultPos);
 
-	wxLogMessage(wxT("setting working dir to : ") + m_defaultPos );
-
-	wxDirDialog *dlg = new wxDirDialog(this, m_dlgCaption, m_defaultPos);
-	if (dlg->ShowModal() == wxID_OK) {
-		// Get the dirname
-		wxString path = dlg->GetPath();
-		SetPath(path);
-	}
-	dlg->Destroy();
+    wxDirDialog* dlg = new wxDirDialog(this, m_dlgCaption, m_defaultPos);
+    if(dlg->ShowModal() == wxID_OK) {
+        // Get the dirname
+        wxString path = dlg->GetPath();
+        SetPath(path);
+    }
+    dlg->Destroy();
 }
 
 wxString DirPicker::GetPath()
 {
-	if (m_style & wxDP_USE_TEXTCTRL)
-		return m_path->GetValue();
-	else {
-		wxString path(m_combo->GetValue());
-		//if the path is not whitin the combobox list, add it
-		if (m_combo->FindString(path) == wxNOT_FOUND) {
-			SetPath(path);
-		}
-		return path;
-	}
+    if(m_style & wxDP_USE_TEXTCTRL)
+        return m_path->GetValue();
+    else {
+        wxString path(m_combo->GetValue());
+        // if the path is not whitin the combobox list, add it
+        if(m_combo->FindString(path) == wxNOT_FOUND) {
+            SetPath(path);
+        }
+        return path;
+    }
 }
 
-void DirPicker::SetPath(const wxString &path)
+void DirPicker::SetPath(const wxString& path)
 {
-	if (m_style & wxDP_USE_TEXTCTRL)
-		m_path->SetValue(path);
-	else {
-		int where = m_combo->FindString(path);
-		if (where != wxNOT_FOUND) {
-			m_combo->SetSelection(where);
-		} else {
-			where = m_combo->Append(path);
-			m_combo->SetSelection(where);
-		}
-	}
+    if(m_style & wxDP_USE_TEXTCTRL)
+        m_path->SetValue(path);
+    else {
+        int where = m_combo->FindString(path);
+        if(where != wxNOT_FOUND) {
+            m_combo->SetSelection(where);
+        } else {
+            where = m_combo->Append(path);
+            m_combo->SetSelection(where);
+        }
+    }
 }
 
-void DirPicker::SetValues(const wxArrayString &values, int sel)
+void DirPicker::SetValues(const wxArrayString& values, int sel)
 {
-	wxASSERT_MSG(m_style & wxDP_USE_COMBOBOX, wxT("SetValues is available only for wxDP_USE_COMBOBOX style"));
-	m_combo->Append(values);
-	m_combo->SetSelection(sel);
+    wxASSERT_MSG(m_style & wxDP_USE_COMBOBOX, wxT("SetValues is available only for wxDP_USE_COMBOBOX style"));
+    m_combo->Append(values);
+    m_combo->SetSelection(sel);
 }
 
 wxArrayString DirPicker::GetValues() const
 {
-	wxArrayString dummy;
-	if(m_style & wxDP_USE_COMBOBOX){
-		return m_combo->GetStrings();
-	} else {
-		return dummy;
-	}
+    wxArrayString dummy;
+    if(m_style & wxDP_USE_COMBOBOX) {
+        return m_combo->GetStrings();
+    } else {
+        return dummy;
+    }
 }
 
 int DirPicker::GetCurrentSelection() const
 {
-	wxCHECK_MSG(m_style & wxDP_USE_COMBOBOX, wxNOT_FOUND, wxT("GetCurrentSelection is available only for wxDP_USE_COMBOBOX style"));
+    wxCHECK_MSG(m_style & wxDP_USE_COMBOBOX,
+                wxNOT_FOUND,
+                wxT("GetCurrentSelection is available only for wxDP_USE_COMBOBOX style"));
 
-	int index = wxNOT_FOUND;
-	wxString stringsel = m_combo->GetValue();
-	if (!stringsel.empty()) {
-		index = m_combo->FindString(stringsel);
-	}
-	
-	return index;
+    int index = wxNOT_FOUND;
+    wxString stringsel = m_combo->GetValue();
+    if(!stringsel.empty()) {
+        index = m_combo->FindString(stringsel);
+    }
+
+    return index;
 }
--- Plugin/dirpicker.h
+++ Plugin/dirpicker.h
@@ -22,7 +22,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
- #ifndef DIRPICKER_H
+#ifndef DIRPICKER_H
 #define DIRPICKER_H
 
 #include "wx/panel.h"
@@ -31,72 +31,70 @@
 #include "wx/textctrl.h"
 #include "wx/combobox.h"
 
-enum {
-	wxDP_USE_TEXTCTRL = 1,
-	wxDP_USE_COMBOBOX = 2
-};
+enum { wxDP_USE_TEXTCTRL = 1, wxDP_USE_COMBOBOX = 2 };
 
 /**
  * This control allows the user to select a directory. with a text control on its right side
  */
-class WXDLLIMPEXP_SDK DirPicker : public wxPanel {
-	wxTextCtrl *m_path;
-	wxComboBox *m_combo;
+class WXDLLIMPEXP_SDK DirPicker : public wxPanel
+{
+    wxTextCtrl* m_path;
+    wxComboBox* m_combo;
 
-	wxButton *m_button;
-	wxString m_buttonCaption;
-	wxString m_dlgCaption;
-	long m_style;
-	wxString m_defaultPos;
+    wxButton* m_button;
+    wxString m_buttonCaption;
+    wxString m_dlgCaption;
+    long m_style;
+    wxString m_defaultPos;
 
 private:
-	void CreateControls();
-	void ConnectEvents();
+    void CreateControls();
+    void ConnectEvents();
 
 protected:
-	/**
-	 * Handle button click event
-	 * \param &event
-	 */
-	virtual void OnButtonClicked(wxCommandEvent &event);
-	virtual void OnText(wxCommandEvent &event);
+    /**
+     * Handle button click event
+     * \param &event
+     */
+    virtual void OnButtonClicked(wxCommandEvent& event);
+    virtual void OnText(wxCommandEvent& event);
 
 public:
-	DirPicker(	wxWindow *parent,
-				wxWindowID id = wxID_ANY,
-				const wxString &buttonCaption = wxT("..."),
-				const wxString &defaultPos = wxEmptyString,
-				const wxString &message = _("Select a folder:"),
-				const wxPoint& pos = wxDefaultPosition,
-				const wxSize& size = wxDefaultSize,
-				long style = wxDP_USE_TEXTCTRL);
+    DirPicker(wxWindow* parent,
+              wxWindowID id = wxID_ANY,
+              const wxString& buttonCaption = wxT("..."),
+              const wxString& defaultPos = wxEmptyString,
+              const wxString& message = _("Select a folder:"),
+              const wxPoint& pos = wxDefaultPosition,
+              const wxSize& size = wxDefaultSize,
+              long style = wxDP_USE_TEXTCTRL);
 
-	virtual ~DirPicker();
+    virtual ~DirPicker();
 
-	/**
-	 * \return the path
-	 */
-	wxString GetPath();
+    /**
+     * \return the path
+     */
+    wxString GetPath();
 
-	/**
-	 * set a value into the text control
-	 * \param path path to set
-	 */
-	void SetPath(const wxString &path);
+    /**
+     * set a value into the text control
+     * \param path path to set
+     */
+    void SetPath(const wxString& path);
 
-	/**
-	 * Set combobox values, this option is available only when using wxDP_USE_COMBOBOX
-	 */
-	void SetValues(const wxArrayString &values, int sel=0);
+    /**
+     * Set combobox values, this option is available only when using wxDP_USE_COMBOBOX
+     */
+    void SetValues(const wxArrayString& values, int sel = 0);
 
-	/**
-	 * Get combobox selection as int; this option is available only when using wxDP_USE_COMBOBOX
-	 * \return the current selection's index, or wxNOT_FOUND if there's none
-	 */
-	int GetCurrentSelection() const;
+    /**
+     * Get combobox selection as int; this option is available only when using wxDP_USE_COMBOBOX
+     * \return the current selection's index, or wxNOT_FOUND if there's none
+     */
+    int GetCurrentSelection() const;
 
-	wxArrayString GetValues() const;
-	DECLARE_EVENT_TABLE()
+    wxArrayString GetValues() const;
+    DECLARE_EVENT_TABLE()
 };
 
 #endif // DIRPICKER_H

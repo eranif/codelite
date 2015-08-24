0d2a6998c504e0904dd52c64a8d844b508c11633
Eran Ifrah <eran@eran-debian.eran-debian>
2015-08-24 11:36:07 +0300

    Fixed: https://github.com/eranif/codelite/issues/899 : optimized the "refactoring.db" database size   the size should be reduced to 1/3 of its original size

--- CodeLite/refactoring_storage.h
+++ CodeLite/refactoring_storage.h
@@ -38,45 +38,49 @@ class CppTokenCacheMakerThread;
 class WXDLLIMPEXP_CL RefactoringStorage : public wxEvtHandler
 {
 public:
-    enum CacheStatus {
-        CACHE_NOT_READY,
-        CACHE_IN_PROGRESS,
-        CACHE_READY
-    };
+    enum CacheStatus { CACHE_NOT_READY, CACHE_IN_PROGRESS, CACHE_READY };
+
 protected:
-    
-    wxSQLite3Database         m_db;
-    wxString                  m_cacheDb;
-    CacheStatus               m_cacheStatus;
-    wxString                  m_workspaceFile;
+    wxSQLite3Database m_db;
+    wxString m_cacheDb;
+    CacheStatus m_cacheStatus;
+    wxString m_workspaceFile;
     CppTokenCacheMakerThread* m_thread;
-    
+
     friend class CppTokenCacheMakerThread;
+
 public:
     RefactoringStorage();
     virtual ~RefactoringStorage();
 
 protected:
-    void DoUpdateFileTimestamp(const wxString &filename);
-    void DoDeleteFile(const wxString &filename);
-    bool IsFileUpToDate(const wxString &filename);
-    
-    void OnWorkspaceLoaded(wxCommandEvent &e);
-    void OnWorkspaceClosed(wxCommandEvent &e);
-    void OnThreadStatus(wxCommandEvent &e);
-    void Open(const wxString &workspacePath);
+    wxLongLong DoUpdateFileTimestamp(const wxString& filename);
+    void DoDeleteFile(wxLongLong fileID);
+    bool IsFileUpToDate(const wxString& filename);
+
+    void OnWorkspaceLoaded(wxCommandEvent& e);
+    void OnWorkspaceClosed(wxCommandEvent& e);
+    void OnThreadStatus(wxCommandEvent& e);
+    void Open(const wxString& workspacePath);
     void Begin();
     void Commit();
     void Rollback();
-    
+
     void JoinWorkerThread();
+    wxString GetSchemaVersion();
+    /**
+     * @brief return the file ID from the database
+     */
+    wxLongLong GetFileID(const wxFileName& filename);
+    /**
+     * @brief return the file ID from the database
+     */
+    wxLongLong GetFileID(const wxString& filename);
     
 public:
-    bool IsCacheReady() const {
-        return m_cacheStatus == CACHE_READY;
-    }
-    void StoreTokens( const wxString &filename, const CppToken::List_t& tokens , bool startTx);
-    void Match(const wxString &symname, const wxString &filename, CppTokensMap& matches);
+    bool IsCacheReady() const { return m_cacheStatus == CACHE_READY; }
+    void StoreTokens(const wxString& filename, const CppToken::List_t& tokens, bool startTx);
+    void Match(const wxString& symname, const wxString& filename, CppTokensMap& matches);
     void InitializeCache(const wxFileList_t& files);
     wxFileList_t FilterUpToDateFiles(const wxFileList_t& files);
     CppToken::List_t GetTokens(const wxString& symname, const wxFileList_t& filelist = wxFileList_t());

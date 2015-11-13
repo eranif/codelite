e088c8ed57545990dfa7192b4568bcdbd1b57da8
Eran <eran.ifrah@gmail.com>
2015-11-13 16:54:50 +0200

    Fixed: Javascript debugger does not show JavaScript Date values     fixes: https://github.com/eranif/codelite/issues/1018

--- WebTools/NodeJSOuptutParser.cpp
+++ WebTools/NodeJSOuptutParser.cpp
@@ -25,14 +25,17 @@ NodeJSHandle NodeJSOuptutParser::ParseRef(const JSONElement& ref, std::map<int, 
     } else if(h.type == "null") {
         h.value = "null";
     } else if(h.type == "object") {
-        
+
         if(ref.hasNamedObject("protoObject")) {
-           h.properties.push_back(std::make_pair(ref.namedObject("protoObject").namedObject("ref").toInt(), "prototype"));
+            h.properties.push_back(
+                std::make_pair(ref.namedObject("protoObject").namedObject("ref").toInt(), "prototype"));
         }
-        
+
         if(ref.hasNamedObject("className") && ref.namedObject("className").toString() == "Array") {
-            h.type = "Array"; 
+            h.type = "Array";
             h.value = "[]";
+        } else if(ref.hasNamedObject("text")) {
+            h.value = ref.namedObject("text").toString();
         } else {
             h.value = "{...}";
         }
@@ -46,7 +49,7 @@ NodeJSHandle NodeJSOuptutParser::ParseRef(const JSONElement& ref, std::map<int, 
             } else if(prop.namedObject("name").isNumber()) {
                 propName << "[" << prop.namedObject("name").toInt() << "]";
             }
-            
+
             int propId = prop.namedObject("ref").toInt();
             h.properties.push_back(std::make_pair(propId, propName));
         }

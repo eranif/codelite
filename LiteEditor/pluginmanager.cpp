a6895a029b8faa329ae4c91f6ce55272d2a8d489
dghart <david@4Pane.co.uk>
2015-11-18 19:47:01 +0000

    Move the plugin-toolbar to the bottom of the screen
    Fixing it to the right meant that each plugin's bar was forced either to be there or to float. Moving it to the bottom makes it possible to detach them and reattach to the main toolbar again.

--- LiteEditor/pluginmanager.cpp
+++ LiteEditor/pluginmanager.cpp
@@ -275,7 +275,7 @@ void PluginManager::Load()
                                                                     .RightDockable(true)
                                                                     .Caption(plugin->GetShortName())
                                                                     .ToolbarPane()
-                                                                    .Right()
+                                                                    .Bottom()
                                                                     .Row(0));
 
                 // Add menu entry at the 'View->Toolbars' menu for this toolbar

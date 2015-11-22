f57a20fd9214741e73e2002f00ef91296b71d3d9
Eran <eran.ifrah@gmail.com>
2015-11-22 20:14:05 +0200

    Re-enable the "stipple brush" drawing for wxAUI sash

--- Plugin/cl_aui_dock_art.cpp
+++ Plugin/cl_aui_dock_art.cpp
@@ -246,7 +246,7 @@ void clAuiDockArt::DrawBorder(wxDC& dc, wxWindow* window, const wxRect& rect, wx
 void clAuiDockArt::DrawSash(wxDC& dc, wxWindow* window, int orientation, const wxRect& rect)
 {
     wxColour sashColour = wxSystemSettings::GetColour(wxSYS_COLOUR_3DFACE);
-#if 0
+#if 1
     if(clGetManager() && clGetManager()->GetStatusBar() && clGetManager()->GetStatusBar()->GetArt()) {
         sashColour = clGetManager()->GetStatusBar()->GetArt()->GetBgColour();
     }
@@ -254,6 +254,6 @@ void clAuiDockArt::DrawSash(wxDC& dc, wxWindow* window, int orientation, const w
     wxUnusedVar(window);
     wxUnusedVar(orientation);
     dc.SetPen(*wxTRANSPARENT_PEN);
-    dc.SetBrush(sashColour);
+    dc.SetBrush(DrawingUtils::GetStippleBrush());
     dc.DrawRectangle(rect);
 }

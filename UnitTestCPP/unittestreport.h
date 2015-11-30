709b7ab0814b57b13cd91415e6391ee4011f9fc2
Eran Ifrah <eran@eran-debian.eran-debian>
2015-11-30 08:04:26 +0200

    UnitTest++: updated the UI to use wxDataViewCtrl and added a "Clear" button

--- UnitTestCPP/unittestreport.h
+++ UnitTestCPP/unittestreport.h
@@ -15,7 +15,8 @@
 #include <wx/sizer.h>
 #include <wx/stattext.h>
 #include "progressctrl.h"
-#include <wx/listctrl.h>
+#include <wx/button.h>
+#include <wx/dataview.h>
 #if wxVERSION_NUMBER >= 2900
 #include <wx/persist.h>
 #include <wx/persist/toplevel.h>
@@ -30,29 +31,32 @@ protected:
     ProgressCtrl* m_progressPassed;
     wxStaticText* m_staticText8;
     ProgressCtrl* m_progressFailed;
+    wxButton* m_button4;
     wxStaticText* m_staticText10;
     wxStaticText* m_staticTextTotalTests;
     wxStaticText* m_staticText12;
     wxStaticText* m_staticTextFailTestsNum;
     wxStaticText* m_staticText14;
     wxStaticText* m_staticTextSuccessTestsNum;
-    wxListCtrl* m_listCtrlErrors;
+    wxDataViewListCtrl* m_dvListCtrlErrors;
 
 protected:
-    virtual void OnItemActivated(wxListEvent& event) { event.Skip(); }
+    virtual void OnClearReport(wxCommandEvent& event) { event.Skip(); }
+    virtual void OnItemActivated(wxDataViewEvent& event) { event.Skip(); }
 
 public:
     wxStaticText* GetStaticText7() { return m_staticText7; }
     ProgressCtrl* GetProgressPassed() { return m_progressPassed; }
     wxStaticText* GetStaticText8() { return m_staticText8; }
     ProgressCtrl* GetProgressFailed() { return m_progressFailed; }
+    wxButton* GetButton4() { return m_button4; }
     wxStaticText* GetStaticText10() { return m_staticText10; }
     wxStaticText* GetStaticTextTotalTests() { return m_staticTextTotalTests; }
     wxStaticText* GetStaticText12() { return m_staticText12; }
     wxStaticText* GetStaticTextFailTestsNum() { return m_staticTextFailTestsNum; }
     wxStaticText* GetStaticText14() { return m_staticText14; }
     wxStaticText* GetStaticTextSuccessTestsNum() { return m_staticTextSuccessTestsNum; }
-    wxListCtrl* GetListCtrlErrors() { return m_listCtrlErrors; }
+    wxDataViewListCtrl* GetDvListCtrlErrors() { return m_dvListCtrlErrors; }
     UnitTestsBasePage(wxWindow* parent, wxWindowID id = wxID_ANY, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize(-1,-1), long style = wxTAB_TRAVERSAL);
     virtual ~UnitTestsBasePage();
 };

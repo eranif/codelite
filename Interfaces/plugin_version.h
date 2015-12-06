90a4ccba38ab58608dc3d1eda3099f2c5d6e7bb0
Eran <eran.ifrah@gmail.com>
2015-12-06 20:23:54 +0200

    Changed the plugin API version to 9060 this will ensure that only plugins with the new SDK API are loaded (the change was done from 9.0.5->9.0.6)

--- Interfaces/plugin_version.h
+++ Interfaces/plugin_version.h
@@ -28,6 +28,6 @@
 
 // Interface version is calcualted as follows: MAJOR * 1000 + MINOR * 100, e.g. codelite 4.1 => 4100, codelite 5.0 =>
 // 5000
-#define PLUGIN_INTERFACE_VERSION 9100
+#define PLUGIN_INTERFACE_VERSION 9060 // 9.0.6
 
 #endif // PLUGIN_VERSION_H

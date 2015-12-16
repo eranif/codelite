9c6651a212c00515ec37ea8f2118a610371ec0b2
Eran Ifrah <eran@eran-debian.eran-debian>
2015-12-16 18:02:10 +0200

    Added "final" to CodeLite indexer ignore list

--- CodeLite/tags_options_data.cpp
+++ CodeLite/tags_options_data.cpp
@@ -122,7 +122,7 @@ TagsOptionsData::TagsOptionsData()
     m_tokens.Add(wxT("WINAPI"));
     m_tokens.Add(wxT("__nonnull"));
     m_tokens.Add(wxT("noexcept"));
-
+    m_tokens.Add("final"); // ignore "final" keyword
 #if defined(__WXGTK__)
     m_tokens.Add(wxT("wxTopLevelWindowNative=wxTopLevelWindowGTK"));
     m_tokens.Add(wxT("wxWindow=wxWindowGTK"));

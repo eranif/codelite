21066f1cead6583b76e36c95b92233362c814436
Eran <eran.ifrah@gmail.com>
2016-01-21 22:03:33 +0200

    C++ workspace view: added 'Open Shell' and 'Open Containing Folder' menu entries to the file context menu in the tree view

--- CodeLiteDiff/codelitediff.cpp
+++ CodeLiteDiff/codelitediff.cpp
@@ -109,6 +109,7 @@ void CodeLiteDiff::OnTabContextMenu(clContextMenuEvent& event)
     // Edit the context menu
     wxMenuItem* mi = new wxMenuItem(event.GetMenu(), XRCID("diff_compare_with"), _("Compare with..."), "");
     mi->SetBitmap(m_mgr->GetStdIcons()->LoadBitmap("diff"));
+    event.GetMenu()->AppendSeparator();
     event.GetMenu()->Append(mi);
     event.GetMenu()->Bind(wxEVT_MENU, &CodeLiteDiff::OnDiff, this, XRCID("diff_compare_with"));
 }

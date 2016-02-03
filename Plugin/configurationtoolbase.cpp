eranif/codelite

commit 44e9ae17bcbdb91809e384ff90cd4c7c37a11120

Fixed: avoid writing XML files directly, instead write to a wxString and then write the wxString to the file system

LiteEditor.workspace
@@ -100,7 +100,7 @@
       <Project Name="wxsqlite3" ConfigName="Win_x86_Release"/>
       <Project Name="ZoomNavigator" ConfigName="Win_x86_Release"/>
     </WorkspaceConfiguration>
-    <WorkspaceConfiguration Name="CMake_Release" Selected="no">
+    <WorkspaceConfiguration Name="CMake_Release" Selected="yes">
       <Environment/>
       <Project Name="ZoomNavigator" ConfigName="DebugUnicode"/>
       <Project Name="wxsqlite3" ConfigName="Win_x86_Release"/>
@@ -284,7 +284,7 @@
       <Project Name="WebTools" ConfigName="DebugUnicode"/>
       <Project Name="HelpPlugin" ConfigName="Win_x64_Debug"/>
     </WorkspaceConfiguration>
-    <WorkspaceConfiguration Name="Win_x64_Release" Selected="yes">
+    <WorkspaceConfiguration Name="Win_x64_Release" Selected="no">
       <Environment/>
       <Project Name="abbreviation" ConfigName="Win_x64_Release"/>
       <Project Name="CallGraph" ConfigName="Win_x64_Release"/>

Plugin/configurationtoolbase.cpp
@@ -22,7 +22,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
- #include "configurationtoolbase.h"
+#include "configurationtoolbase.h"
 #include "editor_config.h"
 #include "xmlutils.h"
 #include "serialized_object.h"
@@ -30,53 +30,50 @@
 #include "wx/filename.h"
 #include "wx/ffile.h"
 #include "cl_standard_paths.h"
+#include "globals.h"
 
 ConfigurationToolBase::ConfigurationToolBase()
-: m_fileName(wxEmptyString)
+    : m_fileName(wxEmptyString)
 {
 }
 
-ConfigurationToolBase::~ConfigurationToolBase()
+ConfigurationToolBase::~ConfigurationToolBase() {}
+
+bool ConfigurationToolBase::Load(const wxString& fileName)
 {
+    // Try to open the file from the user data settings first
+    wxFileName userFile(clStandardPaths::Get().GetUserDataDir() + wxT("/") + fileName);
+    m_fileName = userFile.GetFullPath();
+
+    if(userFile.FileExists() == false) {
+        // no such file, create an empty file
+        wxString content;
+        content << wxT("<") << GetRootName() << wxT("/>");
+        wxFFile file;
+        file.Open(userFile.GetFullPath(), wxT("w+b"));
+        if(file.IsOpened()) {
+            file.Write(content);
+            file.Close();
+        }
+    }
+    return ::SaveXmlToFile(&m_doc, m_fileName);
 }
 
-bool ConfigurationToolBase::Load(const wxString &fileName)
+bool ConfigurationToolBase::WriteObject(const wxString& name, SerializedObject* obj)
 {
-	// Try to open the file from the user data settings first
-	wxFileName userFile(clStandardPaths::Get().GetUserDataDir() + wxT("/") + fileName);
-	m_fileName = userFile.GetFullPath();
+    if(m_doc.IsOk() == false) {
+        return false;
+    }
 
-	if(userFile.FileExists() == false){
-		//no such file, create an empty file
-		wxString content;
-		content << wxT("<") << GetRootName() << wxT("/>");
-		wxFFile file;
-		file.Open(userFile.GetFullPath(), wxT("w+b"));
-		if(file.IsOpened()){
-			file.Write(content);
-			file.Close();
-		}
-	}
-	return m_doc.Load(m_fileName);
+    if(!XmlUtils::StaticWriteObject(m_doc.GetRoot(), name, obj)) return false;
+    return ::SaveXmlToFile(&m_doc, m_fileName);
 }
 
-bool ConfigurationToolBase::WriteObject(const wxString &name, SerializedObject *obj)
+bool ConfigurationToolBase::ReadObject(const wxString& name, SerializedObject* obj)
 {
-	if(m_doc.IsOk() == false){
-		return false;
-	}
+    if(m_doc.IsOk() == false) {
+        return false;
+    }
 
-	if(!XmlUtils::StaticWriteObject(m_doc.GetRoot(), name, obj))
-		return false;
-	return m_doc.Save(m_fileName);
-}
-
-
-bool ConfigurationToolBase::ReadObject(const wxString &name, SerializedObject *obj)
-{
-	if(m_doc.IsOk() == false){
-		return false;
-	}
-
-	return XmlUtils::StaticReadObject(m_doc.GetRoot(), name, obj);
+    return XmlUtils::StaticReadObject(m_doc.GetRoot(), name, obj);
 }

Plugin/configtool.cpp
@@ -30,6 +30,7 @@
 #include "wx/ffile.h"
 #include "editor_config.h"
 #include "wx_xml_compatibility.h"
+#include "globals.h"
 
 ConfigTool::ConfigTool()
     : m_fileName(wxEmptyString)
@@ -64,7 +65,7 @@ bool ConfigTool::WriteObject(const wxString &name, SerializedObject *obj)
 
     if(!XmlUtils::StaticWriteObject(m_doc.GetRoot(), name, obj))
         return false;
-    return m_doc.Save(m_fileName);
+    return ::SaveXmlToFile(&m_doc, m_fileName);
 }
 
 bool ConfigTool::ReadObject(const wxString &name, SerializedObject *obj)

commit 648a64903be082b95952084376843f5cf8c6365a

Fixed: regression in loading configuration file introduced in commit:     https://github.com/eranif/codelite/commit/44e9ae17bcbdb91809e384ff90cd4c7c37a11120

LiteEditor.workspace
@@ -100,7 +100,7 @@
       <Project Name="wxsqlite3" ConfigName="Win_x86_Release"/>
       <Project Name="ZoomNavigator" ConfigName="Win_x86_Release"/>
     </WorkspaceConfiguration>
-    <WorkspaceConfiguration Name="CMake_Release" Selected="yes">
+    <WorkspaceConfiguration Name="CMake_Release" Selected="no">
       <Environment/>
       <Project Name="ZoomNavigator" ConfigName="DebugUnicode"/>
       <Project Name="wxsqlite3" ConfigName="Win_x86_Release"/>
@@ -284,8 +284,9 @@
       <Project Name="WebTools" ConfigName="DebugUnicode"/>
       <Project Name="HelpPlugin" ConfigName="Win_x64_Debug"/>
     </WorkspaceConfiguration>
-    <WorkspaceConfiguration Name="Win_x64_Release" Selected="no">
-      <Environment/>
+    <WorkspaceConfiguration Name="Win_x64_Release" Selected="yes">
+      <Environment>WXWIN=D:\src\wxWidgets
+WXCFG=gcc_dll\mswu</Environment>
       <Project Name="abbreviation" ConfigName="Win_x64_Release"/>
       <Project Name="CallGraph" ConfigName="Win_x64_Release"/>
       <Project Name="CMakePlugin" ConfigName="Win_x64_Release"/>

Plugin/configurationtoolbase.cpp
@@ -56,7 +56,7 @@ bool ConfigurationToolBase::Load(const wxString& fileName)
             file.Close();
         }
     }
-    return ::SaveXmlToFile(&m_doc, m_fileName);
+    return m_doc.Load(m_fileName);
 }
 
 bool ConfigurationToolBase::WriteObject(const wxString& name, SerializedObject* obj)
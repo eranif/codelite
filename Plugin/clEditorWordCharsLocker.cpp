a5349fed807ace756ceb7b327c98774a53c117eb
Eran <eran.ifrah@gmail.com>
2016-02-20 13:25:16 +0200

    Fixed clEditorXmlHelper::GetXmlTagAt() method

--- Plugin/clEditorWordCharsLocker.cpp
+++ Plugin/clEditorWordCharsLocker.cpp
@@ -8,16 +8,13 @@ clEditorXmlHelper::clEditorXmlHelper(wxStyledTextCtrl* stc)
     std::for_each(wordChars.begin(), wordChars.end(), [&](const wxChar& ch) { m_chars.insert((int)ch); });
 }
 
-clEditorXmlHelper::~clEditorXmlHelper()
-{
-}
+clEditorXmlHelper::~clEditorXmlHelper() {}
 
 wxString clEditorXmlHelper::GetXmlTagAt(int pos, int& startPos, int& endPos) const
 {
     startPos = wxNOT_FOUND;
     endPos = wxNOT_FOUND;
-    if(pos > m_stc->GetLastPosition() || pos < 0)
-        return "";
+    if(pos > m_stc->GetLastPosition() || pos < 0) return "";
 
     // going backward
     startPos = pos;
@@ -32,18 +29,15 @@ wxString clEditorXmlHelper::GetXmlTagAt(int pos, int& startPos, int& endPos) con
         break;
     }
 
-    // Did we get anything?
-    if(startPos != pos) {
-        if((GetCharAt(startPos - 1) == (int)'/') && (GetCharAt(startPos - 2) == (int)'<')) {
-            // a closing XML tag
-            startPos -= 2;
-        } else if(GetCharAt(startPos - 1) == (int)'<') {
-            // an opening tag
-            startPos -= 1;
-        } else {
-            // Not an XML tag
-            return "";
-        }
+    if((GetCharAt(startPos - 1) == (int)'/') && (GetCharAt(startPos - 2) == (int)'<')) {
+        // a closing XML tag
+        startPos -= 2;
+    } else if(GetCharAt(startPos - 1) == (int)'<') {
+        // an opening tag
+        startPos -= 1;
+    } else {
+        // Not an XML tag
+        return "";
     }
 
     endPos = pos;
@@ -58,22 +52,19 @@ wxString clEditorXmlHelper::GetXmlTagAt(int pos, int& startPos, int& endPos) con
     }
 
     // Did we get anything?
-    if(endPos != pos) {
-        if((GetCharAt(endPos + 1) == (int)'>')) {
-            // a closing XML tag
-            endPos += 1;
-        } else if((GetCharAt(endPos + 1) != (int)' ') && (GetCharAt(endPos + 1) != (int)'\t')) {
-            // a tag can end in only 2 ways:
-            // with '>' or with space | tab, this tag does not end with either
-            return "";
-        }
+    if((GetCharAt(endPos) == (int)'>')) {
+        // a closing XML tag
+        endPos += 1;
+    } else if((GetCharAt(endPos) != (int)' ') && (GetCharAt(endPos) != (int)'\t')) {
+        // a tag can end in only 2 ways:
+        // with '>' or with space | tab, this tag does not end with either
+        return "";
     }
     return m_stc->GetTextRange(startPos, endPos);
 }
 
 int clEditorXmlHelper::GetCharAt(int pos) const
 {
-    if(pos < 0 || pos > m_stc->GetLastPosition())
-        return 0;
+    if(pos < 0 || pos > m_stc->GetLastPosition()) return 0;
     return m_stc->GetCharAt(pos);
 }

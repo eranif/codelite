23184ff7d377729d4b2a1e576de8a6555721c056
Eran <eran.ifrah@gmail.com>
2016-03-23 18:14:56 +0200

    Fixed: Subversion: show the commit history as a LIFO list

--- Subversion2/commitmessagescache.cpp
+++ Subversion2/commitmessagescache.cpp
@@ -27,28 +27,30 @@
 
 void CommitMessagesCache::GetMessages(wxArrayString& messages, wxArrayString& previews)
 {
-	for(size_t i=0; i<m_messages.GetCount(); i++) {
-		messages.Add(m_messages.Item(i));
-		previews.Add(m_messages.Item(i).BeforeFirst(wxT('\n')));
-	}
+    for(size_t i = 0; i < m_messages.GetCount(); i++) {
+        messages.Add(m_messages.Item(i));
+        previews.Add(m_messages.Item(i).BeforeFirst(wxT('\n')));
+    }
 }
 
 void CommitMessagesCache::AddMessage(const wxString& message)
 {
-	wxString formattedMessage = FormatMessage(message);
-	if(formattedMessage.IsEmpty())
-		return;
+    wxString formattedMessage = FormatMessage(message);
+    if(formattedMessage.IsEmpty()) return;
 
-	if(m_messages.Index(formattedMessage) == wxNOT_FOUND) {
-		m_messages.Add(formattedMessage);
-	}
+    int where = m_messages.Index(formattedMessage);
+    if(where != wxNOT_FOUND) {
+        m_messages.RemoveAt(where);
+    }
+    // Place the new message at the top of the message list
+    m_messages.Insert(formattedMessage, 0);
 }
 
 wxString CommitMessagesCache::FormatMessage(const wxString& message)
 {
-	wxString formattedMessage ( message );
-	formattedMessage.Replace(wxT("\r\n"), wxT("\n"));
-	formattedMessage.Replace(wxT("\v"),   wxT("\n"));
-	formattedMessage.Trim(false).Trim();
-	return formattedMessage;
+    wxString formattedMessage(message);
+    formattedMessage.Replace(wxT("\r\n"), wxT("\n"));
+    formattedMessage.Replace(wxT("\v"), wxT("\n"));
+    formattedMessage.Trim(false).Trim();
+    return formattedMessage;
 }

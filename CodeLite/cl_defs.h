6794e8e91c28fa894fa940179803641eea29f092
Eran <eran.ifrah@gmail.com>
2016-10-26 09:05:36 +0300

    Changed the default wxAUI toolbar style for MSW

--- CodeLite/cl_defs.h
+++ CodeLite/cl_defs.h
@@ -32,8 +32,13 @@
 #include <wx/version.h>
 
 #define clToolBar wxAuiToolBar
+#ifdef __WXMSW__
+#define clTB_DEFAULT_STYLE wxAUI_TB_PLAIN_BACKGROUND
+#define clTB_DEFAULT_STYLE_PLUGIN wxAUI_TB_PLAIN_BACKGROUND
+#else
 #define clTB_DEFAULT_STYLE wxAUI_TB_DEFAULT_STYLE
 #define clTB_DEFAULT_STYLE_PLUGIN wxAUI_TB_DEFAULT_STYLE
+#endif
 #define USE_AUI_TOOLBAR 1
 
 // Defaults

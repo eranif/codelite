c54650cf6d03986dadd9482ee70875084b7e4153
Eran Ifrah <eran.ifrah@gmail.com>
2017-01-24 08:08:00 +0200

    Configuration manager: call 'GetSizer()->Fit()' on the 'Edit' project configuration dialog

--- LiteEditor/edit_configuration.cpp
+++ LiteEditor/edit_configuration.cpp
@@ -1,28 +1,28 @@
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 //
-// copyright            : (C) 2008 by Eran Ifrah                            
-// file name            : edit_configuration.cpp              
-//                                                                          
+// copyright            : (C) 2008 by Eran Ifrah
+// file name            : edit_configuration.cpp
+//
 // -------------------------------------------------------------------------
-// A                                                                        
-//              _____           _      _     _ _                            
-//             /  __ \         | |    | |   (_) |                           
-//             | /  \/ ___   __| | ___| |    _| |_ ___                      
-//             | |    / _ \ / _  |/ _ \ |   | | __/ _ )                     
-//             | \__/\ (_) | (_| |  __/ |___| | ||  __/                     
-//              \____/\___/ \__,_|\___\_____/_|\__\___|                     
-//                                                                          
-//                                                  F i l e                 
-//                                                                          
-//    This program is free software; you can redistribute it and/or modify  
-//    it under the terms of the GNU General Public License as published by  
-//    the Free Software Foundation; either version 2 of the License, or     
-//    (at your option) any later version.                                   
-//                                                                          
+// A
+//              _____           _      _     _ _
+//             /  __ \         | |    | |   (_) |
+//             | /  \/ ___   __| | ___| |    _| |_ ___
+//             | |    / _ \ / _  |/ _ \ |   | | __/ _ )
+//             | \__/\ (_) | (_| |  __/ |___| | ||  __/
+//              \____/\___/ \__,_|\___\_____/_|\__\___|
+//
+//                                                  F i l e
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
- ///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
 // C++ code generated with wxFormBuilder (version May  5 2007)
 // http://www.wxformbuilder.org/
 //
@@ -39,7 +39,7 @@
 
 #else
 #include <wx/wx.h>
-#endif //WX_PRECOMP
+#endif // WX_PRECOMP
 
 #include "edit_configuration.h"
 #include "manager.h"
@@ -48,148 +48,147 @@
 
 ///////////////////////////////////////////////////////////////////////////
 
-EditConfigurationDialog::EditConfigurationDialog( wxWindow* parent, const wxString &projectName, int id, wxString title, wxPoint pos, wxSize size, int style ) 
-: wxDialog( parent, id, title, pos, size, style )
-, m_projectName(projectName)
+EditConfigurationDialog::EditConfigurationDialog(
+    wxWindow* parent, const wxString& projectName, int id, wxString title, wxPoint pos, wxSize size, int style)
+    : wxDialog(parent, id, title, pos, size, style)
+    , m_projectName(projectName)
 {
-	this->SetSizeHints( wxDefaultSize, wxDefaultSize );
-	
-	wxBoxSizer* bSizer15;
-	bSizer15 = new wxBoxSizer( wxVERTICAL );
-	
-	m_panel6 = new wxPanel( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
-	wxBoxSizer* bSizer17;
-	bSizer17 = new wxBoxSizer( wxHORIZONTAL );
-	
-	m_configurationsList = new wxListBox( m_panel6, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0, NULL, 0 ); 
-	bSizer17->Add( m_configurationsList, 1, wxALL|wxEXPAND, 5 );
-	
-	ProjectSettingsPtr settings = ManagerST::Get()->GetProjectSettings(m_projectName);
-	if(settings){
-		ProjectSettingsCookie cookie;
-		BuildConfigPtr bldConf = settings->GetFirstBuildConfiguration(cookie);
-		while(bldConf){
-			m_configurationsList->Append(bldConf->GetName());
-			bldConf = settings->GetNextBuildConfiguration(cookie);
-		}
-	}
-	if(m_configurationsList->GetCount() > 0)
-		m_configurationsList->SetSelection(0);
+    this->SetSizeHints(wxDefaultSize, wxDefaultSize);
 
-	wxBoxSizer* bSizer18;
-	bSizer18 = new wxBoxSizer( wxVERTICAL );
-	
-	m_buttonDelete = new wxButton( m_panel6, wxID_ANY, _("&Delete"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer18->Add( m_buttonDelete, 0, wxALL, 5 );
-	
-	m_buttonRename = new wxButton( m_panel6, wxID_ANY, _("&Rename"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer18->Add( m_buttonRename, 0, wxALL, 5 );
-	bSizer17->Add( bSizer18, 0, wxEXPAND, 5 );
-	
-	m_panel6->SetSizer( bSizer17 );
-	m_panel6->Layout();
-	bSizer17->Fit( m_panel6 );
-	bSizer15->Add( m_panel6, 1, wxALL|wxEXPAND, 5 );
-	
-	m_staticline9 = new wxStaticLine( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxLI_HORIZONTAL );
-	bSizer15->Add( m_staticline9, 0, wxEXPAND | wxALL, 5 );
-	
-	wxBoxSizer* bSizer16;
-	bSizer16 = new wxBoxSizer( wxHORIZONTAL );
-	
-	m_buttonClose = new wxButton( this, wxID_OK, _("Close"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer16->Add( m_buttonClose, 0, wxALL, 5 );
-	
-	bSizer15->Add( bSizer16, 0, wxALIGN_CENTER, 5 );
-	
-	this->SetSizer( bSizer15 );
-	this->Layout();
+    wxBoxSizer* bSizer15;
+    bSizer15 = new wxBoxSizer(wxVERTICAL);
 
-	ConnectListBoxDClick(m_configurationsList, EditConfigurationDialog::OnItemDclick);
-	ConnectButton(m_buttonClose, EditConfigurationDialog::OnButtonClose);
-	ConnectButton(m_buttonRename, EditConfigurationDialog::OnButtonRename);
-	ConnectButton(m_buttonDelete, EditConfigurationDialog::OnButtonDelete);
+    m_panel6 = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL);
+    wxBoxSizer* bSizer17;
+    bSizer17 = new wxBoxSizer(wxHORIZONTAL);
+
+    m_configurationsList = new wxListBox(m_panel6, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0, NULL, 0);
+    bSizer17->Add(m_configurationsList, 1, wxALL | wxEXPAND, 5);
+
+    ProjectSettingsPtr settings = ManagerST::Get()->GetProjectSettings(m_projectName);
+    if(settings) {
+        ProjectSettingsCookie cookie;
+        BuildConfigPtr bldConf = settings->GetFirstBuildConfiguration(cookie);
+        while(bldConf) {
+            m_configurationsList->Append(bldConf->GetName());
+            bldConf = settings->GetNextBuildConfiguration(cookie);
+        }
+    }
+    if(m_configurationsList->GetCount() > 0) m_configurationsList->SetSelection(0);
+
+    wxBoxSizer* bSizer18;
+    bSizer18 = new wxBoxSizer(wxVERTICAL);
+
+    m_buttonDelete = new wxButton(m_panel6, wxID_ANY, _("&Delete"), wxDefaultPosition, wxDefaultSize, 0);
+    bSizer18->Add(m_buttonDelete, 0, wxALL, 5);
+
+    m_buttonRename = new wxButton(m_panel6, wxID_ANY, _("&Rename"), wxDefaultPosition, wxDefaultSize, 0);
+    bSizer18->Add(m_buttonRename, 0, wxALL, 5);
+    bSizer17->Add(bSizer18, 0, wxEXPAND, 5);
+
+    m_panel6->SetSizer(bSizer17);
+    m_panel6->Layout();
+    bSizer17->Fit(m_panel6);
+    bSizer15->Add(m_panel6, 1, wxALL | wxEXPAND, 5);
+
+    m_staticline9 = new wxStaticLine(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxLI_HORIZONTAL);
+    bSizer15->Add(m_staticline9, 0, wxEXPAND | wxALL, 5);
+
+    wxBoxSizer* bSizer16;
+    bSizer16 = new wxBoxSizer(wxHORIZONTAL);
+
+    m_buttonClose = new wxButton(this, wxID_OK, _("Close"), wxDefaultPosition, wxDefaultSize, 0);
+    bSizer16->Add(m_buttonClose, 0, wxALL, 5);
+
+    bSizer15->Add(bSizer16, 0, wxALIGN_CENTER, 5);
+
+    this->SetSizer(bSizer15);
+    GetSizer()->Fit(this);
+    Layout();
+
+    ConnectListBoxDClick(m_configurationsList, EditConfigurationDialog::OnItemDclick);
+    ConnectButton(m_buttonClose, EditConfigurationDialog::OnButtonClose);
+    ConnectButton(m_buttonRename, EditConfigurationDialog::OnButtonRename);
+    ConnectButton(m_buttonDelete, EditConfigurationDialog::OnButtonDelete);
 }
 
-void EditConfigurationDialog::RenameConfiguration(const wxString &oldName, const wxString &newName)
+void EditConfigurationDialog::RenameConfiguration(const wxString& oldName, const wxString& newName)
 {
-	ProjectSettingsPtr settings = ManagerST::Get()->GetProjectSettings(m_projectName);
-	if(settings){
-		BuildConfigPtr bldConf = settings->GetBuildConfiguration(oldName);
-		if(bldConf){
-			settings->RemoveConfiguration(oldName);
-			bldConf->SetName(newName);
-			settings->SetBuildConfiguration(bldConf);
-			//save changes
-			ManagerST::Get()->SetProjectSettings(m_projectName, settings);
+    ProjectSettingsPtr settings = ManagerST::Get()->GetProjectSettings(m_projectName);
+    if(settings) {
+        BuildConfigPtr bldConf = settings->GetBuildConfiguration(oldName);
+        if(bldConf) {
+            settings->RemoveConfiguration(oldName);
+            bldConf->SetName(newName);
+            settings->SetBuildConfiguration(bldConf);
+            // save changes
+            ManagerST::Get()->SetProjectSettings(m_projectName, settings);
 
-			//update the control
-			m_configurationsList->Clear();
-			ProjectSettingsCookie cookie;
-			BuildConfigPtr bldConf = settings->GetFirstBuildConfiguration(cookie);
-			while(bldConf){
-				m_configurationsList->Append(bldConf->GetName());
-				bldConf = settings->GetNextBuildConfiguration(cookie);
-			}
-			if(m_configurationsList->GetCount()>0)
-				m_configurationsList->SetSelection(0);
-		}
-	}
+            // update the control
+            m_configurationsList->Clear();
+            ProjectSettingsCookie cookie;
+            BuildConfigPtr bldConf = settings->GetFirstBuildConfiguration(cookie);
+            while(bldConf) {
+                m_configurationsList->Append(bldConf->GetName());
+                bldConf = settings->GetNextBuildConfiguration(cookie);
+            }
+            if(m_configurationsList->GetCount() > 0) m_configurationsList->SetSelection(0);
+        }
+    }
 }
 
-void EditConfigurationDialog::OnItemDclick(wxCommandEvent &event)
+void EditConfigurationDialog::OnItemDclick(wxCommandEvent& event)
 {
-	wxString oldName = event.GetString();
-	wxTextEntryDialog *dlg = new wxTextEntryDialog(this, _("Enter New Name:"), _("Rename"), oldName);
-	dlg->SetTextValidator(wxFILTER_ASCII);
+    wxString oldName = event.GetString();
+    wxTextEntryDialog* dlg = new wxTextEntryDialog(this, _("Enter New Name:"), _("Rename"), oldName);
+    dlg->SetTextValidator(wxFILTER_ASCII);
 
-	if(dlg->ShowModal() == wxID_OK){
-		wxString newName = dlg->GetValue();
-		RenameConfiguration(oldName, newName);
-	}
+    if(dlg->ShowModal() == wxID_OK) {
+        wxString newName = dlg->GetValue();
+        RenameConfiguration(oldName, newName);
+    }
 }
 
-void EditConfigurationDialog::OnButtonClose(wxCommandEvent &event)
+void EditConfigurationDialog::OnButtonClose(wxCommandEvent& event)
 {
-	wxUnusedVar(event);
-	EndModal(wxID_OK);
+    wxUnusedVar(event);
+    EndModal(wxID_OK);
 }
 
-void EditConfigurationDialog::OnButtonRename(wxCommandEvent &event)
+void EditConfigurationDialog::OnButtonRename(wxCommandEvent& event)
 {
-	wxUnusedVar(event);
-	wxString oldName = m_configurationsList->GetStringSelection();
-	if(oldName.IsEmpty()){
-		return;
-	}
-	wxTextEntryDialog *dlg = new wxTextEntryDialog(this, _("Enter New Name:"), _("Rename"), oldName);
-	dlg->SetTextValidator(wxFILTER_ASCII);
+    wxUnusedVar(event);
+    wxString oldName = m_configurationsList->GetStringSelection();
+    if(oldName.IsEmpty()) {
+        return;
+    }
+    wxTextEntryDialog* dlg = new wxTextEntryDialog(this, _("Enter New Name:"), _("Rename"), oldName);
+    dlg->SetTextValidator(wxFILTER_ASCII);
 
-	if(dlg->ShowModal() == wxID_OK){
-		wxString newName = dlg->GetValue();
-		RenameConfiguration(oldName, newName);
-	}
+    if(dlg->ShowModal() == wxID_OK) {
+        wxString newName = dlg->GetValue();
+        RenameConfiguration(oldName, newName);
+    }
 }
 
-void EditConfigurationDialog::OnButtonDelete(wxCommandEvent &event)
+void EditConfigurationDialog::OnButtonDelete(wxCommandEvent& event)
 {
-	wxUnusedVar(event);
-	wxString selection = m_configurationsList->GetStringSelection() ;
-	if(selection.IsEmpty()){
-		return;
-	}
-	wxString msg(_("Remove configuration '"));
-	msg << selection << _("' ?");
-	if(wxMessageBox(msg, _("Confirm"), wxYES_NO | wxCANCEL | wxICON_QUESTION) == wxYES){
-		ProjectSettingsPtr settings = ManagerST::Get()->GetProjectSettings(m_projectName);
-		if(settings){
-			settings->RemoveConfiguration(selection);
-			m_configurationsList->Delete(m_configurationsList->GetSelection());
-			if(m_configurationsList->GetCount()>0)
-				m_configurationsList->SetSelection(0);
-			
-			//save changes
-			ManagerST::Get()->SetProjectSettings(m_projectName, settings);
-		}
-	}
+    wxUnusedVar(event);
+    wxString selection = m_configurationsList->GetStringSelection();
+    if(selection.IsEmpty()) {
+        return;
+    }
+    wxString msg(_("Remove configuration '"));
+    msg << selection << _("' ?");
+    if(wxMessageBox(msg, _("Confirm"), wxYES_NO | wxCANCEL | wxICON_QUESTION) == wxYES) {
+        ProjectSettingsPtr settings = ManagerST::Get()->GetProjectSettings(m_projectName);
+        if(settings) {
+            settings->RemoveConfiguration(selection);
+            m_configurationsList->Delete(m_configurationsList->GetSelection());
+            if(m_configurationsList->GetCount() > 0) m_configurationsList->SetSelection(0);
+
+            // save changes
+            ManagerST::Get()->SetProjectSettings(m_projectName, settings);
+        }
+    }
 }
--- LiteEditor/edit_configuration.h
+++ LiteEditor/edit_configuration.h
@@ -1,28 +1,28 @@
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 //
-// copyright            : (C) 2008 by Eran Ifrah                            
-// file name            : edit_configuration.h              
-//                                                                          
+// copyright            : (C) 2008 by Eran Ifrah
+// file name            : edit_configuration.h
+//
 // -------------------------------------------------------------------------
-// A                                                                        
-//              _____           _      _     _ _                            
-//             /  __ \         | |    | |   (_) |                           
-//             | /  \/ ___   __| | ___| |    _| |_ ___                      
-//             | |    / _ \ / _  |/ _ \ |   | | __/ _ )                     
-//             | \__/\ (_) | (_| |  __/ |___| | ||  __/                     
-//              \____/\___/ \__,_|\___\_____/_|\__\___|                     
-//                                                                          
-//                                                  F i l e                 
-//                                                                          
-//    This program is free software; you can redistribute it and/or modify  
-//    it under the terms of the GNU General Public License as published by  
-//    the Free Software Foundation; either version 2 of the License, or     
-//    (at your option) any later version.                                   
-//                                                                          
+// A
+//              _____           _      _     _ _
+//             /  __ \         | |    | |   (_) |
+//             | /  \/ ___   __| | ___| |    _| |_ ___
+//             | |    / _ \ / _  |/ _ \ |   | | __/ _ )
+//             | \__/\ (_) | (_| |  __/ |___| | ||  __/
+//              \____/\___/ \__,_|\___\_____/_|\__\___|
+//
+//                                                  F i l e
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
- ///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
 // C++ code generated with wxFormBuilder (version May  5 2007)
 // http://www.wxformbuilder.org/
 //
@@ -40,32 +40,32 @@
 
 ///////////////////////////////////////////////////////////////////////////
 
-
 ///////////////////////////////////////////////////////////////////////////////
 /// Class EditConfigurationDialog
 ///////////////////////////////////////////////////////////////////////////////
-class EditConfigurationDialog : public wxDialog 
+class EditConfigurationDialog : public wxDialog
 {
 private:
-
 protected:
-	wxPanel* m_panel6;
-	wxListBox* m_configurationsList;
-	wxButton* m_buttonDelete;
-	wxButton* m_buttonRename;
-	wxStaticLine* m_staticline9;
-	wxButton* m_buttonClose;
-	wxString m_projectName;
+    wxPanel* m_panel6;
+    wxListBox* m_configurationsList;
+    wxButton* m_buttonDelete;
+    wxButton* m_buttonRename;
+    wxStaticLine* m_staticline9;
+    wxButton* m_buttonClose;
+    wxString m_projectName;
 
-	void OnItemDclick(wxCommandEvent &event);
-	void OnButtonClose(wxCommandEvent &event);
-	void OnButtonRename(wxCommandEvent &event);
-	void OnButtonDelete(wxCommandEvent &event);
+    void OnItemDclick(wxCommandEvent& event);
+    void OnButtonClose(wxCommandEvent& event);
+    void OnButtonRename(wxCommandEvent& event);
+    void OnButtonDelete(wxCommandEvent& event);
 
-	void RenameConfiguration(const wxString &oldName, const wxString &newName);
+    void RenameConfiguration(const wxString& oldName, const wxString& newName);
 
 public:
-	EditConfigurationDialog( wxWindow* parent, const wxString &projectName, int id = wxID_ANY, wxString title = _("Edit Configurations"), wxPoint pos = wxDefaultPosition, wxSize size = wxSize( 338,199 ), int style = wxDEFAULT_DIALOG_STYLE );
+    EditConfigurationDialog(wxWindow* parent, const wxString& projectName, int id = wxID_ANY,
+        wxString title = _("Edit Configurations"), wxPoint pos = wxDefaultPosition, wxSize size = wxSize(338, 199),
+        int style = wxDEFAULT_DIALOG_STYLE);
 };
 
 #endif //__edit_configuration__

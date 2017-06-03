019b09416b656261f0af9523f6cae1a3e9da6291
Eran Ifrah <eran.ifrah@gmail.com>
2017-06-03 10:12:21 +0300

    CodeFormatter: handle wxEVT_PHP_SETTINGS_CHANGED event to reload the PHP Settings

--- CodeFormatter/codeformatter.h
+++ CodeFormatter/codeformatter.h
@@ -42,15 +42,12 @@ protected:
 
 private:
     bool IsPhpConfigValid(const FormatOptions& options);
-    bool DoClangFormat(const wxFileName& filename,
-                       wxString& formattedOutput,
-                       int& cursorPosition,
-                       int startOffset,
-                       int length,
-                       const FormatOptions& options,
-                       const wxFileName& originalFileName);
+    bool DoClangFormat(const wxFileName& filename, wxString& formattedOutput, int& cursorPosition, int startOffset,
+        int length, const FormatOptions& options, const wxFileName& originalFileName);
 
     void DoFormatXmlSource(IEditor* editor);
+
+    void OnPhpSettingsChanged(clCommandEvent& event);
 
 public:
     /**
@@ -64,20 +61,13 @@ public:
      * @param startOffset start of chunk to format
      * @param length chunk length
      */
-    bool ClangFormatFile(const wxFileName& filename,
-                         wxString& formattedOutput,
-                         int& cursorPosition,
-                         int startOffset = wxNOT_FOUND,
-                         int length = wxNOT_FOUND);
+    bool ClangFormatFile(const wxFileName& filename, wxString& formattedOutput, int& cursorPosition,
+        int startOffset = wxNOT_FOUND, int length = wxNOT_FOUND);
     /**
      * @brief same as the above, but work on a buffer instead
      */
-    bool ClangFormatBuffer(const wxString& content,
-                           const wxFileName& filename,
-                           wxString& formattedOutput,
-                           int& cursorPosition,
-                           int startOffset = wxNOT_FOUND,
-                           int length = wxNOT_FOUND);
+    bool ClangFormatBuffer(const wxString& content, const wxFileName& filename, wxString& formattedOutput,
+        int& cursorPosition, int startOffset = wxNOT_FOUND, int length = wxNOT_FOUND);
 
     /**
      * @brief same as the above, but work on a buffer instead

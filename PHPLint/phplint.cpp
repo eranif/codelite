eranif/codelite

commit 8931e33581eb769f5abedaa7463cac4981890ec5

Refactor php linters in to a single plugin
This resolves the issue of clearing markeres in the right order, and
just feels right :)

LiteEditor.workspace
@@ -35,6 +35,7 @@
     <Project Name="Outline" Path="Outline/Outline.project" Active="No"/>
     <Project Name="PHPParser" Path="codelitephp/PHPParser/PHPParser.project" Active="No"/>
     <Project Name="PHPPlugin" Path="codelitephp/php-plugin/PHPPlugin.project" Active="No"/>
+    <Project Name="PHPLint" Path="PHPLint/PHPLint.project" Active="No"/>
     <Project Name="QMakePlugin" Path="QmakePlugin/QMakePlugin.project" Active="No"/>
     <Project Name="SFTP" Path="SFTP/SFTP.project" Active="No"/>
     <Project Name="snipwiz" Path="SnipWiz/snipwiz.project" Active="No"/>
@@ -49,8 +50,6 @@
     <Project Name="ZoomNavigator" Path="ZoomNavigator/ZoomNavigator.project" Active="No"/>
     <Project Name="EditorConfigPlugin" Path="EditorConfigPlugin/EditorConfigPlugin.project" Active="No"/>
     <Project Name="codelite_vim" Path="codelite_vim/codelite_vim.project" Active="No"/>
-    <Project Name="phpcs" Path="phpcs/phpcs.project" Active="Yes"/>
-    <Project Name="phpcs" Path="phpmd/phpmd.project" Active="Yes"/>
   </VirtualDirectory>
   <VirtualDirectory Name="Core">
     <Project Name="CodeLiteIDE" Path="LiteEditor/LiteEditor.project" Active="Yes"/>
@@ -104,6 +103,7 @@
       <Project Name="PHPParser" ConfigName="Release"/>
       <Project Name="PHPParserUnitTests" ConfigName="Release"/>
       <Project Name="PHPPlugin" ConfigName="Win_x86_Release"/>
+      <Project Name="PHPLint" ConfigName="Win_x64_Debug"/>
       <Project Name="plugin_sdk" ConfigName="Win_x86_Release"/>
       <Project Name="QMakePlugin" ConfigName="Win_x86_Release"/>
       <Project Name="SFTP" ConfigName="Win_x86_Release"/>
@@ -120,8 +120,6 @@
       <Project Name="wxshapeframework" ConfigName="Win_x86_Release"/>
       <Project Name="wxsqlite3" ConfigName="Win_x86_Release"/>
       <Project Name="ZoomNavigator" ConfigName="Win_x86_Release"/>
-      <Project Name="phpcs" ConfigName="Win_x64_Debug"/>
-      <Project Name="phpmd" ConfigName="Win_x64_Debug"/>
       <Project Name="codelite_vim" ConfigName="Win_x64_Debug"/>
     </WorkspaceConfiguration>
     <WorkspaceConfiguration Name="CMake_Release" Selected="no">
@@ -143,6 +141,7 @@
       <Project Name="PHPPlugin" ConfigName="Debug_Unix"/>
       <Project Name="PHPParserUnitTests" ConfigName="Debug"/>
       <Project Name="PHPParser" ConfigName="Debug"/>
+      <Project Name="PHPLint" ConfigName="Win_x64_Debug"/>
       <Project Name="PCH" ConfigName="Release"/>
       <Project Name="Outline" ConfigName="WinDebugUnicode"/>
       <Project Name="MemCheck" ConfigName="DebugUnicode"/>
@@ -174,8 +173,6 @@
       <Project Name="AutoSave" ConfigName="Win_x64_Debug"/>
       <Project Name="Tail" ConfigName="Win_x64_Debug"/>
       <Project Name="EditorConfigPlugin" ConfigName="Win_x64_Debug"/>
-      <Project Name="phpcs" ConfigName="Win_x64_Debug"/>
-      <Project Name="phpmd" ConfigName="Win_x64_Debug"/>
       <Project Name="codelite_vim" ConfigName="Win_x64_Debug"/>
     </WorkspaceConfiguration>
     <WorkspaceConfiguration Name="CMake_Debug" Selected="yes">
@@ -198,6 +195,7 @@
       <Project Name="PHPPlugin" ConfigName="Debug_Unix"/>
       <Project Name="PHPParserUnitTests" ConfigName="Debug_Unix"/>
       <Project Name="PHPParser" ConfigName="Debug_Unix"/>
+      <Project Name="PHPLint" ConfigName="Win_x64_Debug"/>
       <Project Name="PCH" ConfigName="Release"/>
       <Project Name="Outline" ConfigName="Win_x86_Release"/>
       <Project Name="MemCheck" ConfigName="DebugUnicode"/>
@@ -228,8 +226,6 @@
       <Project Name="AutoSave" ConfigName="Win_x64_Debug"/>
       <Project Name="Tail" ConfigName="Win_x64_Debug"/>
       <Project Name="EditorConfigPlugin" ConfigName="Win_x64_Debug"/>
-      <Project Name="phpcs" ConfigName="Win_x64_Debug"/>
-      <Project Name="phpmd" ConfigName="Win_x64_Debug"/>
       <Project Name="codelite_vim" ConfigName="Win_x64_Debug"/>
     </WorkspaceConfiguration>
     <WorkspaceConfiguration Name="OSX_Debug" Selected="no">
@@ -274,6 +270,7 @@
       <Project Name="PHPParser" ConfigName="Debug"/>
       <Project Name="PHPParserUnitTests" ConfigName="Debug"/>
       <Project Name="PHPPlugin" ConfigName="Debug_Unix"/>
+      <Project Name="PHPLint" ConfigName="Win_x64_Debug"/>
       <Project Name="WordCompletion" ConfigName="DebugUnicode"/>
       <Project Name="WebTools" ConfigName="DebugUnicode"/>
       <Project Name="HelpPlugin" ConfigName="Win_x64_Debug"/>
@@ -282,8 +279,6 @@
       <Project Name="AutoSave" ConfigName="Win_x64_Debug"/>
       <Project Name="Tail" ConfigName="Win_x64_Debug"/>
       <Project Name="EditorConfigPlugin" ConfigName="Win_x64_Debug"/>
-      <Project Name="phpcs" ConfigName="Win_x64_Debug"/>
-      <Project Name="phpmd" ConfigName="Win_x64_Debug"/>
       <Project Name="codelite_vim" ConfigName="Win_x64_Debug"/>
     </WorkspaceConfiguration>
     <WorkspaceConfiguration Name="OSX_Release" Selected="no">
@@ -328,6 +323,7 @@
       <Project Name="PHPParser" ConfigName="Debug"/>
       <Project Name="PHPParserUnitTests" ConfigName="Debug"/>
       <Project Name="PHPPlugin" ConfigName="Debug_Unix"/>
+      <Project Name="PHPLint" ConfigName="Win_x64_Debug"/>
       <Project Name="WordCompletion" ConfigName="DebugUnicode"/>
       <Project Name="WebTools" ConfigName="DebugUnicode"/>
       <Project Name="HelpPlugin" ConfigName="Win_x64_Debug"/>
@@ -336,8 +332,6 @@
       <Project Name="AutoSave" ConfigName="Win_x64_Debug"/>
       <Project Name="Tail" ConfigName="Win_x64_Debug"/>
       <Project Name="EditorConfigPlugin" ConfigName="Win_x64_Debug"/>
-      <Project Name="phpcs" ConfigName="Win_x64_Debug"/>
-      <Project Name="phpmd" ConfigName="Win_x64_Debug"/>
       <Project Name="codelite_vim" ConfigName="Win_x64_Debug"/>
     </WorkspaceConfiguration>
     <WorkspaceConfiguration Name="Win_x64_Release" Selected="no">
@@ -374,6 +368,7 @@ WXCFG=gcc_dll\mswu</Environment>
       <Project Name="PHPParser" ConfigName="Win_x64_Release"/>
       <Project Name="PHPParserUnitTests" ConfigName="Win_x64_Release"/>
       <Project Name="PHPPlugin" ConfigName="Win_x64_Release"/>
+      <Project Name="PHPLint" ConfigName="Win_x64_Release"/>
       <Project Name="plugin_sdk" ConfigName="Win_x64_Release"/>
       <Project Name="QMakePlugin" ConfigName="Win_x64_Release"/>
       <Project Name="SFTP" ConfigName="Win_x64_Release"/>
@@ -391,8 +386,6 @@ WXCFG=gcc_dll\mswu</Environment>
       <Project Name="wxsqlite3" ConfigName="Win_x64_Release"/>
       <Project Name="ZoomNavigator" ConfigName="Win_x64_Release"/>
       <Project Name="EditorConfigPlugin" ConfigName="Win_x64_Release"/>
-      <Project Name="phpcs" ConfigName="Win_x64_Release"/>
-      <Project Name="phpmd" ConfigName="Win_x64_Release"/>
       <Project Name="codelite_vim" ConfigName="Win_x64_Release"/>
     </WorkspaceConfiguration>
     <WorkspaceConfiguration Name="Win_x64_Debug" Selected="no">
@@ -428,6 +421,7 @@ WXCFG=gcc_dll\mswu</Environment>
       <Project Name="PHPParser" ConfigName="Win_x64_Debug"/>
       <Project Name="PHPParserUnitTests" ConfigName="Win_x64_Debug"/>
       <Project Name="PHPPlugin" ConfigName="Win_x64_Debug"/>
+      <Project Name="PHPLint" ConfigName="Win_x64_Debug"/>
       <Project Name="plugin_sdk" ConfigName="Win_x64_Debug"/>
       <Project Name="QMakePlugin" ConfigName="Win_x64_Debug"/>
       <Project Name="SFTP" ConfigName="Win_x64_Debug"/>
@@ -445,8 +439,6 @@ WXCFG=gcc_dll\mswu</Environment>
       <Project Name="wxsqlite3" ConfigName="Win_x64_Debug"/>
       <Project Name="ZoomNavigator" ConfigName="Win_x64_Debug"/>
       <Project Name="EditorConfigPlugin" ConfigName="Win_x64_Debug"/>
-      <Project Name="phpcs" ConfigName="Win_x64_Debug"/>
-      <Project Name="phpmd" ConfigName="Win_x64_Debug"/>
       <Project Name="codelite_vim" ConfigName="Win_x64_Debug"/>
     </WorkspaceConfiguration>
   </BuildMatrix>

PHPLint/phplint.cpp
@@ -1,0 +1,274 @@
+#include "asyncprocess.h"
+#include "file_logger.h"
+#include "globals.h"
+#include "phplint.h"
+#include "processreaderthread.h"
+#include <event_notifier.h>
+#include <wx/sstream.h>
+#include <wx/xrc/xmlres.h>
+#include "wx/menu.h"
+#include "phplintdlg.h"
+
+static PHPLint* thePlugin = NULL;
+
+// Define the plugin entry point
+CL_PLUGIN_API IPlugin* CreatePlugin(IManager* manager)
+{
+    if(thePlugin == NULL) {
+        thePlugin = new PHPLint(manager);
+    }
+    return thePlugin;
+}
+
+CL_PLUGIN_API PluginInfo* GetPluginInfo()
+{
+    static PluginInfo info;
+    info.SetAuthor(wxT("Anders Jenbo"));
+    info.SetName(wxT("PHPLint"));
+    info.SetDescription(_("Run code style checking on PHP source files"));
+    info.SetVersion(wxT("v1.0"));
+    return &info;
+}
+
+CL_PLUGIN_API int GetPluginInterfaceVersion()
+{
+    return PLUGIN_INTERFACE_VERSION;
+}
+
+PHPLint::PHPLint(IManager* manager)
+    : IPlugin(manager)
+{
+    m_longName = _("Run code style checking on PHP source files");
+    m_shortName = wxT("PHPLint");
+
+    Bind(wxEVT_ASYNC_PROCESS_OUTPUT, &PHPLint::OnProcessOutput, this);
+    Bind(wxEVT_ASYNC_PROCESS_TERMINATED, &PHPLint::OnProcessTerminated, this);
+}
+
+PHPLint::~PHPLint()
+{
+}
+
+clToolBar* PHPLint::CreateToolBar(wxWindow* parent)
+{
+    // Create the toolbar to be used by the plugin
+    clToolBar* tb(NULL);
+
+    // Connect the events to us
+    EventNotifier::Get()->Connect(wxEVT_FILE_SAVED, clCommandEventHandler(PHPLint::OnFileAction), NULL, this);
+    EventNotifier::Get()->Connect(wxEVT_FILE_LOADED, clCommandEventHandler(PHPLint::OnFileAction), NULL, this);
+
+    return tb;
+}
+
+void PHPLint::CreatePluginMenu(wxMenu* pluginsMenu)
+{
+    wxMenu* menu = new wxMenu();
+    wxMenuItem* item(NULL);
+    item = new wxMenuItem(
+        menu, 2005, _("Lint Current Source"), _("Lint Current Source"), wxITEM_NORMAL);
+    menu->Append(item);
+    menu->AppendSeparator();
+    item = new wxMenuItem(menu, 2006, _("Options..."), wxEmptyString, wxITEM_NORMAL);
+    menu->Append(item);
+    pluginsMenu->Append(wxID_ANY, _("PHP Linter"), menu);
+
+
+    menu->Connect(2005, wxEVT_COMMAND_MENU_SELECTED,
+                  wxCommandEventHandler(PHPLint::OnMenuRunLint), NULL, (wxEvtHandler*)this);
+    menu->Connect(2006, wxEVT_COMMAND_MENU_SELECTED,
+                  wxCommandEventHandler(PHPLint::OnMenuCommand), NULL, (wxEvtHandler*)this);
+}
+
+void PHPLint::OnMenuCommand(wxCommandEvent& e)
+{
+    wxUnusedVar(e);
+
+    PHPLintDlg dlg(m_mgr->GetTheApp()->GetTopWindow());
+    dlg.ShowModal();
+}
+
+void PHPLint::OnMenuRunLint(wxCommandEvent& e)
+{
+    wxUnusedVar(e);
+
+    RunLint();
+}
+
+void PHPLint::UnPlug()
+{
+    m_mgr->GetTheApp()->Disconnect(2005, wxEVT_COMMAND_MENU_SELECTED,
+                                   wxCommandEventHandler(PHPLint::OnMenuRunLint), NULL, (wxEvtHandler*)this);
+    m_mgr->GetTheApp()->Disconnect(2006, wxEVT_COMMAND_MENU_SELECTED,
+                                   wxCommandEventHandler(PHPLint::OnMenuCommand), NULL, (wxEvtHandler*)this);
+
+    EventNotifier::Get()->Disconnect(wxEVT_FILE_SAVED, clCommandEventHandler(PHPLint::OnFileAction), NULL, this);
+    EventNotifier::Get()->Disconnect(wxEVT_FILE_LOADED, clCommandEventHandler(PHPLint::OnFileAction), NULL, this);
+}
+
+void PHPLint::OnFileAction(clCommandEvent& e)
+{
+    e.Skip();
+
+    RunLint();
+
+}
+
+void PHPLint::RunLint()
+{
+    IEditor* editor = m_mgr->GetActiveEditor();
+    CHECK_PTR_RET(editor);
+
+    if(FileExtManager::IsPHPFile(editor->GetFileName())) {
+        if(m_mgr->GetActiveEditor()) {
+            m_mgr->GetActiveEditor()->DelAllCompilerMarkers();
+        }
+        PHPLint::DoCheckFile(editor->GetFileName());
+    }
+}
+
+void PHPLint::DoCheckFile(const wxFileName& filename)
+{
+    m_output.Clear();
+
+    // Build the commands
+    wxString command;
+
+    wxString file = filename.GetFullPath();
+    ::WrapWithQuotes(file);
+
+    command = "/usr/bin/phpmd ";
+    command << file << " xml /home/ajenbo/code/ArmsGallery/phpmd.xml";
+    DispatchCommand(command, filename);
+
+    command = "/usr/bin/phpcs --report=xml ";
+    command << file;
+    DispatchCommand(command, filename);
+
+    command = "/usr/bin/php -l ";
+    command << file;
+    DispatchCommand(command, filename);
+}
+
+void PHPLint::DispatchCommand(const wxString& command, const wxFileName& filename)
+{
+    // Run the check command
+    m_process = ::CreateAsyncProcess(this, command);
+    if(!m_process) {
+        // failed to run the command
+        CL_WARNING("PHPLint: could not run command '%s'", command);
+        DoProcessQueue();
+        m_currentFileBeingProcessed.Clear();
+
+    } else {
+        CL_DEBUG("PHPLint: running check: %s", command);
+        m_currentFileBeingProcessed = filename.GetFullPath();
+    }
+}
+
+void PHPLint::DoProcessQueue()
+{
+    if(!m_process && !m_queue.empty()) {
+        wxFileName filename = m_queue.front();
+        m_queue.pop_front();
+        DoCheckFile(filename);
+    }
+}
+
+void PHPLint::OnProcessTerminated(clProcessEvent& event)
+{
+    CL_DEBUG("PHPLint: process terminated. output: %s", m_output);
+    wxDELETE(m_process);
+    CallAfter(&PHPLint::OnLintingDone, m_output, m_currentFileBeingProcessed);
+    m_output = "";
+    // Check the queue for more files
+    DoProcessQueue();
+}
+
+void PHPLint::OnProcessOutput(clProcessEvent& event)
+{
+    m_output << event.GetOutput();
+}
+
+void PHPLint::OnLintingDone(const wxString& lintOutput, const wxString& filename)
+{
+    // Find the editor
+    CL_DEBUG("PHPLint: searching editor for file: %s", filename);
+
+    IEditor* editor = m_mgr->FindEditor(filename);
+    CHECK_PTR_RET(editor);
+
+    if (lintOutput.Contains("PHP Parse error:") && lintOutput.Contains(" in ")) {
+        ProcessPhpError(lintOutput, editor);
+        return;
+    }
+
+    ProcessXML(lintOutput, editor);
+}
+
+void PHPLint::ProcessPhpError(const wxString& lintOutput, IEditor*& editor)
+{
+    wxRegEx reLine("[ \t]*on line ([0-9]+)");
+    // get the line number
+    if(reLine.Matches(lintOutput)) {
+        wxString strLine = reLine.GetMatch(lintOutput, 1);
+
+        int start = lintOutput.Find("PHP Parse error:");
+        int end = lintOutput.Find(" in ");
+        wxString errorMessage = lintOutput.Mid(start, end - start);
+
+        MarkError(errorMessage, strLine, editor);
+    }
+}
+
+void PHPLint::ProcessXML(const wxString& lintOutput, IEditor*& editor)
+{
+    wxStringInputStream lintOutputStream(lintOutput);
+    wxXmlDocument doc;
+    if(!doc.Load(lintOutputStream))
+        return;
+
+    wxXmlNode* file = doc.GetRoot()->GetChildren();
+    if(!file)
+        return;
+
+    wxString linter = doc.GetRoot()->GetName();
+
+    wxXmlNode* violation = file->GetChildren();
+    while(violation) {
+        wxString errorMessage = violation->GetNodeContent();
+        wxString strLine = violation->GetAttribute(linter == "pmd" ? "beginline" : "line");
+        bool isWarning = IsWarning(violation, linter);
+        MarkError(errorMessage, strLine, editor, isWarning);
+
+        violation = violation->GetNext();
+    }
+}
+
+bool PHPLint::IsWarning(wxXmlNode* violation, const wxString& linter)
+{
+    if (linter == "pmd") {
+        wxString priority = violation->GetAttribute("priority", "1");
+        long nPriority(wxNOT_FOUND);
+        return priority.ToCLong(&nPriority) > 2;
+    }
+
+    return violation->GetName() == "warning";
+}
+
+void PHPLint::MarkError(wxString& errorMessage, const wxString& strLine, IEditor*& editor, bool isWarning)
+{
+    errorMessage = errorMessage.Trim().Trim(false);
+
+    long nLine(wxNOT_FOUND);
+    if(strLine.ToCLong(&nLine)) {
+        CL_DEBUG("PHPLint: adding error marker @%d", (int)nLine - 1);
+
+        if(isWarning) {
+            editor->SetWarningMarker(nLine - 1, errorMessage);
+            return;
+        }
+
+        editor->SetErrorMarker(nLine - 1, errorMessage);
+    }
+}

codelitephp/php-plugin/php.cpp
@@ -63,7 +63,10 @@ CL_PLUGIN_API PluginInfo* GetPluginInfo()
     return &info;
 }
 
-CL_PLUGIN_API int GetPluginInterfaceVersion() { return PLUGIN_INTERFACE_VERSION; }
+CL_PLUGIN_API int GetPluginInterfaceVersion()
+{
+    return PLUGIN_INTERFACE_VERSION;
+}
 
 PhpPlugin::PhpPlugin(IManager* manager)
     : IPlugin(manager)
@@ -75,8 +78,6 @@ PhpPlugin::PhpPlugin(IManager* manager)
     , m_showWelcomePage(false)
     , m_toggleToolbar(false)
 {
-    m_lint.Reset(new PHPLint(this));
-
     // Add new workspace type
     clWorkspaceManager::Get().RegisterWorkspace(new PHPWorkspace());
 
@@ -134,8 +135,6 @@ PhpPlugin::PhpPlugin(IManager* manager)
     EventNotifier::Get()->Connect(
         wxEVT_CMD_FIND_IN_FILES_DISMISSED, clCommandEventHandler(PhpPlugin::OnFindInFilesDismissed), NULL, this);
 
-    EventNotifier::Get()->Connect(wxEVT_FILE_SAVED, clCommandEventHandler(PhpPlugin::OnFileAction), NULL, this);
-    EventNotifier::Get()->Connect(wxEVT_FILE_LOADED, clCommandEventHandler(PhpPlugin::OnFileAction), NULL, this);
     EventNotifier::Get()->Bind(wxEVT_FILES_MODIFIED_REPLACE_IN_FILES, &PhpPlugin::OnReplaceInFiles, this);
     EventNotifier::Get()->Connect(wxEVT_PHP_LOAD_URL, PHPEventHandler(PhpPlugin::OnLoadURL), NULL, this);
     EventNotifier::Get()->Connect(
@@ -162,7 +161,7 @@ PhpPlugin::PhpPlugin(IManager* manager)
         fnSampleFile.AppendDir("cc");
         PHPConfigurationData config;
         if(!fnSampleFile.Exists() || // the sample file does not exists
-                                     // Or the resource file (PHP.zip) is newer than the sample file
+           // Or the resource file (PHP.zip) is newer than the sample file
            (phpResources.GetModificationTime().GetTicks() > fnSampleFile.GetModificationTime().GetTicks())) {
 
             targetDir.Mkdir(wxS_DIR_DEFAULT, wxPATH_MKDIR_FULL);
@@ -256,8 +255,6 @@ void PhpPlugin::UnPlug()
                                      this);
     EventNotifier::Get()->Disconnect(
         wxEVT_CMD_GET_ACTIVE_PROJECT_FILES, wxCommandEventHandler(PhpPlugin::OnGetActiveProjectFiles), NULL, this);
-    EventNotifier::Get()->Disconnect(wxEVT_FILE_SAVED, clCommandEventHandler(PhpPlugin::OnFileAction), NULL, this);
-    EventNotifier::Get()->Disconnect(wxEVT_FILE_LOADED, clCommandEventHandler(PhpPlugin::OnFileAction), NULL, this);
     EventNotifier::Get()->Unbind(wxEVT_FILES_MODIFIED_REPLACE_IN_FILES, &PhpPlugin::OnReplaceInFiles, this);
     EventNotifier::Get()->Disconnect(wxEVT_PHP_LOAD_URL, PHPEventHandler(PhpPlugin::OnLoadURL), NULL, this);
     EventNotifier::Get()->Disconnect(
@@ -557,32 +554,11 @@ void PhpPlugin::OnMenuCommand(wxCommandEvent& e)
     case wxID_PHP_SETTINGS: {
         PHPSettingsDlg dlg(FRAME);
         dlg.ShowModal();
-    } break;
+    }
+    break;
     default:
         e.Skip();
         break;
-    }
-}
-
-void PhpPlugin::OnFileAction(clCommandEvent& e)
-{
-    e.Skip();
-
-    if(PHPWorkspace::Get()->IsOpen()) {
-        DoSyncFileWithRemote(e.GetString());
-    }
-
-    // Run php lint
-    IEditor* editor = m_mgr->GetActiveEditor();
-    CHECK_PTR_RET(editor);
-
-    PHPConfigurationData conf;
-    conf.Load();
-    if(FileExtManager::IsPHPFile(editor->GetFileName()) && conf.IsRunLint()) {
-        if(m_mgr->GetActiveEditor()) {
-            m_mgr->GetActiveEditor()->DelAllCompilerMarkers();
-        }
-        m_lint->CheckCode(e.GetFileName());
     }
 }
 
@@ -647,7 +623,10 @@ void PhpPlugin::OnXDebugDeleteAllBreakpoints(clDebugEvent& e)
     EventNotifier::Get()->AddPendingEvent(eventDelAllBP);
 }
 
-void PhpPlugin::OnXDebugShowBreakpointsWindow(wxCommandEvent& e) { DoEnsureXDebugPanesVisible(_("Breakpoints")); }
+void PhpPlugin::OnXDebugShowBreakpointsWindow(wxCommandEvent& e)
+{
+    DoEnsureXDebugPanesVisible(_("Breakpoints"));
+}
 
 void PhpPlugin::DoEnsureXDebugPanesVisible(const wxString& selectWindow)
 {
@@ -743,7 +722,10 @@ void PhpPlugin::OnAllEditorsClosed(wxCommandEvent& e)
     }
 }
 
-void PhpPlugin::SetEditorActive(IEditor* editor) { editor->SetActive(); }
+void PhpPlugin::SetEditorActive(IEditor* editor)
+{
+    editor->SetActive();
+}
 
 void PhpPlugin::RunXDebugDiagnostics()
 {
@@ -791,13 +773,13 @@ void PhpPlugin::FinalizeStartup()
     m_debuggerPane = new PHPDebugPane(parent);
     m_mgr->GetDockingManager()->AddPane(m_debuggerPane,
                                         wxAuiPaneInfo()
-                                            .Name("XDebug")
-                                            .Caption("Call Stack & Breakpoints")
-                                            .Hide()
-                                            .CloseButton()
-                                            .MaximizeButton()
-                                            .Bottom()
-                                            .Position(3));
+                                        .Name("XDebug")
+                                        .Caption("Call Stack & Breakpoints")
+                                        .Hide()
+                                        .CloseButton()
+                                        .MaximizeButton()
+                                        .Bottom()
+                                        .Position(3));
 
     m_xdebugLocalsView = new LocalsView(parent);
     m_mgr->GetDockingManager()->AddPane(
@@ -824,36 +806,9 @@ void PhpPlugin::FinalizeStartup()
     }
 }
 
-void PhpPlugin::OnGoingDown(clCommandEvent& event) { event.Skip(); }
-
-void PhpPlugin::PhpLintDone(const wxString& lintOutput, const wxString& filename)
+void PhpPlugin::OnGoingDown(clCommandEvent& event)
 {
-    // Find the editor
-    CL_DEBUG("PHPLint: searching editor for file: %s", filename);
-    IEditor* editor = m_mgr->FindEditor(filename);
-    CHECK_PTR_RET(editor);
-
-    wxRegEx reLine("[ \t]*on line ([0-9]+)");
-    wxArrayString lines = ::wxStringTokenize(lintOutput, "\n", wxTOKEN_STRTOK);
-    for(size_t i = 0; i < lines.GetCount(); ++i) {
-        wxString errorString = lines.Item(i);
-        errorString.Trim().Trim(false);
-        if(errorString.Contains("syntax error")) {
-            // get the line number
-            if(reLine.Matches(errorString)) {
-                wxString strLine = reLine.GetMatch(errorString, 1);
-                int where = errorString.Find(" in ");
-                if(where != wxNOT_FOUND) {
-                    errorString.Truncate(where);
-                }
-                long nLine(wxNOT_FOUND);
-                if(strLine.ToCLong(&nLine)) {
-                    CL_DEBUG("PHPLint: adding error marker @%d", (int)nLine - 1);
-                    editor->SetErrorMarker(nLine - 1, errorString);
-                }
-            }
-        }
-    }
+    event.Skip();
 }
 
 void PhpPlugin::OnFindInFilesDismissed(clCommandEvent& e)
@@ -894,7 +849,7 @@ void PhpPlugin::DoSyncFileWithRemote(const wxFileName& localFile)
         clDEBUG() << localFile << "is not a PHP workspace file, will not sync it with remote" << clEndl;
         return;
     }
-    
+
     SSHWorkspaceSettings settings;
     settings.Load();
 

codelitephp/php-plugin/php.h
@@ -33,7 +33,6 @@
 #include "php_event.h"
 #include <wx/sharedptr.h>
 #include "XDebugManager.h"
-#include "PHPLint.h"
 
 class EvalPane;
 class LocalsView;
@@ -55,7 +54,6 @@ protected:
     LocalsView* m_xdebugLocalsView;
     EvalPane* m_xdebugEvalPane;
     bool m_showWelcomePage;
-    PHPLint::Ptr_t m_lint;
     bool m_toggleToolbar;
 
 public:
@@ -73,9 +71,10 @@ public:
     void SafelyDetachAndDestroyPane(wxWindow* pane, const wxString& name);
     void EnsureAuiPaneIsVisible(const wxString& paneName, bool update = false);
     void FinalizeStartup();
-    void PhpLintDone(const wxString& lintOutput, const wxString& filename);
 
-    PHPDebugPane* GetDebuggerPane() { return m_debuggerPane; }
+    PHPDebugPane* GetDebuggerPane() {
+        return m_debuggerPane;
+    }
 
 protected:
     bool IsWorkspaceViewDetached();
@@ -95,7 +94,9 @@ public:
     virtual void UnPlug();
     void RunXDebugDiagnostics();
 
-    IManager* GetManager() { return m_mgr; }
+    IManager* GetManager() {
+        return m_mgr;
+    }
     // Event handlers
 
     void SetEditorActive(IEditor* editor);
@@ -122,7 +123,6 @@ public:
     void OnFindInFilesDismissed(clCommandEvent& e);
     void OnRunXDebugDiagnostics(wxCommandEvent& e);
     void OnMenuCommand(wxCommandEvent& e);
-    void OnFileAction(clCommandEvent& e);
     void OnXDebugShowBreakpointsWindow(wxCommandEvent& e);
     void OnXDebugDeleteAllBreakpoints(clDebugEvent& e);
     void OnXDebugSettings(wxCommandEvent& e);

commit 54d62fb59afda5e4999c86e48a32e85a9cb9be80

PHPLint: dont use message box to notify about lint-failure, use status message PHP plugin: fixed regression introduced in commit 8931e33: PHP auto-sync files with SFTP is no longer working

LiteEditor.workspace
@@ -178,7 +178,7 @@
       <Project Name="codelite_vim" ConfigName="Win_x64_Debug"/>
       <Project Name="codelite_vim" ConfigName="Win_x64_Debug"/>
     </WorkspaceConfiguration>
-    <WorkspaceConfiguration Name="CMake_Debug" Selected="yes">
+    <WorkspaceConfiguration Name="CMake_Debug" Selected="no">
       <Environment/>
       <Project Name="ZoomNavigator" ConfigName="Win_x86_Release"/>
       <Project Name="wxsqlite3" ConfigName="Win_x86_Release"/>
@@ -340,7 +340,7 @@
       <Project Name="codelite_vim" ConfigName="Win_x64_Debug"/>
       <Project Name="codelite_vim" ConfigName="Win_x64_Debug"/>
     </WorkspaceConfiguration>
-    <WorkspaceConfiguration Name="Win_x64_Release" Selected="no">
+    <WorkspaceConfiguration Name="Win_x64_Release" Selected="yes">
       <Environment>WXWIN=D:\src\wxWidgets
 WXCFG=gcc_dll\mswu</Environment>
       <Project Name="abbreviation" ConfigName="Win_x64_Release"/>

PHPLint/phplint.cpp
@@ -150,9 +150,10 @@ void PHPLint::DoCheckFile(const wxFileName& filename)
 
     wxFileName php(m_settingsPhp.GetPhpExe());
     if(!php.Exists()) {
-        ::wxMessageBox(_("Can not lint file: Missing PHP executable path"), "PHPLint", wxICON_ERROR | wxOK | wxCENTER);
+        clGetManager()->SetStatusMessage(_("PHPLint: can not lint file. Missing PHP executable path"), 5);
         return;
     }
+    
     wxString phpPath = php.GetFullPath();
     ::WrapWithQuotes(phpPath);
 

codelitephp/php-plugin/php.cpp
@@ -145,6 +145,9 @@ PhpPlugin::PhpPlugin(IManager* manager)
     EventNotifier::Get()->Connect(wxEVT_GOING_DOWN, clCommandEventHandler(PhpPlugin::OnGoingDown), NULL, this);
     EventNotifier::Get()->Bind(wxEVT_FILE_SYSTEM_UPDATED, &PhpPlugin::OnFileSysetmUpdated, this);
     EventNotifier::Get()->Bind(wxEVT_SAVE_SESSION_NEEDED, &PhpPlugin::OnSaveSession, this);
+    EventNotifier::Get()->Bind(wxEVT_FILE_SAVED, &PhpPlugin::OnFileAction, this);
+    EventNotifier::Get()->Bind(wxEVT_FILE_LOADED, &PhpPlugin::OnFileAction, this);
+    
     CallAfter(&PhpPlugin::FinalizeStartup);
 
     // Extract all CC files from PHP.zip into the folder ~/.codelite/php-plugin/cc
@@ -264,7 +267,10 @@ void PhpPlugin::UnPlug()
     EventNotifier::Get()->Disconnect(wxEVT_GOING_DOWN, clCommandEventHandler(PhpPlugin::OnGoingDown), NULL, this);
     EventNotifier::Get()->Unbind(wxEVT_FILE_SYSTEM_UPDATED, &PhpPlugin::OnFileSysetmUpdated, this);
     EventNotifier::Get()->Unbind(wxEVT_SAVE_SESSION_NEEDED, &PhpPlugin::OnSaveSession, this);
-
+    
+    EventNotifier::Get()->Unbind(wxEVT_FILE_SAVED, &PhpPlugin::OnFileAction, this);
+    EventNotifier::Get()->Unbind(wxEVT_FILE_LOADED, &PhpPlugin::OnFileAction, this);
+    
     SafelyDetachAndDestroyPane(m_debuggerPane, "XDebug");
     SafelyDetachAndDestroyPane(m_xdebugLocalsView, "XDebugLocals");
     SafelyDetachAndDestroyPane(m_xdebugEvalPane, "XDebugEval");
@@ -869,5 +875,13 @@ void PhpPlugin::OnReplaceInFiles(clFileSystemEvent& e)
         for(size_t i = 0; i < files.size(); ++i) {
             DoSyncFileWithRemote(files.Item(i));
         }
+    }
+}
+
+void PhpPlugin::OnFileAction(clCommandEvent& e)
+{
+    e.Skip();
+    if(PHPWorkspace::Get()->IsOpen()) {
+        DoSyncFileWithRemote(e.GetFileName());
     }
 }

codelitephp/php-plugin/php.h
@@ -134,6 +134,7 @@ public:
     void OnFileSysetmUpdated(clFileSystemEvent& event);
     void OnSaveSession(clCommandEvent& event);
     void OnReplaceInFiles(clFileSystemEvent& e);
+    void OnFileAction(clCommandEvent& e);
 };
 
 #endif // PHP
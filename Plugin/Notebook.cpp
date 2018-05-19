90ef0c23c179aee8f79152adb96b5508d6cdaae0
Eran Ifrah <eran.ifrah@gmail.com>
2018-05-19 18:49:02 +0300

    Fixed: avoid loop on Linux by sending multiple wxEVT_BOOK_CHANGED event

--- Plugin/Notebook.cpp
+++ Plugin/Notebook.cpp
@@ -597,11 +597,7 @@ int clTabCtrl::ChangeSelection(size_t tabIdx)
 
 int clTabCtrl::SetSelection(size_t tabIdx)
 {
-#if defined(__WXMSW__)||defined(__WXOSX__)
     DoChangeSelection(tabIdx);
-#else
-    CallAfter(&clTabCtrl::DoChangeSelection, tabIdx);
-#endif
     return wxNOT_FOUND;
 }
 

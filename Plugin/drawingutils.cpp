b2f61aa386859fcfb731d69278c0c76c540c0c5a
Eran Ifrah <eran.ifrah@gmail.com>
2018-07-07 11:28:55 +0300

    OSX: Minor UI fix

--- Plugin/drawingutils.cpp
+++ Plugin/drawingutils.cpp
@@ -528,6 +528,9 @@ void DrawingUtils::FillMenuBarBgColour(wxDC& dc, const wxRect& rect)
     wxColour startColour("rgb(231, 229, 231)");
     wxColour endColour("rgb(180, 180, 180)");
     dc.GradientFillLinear(rect, startColour, endColour, wxSOUTH);
+    endColour = endColour.ChangeLightness(70);
+    dc.SetPen(wxPen(endColour, 2));
+    dc.DrawLine(rect.GetBottomLeft(), rect.GetBottomRight());
 #else
     dc.SetPen(GetMenuBarBgColour());
     dc.SetBrush(GetMenuBarBgColour());

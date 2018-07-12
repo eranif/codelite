15956b6001c2e2e79e53dbf7222cd3a6997aa40d
Eran <eran.ifrah@gmail.com>
2018-07-12 14:14:57 +0300

    Add clNetworkMessage class

---
+++ CodeLite/clNetworkMessage.cpp
@@ -0,0 +1,8 @@
+#include "clNetworkMessage.h"
+#include "json_node.h"
+
+clNetworkMessage::clNetworkMessage() {}
+
+clNetworkMessage::~clNetworkMessage() {}
+
+wxString clNetworkMessage::ToString() const { return ""; }

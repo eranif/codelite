1cf6076e38ed661e7f5bf9ef3066cf568b7f9626
Eran Ifrah <eran.ifrah@gmail.com>
2018-09-25 01:12:34 +0300

    When first time creating an instance of type clThemedTreeCtrl, apply the active theme

--- Plugin/clThemedTreeCtrl.cpp
+++ Plugin/clThemedTreeCtrl.cpp
@@ -4,6 +4,29 @@
 #include "event_notifier.h"
 #include <clColours.h>
 
+static void ApplyTheme(clThemedTreeCtrl* tree)
+{
+    LexerConf::Ptr_t lexer = ColoursAndFontsManager::Get().GetLexer("text");
+    const StyleProperty& defaultStyle = lexer->GetProperty(0);
+    clColours colours;
+    if(lexer->IsDark()) {
+        colours.InitDefaults();
+        wxColour bgColour = colours.GetBgColour();
+        wxColour oddLineColour = bgColour.ChangeLightness(97);
+        colours.SetBgColour(bgColour);
+        colours.SetAlternateColourEven(bgColour);
+        colours.SetAlternateColourOdd(oddLineColour);
+    } else {
+        colours.InitDefaults();
+        wxColour bgColour = defaultStyle.GetBgColour(); // Use the current style background colour
+        wxColour oddLineColour = bgColour.ChangeLightness(97);
+        colours.SetBgColour(bgColour);
+        colours.SetAlternateColourEven(bgColour);
+        colours.SetAlternateColourOdd(oddLineColour);
+    }
+    tree->SetColours(colours);
+}
+
 clThemedTreeCtrl::clThemedTreeCtrl(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
     : clTreeCtrl(parent, id, pos, size, style | wxTR_ROW_LINES)
 {
@@ -11,7 +34,7 @@ clThemedTreeCtrl::clThemedTreeCtrl(wxWindow* parent, wxWindowID id, const wxPoin
     colours.InitDefaults();
     SetColours(colours);
     EventNotifier::Get()->Bind(wxEVT_CL_THEME_CHANGED, &clThemedTreeCtrl::OnThemeChanged, this);
-    // SetShowScrollBarOnFocus(true);
+    ApplyTheme(this);
 }
 
 bool clThemedTreeCtrl::Create(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
@@ -34,23 +57,5 @@ clThemedTreeCtrl::~clThemedTreeCtrl()
 void clThemedTreeCtrl::OnThemeChanged(wxCommandEvent& event)
 {
     event.Skip();
-    LexerConf::Ptr_t lexer = ColoursAndFontsManager::Get().GetLexer("text");
-    const StyleProperty& defaultStyle = lexer->GetProperty(0);
-    clColours colours;
-    if(lexer->IsDark()) {
-        colours.InitDefaults();
-        wxColour bgColour = colours.GetBgColour();
-        wxColour oddLineColour = bgColour.ChangeLightness(97);
-        colours.SetBgColour(bgColour);
-        colours.SetAlternateColourEven(bgColour);
-        colours.SetAlternateColourOdd(oddLineColour);
-    } else {
-        colours.InitDefaults();
-        wxColour bgColour = defaultStyle.GetBgColour(); // Use the current style background colour
-        wxColour oddLineColour = bgColour.ChangeLightness(97);
-        colours.SetBgColour(bgColour);
-        colours.SetAlternateColourEven(bgColour);
-        colours.SetAlternateColourOdd(oddLineColour);
-    }
-    SetColours(colours);
+    ApplyTheme(this);
 }

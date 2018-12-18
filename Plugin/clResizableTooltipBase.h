f9f70b57e3c8387a2ee19af20b0c3a949806a912
dghart <david@4Pane.co.uk>
2018-12-18 12:06:52 +0000

    Don't spam taskbar entries for debugger tooltips
    Tooltips now use the new clResizableTooltip, a wxMiniFrame, which doesn't officially support the wxFRAME_NO_TASKBAR style.
    It nevertheless seems to work in practice, at least on Linux, and probably will on Windows too.

--- Plugin/clResizableTooltipBase.h
+++ Plugin/clResizableTooltipBase.h
@@ -7,20 +7,21 @@
 #ifndef _CODELITE_PLUGIN_CLRESIZABLETOOLTIPBASE_BASE_CLASSES_H
 #define _CODELITE_PLUGIN_CLRESIZABLETOOLTIPBASE_BASE_CLASSES_H
 
-#include "clThemedTreeCtrl.h"
-#include <wx/artprov.h>
+#include <wx/settings.h>
+#include <wx/xrc/xmlres.h>
+#include <wx/xrc/xh_bmp.h>
 #include <wx/frame.h>
 #include <wx/iconbndl.h>
-#include <wx/minifram.h>
-#include <wx/settings.h>
+#include <wx/artprov.h>
 #include <wx/sizer.h>
+#include <wx/minifram.h>
+#include "wx/frame.h"
 #include <wx/treectrl.h>
-#include <wx/xrc/xh_bmp.h>
-#include <wx/xrc/xmlres.h>
+#include "clThemedTreeCtrl.h"
 #if wxVERSION_NUMBER >= 2900
 #include <wx/persist.h>
-#include <wx/persist/bookctrl.h>
 #include <wx/persist/toplevel.h>
+#include <wx/persist/bookctrl.h>
 #include <wx/persist/treebook.h>
 #endif
 
@@ -41,14 +42,21 @@ protected:
     clThemedTreeCtrl* m_treeCtrl;
 
 protected:
-    virtual void OnItemExpanding(wxTreeEvent& event) { event.Skip(); }
-    virtual void OnKeyDown(wxTreeEvent& event) { event.Skip(); }
+    virtual void OnItemExpanding(wxTreeEvent& event)
+    {
+        event.Skip();
+    }
+    virtual void OnKeyDown(wxTreeEvent& event)
+    {
+        event.Skip();
+    }
 
 public:
-    clThemedTreeCtrl* GetTreeCtrl() { return m_treeCtrl; }
-    clResizableTooltipBase(wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("Inspect Variable"),
-                           const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize(300, 200),
-                           long style = wxRESIZE_BORDER);
+    clThemedTreeCtrl* GetTreeCtrl()
+    {
+        return m_treeCtrl;
+    }
+    clResizableTooltipBase(wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("Inspect Variable"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize(300,200), long style = wxRESIZE_BORDER|wxFRAME_NO_TASKBAR);
     virtual ~clResizableTooltipBase();
 };
 

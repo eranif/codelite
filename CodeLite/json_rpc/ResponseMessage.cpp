55a7c630497ffa08f4b685d94dffb9753fa4c0b0
eranif <eran.ifrah@gmail.com>
2019-03-06 07:22:16 +0200

    LSP reponse message is now generic

--- CodeLite/json_rpc/ResponseMessage.cpp
+++ CodeLite/json_rpc/ResponseMessage.cpp
@@ -1,6 +1,14 @@
 #include "ResponseMessage.h"
 
-json_rpc::ResponseMessage::ResponseMessage() {}
+json_rpc::ResponseMessage::ResponseMessage(const wxString& message)
+{
+    m_json.reset(new JSON(message));
+    if(!m_json->isOk()) {
+        m_json.reset(nullptr);
+    } else {
+        FromJSON(m_json->toElement());
+    }
+}
 
 json_rpc::ResponseMessage::~ResponseMessage() {}
 
@@ -13,5 +21,15 @@ void json_rpc::ResponseMessage::FromJSON(const JSONItem& json)
 {
     Message::FromJSON(json);
     m_id = json.namedObject("id").toInt();
-    if(json.hasNamedObject("result") && m_result) { m_result->FromJSON(json.namedObject("result")); }
+}
+
+bool json_rpc::ResponseMessage::Has(const wxString& property) const
+{
+    return m_json && m_json->toElement().hasNamedObject(property);
+}
+
+JSONItem json_rpc::ResponseMessage::Get(const wxString& property) const
+{
+    if(!Has(property)) { return JSONItem(nullptr); }
+    return m_json->toElement().namedObject(property);
 }
--- CodeLite/json_rpc/ResponseMessage.h
+++ CodeLite/json_rpc/ResponseMessage.h
@@ -2,7 +2,7 @@
 #define RESPONSEMESSAGE_H
 
 #include "json_rpc/Message.h"
-#include "json_rpc/json_rpc_results.h"
+#include <wx/sharedptr.h>
 
 namespace json_rpc
 {
@@ -10,22 +10,25 @@ namespace json_rpc
 class WXDLLIMPEXP_CL ResponseMessage : public json_rpc::Message
 {
     int m_id = wxNOT_FOUND;
-    Result::Ptr_t m_result;
+    wxSharedPtr<JSON> m_json;
 
 public:
-    ResponseMessage();
+    ResponseMessage(const wxString& message);
     virtual ~ResponseMessage();
-    virtual wxString ToString() const;
-
     virtual JSONItem ToJSON(const wxString& name) const;
     virtual void FromJSON(const JSONItem& json);
 
+    virtual wxString ToString() const;
     ResponseMessage& SetId(int id)
     {
         this->m_id = id;
         return *this;
     }
     int GetId() const { return m_id; }
+    
+    bool IsOk() const { return m_json != nullptr; }
+    bool Has(const wxString& property) const;
+    JSONItem Get(const wxString& property) const;
 };
 
 }; // namespace json_rpc

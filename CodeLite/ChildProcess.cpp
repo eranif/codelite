014a4cd6860d696806495e1fcf57b793bf6c53fc
eran <eran.ifrah@gmail.com>
2019-03-14 01:05:18 +0200

    Make ChildProcess a thin wrapper around IProcess

--- CodeLite/ChildProcess.cpp
+++ CodeLite/ChildProcess.cpp
@@ -1,16 +1,11 @@
 #include "ChildProcess.h"
 #include <signal.h>
 #include <string.h>
-#if !USE_IPROCESS
-#include <sys/select.h>
-#endif
-#include <sys/types.h>
 #include "fileutils.h"
 #include "cl_exception.h"
 #include <cstring>
 #include "asyncprocess.h"
 #include "processreaderthread.h"
-#include "SocketAPI/clSocketBase.h"
 #include "file_logger.h"
 
 static wxString wrap_spaces(const wxString& str)
@@ -26,151 +21,25 @@ wxDEFINE_EVENT(wxEVT_CHILD_PROCESS_EXIT, clProcessEvent);
 
 ChildProcess::ChildProcess()
 {
-#if USE_IPROCESS
     Bind(wxEVT_ASYNC_PROCESS_OUTPUT, &ChildProcess::OnProcessOutput, this);
     Bind(wxEVT_ASYNC_PROCESS_STDERR, &ChildProcess::OnProcessStderr, this);
     Bind(wxEVT_ASYNC_PROCESS_TERMINATED, &ChildProcess::OnProcessTerminated, this);
-#endif
+}
+
+ChildProcess::~ChildProcess()
+{
+    Unbind(wxEVT_ASYNC_PROCESS_OUTPUT, &ChildProcess::OnProcessOutput, this);
+    Unbind(wxEVT_ASYNC_PROCESS_STDERR, &ChildProcess::OnProcessStderr, this);
+    Unbind(wxEVT_ASYNC_PROCESS_TERMINATED, &ChildProcess::OnProcessTerminated, this);
+    Stop();
 }
 
 void ChildProcess::Start(const wxArrayString& argv, const wxString& workingDirectory)
 {
-#if !USE_IPROCESS
-    Cleanup();
-    child_pid = fork();
-    if(child_pid == -1) {
-        clERROR() << "fork error: " << strerror(errno);
-        return;
-    }
-    if(child_pid == 0) {
-        setsid();
-        // Child
-        for(int i=3; i<65536; ++i) { 
-            if( i == childStdin->read_fd() || 
-                i == childStdout->write_fd() ||
-                i == childStderr->write_fd()) { continue; }
-            ::close(i); 
-        }
-        // create c-style array
-        wxString commandToExecute;
-        char** pargv = new char*[argv.size() + 1];
-        for(size_t i = 0; i < argv.size(); ++i) {
-            std::string cstr = FileUtils::ToStdString(wrap_spaces(argv[i]));
-            pargv[i] = new char[cstr.length() + 1];
-            strcpy(pargv[i], cstr.c_str());
-            commandToExecute << pargv[i] << " ";
-        }
-        pargv[argv.size()] = NULL;
-
-        clDEBUG() << "Will execute the command:" << commandToExecute;
-
-        // In child process
-        dup2(childStdin->read_fd(), STDIN_FILENO);
-        dup2(childStdout->write_fd(), STDOUT_FILENO);
-        dup2(childStderr->write_fd(), STDERR_FILENO);
-        
-        childStdin->close();
-        childStdout->close();
-        childStderr->close();
-        
-        // Change the working directory
-        if(!workingDirectory.IsEmpty()) { ::wxSetWorkingDirectory(workingDirectory); }
-
-        // Before we start the process, let the parent process do its magic
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-        int result = execvp(pargv[0], const_cast<char* const*>(pargv));
-        if(result == -1) {
-            // Note: no point writing to stdout here, it has been redirected
-            std::cerr << "Error: Failed to launch program" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-    } else {
-        // parent process
-        close(childStdin->read_fd());
-        close(childStdout->write_fd());
-        close(childStderr->write_fd());
-        
-        // Start the reader thread here
-        DoAsyncRead();
-    }
-#else
     m_process = ::CreateAsyncProcess(this, BuildCommand(argv), IProcessCreateDefault, workingDirectory);
     if(!m_process) { clERROR() << "CreateAsyncProcess() error." << BuildCommand(argv); }
-#endif
 }
-#if !USE_IPROCESS
-bool ChildProcess::DoReadAll(int fd, std::string& content, int timeoutMilliseconds)
-{
-    fd_set rset;
-    char buff[4096];
-    FD_ZERO(&rset);
-    FD_SET(fd, &rset);
-
-    int seconds = timeoutMilliseconds / 1000;
-    int ms = timeoutMilliseconds % 1000;
-
-    // clSocketBase c(fd);
-    // c.MakeSocketBlocking(false);
-
-    struct timeval tv = { seconds, ms * 1000 }; //  10 milliseconds timeout
-    int rc = ::select(fd + 1, &rset, nullptr, nullptr, &tv);
-    int selectErrno = errno;
-    if(rc > 0) {
-        memset(buff, 0, sizeof(buff));
-        errno = 0;
-        int bytes = read(fd, buff, sizeof(buff) - 1);
-        int readErrno = errno;
-        if(bytes > 0) {
-            // read success
-            content.append(buff);
-            return true;
-        } else if(bytes == 0) {
-            // process terminated
-            return false;
-        } else {
-            // read error
-            return (readErrno == EWOULDBLOCK) || (readErrno == EAGAIN) || (readErrno == EINTR);
-        }
-    } else if(rc == 0) {
-        // select timeout
-        return true;
-    } else {
-        // select error
-        return (selectErrno == EWOULDBLOCK) || (selectErrno == EAGAIN) || (selectErrno == EINTR);
-    }
-    return false;
-}
-
-bool ChildProcess::DoWrite(int fd, const std::string& message)
-{
-    int bytes = 0;
-    std::string tmp = std::move(message);
-    while(!tmp.empty()) {
-        errno = 0;
-        bytes = ::write(fd, tmp.c_str(), tmp.length());
-        int errCode = errno;
-        if(bytes < 0) {
-            if(errCode == EINTR || errCode == EWOULDBLOCK || errCode == EAGAIN) { continue; }
-            break;
-
-        } else if(bytes) {
-            tmp.erase(0, bytes);
-        }
-    }
-    return tmp.empty();
-}
-#endif
-
-int ChildProcess::Wait()
-{
-#if !USE_IPROCESS
-    int status = 0;
-    waitpid(child_pid, &status, WNOHANG);
-    return WEXITSTATUS(status);
-#else
-    return 0;
-#endif
-}
+int ChildProcess::Wait() { return 0; }
 
 void ChildProcess::Stop() { Cleanup(); }
 
@@ -182,57 +51,9 @@ void ChildProcess::Write(const wxString& message)
 
 void ChildProcess::Write(const std::string& message)
 {
-#if !USE_IPROCESS
-    std::thread worker([](int fd, const std::string& message) { ChildProcess::DoWrite(fd, message); },
-                       childStdin->write_fd(), message);
-    worker.detach();
-#else
     if(m_process) { m_process->Write(message); }
-#endif
 }
 
-#if !USE_IPROCESS
-void ChildProcess::OnDataRead(ChildProcess* process, const std::string& dataStdout, const std::string& dataStderr)
-{
-    if(!dataStdout.empty()) {
-        clProcessEvent event(wxEVT_CHILD_PROCESS_STDOUT);
-        event.SetOutput(wxString() << dataStdout);
-        process->AddPendingEvent(event);
-    }
-    if(!dataStderr.empty()) {
-        clProcessEvent event(wxEVT_CHILD_PROCESS_STDERR);
-        event.SetOutput(wxString() << dataStderr);
-        process->AddPendingEvent(event);
-    }
-}
-
-void ChildProcess::DoAsyncRead()
-{
-    m_reader = new std::thread(
-        [](ChildProcess* process, int fd, int stderrFd) {
-            std::string content;
-            std::string errContent;
-            while(true) {
-                if(!ChildProcess::DoReadAll(fd, content, 100) || 
-                    !ChildProcess::DoReadAll(stderrFd, errContent, 100)) { break; }
-
-                if(!content.empty() || !errContent.empty()) {
-                    ChildProcess::OnDataRead(process, content, errContent); 
-                }
-                content.clear();
-                errContent.clear();
-                // are we going down?
-                if(process->m_goingDown.load()) { break; }
-            }
-
-            // Notify about process termination event here
-            process->CallAfter(&ChildProcess::ReaderThreadExit);
-        },
-        this, childStdout->read_fd(), childStderr->read_fd());
-}
-#endif
-
-#if USE_IPROCESS
 void ChildProcess::OnProcessOutput(clProcessEvent& event)
 {
     clProcessEvent evtStdout(wxEVT_CHILD_PROCESS_STDOUT);
@@ -252,26 +73,8 @@ void ChildProcess::OnProcessTerminated(clProcessEvent& event)
     clProcessEvent exitEvent(wxEVT_CHILD_PROCESS_EXIT);
     AddPendingEvent(exitEvent);
 }
-#endif
 
-ChildProcess::~ChildProcess()
-{
-#if USE_IPROCESS
-    Unbind(wxEVT_ASYNC_PROCESS_OUTPUT, &ChildProcess::OnProcessOutput, this);
-    Unbind(wxEVT_ASYNC_PROCESS_STDERR, &ChildProcess::OnProcessStderr, this);
-    Unbind(wxEVT_ASYNC_PROCESS_TERMINATED, &ChildProcess::OnProcessTerminated, this);
-#endif
-    Stop();
-}
-
-bool ChildProcess::IsRunning() const
-{
-#if USE_IPROCESS
-    return m_process != nullptr;
-#else
-    return (child_pid != wxNOT_FOUND) && (::kill(child_pid, 0) == 0);
-#endif
-}
+bool ChildProcess::IsRunning() const { return m_process != nullptr; }
 
 wxString ChildProcess::BuildCommand(const wxArrayString& argv)
 {
@@ -285,35 +88,4 @@ wxString ChildProcess::BuildCommand(const wxArrayString& argv)
     return command;
 }
 
-void ChildProcess::Cleanup()
-{
-#if !USE_IPROCESS
-    m_goingDown.store(true);
-    if(m_reader) {
-        m_reader->join();
-        wxDELETE(m_reader);
-    }
-    if(child_pid != wxNOT_FOUND) {
-        ::kill(child_pid, SIGTERM);
-        Wait();
-    }
-    // Prepare it for next execution
-    m_goingDown.store(false);
-    childStdout.reset(new CPipe());
-    childStdin.reset(new CPipe());
-    childStderr.reset(new CPipe());
-    child_pid = wxNOT_FOUND;
-#else
-    wxDELETE(m_process);
-#endif
-}
-
-#if !USE_IPROCESS
-void ChildProcess::ReaderThreadExit()
-{
-    clProcessEvent exitEvent(wxEVT_CHILD_PROCESS_EXIT);
-    AddPendingEvent(exitEvent);
-    // Make sure we clean this object
-    Cleanup();
-}
-#endif
+void ChildProcess::Cleanup() { wxDELETE(m_process); }
--- CodeLite/ChildProcess.h
+++ CodeLite/ChildProcess.h
@@ -1,12 +1,6 @@
 #ifndef CHILD_PROCESS_H
 #define CHILD_PROCESS_H
 
-#if defined(__WXGTK__) || defined(__WXOSX__)
-#define USE_IPROCESS 0
-#else
-#define USE_IPROCESS 1
-#endif
-
 #include <exception>
 #include <functional>
 #include <iostream>
@@ -18,76 +12,29 @@
 #include <wx/event.h>
 #include "cl_command_event.h"
 #include <atomic>
-#if !USE_IPROCESS
-#include <sys/wait.h>
-#include <unistd.h>
-#endif
 #include "cl_exception.h"
 #include "file_logger.h"
-
-class IProcess;
-#if !USE_IPROCESS
-// Wrapping pipe in a class makes sure they are closed when we leave scope
-class CPipe
-{
-private:
-    int fd[2];
-
-public:
-    const inline int read_fd() const { return fd[0]; }
-    const inline int write_fd() const { return fd[1]; }
-    CPipe()
-    {
-        if(pipe(fd)) { clERROR() << "pipe() error:" << strerror(errno); }
-    }
-    void close()
-    {
-        ::close(fd[0]);
-        ::close(fd[1]);
-    }
-    ~CPipe() { close(); }
-    typedef wxSharedPtr<CPipe> Ptr_t;
-};
-#endif
 
 wxDECLARE_EXPORTED_EVENT(WXDLLIMPEXP_CL, wxEVT_CHILD_PROCESS_STDOUT, clProcessEvent);
 wxDECLARE_EXPORTED_EVENT(WXDLLIMPEXP_CL, wxEVT_CHILD_PROCESS_STDERR, clProcessEvent);
 wxDECLARE_EXPORTED_EVENT(WXDLLIMPEXP_CL, wxEVT_CHILD_PROCESS_EXIT, clProcessEvent);
+
+class IProcess;
 class WXDLLIMPEXP_CL ChildProcess : public wxEvtHandler
 {
 private:
-#if !USE_IPROCESS
-    CPipe::Ptr_t childStdout;
-    CPipe::Ptr_t childStdin;
-    CPipe::Ptr_t childStderr;
-    std::thread* m_reader = nullptr;
-    std::atomic_bool m_goingDown;
-#else
     IProcess* m_process = nullptr;
-#endif
 
-    // sync operations
-#if !USE_IPROCESS
-    static bool DoReadAll(int fd, std::string& content, int timeoutMilliseconds);
-    static bool DoWrite(int fd, const std::string& message);
-    static void OnDataRead(ChildProcess* process, const std::string& dataStdout, const std::string& dataStderr);
-#endif
-
-#if USE_IPROCESS
+protected:
     void OnProcessOutput(clProcessEvent& event);
     void OnProcessStderr(clProcessEvent& event);
     void OnProcessTerminated(clProcessEvent& event);
-#else
-    void DoAsyncRead();
-    void ReaderThreadExit();
-#endif
 
     // wait for process termination
     int Wait();
     void Cleanup();
 
 public:
-    int child_pid = wxNOT_FOUND;
     typedef wxSharedPtr<ChildProcess> Ptr_t;
 
     /**

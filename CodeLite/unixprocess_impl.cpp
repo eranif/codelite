9f70b93f44d7b44282a31e8879997e9cae7e4160
Eran Ifrah <eran.ifrah@gmail.com>
2019-08-10 22:14:14 +0300

    Fixed possible buffer overflow

--- CodeLite/unixprocess_impl.cpp
+++ CodeLite/unixprocess_impl.cpp
@@ -256,7 +256,9 @@ static void RemoveTerminalColoring(char* buffer)
     StringUtils::StripTerminalColouring(cinput, coutout);
 
     // coutout is ALWAYS <= cinput, so we can safely copy the content to the buffer
-    strcpy(buffer, coutout.c_str());
+    if(coutout.length() < cinput.length()) {
+        strcpy(buffer, coutout.c_str());
+    }
 }
 
 UnixProcessImpl::UnixProcessImpl(wxEvtHandler* parent)

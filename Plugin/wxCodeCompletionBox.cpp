39c08bd020e7168bc25bab14e37288257b4746fe
Eran Ifrah <eran.ifrah@gmail.com>
2019-08-14 18:41:45 +0300

    Fixed: use the default font for the code completion windows

--- Plugin/wxCodeCompletionBox.cpp
+++ Plugin/wxCodeCompletionBox.cpp
@@ -21,6 +21,7 @@
 #include "ieditor.h"
 #include "imanager.h"
 #include <wx/display.h>
+#include "ColoursAndFontsManager.h"
 
 static int LINES_PER_PAGE = 8;
 static int Y_SPACER = 2;
@@ -42,7 +43,8 @@ wxCodeCompletionBox::wxCodeCompletionBox(wxWindow* parent, wxEvtHandler* eventOb
     SetBackgroundStyle(wxBG_STYLE_PAINT);
     // Use the active editor's font (if any)
     IEditor* editor = clGetManager()->GetActiveEditor();
-    m_ccFont = DrawingUtils::GetBestFixedFont(editor);
+    LexerConf::Ptr_t lexer = ColoursAndFontsManager::Get().GetLexer("text");
+    m_ccFont = lexer->GetFontForSyle(0);
     SetCursor(wxCURSOR_HAND);
 
     // Set the default bitmap list

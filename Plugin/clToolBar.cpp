bb57271f16ca3fdc0d7ebfde9b0fdd6398d07383
Eran Ifrah <eran.ifrah@gmail.com>
2019-08-29 22:33:47 +0300

    Minor UI changes

--- Plugin/clToolBar.cpp
+++ Plugin/clToolBar.cpp
@@ -127,7 +127,7 @@ void clToolBar::OnPaint(wxPaintEvent& event)
         m_chevronRect = chevronRect;
     }
 	
-	tbBgColour = tbBgColour.ChangeLightness(DrawingUtils::IsDark(tbBgColour) ? 60 : 60);
+	tbBgColour = tbBgColour.ChangeLightness(DrawingUtils::IsDark(tbBgColour) ? 50 : 200);
 	gcdc.SetPen(tbBgColour);
 	gcdc.DrawLine(GetClientRect().GetLeftBottom(), GetClientRect().GetRightBottom());
 }

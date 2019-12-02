8fcd9731eb7ea485da5d9819bbe761ab29ee76d4
Eran Ifrah <eran.ifrah@gmail.com>
2019-12-02 08:52:06 +0200

    Changed the code completion box border to wxBORDER_SIMPLE

--- Plugin/wxCodeCompletionBoxBase.cpp
+++ Plugin/wxCodeCompletionBoxBase.cpp
@@ -26,7 +26,7 @@ wxCodeCompletionBoxBase::wxCodeCompletionBoxBase(wxWindow* parent, long style)
     this->SetSizer(mainSizer);
 
     m_mainPanel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDLG_UNIT(this, wxSize(400, 200)),
-                              wxTAB_TRAVERSAL | wxBORDER_THEME);
+                              wxTAB_TRAVERSAL | wxBORDER_SIMPLE);
 
     mainSizer->Add(m_mainPanel, 1, wxEXPAND, WXC_FROM_DIP(5));
 
@@ -34,7 +34,7 @@ wxCodeCompletionBoxBase::wxCodeCompletionBoxBase(wxWindow* parent, long style)
     m_mainPanel->SetSizer(boxSizer43);
 
     m_list = new clThemedListCtrl(m_mainPanel, wxID_ANY, wxDefaultPosition, wxDLG_UNIT(m_mainPanel, wxSize(-1, -1)),
-                                  wxDV_NO_HEADER | wxDV_ROW_LINES | wxDV_SINGLE);
+                                  wxDV_NO_HEADER | wxDV_ROW_LINES | wxDV_SINGLE | wxBORDER_NONE);
 
     boxSizer43->Add(m_list, 1, wxEXPAND, WXC_FROM_DIP(5));
 

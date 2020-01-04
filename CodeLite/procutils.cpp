6947b69472b4914f9a2ae11117a2e26a3aba9b2a
Eran Ifrah <eran@codelite.org>
2020-01-04 12:19:57 +0200

    Fixed build error on Linux

--- CodeLite/procutils.cpp
+++ CodeLite/procutils.cpp
@@ -64,7 +64,6 @@ static wxString WrapWithShell(const wxString& cmd)
         command << cmd;
     }
 #else
-    wxString tmpcmd = cmd;
     command << wxT("/bin/sh -c '");
     wxString tmpcmd = cmd;
     // escape any single quoutes

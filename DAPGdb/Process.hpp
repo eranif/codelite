68ade0a53a30393f92c14af25f60924ef9032d13
eifrah <eifrah@amazon.com>
2020-01-16 20:28:24 +0200

    added more missing files

---
+++ DAPGdb/Process.hpp
@@ -0,0 +1,32 @@
+#ifndef PROCESS_H__
+#define PROCESS_H__
+
+#include <string>
+using namespace std;
+
+enum eProcessCreateFlags {
+    IProcessCreateConsole = (1 << 0),
+    IProcessCreateWithHiddenConsole = (1 << 1),
+};
+
+class Process
+{
+public:
+    Process() {}
+    virtual ~Process() {}
+
+    virtual bool Read(string& str, string& err_buff) = 0;
+    virtual bool Write(const string& str) = 0;
+    virtual bool IsAlive() const = 0;
+    virtual void Cleanup() = 0;
+	virtual void Terminate() = 0;
+};
+/**
+ * @brief Create process and return the handle to it
+ * @param cmd process command
+ * @param workingDir process's working directory
+ * @param flags process creation flags
+ * @return pointer to Process object
+ */
+Process* ExecuteProcess(const string& cmd, const string& workingDir, size_t flags);
+#endif // PROCESS_H__

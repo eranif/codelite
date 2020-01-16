616f41cb705ea3993d9e3d8010008db83f275631
eifrah <eifrah@amazon.com>
2020-01-16 20:20:22 +0200

    Restructured the project layout Added CMake build support

--- DAPGdb/DAPGdb/StringUtils.cpp
+++
@@ -1,6 +0,0 @@
-#include "StringUtils.hpp"
-#include <codecvt>
-#include <locale>
-
-void StringUtils::Rtrim(wstring& str) { str.erase(str.find_last_not_of(L" \n\r\t") + 1); }
-void StringUtils::Rtrim(string& str) { str.erase(str.find_last_not_of(" \n\r\t") + 1); }

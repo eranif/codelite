eranif/codelite

commit 5e4cd5af37045937df8190bd335d95622120e57e

Fixed: default C++ debug project broken due to bug in File System Workspace Editor: when zooming in and out, adjust the line number margin width Editor: line number margin width is set according to the number of lines in the editor

LiteEditor/cl_editor.cpp
@@ -362,7 +362,7 @@ clEditor::clEditor(wxWindow* parent)
     Bind(wxEVT_FRD_CLOSE, &clEditor::OnFindDialog, this);
     Bind(wxEVT_FRD_CLEARBOOKMARKS, &clEditor::OnFindDialog, this);
     Bind(wxCMD_EVENT_REMOVE_MATCH_INDICATOR, &clEditor::OnRemoveMatchInidicator, this);
-
+    Bind(wxEVT_STC_ZOOM, &clEditor::OnZoom, this);
     DoUpdateOptions();
     PreferencesChanged();
     EventNotifier::Get()->Bind(wxEVT_EDITOR_CONFIG_CHANGED, &clEditor::OnEditorConfigChanged, this);
@@ -657,11 +657,8 @@ void clEditor::SetProperties()
     // allow everything except for the folding symbols
     SetMarginMask(SYMBOLS_MARGIN_ID, ~(wxSTC_MASK_FOLDERS));
 
-    // Line number margin
-    int pixelWidth = 4 + 5 * TextWidth(wxSTC_STYLE_LINENUMBER, wxT("9"));
-
     // Show number margin according to settings.
-    SetMarginWidth(NUMBER_MARGIN_ID, options->GetDisplayLineNumbers() ? pixelWidth : 0);
+    UpdateLineNumberMarginWidth();
 
     // Show the fold margin
     SetMarginWidth(FOLD_MARGIN_ID, options->GetDisplayFoldMargin() ? 16 : 0); // Fold margin
@@ -3057,6 +3054,7 @@ void clEditor::OpenFile()
     // Update the editor properties
     DoUpdateOptions();
     SetProperties();
+    UpdateLineNumberMarginWidth();
     UpdateColours();
     SetEOL();
 
@@ -4356,6 +4354,16 @@ void clEditor::OnChange(wxStyledTextEvent& event)
     bool isUndo = event.GetModificationType() & wxSTC_PERFORMED_UNDO;
     bool isRedo = event.GetModificationType() & wxSTC_PERFORMED_REDO;
 
+    int newLineCount = GetLineCount();
+    if(m_lastLineCount != newLineCount) {
+        int lastWidthCount = log10(m_lastLine) + 2;
+        int newWidthCount = log10(newLineCount) + 2;
+        m_lastLine = newLineCount;
+        if(newWidthCount != lastWidthCount) {
+            UpdateLineNumberMarginWidth();
+        }
+    }
+
     // Remove any code completion annotations if we have some...
     if(m_hasCCAnnotation) {
         CallAfter(&clEditor::AnnotationClearAll);
@@ -4365,7 +4373,7 @@ void clEditor::OnChange(wxStyledTextEvent& event)
     // Notify about this editor being changed
     clCommandEvent eventMod(wxEVT_EDITOR_MODIFIED);
     eventMod.SetFileName(GetFileName().GetFullPath());
-    EventNotifier::Get()->AddPendingEvent(eventMod);
+    EventNotifier::Get()->QueueEvent(eventMod.Clone());
 
     if((m_autoAddNormalBraces && !m_disableSmartIndent) || GetOptions()->GetAutoCompleteDoubleQuotes()) {
         if((event.GetModificationType() & wxSTC_MOD_BEFOREDELETE) &&
@@ -5356,7 +5364,7 @@ void clEditor::QuickAddNext()
         SetSelection(start, end);
         SetMainSelection(0);
     }
-    
+
     // Use the find flags of the quick find bar for this
     int searchFlags = clMainFrame::Get()->GetMainBook()->GetFindBar()->m_searchFlags;
     clMainFrame::Get()->GetMainBook()->ShowQuickBarToolBar(true);
@@ -5386,12 +5394,12 @@ void clEditor::QuickFindAll()
 
     int matches(0);
     int firstMatch(wxNOT_FOUND);
-    
+
     // Use the find flags of the quick find bar for this
     int searchFlags = clMainFrame::Get()->GetMainBook()->GetFindBar()->m_searchFlags;
     clMainFrame::Get()->GetMainBook()->ShowQuickBarToolBar(true);
     CallAfter(&clEditor::SetFocus);
-    
+
     // clWordCharslocker wcl(this);
     int where = this->FindText(0, GetLength(), findWhat, searchFlags);
     while(where != wxNOT_FOUND) {
@@ -5628,6 +5636,20 @@ int clEditor::GetFirstNonWhitespacePos(bool backward)
         }
     }
     return pos;
+}
+
+void clEditor::UpdateLineNumberMarginWidth()
+{
+    int newLineCount = GetLineCount();
+    int newWidthCount = log10(newLineCount) + 2;
+    SetMarginWidth(NUMBER_MARGIN_ID, newWidthCount * TextWidth(wxSTC_STYLE_LINENUMBER, "X"));
+}
+
+void clEditor::OnZoom(wxStyledTextEvent& event)
+{
+    event.Skip();
+    // When zooming, update the line number margin
+    UpdateLineNumberMarginWidth();
 }
 
 // ----------------------------------

LiteEditor/CodeLiteIDE.project
@@ -2102,7 +2102,7 @@ resources.cpp: resources.xrc
         <Library Value="libwxsqlite3ud.dll"/>
       </Linker>
       <ResourceCompiler Options="$(shell wx-config --rcflags)" Required="yes"/>
-      <General OutputFile="codelite-dbg.exe" IntermediateDirectory="" Command="codelite-dbg.exe" CommandArguments="-b . " UseSeparateDebugArgs="no" DebugArguments="-b . --no-plugins" WorkingDirectory="../Runtime" PauseExecWhenProcTerminates="no" IsGUIProgram="yes" IsEnabled="yes"/>
+      <General OutputFile="codelite-dbg.exe" IntermediateDirectory="" Command="$(WorkspacePath)/Runtime/codelite-dbg.exe" CommandArguments="-b . " UseSeparateDebugArgs="yes" DebugArguments="-b . --no-plugins" WorkingDirectory="$(WorkspacePath)/Runtime" PauseExecWhenProcTerminates="no" IsGUIProgram="yes" IsEnabled="yes"/>
       <BuildSystem Name="CodeLite Make Generator"/>
       <Environment EnvVarSetName="Default" DbgSetName="&lt;Use Defaults&gt;">
         <![CDATA[PATH=../sdk/clang/lib;$(WXWIN)\lib\gcc_dll;$(PATH)]]>
@@ -2134,7 +2134,8 @@ resources.cpp: resources.xrc
         <CustomPostBuild/>
         <CustomPreBuild>resources.cpp
 resources.cpp: resources.xrc
-	wxrc /c /v /o resources.cpp resources.xrc</CustomPreBuild>
+	wxrc /c /v /o resources.cpp resources.xrc
+</CustomPreBuild>
       </AdditionalRules>
       <Completion EnableCpp11="yes" EnableCpp14="no">
         <ClangCmpFlagsC/>
@@ -2254,8 +2255,7 @@ resources.cpp: resources.xrc
         <CustomPostBuild/>
         <CustomPreBuild>resources.cpp
 resources.cpp: resources.xrc
-	wxrc /c /v /o resources.cpp resources.xrc
-</CustomPreBuild>
+	wxrc /c /v /o resources.cpp resources.xrc</CustomPreBuild>
       </AdditionalRules>
       <Completion EnableCpp11="yes" EnableCpp14="no">
         <ClangCmpFlagsC/>

LiteEditor/cl_editor.h
@@ -25,6 +25,7 @@
 #ifndef LITEEDITOR_EDITOR_H
 #define LITEEDITOR_EDITOR_H
 
+#include "LSP/CompletionItem.h"
 #include "bookmark_manager.h"
 #include "browse_record.h"
 #include "clEditorStateLocker.h"
@@ -47,7 +48,6 @@
 #include <wx/bitmap.h>
 #include <wx/cmndata.h>
 #include <wx/stc/stc.h>
-#include "LSP/CompletionItem.h"
 
 #define DEBUGGER_INDICATOR 11
 #define MATCH_INDICATOR 10
@@ -120,7 +120,7 @@ class clEditor : public wxStyledTextCtrl, public IEditor
 {
 private:
     struct SelectionInfo {
-        std::vector<std::pair<int, int> > selections;
+        std::vector<std::pair<int, int>> selections;
 
         SelectionInfo() {}
 
@@ -230,7 +230,7 @@ protected:
     bool m_hightlightMatchedBraces;
     bool m_autoAddMatchedCurlyBrace;
     bool m_autoAddNormalBraces;
-    std::map<int, std::vector<BreakpointInfo> > m_breakpointsInfo;
+    std::map<int, std::vector<BreakpointInfo>> m_breakpointsInfo;
     bool m_autoAdjustHScrollbarWidth;
     bool m_reloadingFile;
     bool m_disableSmartIndent;
@@ -244,7 +244,7 @@ protected:
     BOM m_fileBom;
     int m_positionToEnsureVisible;
     bool m_preserveSelection;
-    std::vector<std::pair<int, int> > m_savedMarkers;
+    std::vector<std::pair<int, int>> m_savedMarkers;
     bool m_findBookmarksActive;
     std::map<int, wxString> m_compilerMessagesMap;
     CLCommandProcessor m_commandsProcessor;
@@ -519,7 +519,7 @@ public:
     /**
      * @brief return list of bookmarks (active type)
      */
-    virtual size_t GetFindMarkers(std::vector<std::pair<int, wxString> >& bookmarksVector);
+    virtual size_t GetFindMarkers(std::vector<std::pair<int, wxString>>& bookmarksVector);
 
     /**
      * Sets whether the currently-active bookmark level is Find', or the current standard-bookmark type
@@ -972,6 +972,7 @@ public:
     void PasteLineAbove();
 
 private:
+    void UpdateLineNumberMarginWidth();
     void DoUpdateTLWTitle(bool raise);
     void DoWrapPrevSelectionWithChars(wxChar first, wxChar last);
     void DoUpdateOptions();
@@ -1021,6 +1022,7 @@ private:
     void OnCharAdded(wxStyledTextEvent& event);
     void OnMarginClick(wxStyledTextEvent& event);
     void OnChange(wxStyledTextEvent& event);
+    void OnZoom(wxStyledTextEvent& event);
     void OnDwellStart(wxStyledTextEvent& event);
     void OnDwellEnd(wxStyledTextEvent& event);
     void OnCallTipClick(wxStyledTextEvent& event);

Plugin/clFileSystemWorkspace.cpp
@@ -814,8 +814,14 @@ void clFileSystemWorkspace::FileSystemUpdated() { CacheFiles(true); }
 
 void clFileSystemWorkspace::OnDebug(clDebugEvent& event)
 {
-    event.Skip(!IsOpen());
-
+    if(!IsOpen()) {
+        event.Skip();
+        return;
+    }
+    
+    // This is ours to handle. Stop processing it here
+    event.Skip(false);
+    
     DebuggerMgr::Get().SetActiveDebugger(GetConfig()->GetDebugger());
     IDebugger* dbgr = DebuggerMgr::Get().GetActiveDebugger();
     if(!dbgr) { return; }

commit 1c6d238db9d29b63379228b73923b661225ffa9d

Fixed: Regression that prevented hiding the Line-numbers margin
5e4cd5af introduced clEditor::UpdateLineNumberMarginWidth, which omitted checking the option.

LiteEditor/cl_editor.cpp
@@ -5921,7 +5921,8 @@ void clEditor::UpdateLineNumberMarginWidth()
 {
     int newLineCount = GetLineCount();
     int newWidthCount = log10(newLineCount) + 2;
-    SetMarginWidth(NUMBER_MARGIN_ID, newWidthCount * TextWidth(wxSTC_STYLE_LINENUMBER, "X"));
+    SetMarginWidth(NUMBER_MARGIN_ID, GetOptions()->GetDisplayLineNumbers()
+        ? (newWidthCount * TextWidth(wxSTC_STYLE_LINENUMBER, "X")) : 0);
 }
 
 void clEditor::OnZoom(wxStyledTextEvent& event)
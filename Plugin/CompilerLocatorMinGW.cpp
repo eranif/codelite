492da6391668cd3b08858acddf17c03a6eb0a728
Eran Ifrah <eran.ifrah@gmail.com>
2020-02-29 22:31:46 +0200

    indentation fix

--- Plugin/CompilerLocatorMinGW.cpp
+++ Plugin/CompilerLocatorMinGW.cpp
@@ -24,27 +24,23 @@
 //////////////////////////////////////////////////////////////////////////////
 
 #include "CompilerLocatorMinGW.h"
+#include "file_logger.h"
+#include "procutils.h"
+#include <globals.h>
 #include <wx/dir.h>
 #include <wx/filefn.h>
 #include <wx/tokenzr.h>
-#include <globals.h>
-#include "file_logger.h"
-#include "procutils.h"
-#include <wx/wfstream.h>
 #include <wx/txtstrm.h>
+#include <wx/wfstream.h>
 
 #ifdef __WXMSW__
-#   include <wx/msw/registry.h>
-#   include <wx/volume.h>
+#include <wx/msw/registry.h>
+#include <wx/volume.h>
 #endif
 
-CompilerLocatorMinGW::CompilerLocatorMinGW()
-{
-}
+CompilerLocatorMinGW::CompilerLocatorMinGW() {}
 
-CompilerLocatorMinGW::~CompilerLocatorMinGW()
-{
-}
+CompilerLocatorMinGW::~CompilerLocatorMinGW() {}
 
 CompilerPtr CompilerLocatorMinGW::Locate(const wxString& folder)
 {
@@ -55,14 +51,14 @@ CompilerPtr CompilerLocatorMinGW::Locate(const wxString& folder)
 #endif
 
     bool found = gcc.FileExists();
-    if ( ! found ) {
+    if(!found) {
         // try to see if we have a bin folder here
         gcc.AppendDir("bin");
         found = gcc.FileExists();
     }
 
-    if ( found ) {
-        AddTools(gcc.GetPath(), GetGCCVersion(gcc.GetFullPath() ));
+    if(found) {
+        AddTools(gcc.GetPath(), GetGCCVersion(gcc.GetFullPath()));
         return *m_compilers.begin();
     }
     return NULL;
@@ -74,7 +70,7 @@ wxString CompilerLocatorMinGW::GetGCCVersion(const wxString& gccBinary)
     wxArrayString stdoutArr;
     command << gccBinary << " --version";
     ProcUtils::SafeExecuteCommand(command, stdoutArr);
-    if ( !stdoutArr.IsEmpty() ) {
+    if(!stdoutArr.IsEmpty()) {
         wxString versionString = stdoutArr.Item(0).Trim().Trim(false);
         return versionString;
     }
@@ -93,12 +89,12 @@ bool CompilerLocatorMinGW::Locate()
         // HKEY_LOCAL_MACHINE\SOFTWARE\codelite\settings
         wxRegKey regClMinGW(wxRegKey::HKLM, "SOFTWARE\\codelite\\settings");
         wxString clInstallFolder;
-        if ( regClMinGW.QueryValue("MinGW", clInstallFolder) && wxDirExists(clInstallFolder)) {
+        if(regClMinGW.QueryValue("MinGW", clInstallFolder) && wxDirExists(clInstallFolder)) {
             wxFileName gccExe(clInstallFolder, "gcc.exe");
             wxString ver;
             regClMinGW.QueryValue("MinGW_Version", ver);
             gccExe.AppendDir("bin");
-            if ( gccExe.FileExists() ) {
+            if(gccExe.FileExists()) {
                 AddTools(gccExe.GetPath(), "CodeLite-" + ver);
             }
         }
@@ -108,12 +104,12 @@ bool CompilerLocatorMinGW::Locate()
         // HKEY_LOCAL_MACHINE\SOFTWARE\codelite\settings
         wxRegKey regClMinGW(wxRegKey::HKLM, "SOFTWARE\\Wow6432Node\\codelite\\settings");
         wxString clInstallFolder;
-        if ( regClMinGW.QueryValue("MinGW", clInstallFolder) && wxDirExists(clInstallFolder)) {
+        if(regClMinGW.QueryValue("MinGW", clInstallFolder) && wxDirExists(clInstallFolder)) {
             wxFileName gccExe(clInstallFolder, "gcc.exe");
             wxString ver;
             regClMinGW.QueryValue("MinGW_Version", ver);
             gccExe.AppendDir("bin");
-            if ( gccExe.FileExists() ) {
+            if(gccExe.FileExists()) {
                 AddTools(gccExe.GetPath(), "CodeLite-" + ver);
             }
         }
@@ -122,30 +118,30 @@ bool CompilerLocatorMinGW::Locate()
     // locate codeblock's MinGW
     wxRegKey regCB(wxRegKey::HKCU, "SOFTWARE\\CodeBlocks");
     wxString cbInstallPath;
-    if ( regCB.QueryValue("Path", cbInstallPath) ) {
-        wxFileName mingwBinFolder( cbInstallPath, "" );
+    if(regCB.QueryValue("Path", cbInstallPath)) {
+        wxFileName mingwBinFolder(cbInstallPath, "");
         mingwBinFolder.AppendDir("MinGW");
         mingwBinFolder.AppendDir("bin");
-        if ( mingwBinFolder.DirExists() && wxFileName(mingwBinFolder.GetFullPath(), "gcc.exe").FileExists() ) {
+        if(mingwBinFolder.DirExists() && wxFileName(mingwBinFolder.GetFullPath(), "gcc.exe").FileExists()) {
             AddTools(mingwBinFolder.GetPath(), "Code::Blocks");
         }
     }
-    
+
     // TDM-GCC installations in %ProgramData%/TDM-GCC/installations.txt
     wxString programDataDir;
     wxGetEnv("ProgramData", &programDataDir);
     wxFileName tdmgccInstall(programDataDir, "installations.txt");
     tdmgccInstall.AppendDir("TDM-GCC");
-    
-    if (tdmgccInstall.FileExists()) {
+
+    if(tdmgccInstall.FileExists()) {
         wxFileInputStream input(tdmgccInstall.GetFullPath());
         wxTextInputStream text(input);
-        while (input.IsOk() && !input.Eof()) {
+        while(input.IsOk() && !input.Eof()) {
             wxString instDir = text.ReadLine();
-            if (!instDir.IsEmpty() && wxDirExists(instDir)) {
+            if(!instDir.IsEmpty() && wxDirExists(instDir)) {
                 wxFileName tdmBinDir(instDir, "g++.exe");
                 tdmBinDir.AppendDir("bin");
-                if (tdmBinDir.FileExists())
+                if(tdmBinDir.FileExists())
                     AddTools(tdmBinDir.GetPath());
             }
         }
@@ -160,10 +156,10 @@ bool CompilerLocatorMinGW::Locate()
     regBase.push_back(wxRegKey::HKCU);
     regBase.push_back(wxRegKey::HKLM);
 
-    for (size_t i = 0; i < regBase.size(); ++i) {
-        for (size_t j = 0; j < unInstKey.size(); ++j) {
+    for(size_t i = 0; i < regBase.size(); ++i) {
+        for(size_t j = 0; j < unInstKey.size(); ++j) {
             wxRegKey regKey(regBase[i], unInstKey[j]);
-            if (!regKey.Exists() || !regKey.Open(wxRegKey::Read))
+            if(!regKey.Exists() || !regKey.Open(wxRegKey::Read))
                 continue;
 
             size_t subkeys = 0;
@@ -172,17 +168,15 @@ bool CompilerLocatorMinGW::Locate()
             long keyIndex = 0;
             regKey.GetFirstKey(keyName, keyIndex);
 
-            for (size_t k = 0; k < subkeys; ++k) {
+            for(size_t k = 0; k < subkeys; ++k) {
                 wxRegKey subKey(regKey, keyName);
-                if (!subKey.Exists() || !subKey.Open(wxRegKey::Read))
+                if(!subKey.Exists() || !subKey.Open(wxRegKey::Read))
                     continue;
 
                 wxString displayName, installFolder;
-                if (subKey.HasValue("DisplayName")
-                    && subKey.HasValue("InstallLocation")
-                    && subKey.QueryValue("DisplayName", displayName)
-                    && subKey.QueryValue("InstallLocation", installFolder))
-                {
+                if(subKey.HasValue("DisplayName") && subKey.HasValue("InstallLocation") &&
+                   subKey.QueryValue("DisplayName", displayName) &&
+                   subKey.QueryValue("InstallLocation", installFolder)) {
                     CheckRegKey(displayName, installFolder);
                 }
 
@@ -199,28 +193,28 @@ bool CompilerLocatorMinGW::Locate()
     wxArrayString volumes = wxFSVolume::GetVolumes();
     wxArrayString mingwFolderArr;
     // Get list of folders for the volume only
-    for(size_t i=0; i<volumes.GetCount(); ++i) {
-        wxDir dir( volumes.Item(i) );
-        if ( dir.IsOpened() ) {
+    for(size_t i = 0; i < volumes.GetCount(); ++i) {
+        wxDir dir(volumes.Item(i));
+        if(dir.IsOpened()) {
             wxString path;
             bool cont = dir.GetFirst(&path, "*mingw*", wxDIR_DIRS);
-            while (cont ) {
+            while(cont) {
                 wxString fullpath;
                 fullpath << volumes.Item(i) << path;
                 CL_DEBUG("Found folder containing MinGW: %s", fullpath);
-                mingwFolderArr.Add( fullpath );
-                cont = dir.GetNext( &path );
+                mingwFolderArr.Add(fullpath);
+                cont = dir.GetNext(&path);
             }
         }
     }
 
-    for(size_t i=0; i<mingwFolderArr.GetCount(); ++i) {
-        wxString binFolder = FindBinFolder( mingwFolderArr.Item(i) );
-        if ( binFolder.IsEmpty() )
+    for(size_t i = 0; i < mingwFolderArr.GetCount(); ++i) {
+        wxString binFolder = FindBinFolder(mingwFolderArr.Item(i));
+        if(binFolder.IsEmpty())
             continue;
 
         wxFileName gcc(binFolder, "gcc.exe");
-        if( gcc.FileExists() ) {
+        if(gcc.FileExists()) {
             AddTools(gcc.GetPath());
         }
     }
@@ -230,18 +224,18 @@ bool CompilerLocatorMinGW::Locate()
     wxString pathValues;
     wxGetEnv("PATH", &pathValues);
 
-    if ( !pathValues.IsEmpty() ) {
+    if(!pathValues.IsEmpty()) {
         wxArrayString pathArray = ::wxStringTokenize(pathValues, wxPATH_SEP, wxTOKEN_STRTOK);
-        for (size_t i = 0; i < pathArray.GetCount(); ++i) {
-            wxFileName gccComp( pathArray.Item(i), "gcc.exe" );
-            if ( gccComp.GetDirs().Last() == "bin" && gccComp.Exists() ) {
+        for(size_t i = 0; i < pathArray.GetCount(); ++i) {
+            wxFileName gccComp(pathArray.Item(i), "gcc.exe");
+            if(gccComp.GetDirs().Last() == "bin" && gccComp.Exists()) {
                 // We found gcc.exe
                 wxString pathToGcc = gccComp.GetPath();
                 pathToGcc.MakeLower();
 
                 // Don't mix cygwin and mingw
-                if ( !pathToGcc.Contains("cygwin") ) {
-                    AddTools( gccComp.GetPath() );
+                if(!pathToGcc.Contains("cygwin")) {
+                    AddTools(gccComp.GetPath());
                 }
             }
         }
@@ -252,27 +246,27 @@ bool CompilerLocatorMinGW::Locate()
 
 void CompilerLocatorMinGW::CheckRegKey(const wxString& displayName, const wxString& installFolder)
 {
-    if (displayName.StartsWith("TDM-GCC")) {
+    if(displayName.StartsWith("TDM-GCC")) {
         wxFileName fnTDMBinFolder(installFolder, "");
         fnTDMBinFolder.AppendDir("bin");
         fnTDMBinFolder.SetFullName("g++.exe");
-        if (fnTDMBinFolder.FileExists())
+        if(fnTDMBinFolder.FileExists())
             AddTools(fnTDMBinFolder.GetPath(), displayName);
     }
 
-    else if (displayName.StartsWith("MSYS2")) {
+    else if(displayName.StartsWith("MSYS2")) {
         wxFileName fnMingw32BinFolder(installFolder, "");
         fnMingw32BinFolder.AppendDir("mingw32");
         fnMingw32BinFolder.AppendDir("bin");
         fnMingw32BinFolder.SetFullName("g++.exe");
-        if (fnMingw32BinFolder.FileExists())
+        if(fnMingw32BinFolder.FileExists())
             AddTools(fnMingw32BinFolder.GetPath(), "MinGW 32bit ( " + displayName + " )");
 
         wxFileName fnMingw64BinFolder(installFolder, "");
         fnMingw64BinFolder.AppendDir("mingw64");
         fnMingw64BinFolder.AppendDir("bin");
         fnMingw64BinFolder.SetFullName("g++.exe");
-        if (fnMingw64BinFolder.FileExists())
+        if(fnMingw64BinFolder.FileExists())
             AddTools(fnMingw64BinFolder.GetPath(), "MinGW 64bit ( " + displayName + " )");
     }
 }
@@ -281,24 +275,24 @@ void CompilerLocatorMinGW::AddTools(const wxString& binFolder, const wxString& n
 {
     wxFileName masterPath(binFolder, "");
     masterPath.RemoveLastDir();
-    if ( m_locatedFolders.count(masterPath.GetPath()) ) {
+    if(m_locatedFolders.count(masterPath.GetPath())) {
         return;
     }
 
     // Create an empty compiler
-    CompilerPtr compiler( new Compiler(NULL) );
+    CompilerPtr compiler(new Compiler(NULL));
     compiler->SetCompilerFamily(COMPILER_FAMILY_MINGW);
     compiler->SetGenerateDependeciesFile(true);
-    m_compilers.push_back( compiler );
-    m_locatedFolders.insert( masterPath.GetPath() );
+    m_compilers.push_back(compiler);
+    m_locatedFolders.insert(masterPath.GetPath());
 
-    if ( name.IsEmpty() )
+    if(name.IsEmpty())
         compiler->SetName("MinGW ( " + masterPath.GetDirs().Last() + " )");
-    else if (!name.Lower().Contains("mingw"))
+    else if(!name.Lower().Contains("mingw"))
         compiler->SetName("MinGW ( " + name + " )");
     else
         compiler->SetName(name);
-    compiler->SetInstallationPath( masterPath.GetPath() );
+    compiler->SetInstallationPath(masterPath.GetPath());
 
     CL_DEBUG("Found MinGW compiler under: %s. \"%s\"", masterPath.GetPath(), compiler->GetName());
     wxFileName toolFile(binFolder, "");
@@ -322,19 +316,19 @@ void CompilerLocatorMinGW::AddTools(const wxString& binFolder, const wxString& n
 
     toolFile.SetFullName("make.exe");
     wxString makeExtraArgs;
-    if ( wxThread::GetCPUCount() > 1 ) {
+    if(wxThread::GetCPUCount() > 1) {
         makeExtraArgs << "-j" << wxThread::GetCPUCount();
     }
 
     // This is needed under MinGW
-    makeExtraArgs <<  " SHELL=cmd.exe ";
+    makeExtraArgs << " SHELL=cmd.exe ";
 
-    if ( toolFile.FileExists() ) {
+    if(toolFile.FileExists()) {
         AddTool(compiler, "MAKE", toolFile.GetFullPath(), makeExtraArgs);
 
     } else {
         toolFile.SetFullName("mingw32-make.exe");
-        if ( toolFile.FileExists() ) {
+        if(toolFile.FileExists()) {
             AddTool(compiler, "MAKE", toolFile.GetFullPath(), makeExtraArgs);
         }
     }
@@ -345,7 +339,8 @@ void CompilerLocatorMinGW::AddTools(const wxString& binFolder, const wxString& n
     }
 }
 
-void CompilerLocatorMinGW::AddTool(CompilerPtr compiler, const wxString& toolname, const wxString& toolpath, const wxString& extraArgs)
+void CompilerLocatorMinGW::AddTool(CompilerPtr compiler, const wxString& toolname, const wxString& toolpath,
+                                   const wxString& extraArgs)
 {
     wxString tool = toolpath;
     ::WrapWithQuotes(tool);
@@ -358,30 +353,30 @@ void CompilerLocatorMinGW::AddTool(CompilerPtr compiler, const wxString& toolnam
 wxString CompilerLocatorMinGW::FindBinFolder(const wxString& parentPath)
 {
     std::queue<wxString> dirs;
-    dirs.push( parentPath );
+    dirs.push(parentPath);
 
-    while ( !dirs.empty() ) {
+    while(!dirs.empty()) {
         wxString curdir = dirs.front();
         dirs.pop();
 
-        wxFileName fn(curdir, "" );
+        wxFileName fn(curdir, "");
         fn.AppendDir("bin");
 
-        if ( fn.DirExists() && fn.GetDirCount() && fn.GetDirs().Last() == "bin") {
+        if(fn.DirExists() && fn.GetDirCount() && fn.GetDirs().Last() == "bin") {
             return fn.GetPath();
         }
 
         // Check to see if there are more directories to recurse
         wxDir dir;
-        if ( dir.Open( curdir ) ) {
+        if(dir.Open(curdir)) {
             wxString dirname;
-            bool cont = dir.GetFirst( &dirname, "", wxDIR_DIRS );
-            while ( cont ) {
+            bool cont = dir.GetFirst(&dirname, "", wxDIR_DIRS);
+            while(cont) {
                 wxString new_dir;
                 new_dir << curdir << wxFileName::GetPathSeparator() << dirname;
-                dirs.push( new_dir );
+                dirs.push(new_dir);
                 dirname.Clear();
-                cont = dir.GetNext( &dirname );
+                cont = dir.GetNext(&dirname);
             }
         }
     }

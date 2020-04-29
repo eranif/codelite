5d6a7988baeae2b25b7c8bc095821064d74d9baf
dghart <dghart david@4Pane.co.uk>
2020-04-29 11:52:47 +0100

    Remove Wayland testing code made redundant by the last commit

--- LiteEditor/frame.cpp
+++ LiteEditor/frame.cpp
@@ -850,9 +850,7 @@ clMainFrame::clMainFrame(wxWindow* pParent, wxWindowID id, const wxString& title
 
 #ifdef __WXGTK__
     // Try to detect if this is a Wayland session; we have some Wayland-workaround code
-    wxString sesstype("XDG_SESSION_TYPE"), session_type;
-    wxGetEnv(sesstype, &session_type);
-    m_isWaylandSession = session_type.Lower().Contains("wayland");
+    m_isWaylandSession = clIsWaylandSession();
 #endif
 }
 

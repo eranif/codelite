9c922c63867669a1115d46e31cfa830c198fa6aa
Eran Ifrah <eran.ifrah@gmail.com>
2021-03-01 12:21:25 +0200

    fixed build error with recent gcc compilers

--- CodeLite/fileutils.cpp
+++ CodeLite/fileutils.cpp
@@ -631,7 +631,7 @@ std::string FileUtils::ToStdString(const wxString& str) { return StringUtils::To
 
 bool FileUtils::ReadBufferFromFile(const wxFileName& fn, wxString& data, size_t bufferSize)
 {
-    std::wifstream fin(fn.GetFullPath().c_str(), std::ios::binary);
+    std::wifstream fin(fn.GetFullPath().ToStdString(), std::ios::binary);
     if(fin.bad()) {
         clERROR() << "Failed to open file:" << fn;
         return false;

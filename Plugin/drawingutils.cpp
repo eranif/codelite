95f8c969930f238c072bbed9ba5384a711c0e649
Eran Ifrah <eran.ifrah@gmail.com>
2021-03-15 20:47:49 +0200

    fixed build warnings

--- Plugin/drawingutils.cpp
+++ Plugin/drawingutils.cpp
@@ -60,42 +60,6 @@
 #undef GSocket
 #endif
 
-static void RGBtoHSB(int r, int g, int b, float* h, float* s, float* br)
-{
-    float hue, saturation, brightness;
-    int cmax = (r > g) ? r : g;
-    if(b > cmax)
-        cmax = b;
-    int cmin = (r < g) ? r : g;
-    if(b < cmin)
-        cmin = b;
-
-    brightness = ((float)cmax) / 255.0f;
-    if(cmax != 0)
-        saturation = ((float)(cmax - cmin)) / ((float)cmax);
-    else
-        saturation = 0;
-    if(saturation == 0)
-        hue = 0;
-    else {
-        float redc = ((float)(cmax - r)) / ((float)(cmax - cmin));
-        float greenc = ((float)(cmax - g)) / ((float)(cmax - cmin));
-        float bluec = ((float)(cmax - b)) / ((float)(cmax - cmin));
-        if(r == cmax)
-            hue = bluec - greenc;
-        else if(g == cmax)
-            hue = 2.0f + redc - bluec;
-        else
-            hue = 4.0f + greenc - redc;
-        hue = hue / 6.0f;
-        if(hue < 0)
-            hue = hue + 1.0f;
-    }
-    (*h) = hue;
-    (*s) = saturation;
-    (*br) = brightness;
-}
-
 //////////////////////////////////////////////////
 // Colour methods to convert HSL <-> RGB
 //////////////////////////////////////////////////

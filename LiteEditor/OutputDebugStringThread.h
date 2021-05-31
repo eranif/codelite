a686737030a58d56679e0cf6d378fa83579c78ad
Tim Stahlhut <stahta01@gmail.com>
2021-05-30 22:06:47 -0400

    LiteEditor: Reformat a few files

--- LiteEditor/OutputDebugStringThread.h
+++ LiteEditor/OutputDebugStringThread.h
@@ -1,8 +1,8 @@
 #ifndef OUTPUTDEBUGSTRINGTHREAD_H
 #define OUTPUTDEBUGSTRINGTHREAD_H
 
-#include <wx/thread.h> // Base class: wxThread
 #include "cl_command_event.h"
+#include <wx/thread.h> // Base class: wxThread
 
 wxDECLARE_EVENT(wxEVT_OUTPUT_DEBUG_STRING, clCommandEvent);
 class OutputDebugStringThread : public wxThread
@@ -42,7 +42,7 @@ public:
      * \note This call must be called from the context of other thread (e.g. main thread)
      */
     void Start(int priority = WXTHREAD_DEFAULT_PRIORITY);
-    
+
     /**
      * @brief enable collection
      */

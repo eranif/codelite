d7413ae55b73dc69c2273879bfd0f0b0243cf750
Tim Stahlhut <stahta01@gmail.com>
2021-05-30 22:06:48 -0400

    LiteEditor: wxWidgets changes for MS Windows
    Use "wx/msw/wrapwin.h" to define DWORD

--- LiteEditor/OutputDebugStringThread.h
+++ LiteEditor/OutputDebugStringThread.h
@@ -2,6 +2,10 @@
 #define OUTPUTDEBUGSTRINGTHREAD_H
 
 #include "cl_command_event.h"
+#include "wx/defs.h"
+#ifdef __WINDOWS__          // __WINDOWS__ defined by wx/defs.h
+#include "wx/msw/wrapwin.h" // includes windows.h
+#endif
 #include <wx/thread.h> // Base class: wxThread
 
 wxDECLARE_EVENT(wxEVT_OUTPUT_DEBUG_STRING, clCommandEvent);

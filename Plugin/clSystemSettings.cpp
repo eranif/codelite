d53e436b42f91422ebac5db5e6137a1f0e105e03
Eran Ifrah <eran@codelite.org>
2021-06-16 20:49:36 +0300

    more linux fixes (drawing, themes support)

--- Plugin/clSystemSettings.cpp
+++ Plugin/clSystemSettings.cpp
@@ -36,6 +36,16 @@ namespace
 /// we use this to detect any theme changes done to the system
 /// the checks are done in the OnAppAcitvated event
 wxColour startupBackgroundColour;
+
+double GetLuminance(const wxColour& color)
+{
+    double r = color.Red();
+    double g = color.Green();
+    double b = color.Blue();
+
+    double luma = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+    return luma;
+}
 } // namespace
 
 wxDEFINE_EVENT(wxEVT_SYS_COLOURS_CHANGED, clCommandEvent);
@@ -88,12 +98,26 @@ wxColour clSystemSettings::GetColour(int index)
         }
     } else {
         bool is_dark = DrawingUtils::IsDark(wxSYS_COLOUR_3DFACE);
+        wxColour bg_colur = wxSystemSettings::GetColour(wxSYS_COLOUR_3DFACE);
+#ifdef __WXGTK__
+        if(is_dark) {
+            wxColour window_colour = wxSystemSettings::GetColour(wxSYS_COLOUR_WINDOW);
+            wxColour face_colour = wxSystemSettings::GetColour(wxSYS_COLOUR_3DFACE);
+            if(GetLuminance(window_colour) < GetLuminance(face_colour)) {
+                // window colour is darker than the face colour, use the face colour
+                bg_colur = window_colour;
+            } else {
+                // use button face colour
+                bg_colur = face_colour;
+            }
+        }
+#endif
         if(index == wxSYS_COLOUR_TOOLBAR) {
             return GetDefaultPanelColour();
         } else if(index == wxSYS_COLOUR_TOOLBARTEXT) {
             return wxSystemSettings::GetColour(wxSYS_COLOUR_BTNTEXT);
-        } else if(index == wxSYS_COLOUR_WINDOW) {
-            return wxSystemSettings::GetColour(IS_GTK ? wxSYS_COLOUR_WINDOW : wxSYS_COLOUR_3DFACE);
+        } else if(index == wxSYS_COLOUR_WINDOW || index == wxSYS_COLOUR_3DFACE) {
+            return bg_colur;
         } else if(index == wxSYS_COLOUR_LISTBOX) {
             return GetDefaultPanelColour().ChangeLightness(is_dark ? 110 : 90);
         } else {

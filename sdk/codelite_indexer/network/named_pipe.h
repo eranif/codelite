2381fa8387f232bb75932d32e49d990db9f5c01e
Tim Stahlhut <stahta01@gmail.com>
2021-06-23 17:50:28 -0400

    codelite_indexer: Reformat a few files

--- sdk/codelite_indexer/network/named_pipe.h
+++ sdk/codelite_indexer/network/named_pipe.h
@@ -29,7 +29,7 @@
 #ifdef __WXMSW__
 // Windows
 #ifndef _WIN64
-#   include <WinSock2.h>
+#include <WinSock2.h>
 #endif
 
 #include <Windows.h>
@@ -41,75 +41,58 @@
 #else
 
 // *uix
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <string>
+#include <unistd.h>
 
 #define PIPE_HANDLE int
 #define INVALID_PIPE_HANDLE -1
 #endif
 
-class  clNamedPipe
+class clNamedPipe
 {
 public:
+    enum ZNP_ERROR {
+        ZNP_OK = 0,
+        ZNP_TIMEOUT,
+        ZNP_CONNECT_ERROR,
+        ZNP_CONNECT_WAIT_ERROR,
+        ZNP_BAD_WRITE_SIZE,
+        ZNP_READ_ERROR,
+        ZNP_WRITE_ERROR,
+        ZNP_IMPERSONATION_ERROR,
+        ZNP_CONN_CLOSED,
+        ZNP_UNKNOWN
+    };
 
-	enum ZNP_ERROR {
-		ZNP_OK = 0
-		, ZNP_TIMEOUT
-		, ZNP_CONNECT_ERROR
-		, ZNP_CONNECT_WAIT_ERROR
-		, ZNP_BAD_WRITE_SIZE
-		, ZNP_READ_ERROR
-		, ZNP_WRITE_ERROR
-		, ZNP_IMPERSONATION_ERROR
-		, ZNP_CONN_CLOSED
-		, ZNP_UNKNOWN
-	};
+    clNamedPipe(const char* path);
 
+    virtual ~clNamedPipe();
 
+    virtual bool write(const void* data, size_t dataLength, size_t* written, long timeToLive);
 
-	clNamedPipe(const char* path);
+    virtual bool read(void* data, size_t dataLength, size_t* read, long timeToLive);
 
+    virtual void disconnect() = 0;
 
-	virtual ~clNamedPipe();
+    const char* getPipePath() const { return _pipePath.c_str(); }
 
-	virtual bool write(const void* data, size_t dataLength, size_t *written, long timeToLive);
+    ZNP_ERROR getLastError() { return _lastError; }
 
-	virtual bool read(void* data, size_t dataLength, size_t *read, long timeToLive);
-
-	virtual void disconnect() = 0;
-
-	const char* getPipePath() const {
-		return _pipePath.c_str();
-	}
-
-	ZNP_ERROR getLastError() {
-		return _lastError;
-	}
-
-	void setLastError(ZNP_ERROR code) {
-		_lastError = code ;
-	}
-
-
+    void setLastError(ZNP_ERROR code) { _lastError = code; }
 
 protected:
+    void setPipePath(const char* path) { _pipePath = path; }
 
-	void setPipePath(const char* path) {
-		_pipePath = path;
-	}
+    void setPipeHandle(PIPE_HANDLE handle) { _pipeHandle = handle; }
 
-	void setPipeHandle(PIPE_HANDLE handle) {
-		_pipeHandle = handle;
-	}
-
-	PIPE_HANDLE _pipeHandle;
+    PIPE_HANDLE _pipeHandle;
 
 private:
-	std::string _pipePath;
-	ZNP_ERROR   _lastError;
+    std::string _pipePath;
+    ZNP_ERROR _lastError;
 };
 
 #endif // named_pipe_h__

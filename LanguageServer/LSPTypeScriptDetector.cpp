f0e3b0ec3ff3c2ddeba643d316e7434c9b03dd8f
Eran Ifrah <eran@codelite.org>
2021-10-21 00:13:31 +0300

    Change the way for search for typescript-language-server.cmd under windows

--- LanguageServer/LSPTypeScriptDetector.cpp
+++ LanguageServer/LSPTypeScriptDetector.cpp
@@ -12,30 +12,20 @@ LSPTypeScriptDetector::~LSPTypeScriptDetector() {}
 
 bool LSPTypeScriptDetector::DoLocate()
 {
-    NodeJSLocator node;
-    node.Locate();
-
-    if(node.GetNpm().empty()) {
-        return false;
-    }
-
     wxFileName typescript_lsp;
     wxArrayString hints;
 
     wxString fullname = "typescript-language-server";
 #ifdef __WXMSW__
-    hints.Add(clStandardPaths::Get().GetUserDataDir());
+    wxFileName roaming(clStandardPaths::Get().GetUserDataDir(), wxEmptyString);
+    roaming.RemoveLastDir();
+    roaming.AppendDir("npm");
+    hints.Add(roaming.GetPath());
     fullname << ".cmd";
-#else
-    hints.Add(wxFileName(node.GetNpm()).GetPath());
 #endif
 
     if(!FileUtils::FindExe(fullname, typescript_lsp, hints)) {
         return false;
-    }
-
-    if(!typescript_lsp.FileExists()) {
-        return true;
     }
 
     wxString command;

b9ace38b76093a9f471349a4ad6f04245fa590b7
rlbxku1r <32811754+rlbxku1r@users.noreply.github.com>
2021-11-27 18:52:34 +0900

    LSP Hover: Remove unused member variables

--- CodeLite/LSP/HoverRequest.hpp
+++ CodeLite/LSP/HoverRequest.hpp
@@ -3,20 +3,14 @@
 
 #include "LSP/Request.h"
 #include "LSP/ResponseMessage.h"
-#include <wx/filename.h>
 
 namespace LSP
 {
 class WXDLLIMPEXP_CL HoverRequest : public LSP::Request
 {
-    wxString m_filename;
-    size_t m_line = 0;
-    size_t m_column = 0;
-
 public:
     explicit HoverRequest(const wxString& filename, size_t line, size_t column);
     virtual ~HoverRequest();
-
     void OnResponse(const LSP::ResponseMessage& response, wxEvtHandler* owner);
 };
 };     // namespace LSP

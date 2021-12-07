71e3ee033cc1fcacb648c7a5732e1caf79ac3562
Eran Ifrah <eran@codelite.org>
2021-12-07 19:16:23 +0200

    missing files

---
+++ Plugin/EclipseRubyThemeImporter.cpp
@@ -0,0 +1,42 @@
+#include "EclipseRubyThemeImporter.hpp"
+
+EclipseRubyThemeImporter::EclipseRubyThemeImporter()
+{
+    SetKeywords0("__ENCODING__ __LINE__ __FILE__ BEGIN END alias and begin break case class def defined? do else elsif "
+                 "end ensure false for if in module next nil not or redo rescue retry return self super then true "
+                 "undef unless until when while yield");
+    SetFileExtensions("*.rb");
+    m_langName = "ruby";
+}
+
+EclipseRubyThemeImporter::~EclipseRubyThemeImporter() {}
+
+LexerConf::Ptr_t EclipseRubyThemeImporter::Import(const wxFileName& eclipseXmlFile)
+{
+    LexerConf::Ptr_t lexer = InitializeImport(eclipseXmlFile, GetLangName(), wxSTC_LEX_RUBY);
+    AddProperty(lexer, wxSTC_RB_DEFAULT, "Default", m_foreground.colour, m_background.colour);
+    AddProperty(lexer, wxSTC_RB_COMMENTLINE, "Single Line Comment", m_multiLineComment.colour, m_background.colour);
+    AddProperty(lexer, wxSTC_RB_NUMBER, "Number", m_number.colour, m_background.colour);
+    AddProperty(lexer, wxSTC_RB_STRING, "String", m_string.colour, m_background.colour);
+    AddProperty(lexer, wxSTC_RB_CHARACTER, "Character", m_string.colour, m_background.colour);
+    AddProperty(lexer, wxSTC_RB_WORD, "Word", m_keyword.colour, m_background.colour); // keywords
+    AddProperty(lexer, wxSTC_RB_DEFNAME, "Def Name", m_function.colour, m_background.colour);
+    AddProperty(lexer, wxSTC_RB_CLASSNAME, "Class Name", m_klass.colour, m_background.colour);
+    AddProperty(lexer, wxSTC_RB_MODULE_NAME, "Module Name", m_enum.colour, m_background.colour);
+    AddProperty(lexer, wxSTC_RB_SYMBOL, "Symbol", m_field.colour, m_background.colour);
+    AddProperty(lexer, wxSTC_RB_OPERATOR, "Operator", m_oper.colour, m_background.colour);
+    AddProperty(lexer, wxSTC_RB_CLASS_VAR, "Class Variable", m_variable.colour, m_background.colour);
+    AddProperty(lexer, wxSTC_RB_INSTANCE_VAR, "Instance Variable", m_variable.colour, m_background.colour);
+    AddProperty(lexer, wxSTC_RB_IDENTIFIER, "Identifier", m_foreground.colour, m_background.colour);
+    AddProperty(lexer, wxSTC_RB_HERE_DELIM, "HERE delim", m_string.colour, m_background.colour);
+    AddProperty(lexer, wxSTC_RB_HERE_Q, "HERE Q", m_string.colour, m_background.colour);
+    AddProperty(lexer, wxSTC_RB_HERE_QQ, "HERE QQ", m_string.colour, m_background.colour);
+    AddProperty(lexer, wxSTC_RB_HERE_QX, "HERE QX", m_string.colour, m_background.colour);
+    AddProperty(lexer, wxSTC_RB_STRING_Q, "String Q", m_string.colour, m_background.colour);
+    AddProperty(lexer, wxSTC_RB_STRING_QQ, "String QQ", m_string.colour, m_background.colour);
+    AddProperty(lexer, wxSTC_RB_STRING_QX, "String QX", m_string.colour, m_background.colour);
+    AddProperty(lexer, wxSTC_RB_STRING_QR, "String QR", m_string.colour, m_background.colour);
+    AddProperty(lexer, wxSTC_RB_STRING_QW, "String QW", m_string.colour, m_background.colour);
+    FinalizeImport(lexer);
+    return lexer;
+}
---
+++ Plugin/EclipseRubyThemeImporter.hpp
@@ -0,0 +1,40 @@
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//
+// Copyright            : (C) 2021 Eran Ifrah
+// File name            : EclipseRubyThemeImporter.hpp
+//
+// -------------------------------------------------------------------------
+// A
+//              _____           _      _     _ _
+//             /  __ \         | |    | |   (_) |
+//             | /  \/ ___   __| | ___| |    _| |_ ___
+//             | |    / _ \ / _  |/ _ \ |   | | __/ _ )
+//             | \__/\ (_) | (_| |  __/ |___| | ||  __/
+//              \____/\___/ \__,_|\___\_____/_|\__\___|
+//
+//                                                  F i l e
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef ECLIPSERUBYTHEMEIMPORTER_H
+#define ECLIPSERUBYTHEMEIMPORTER_H
+
+#include "EclipseThemeImporterBase.h"
+#include "codelite_exports.h"
+
+class WXDLLIMPEXP_SDK EclipseRubyThemeImporter : public EclipseThemeImporterBase
+{
+public:
+    virtual LexerConf::Ptr_t Import(const wxFileName& eclipseXmlFile);
+    EclipseRubyThemeImporter();
+    ~EclipseRubyThemeImporter();
+};
+
+#endif // ECLIPSERUBYTHEMEIMPORTER_H

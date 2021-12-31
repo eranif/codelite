58629e8b3a9ca34de21cf1a289d3b9023042a22b
Eran Ifrah <eran@codelite.org>
2021-12-31 13:53:50 +0200

    Language server log view: added context menu

--- LanguageServer/LanguageServerLogView.cpp
+++ LanguageServer/LanguageServerLogView.cpp
@@ -3,9 +3,19 @@
 LanguageServerLogView::LanguageServerLogView(wxWindow* parent)
     : LanguageServerLogViewBase(parent)
 {
+    m_dvListCtrl->Bind(wxEVT_CONTEXT_MENU, [this](wxContextMenuEvent& event) {
+        wxMenu menu;
+        menu.Append(wxID_CLEAR);
+        menu.Bind(
+            wxEVT_MENU,
+            [this](wxCommandEvent& event) {
+                wxUnusedVar(event);
+                // clear the view
+                m_dvListCtrl->DeleteAllItems();
+            },
+            wxID_CLEAR);
+        m_dvListCtrl->PopupMenu(&menu);
+    });
 }
 
-LanguageServerLogView::~LanguageServerLogView()
-{
-}
-
+LanguageServerLogView::~LanguageServerLogView() {}

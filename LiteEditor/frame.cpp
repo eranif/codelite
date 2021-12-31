845ed627f7e022d6c5cdf4e17ef5e51b94085415
Eran Ifrah <eran@codelite.org>
2021-12-31 14:10:35 +0200

    updated copyright years to 2022

--- LiteEditor/frame.cpp
+++ LiteEditor/frame.cpp
@@ -1658,7 +1658,7 @@ void clMainFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
     // Misc
     info.SetWebSite("https://codelite.org", _("CodeLite Home"));
     info.SetVersion(CODELITE_VERSION_STRING);
-    info.SetCopyright("Eran Ifrah 2007-2021");
+    info.SetCopyright("Eran Ifrah 2007-2022");
 
     // Load the license file
     wxFileName license(clStandardPaths::Get().GetDataDir(), "LICENSE");

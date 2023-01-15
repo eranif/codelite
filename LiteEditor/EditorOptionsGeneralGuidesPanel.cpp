edf2e98e3462c5f8a1d94f5413e599cfe1e81d21
rlbxku1r <32811754+rlbxku1r@users.noreply.github.com>
2023-01-16 05:23:36 +0900

    Preferences Dialog: Add missing translations
    Additionally, add `Windows (CRLF)` as an EOL style option.

--- LiteEditor/EditorOptionsGeneralGuidesPanel.cpp
+++ LiteEditor/EditorOptionsGeneralGuidesPanel.cpp
@@ -6,40 +6,42 @@
 EditorOptionsGeneralGuidesPanel::EditorOptionsGeneralGuidesPanel(wxWindow* parent, OptionsConfigPtr options)
     : OptionsConfigPage(parent, options)
 {
-    AddHeader("Line numbers");
-    AddProperty("Display line numbers", m_options->GetDisplayLineNumbers(), UPDATE_BOOL_CB(SetDisplayLineNumbers));
-    AddProperty("Highlight current line number", m_options->IsLineNumberHighlightCurrent(),
+    AddHeader(_("Line numbers"));
+    AddProperty(_("Display line numbers"), m_options->GetDisplayLineNumbers(), UPDATE_BOOL_CB(SetDisplayLineNumbers));
+    AddProperty(_("Highlight current line number"), m_options->IsLineNumberHighlightCurrent(),
                 UPDATE_BOOL_CB(SetLineNumberHighlightCurrent));
-    AddProperty("Use relative line numbers", m_options->GetRelativeLineNumbers(),
+    AddProperty(_("Use relative line numbers"), m_options->GetRelativeLineNumbers(),
                 UPDATE_BOOL_CB(SetRelativeLineNumbers));
 
-    AddHeader("What to highlight?");
-    AddProperty("Indentation lines", m_options->GetShowIndentationGuidelines(),
+    AddHeader(_("What to highlight?"));
+    AddProperty(_("Indentation lines"), m_options->GetShowIndentationGuidelines(),
                 UPDATE_BOOL_CB(SetShowIndentationGuidelines));
-    AddProperty("Matching braces", m_options->GetHighlightMatchedBraces(), UPDATE_BOOL_CB(SetHighlightMatchedBraces));
-    AddProperty("Modified lines", m_options->IsTrackChanges(), UPDATE_BOOL_CB(SetTrackChanges));
+    AddProperty(_("Matching braces"), m_options->GetHighlightMatchedBraces(),
+                UPDATE_BOOL_CB(SetHighlightMatchedBraces));
+    AddProperty(_("Modified lines"), m_options->IsTrackChanges(), UPDATE_BOOL_CB(SetTrackChanges));
 
-    AddHeader("Caret line");
-    AddProperty("Enable background colour", m_options->GetHighlightCaretLine(), UPDATE_BOOL_CB(SetHighlightCaretLine));
-    AddProperty("Background colour", m_options->GetCaretLineColour(), UPDATE_COLOUR_CB(SetCaretLineColour));
+    AddHeader(_("Caret line"));
+    AddProperty(_("Enable background colour"), m_options->GetHighlightCaretLine(),
+                UPDATE_BOOL_CB(SetHighlightCaretLine));
+    AddProperty(_("Background colour"), m_options->GetCaretLineColour(), UPDATE_COLOUR_CB(SetCaretLineColour));
 
     AddHeader(_("Right margin indicator"));
     AddProperty(_("Show right margin indicator"), m_options->IsShowRightMarginIndicator(),
                 UPDATE_BOOL_CB(SetShowRightMarginIndicator));
     AddProperty(_("Indicator column"), m_options->GetRightMarginColumn(), UPDATE_INT_CB(SetRightMarginColumn));
 
-    AddHeader("Debugger line");
-    AddProperty("Enable background colour", m_options->HasOption(OptionsConfig::Opt_Mark_Debugger_Line),
+    AddHeader(_("Debugger line"));
+    AddProperty(_("Enable background colour"), m_options->HasOption(OptionsConfig::Opt_Mark_Debugger_Line),
                 UPDATE_OPTION_CB(Opt_Mark_Debugger_Line));
-    AddProperty("Background colour", m_options->GetDebuggerMarkerLine(), UPDATE_COLOUR_CB(SetDebuggerMarkerLine));
+    AddProperty(_("Background colour"), m_options->GetDebuggerMarkerLine(), UPDATE_COLOUR_CB(SetDebuggerMarkerLine));
 
-    AddHeader("Whitespace");
+    AddHeader(_("Whitespace"));
     {
         wxArrayString options;
-        options.Add("Invisible");
-        options.Add("Visible always");
-        options.Add("Visible after indentation");
-        AddProperty("Indentation visibility", options, m_options->GetShowWhitspaces(),
+        options.Add(_("Invisible"));
+        options.Add(_("Visible always"));
+        options.Add(_("Visible after indentation"));
+        AddProperty(_("Indentation visibility"), options, m_options->GetShowWhitspaces(),
                     [this, options](const wxString& label, const wxAny& value) {
                         wxString value_str;
                         if(value.GetAs(&value_str)) {
@@ -56,11 +58,12 @@ EditorOptionsGeneralGuidesPanel::EditorOptionsGeneralGuidesPanel(wxWindow* paren
         options.Add("Default");
         options.Add("Mac (CR)");
         options.Add("Unix (LF)");
-        AddProperty("EOL Style", options, m_options->GetEolMode(), UPDATE_TEXT_CB(SetEolMode));
+        options.Add("Windows (CRLF)");
+        AddProperty(_("EOL Style"), options, m_options->GetEolMode(), UPDATE_TEXT_CB(SetEolMode));
     }
 
     long line_spacing = clConfig::Get().Read("extra_line_spacing", (int)0);
-    AddProperty("Line spacing", line_spacing, [&](const wxString& label, const wxAny& value) {
+    AddProperty(_("Line spacing"), line_spacing, [&](const wxString& label, const wxAny& value) {
         wxUnusedVar(label);
         long spacing;
         if(value.GetAs(&spacing)) {

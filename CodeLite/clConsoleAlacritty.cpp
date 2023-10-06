b8a1cca675495fe1cf68b520466ec17ebd4cef85
Eran Ifrah <eifrah@Erans-Mac-mini.local>
2023-10-06 15:03:43 +0300

    fixed: Alacritty terminal does not "cd" to the target folder

--- CodeLite/clConsoleAlacritty.cpp
+++ CodeLite/clConsoleAlacritty.cpp
@@ -20,46 +20,41 @@ bool clConsoleAlacritty::Start() { return StartProcess(PrepareCommand()); }
 
 wxString clConsoleAlacritty::PrepareCommand()
 {
-    // no args? just return the cmd command
-    const wxString& command = GetCommand();
-    if(command.empty() || m_terminal.empty()) {
-        return m_terminal;
-    }
-
-    wxString command_args;
-    wxString base_command;
+    wxString full_command;
+    wxString command = GetCommand();
+    command.Trim().Trim(false);
 
     if(IsTerminalNeeded()) {
-#ifdef __WXMAC__
-        // m_terminal is already wrapped
-        base_command << m_terminal << " --args";
-#else
-        base_command = WrapWithQuotesIfNeeded(m_terminal);
-#endif
+        if(m_terminal.empty()) {
+            // no terminal, but a terminal is required...
+            return wxEmptyString;
+        }
+
+        // on mac, add `--args` here
+        full_command = m_terminal;
+        MacAddArgsIfNeeded(&full_command);
+        if(!GetWorkingDirectory().empty()) {
+            full_command << " --working-directory " << WrapWithQuotesIfNeeded(GetWorkingDirectory());
+        }
 
         if(IsWaitWhenDone()) {
-            command_args << " --hold";
+            full_command << " --hold";
         }
 
-        if(!GetWorkingDirectory().empty()) {
-            command_args << " --working-directory " << WrapWithQuotesIfNeeded(GetWorkingDirectory());
-        }
-
-        command_args << " -t " << WrapWithQuotesIfNeeded(command);
-
-        // append the command to run
-        command_args << " -e " << WrapWithQuotesIfNeeded(command);
-        if(!GetCommandArgs().IsEmpty()) {
-            command_args << " " << GetCommandArgs();
+        // set the title
+        if(!command.empty()) {
+            full_command << " -t " << WrapWithQuotesIfNeeded(command);
+            full_command << " -e " << command;
         }
     } else {
-        base_command = WrapWithQuotesIfNeeded(command);
-        if(!GetCommandArgs().IsEmpty()) {
-            command_args << " " << GetCommandArgs();
-        }
+        full_command << " " << command;
     }
 
-    wxString cmd = base_command + command_args;
-    clDEBUG() << "Running:" << cmd << endl;
-    return cmd;
+    if(!GetCommandArgs().IsEmpty()) {
+        full_command << " " << GetCommandArgs();
+    }
+
+    full_command.Trim().Trim(false);
+    clDEBUG() << "Running:" << full_command << endl;
+    return full_command;
 }

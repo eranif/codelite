0220080557b3466e627de009958b990e9c800bd4
Eran Ifrah <eran@codelite.org>
2024-03-26 00:05:31 +0200

    use native buttons on GTK, always

--- Plugin/wxCustomControlsGTK.hpp
+++ Plugin/wxCustomControlsGTK.hpp
@@ -1,12 +1,7 @@
 #define wxUSE_NATIVE_CHOICE 0
-#define wxUSE_NATIVE_BUTTON 0
-#if defined(__WXGTK__) && defined(__WXGTK3__)
+#define wxUSE_NATIVE_BUTTON 1
 #define wxUSE_NATIVE_SCROLLBAR 1
-#else
-#define wxUSE_NATIVE_SCROLLBAR 0
-#endif
 #define wxUSE_NATIVE_MENUBAR 1
 #define wxUSE_NATIVE_CAPTION 1
-
-#define wxUSE_NATIVE_TOOLBAR 0
+#define wxUSE_NATIVE_TOOLBAR 1
 #define wxUSE_NATIVE_COMBOBOX 1

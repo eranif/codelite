903353c7f0cb3c09418ac05787653450baa1c13a
Joris Dauphin <Jarod42@users.noreply.github.com>
2024-04-15 21:36:19 +0200

    Remove old sqlite3 references. (#3350)

--- CommentParser/CommentParser/main.cpp
+++ CommentParser/CommentParser/main.cpp
@@ -6,7 +6,6 @@
 int main(int argc, char **argv)
 {
 	CommentParseResult comments;
-//	ParseComments("C:\\Users\\eran\\src\\Development\\C++\\codelite\\CL_trunk\\sqlite3\\sqlite3.h", comments);
 	ParseComments("../test.h", comments);
 	
 	comments.print();

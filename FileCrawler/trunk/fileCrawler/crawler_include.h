3e78709fffc7905e4aecd5591d73c36e654e2393
Jarod42 <Jarod42@users.noreply.github.com>
2024-04-17 13:49:21 +0200

    Remove FileCrawler project. Purpose is to copy/test file generated by crawler.l

--- FileCrawler/trunk/fileCrawler/crawler_include.h
+++
@@ -1,9 +0,0 @@
-#ifndef CRAWLER_INCLUDE_H
-#define CRAWLER_INCLUDE_H
-
-#include <vector>
-#include "fc_fileopener.h"
-
-extern WXDLLIMPEXP_CL int crawlerScan        ( const char* filePath );
-
-#endif

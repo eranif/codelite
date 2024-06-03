cbb3cd883285c87d4116a45ac21c245406bbc108
Anders Jenbo <anders@jenbo.dk>
2024-06-03 12:15:36 +0200

    Update PHP stubs

--- codelitephp/resources/cc/mysqlnd.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-// Start of mysqlnd v.mysqlnd 5.0.11-dev - 20120503 - $Id: f373ea5dd5538761406a8022a4b8a374418b240e $
-// End of mysqlnd v.mysqlnd 5.0.11-dev - 20120503 - $Id: f373ea5dd5538761406a8022a4b8a374418b240e $
-?>
--- codelitephp/resources/cc/superglobals.php
+++ codelitephp/resources/cc/superglobals.php
@@ -1,14 +1,12 @@
 <?php
 
-define ('$_FILES', "");
+define('$_FILES', "");
 define('$_ENV', "");
 define('$_FILES', "");
 define('$_GET', "");
 define('$_POST', "");
-define('$_REQUEST', ""); 
-define('$_SERVER', ""); 
+define('$_REQUEST', "");
+define('$_SERVER', "");
 define('$_SESSION', "");
 define('$GLOBALS', "");
 define('$_COOKIE', "");
-
-?>

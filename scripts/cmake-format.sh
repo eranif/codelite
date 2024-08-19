c7d1806eb37ed5722b23d6e04771701111e2f3a3
Jarod42 <Jarod42@users.noreply.github.com>
2024-08-19 18:48:56 +0200

    Move yaml-cpp/ into directory submodules/

--- scripts/cmake-format.sh
+++ scripts/cmake-format.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cmake_files=$(find . -name "CMakeLists.txt"|grep -v "submodules"|grep -v yaml-cpp|grep -v "build-*"|grep -v ctagsd)
+cmake_files=$(find . -name "CMakeLists.txt"|grep -v "submodules"|grep -v "build-*"|grep -v ctagsd)
 for cmake_lists in $cmake_files; do
   echo Formatting ... $cmake_lists
   cmake-format --line-width=120 --tab-size=4 -i $cmake_lists

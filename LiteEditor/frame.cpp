005407a4d5bacc8503040efabae7bd59f63e03a7
Eran Ifrah <eran@codelite.org>
2024-11-16 21:11:21 +0200

    Removed log messages (used for debugging purposes)
    Signed-off-by: Eran Ifrah <eran@codelite.org>

--- LiteEditor/frame.cpp
+++ LiteEditor/frame.cpp
@@ -1924,44 +1924,35 @@ void clMainFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
 
 void clMainFrame::OnClose(wxCloseEvent& event)
 {
-    clSYSTEM() << "Closing main frame" << endl;
     if (!SaveLayoutAndSession()) {
         event.Veto();
         event.Skip(false);
         return;
     }
 
-    clSYSTEM() << "..Saving general settings" << endl;
     SaveGeneralSettings();
-
     event.Skip();
 
     wxString msg;
     ManagerST::Get()->SetShutdownInProgress(true);
 
     // Notify the plugins that we are going down
-    clSYSTEM() << "..Notifying plugins to terminate" << endl;
     clCommandEvent eventGoingDown(wxEVT_GOING_DOWN);
     EventNotifier::Get()->ProcessEvent(eventGoingDown);
-    clSYSTEM() << "..Notifying plugins to terminate...done" << endl;
 
     // Stop the search thread
-    clSYSTEM() << "..Stopping search threads" << endl;
     ManagerST::Get()->KillProgram();
     SearchThreadST::Get()->StopSearch();
 
     // Stop any debugging session if any
-    clSYSTEM() << "..Stopping any running debugger" << endl;
     IDebugger* debugger = DebuggerMgr::Get().GetActiveDebugger();
     if (debugger && debugger->IsRunning()) {
         ManagerST::Get()->DbgStop();
     }
 
-    clSYSTEM() << "..Flushing clipboard" << endl;
     // In case we got some data in the clipboard, flush it so it will be available
     // after our process exits
     wxTheClipboard->Flush();
-    clSYSTEM() << "Closing main frame...done" << endl;
 }
 
 void clMainFrame::LoadSession(const wxString& sessionName)

dd85402dce11fff62d71b44efb3fcd7b2cf28030
Eran Ifrah <eran@codelite.org>
2025-01-02 00:08:28 +0200

    Fixed build error on Windows
    Signed-off-by: Eran Ifrah <eran@codelite.org>

--- TestDir/makedir.cpp
+++ TestDir/makedir.cpp
@@ -8,12 +8,18 @@
 #include <wx/tokenzr.h>
 
 #if wxVERSION_NUMBER < 2900
-static const wxCmdLineEntryDesc cmdLineDesc[] = { { wxCMD_LINE_PARAM, NULL, NULL, wxT("Directory name"),
+static const wxCmdLineEntryDesc cmdLineDesc[] = { { wxCMD_LINE_PARAM,
+                                                    NULL,
+                                                    NULL,
+                                                    wxT("Directory name"),
                                                     wxCMD_LINE_VAL_STRING,
                                                     wxCMD_LINE_PARAM_MULTIPLE | wxCMD_LINE_PARAM_OPTIONAL },
                                                   { wxCMD_LINE_NONE } };
 #else
-static const wxCmdLineEntryDesc cmdLineDesc[] = { { wxCMD_LINE_PARAM, NULL, NULL, "Directory name",
+static const wxCmdLineEntryDesc cmdLineDesc[] = { { wxCMD_LINE_PARAM,
+                                                    NULL,
+                                                    NULL,
+                                                    "Directory name",
                                                     wxCMD_LINE_VAL_STRING,
                                                     wxCMD_LINE_PARAM_MULTIPLE | wxCMD_LINE_PARAM_OPTIONAL },
                                                   { wxCMD_LINE_NONE } };
@@ -30,13 +36,13 @@ int main(int argc, char** argv)
     parser.SetCmdLine(argc, argv);
     parser.SetDesc(cmdLineDesc);
 
-    if(parser.Parse() != 0) {
+    if (parser.Parse() != 0) {
         return -1;
     }
 
-    for(size_t i = 0; i < parser.GetParamCount(); i++) {
+    for (size_t i = 0; i < parser.GetParamCount(); i++) {
         wxString argument = parser.GetParam(i);
-        if(!wxDir::Exists(argument)) {
+        if (!wxDir::Exists(argument)) {
             argument.Replace(wxT("\\"), wxT("/"));
             wxArrayString arr = wxStringTokenize(argument, wxT("/"), wxTOKEN_STRTOK);
             wxString path;

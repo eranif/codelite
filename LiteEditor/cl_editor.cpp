e12734739f3f0dbf383ac9e42659f8b6111dd164
Jarod42 <Jarod42@users.noreply.github.com>
2025-06-25 07:51:10 +0200

    [cleanup] Replace `wxIsFileSymlink` by `FileUtils::IsSymlink`

--- LiteEditor/cl_editor.cpp
+++ LiteEditor/cl_editor.cpp
@@ -1810,7 +1810,7 @@ bool clEditor::SaveToFile(const wxFileName& fileName)
     file.Close();
 
     wxFileName symlinkedFile = fileName;
-    if (wxIsFileSymlink(fileName)) {
+    if (FileUtils::IsSymlink(fileName)) {
         symlinkedFile = wxReadLink(fileName);
     }
 

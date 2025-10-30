5005a20784b29ebbf19be9a1bec09fe64c5a62d1
Eran Ifrah <eran@codelite.org>
2025-10-30 23:18:34 +0200

    Add conditional ZoomNavigator support and improve CMake utilities
    - Detect wxWidgets version (≥ 3.3.0) and automatically disable the ZoomNavigator plugin, enabling MiniMap instead on UNIX, macOS and MinGW builds.
    - Introduced `extract_wx_version` function in **UtilsHelper.cmake** to parse `wx/version.h` for major/minor numbers.
    - Updated `CMakeLists.txt` to use the new version detection logic, wrap ZoomNavigator inclusion in `if(wxWITH_ZOOM_NAVIGATOR)`, and add status messages.
    - Refactored OSX installation macros in **OSXInstall.cmake** for cleaner indentation, consistent `if(APPLE)` blocks, and streamlined file installations.
    - Simplified script and executable install macros, added executable permission handling, and removed redundant code.
    - Re‑worked OS detection logic to reliably distinguish MSYS2, Cygwin, and native Windows shells.
    - Added comments and improved readability throughout the CMake modules.
    
    ** Generated by CodeLite. **
    
    Signed-off-by: Eran Ifrah <eran@codelite.org>

--- cmake/Modules/OSXInstall.cmake
+++ cmake/Modules/OSXInstall.cmake
@@ -1,183 +1,213 @@
 cmake_minimum_required(VERSION 3.5)
 
 macro(_FIND_WX_LIBRARIES)
-    if(APPLE)
-        if(NOT _WX_LIBS_DIR)
-            execute_process(
-                COMMAND wx-config --libs
-                OUTPUT_VARIABLE WX_LIBSOUTPUT
-                OUTPUT_STRIP_TRAILING_WHITESPACE)
-            if(${WX_LIBSOUTPUT} MATCHES "^-L.+") # In recent, multi-architecture, distro versions it'll start with
-                                                 # -L/foo/bar
-                string(REGEX REPLACE "^-L([^ ;]+).*" "\\1" _WX_LIBS_DIR ${WX_LIBSOUTPUT})
-                message("-- _WX_LIBS_DIR is set to ${_WX_LIBS_DIR}")
-            endif()
-            set(minusElPos -1)
-            string(FIND ${WX_LIBSOUTPUT} "-l" minusElPos REVERSE)
-            math(EXPR minusElPos "${minusElPos}+2") # Skip the -l
-            string(SUBSTRING ${WX_LIBSOUTPUT} ${minusElPos} -1 _WX_LIB_NAME)
-            message("-- _WX_LIB_NAME is set to ${_WX_LIB_NAME}")
-        endif()
+  if(APPLE)
+    if(NOT _WX_LIBS_DIR)
+      execute_process(
+        COMMAND wx-config --libs
+        OUTPUT_VARIABLE WX_LIBSOUTPUT
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+      if(${WX_LIBSOUTPUT} MATCHES "^-L.+") # In recent, multi-architecture,
+                                           # distro versions it'll start with
+                                           # -L/foo/bar
+        string(REGEX REPLACE "^-L([^ ;]+).*" "\\1" _WX_LIBS_DIR
+                             ${WX_LIBSOUTPUT})
+        message("-- _WX_LIBS_DIR is set to ${_WX_LIBS_DIR}")
+      endif()
+      set(minusElPos -1)
+      string(FIND ${WX_LIBSOUTPUT} "-l" minusElPos REVERSE)
+      math(EXPR minusElPos "${minusElPos}+2") # Skip the -l
+      string(SUBSTRING ${WX_LIBSOUTPUT} ${minusElPos} -1 _WX_LIB_NAME)
+      message("-- _WX_LIB_NAME is set to ${_WX_LIB_NAME}")
     endif()
+  endif()
 endmacro()
 
 # execute the macro once this file is included
 _find_wx_libraries()
 
 # --------------------------------------------------------------------
-# Install a file into /usr/lib/codelite/share or codelite.app/Contents/SharedSupport
+# Install a file into /usr/lib/codelite/share or
+# codelite.app/Contents/SharedSupport
 # --------------------------------------------------------------------
 macro(CL_INSTALL_FILE_SHARED _filename_)
-    install(FILES ${_filename_} DESTINATION ${CL_RESOURCES_DIR})
+  install(FILES ${_filename_} DESTINATION ${CL_RESOURCES_DIR})
 endmacro()
 
 # ------------------------------------
 # install a plugin
 # ------------------------------------
 macro(CL_INSTALL_PLUGIN _target_)
-    if(APPLE)
-        install(TARGETS ${_target_} DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/plugins)
-    else()
-        install(TARGETS ${_target_} DESTINATION ${PLUGINS_DIR})
-    endif()
+  if(APPLE)
+    install(
+      TARGETS ${_target_}
+      DESTINATION
+        ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/plugins)
+  else()
+    install(TARGETS ${_target_} DESTINATION ${PLUGINS_DIR})
+  endif()
 endmacro()
 
 # ------------------------------------
 # install an executable
 # ------------------------------------
 macro(CL_INSTALL_EXECUTABLE _target_)
-    if(APPLE)
-        install(TARGETS ${_target_} DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/MacOS/)
-    else()
-        set(EXE_PERM
-            OWNER_EXECUTE
-            OWNER_WRITE
-            OWNER_READ
-            GROUP_EXECUTE
-            GROUP_READ
-            WORLD_EXECUTE
-            WORLD_READ)
+  if(APPLE)
+    install(TARGETS ${_target_}
+            DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/MacOS/)
+  else()
+    set(EXE_PERM
+        OWNER_EXECUTE
+        OWNER_WRITE
+        OWNER_READ
+        GROUP_EXECUTE
+        GROUP_READ
+        WORLD_EXECUTE
+        WORLD_READ)
 
-        install(
-            TARGETS ${_target_}
-            DESTINATION ${CL_INSTALL_BIN}
-            PERMISSIONS ${EXE_PERM})
-    endif()
+    install(
+      TARGETS ${_target_}
+      DESTINATION ${CL_INSTALL_BIN}
+      PERMISSIONS ${EXE_PERM})
+  endif()
 endmacro()
 
 # ------------------------------------
 # install a debugger shared library
 # ------------------------------------
 macro(CL_INSTALL_DEBUGGER _target_)
-    if(APPLE)
-        install(TARGETS ${_target_} DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/debuggers)
-    elseif(MINGW)
-        install(TARGETS ${_target_} DESTINATION ${CL_INSTALL_BIN}/debuggers)
-    else()
-        install(TARGETS ${PLUGIN_NAME} DESTINATION ${PLUGINS_DIR}/debuggers)
-    endif()
+  if(APPLE)
+    install(
+      TARGETS ${_target_}
+      DESTINATION
+        ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/debuggers)
+  elseif(MINGW)
+    install(TARGETS ${_target_} DESTINATION ${CL_INSTALL_BIN}/debuggers)
+  else()
+    install(TARGETS ${PLUGIN_NAME} DESTINATION ${PLUGINS_DIR}/debuggers)
+  endif()
 endmacro()
 
 # -------------------------------------------------
 # Prepare a skeleton bundle for CodeLite
 # -------------------------------------------------
 macro(OSX_MAKE_BUNDLE_DIRECTORY)
-    if(APPLE)
-        if(NOT CL_SRC_ROOT)
-            set(CL_SRC_ROOT "/Users/eran/devl/codelite")
-        endif()
-
-        file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/codelite.app)
-        file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/codelite.app/Contents)
-        file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/codelite.app/Contents/MacOS)
-        file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/codelite.app/Contents/Resources)
-        file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport)
-        file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/plugins)
-        file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/resources)
-        file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/debuggers)
-        file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/config)
-        file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/config/cppcheck)
-        file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/dics)
-
-        file(COPY ${CL_SRC_ROOT}/svgs/logo/osx/icon.icns
-             DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/Resources)
-
-        file(COPY ${CL_SRC_ROOT}/Runtime/cl_workspace.icns
-             DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/Resources)
-
-        # Copy Info.plist
-        file(COPY ${CL_SRC_ROOT}/Runtime/Info.plist DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents)
-
-        # Copy external libraries into the bundle folder
-        _find_wx_libraries()
-
-        set(WX_DYLIB ${_WX_LIBS_DIR}/lib${_WX_LIB_NAME}.dylib)
-        message(STATUS "wxWidgets lib is: ${WX_DYLIB}")
-
-        file(GLOB WXLIBS ${_WX_LIBS_DIR}/lib${_WX_LIB_NAME}*.dylib)
-        foreach(WXLIB ${WXLIBS})
-            file(COPY ${WXLIB} DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/MacOS)
-        endforeach()
-        # Copy Terminal.app launcher script
-        file(
-            COPY ${CL_SRC_ROOT}/Runtime/osx-terminal.sh
-            DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/MacOS
-            FILE_PERMISSIONS ${EXE_PERM})
-
-        # codelite-clang-format
-        file(COPY ${CL_SRC_ROOT}/tools/macOS/clang-format DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/MacOS/)
-
-        # folders
-        install(
-            DIRECTORY ${CL_SRC_ROOT}/Runtime/plugins/resources
-            DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/
-            USE_SOURCE_PERMISSIONS
-            PATTERN ".svn" EXCLUDE
-            PATTERN ".git" EXCLUDE)
-
-        install(
-            DIRECTORY ${CL_SRC_ROOT}/Runtime/gdb_printers ${CL_SRC_ROOT}/Runtime/src/
-                      ${CL_SRC_ROOT}/Runtime/lexers ${CL_SRC_ROOT}/Runtime/templates ${CL_SRC_ROOT}/Runtime/rc
-            DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/
-            USE_SOURCE_PERMISSIONS
-            PATTERN ".svn" EXCLUDE
-            PATTERN ".git" EXCLUDE)
-
-        install(FILES ${CL_SRC_ROOT}/Runtime/config/codelite.layout.default
-                DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/codelite.layout)
-
-        install(FILES ${CL_SRC_ROOT}/Runtime/config/codelite.layout.default
-                DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/codelite.layout)
-
-        install(
-            DIRECTORY ${CL_SRC_ROOT}/sdk/codelite_cppcheck/cfg/
-            DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/config/cppcheck/
-            FILES_MATCHING
-            PATTERN "*.cfg")
-
-        install(
-            DIRECTORY ${CL_SRC_ROOT}/Runtime/
-            DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/
-            FILES_MATCHING
-            PATTERN "*.zip")
-
-        install(
-            FILES ${CL_SRC_ROOT}/Runtime/config/build_settings.xml.default.mac
-            DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/config/
-            RENAME build_settings.xml.default)
-
-        install(FILES ${CL_SRC_ROOT}/Runtime/config/plugins.xml.default
-                DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/config/)
-        install(FILES ${CL_SRC_ROOT}/LICENSE DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport)
-
-        install(
-            FILES ${CL_SRC_ROOT}/Runtime/config/codelite.xml.default.mac
-            DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/config
-            RENAME codelite.xml.default)
-
-        install(
-            FILES ${CL_SRC_ROOT}/Runtime/config/debuggers.xml.mac
-            DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/config
-            RENAME debuggers.xml.default)
+  if(APPLE)
+    if(NOT CL_SRC_ROOT)
+      set(CL_SRC_ROOT "/Users/eran/devl/codelite")
     endif()
+
+    file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/codelite.app)
+    file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/codelite.app/Contents)
+    file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/codelite.app/Contents/MacOS)
+    file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/codelite.app/Contents/Resources)
+    file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport)
+    file(MAKE_DIRECTORY
+         ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/plugins)
+    file(MAKE_DIRECTORY
+         ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/resources)
+    file(MAKE_DIRECTORY
+         ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/debuggers)
+    file(MAKE_DIRECTORY
+         ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/config)
+    file(
+      MAKE_DIRECTORY
+      ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/config/cppcheck)
+    file(MAKE_DIRECTORY
+         ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/dics)
+
+    file(COPY ${CL_SRC_ROOT}/svgs/logo/osx/icon.icns
+         DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/Resources)
+
+    file(COPY ${CL_SRC_ROOT}/Runtime/cl_workspace.icns
+         DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/Resources)
+
+    # Copy Info.plist
+    file(COPY ${CL_SRC_ROOT}/Runtime/Info.plist
+         DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents)
+
+    # Copy external libraries into the bundle folder
+    _find_wx_libraries()
+
+    set(WX_DYLIB ${_WX_LIBS_DIR}/lib${_WX_LIB_NAME}.dylib)
+    message(STATUS "wxWidgets lib is: ${WX_DYLIB}")
+
+    file(GLOB WXLIBS ${_WX_LIBS_DIR}/lib${_WX_LIB_NAME}*.dylib)
+    foreach(WXLIB ${WXLIBS})
+      file(COPY ${WXLIB}
+           DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/MacOS)
+    endforeach()
+    # Copy Terminal.app launcher script
+    file(
+      COPY ${CL_SRC_ROOT}/Runtime/osx-terminal.sh
+      DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/MacOS
+      FILE_PERMISSIONS ${EXE_PERM})
+
+    # codelite-clang-format
+    file(COPY ${CL_SRC_ROOT}/tools/macOS/clang-format
+         DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/MacOS/)
+
+    # folders
+    install(
+      DIRECTORY ${CL_SRC_ROOT}/Runtime/plugins/resources
+      DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/
+      USE_SOURCE_PERMISSIONS
+      PATTERN ".svn" EXCLUDE
+      PATTERN ".git" EXCLUDE)
+
+    install(
+      DIRECTORY ${CL_SRC_ROOT}/Runtime/gdb_printers ${CL_SRC_ROOT}/Runtime/src/
+                ${CL_SRC_ROOT}/Runtime/lexers ${CL_SRC_ROOT}/Runtime/templates
+                ${CL_SRC_ROOT}/Runtime/rc
+      DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/
+      USE_SOURCE_PERMISSIONS
+      PATTERN ".svn" EXCLUDE
+      PATTERN ".git" EXCLUDE)
+
+    install(
+      FILES ${CL_SRC_ROOT}/Runtime/config/codelite.layout.default
+      DESTINATION
+        ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/codelite.layout)
+
+    install(
+      FILES ${CL_SRC_ROOT}/Runtime/config/codelite.layout.default
+      DESTINATION
+        ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/codelite.layout)
+
+    install(
+      DIRECTORY ${CL_SRC_ROOT}/sdk/codelite_cppcheck/cfg/
+      DESTINATION
+        ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/config/cppcheck/
+      FILES_MATCHING
+      PATTERN "*.cfg")
+
+    install(
+      DIRECTORY ${CL_SRC_ROOT}/Runtime/
+      DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/
+      FILES_MATCHING
+      PATTERN "*.zip")
+
+    install(
+      FILES ${CL_SRC_ROOT}/Runtime/config/build_settings.xml.default.mac
+      DESTINATION
+        ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/config/
+      RENAME build_settings.xml.default)
+
+    install(
+      FILES ${CL_SRC_ROOT}/Runtime/config/plugins.xml.default
+      DESTINATION
+        ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/config/)
+    install(FILES ${CL_SRC_ROOT}/LICENSE
+            DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport)
+
+    install(
+      FILES ${CL_SRC_ROOT}/Runtime/config/codelite.xml.default.mac
+      DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/config
+      RENAME codelite.xml.default)
+
+    install(
+      FILES ${CL_SRC_ROOT}/Runtime/config/debuggers.xml.mac
+      DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/SharedSupport/config
+      RENAME debuggers.xml.default)
+  endif()
 endmacro()
--- cmake/Modules/UtilsHelper.cmake
+++ cmake/Modules/UtilsHelper.cmake
@@ -4,10 +4,77 @@ set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/cmake/Modules/")
 
 include(OSXInstall)
 
+# --------------------------------------------------------------------
+# extract_wx_version( HEADER_FILE          – path to the header that contains
+# the macros OUT_MAJOR_VAR        – name of the variable that will receive the
+# major number OUT_MINOR_VAR        – name of the variable that will receive the
+# minor number )
+#
+# The function expects the header to contain lines that look like: #define
+# wxMAJOR_VERSION  3 #define wxMINOR_VERSION  3
+#
+# If a macro cannot be found the corresponding output variable is left untouched
+# (you can test it with `if(DEFINED ...)` after the call).
+# --------------------------------------------------------------------
+function(extract_wx_version HEADER_FILE OUT_MAJOR_VAR OUT_MINOR_VAR)
+  # -----------------------------------------------------------------
+  # 1. Read the whole file into a CMake string.
+  # -----------------------------------------------------------------
+  if(NOT EXISTS "${HEADER_FILE}")
+    message(FATAL_ERROR "Header file not found: ${HEADER_FILE}")
+  endif()
+
+  file(READ "${HEADER_FILE}" _header_content)
+
+  # -----------------------------------------------------------------
+  # 1. Use a regular expression to pull the numbers out.
+  # -----------------------------------------------------------------
+  string(REGEX MATCH "define wxMAJOR_VERSION([ ])+([0-9]+)" _major_match
+               "${_header_content}")
+  string(REGEX MATCH "define wxMINOR_VERSION([ ])+([0-9]+)" _minor_match
+               "${_header_content}")
+
+  # Extract the captured numbers (they end up in CMAKE_MATCH_2)
+  if(_major_match)
+    set(${OUT_MAJOR_VAR}
+        "${CMAKE_MATCH_2}"
+        PARENT_SCOPE)
+  else()
+    message(WARNING "wxMAJOR_VERSION macro not found in ${HEADER_FILE}")
+  endif()
+
+  if(_minor_match)
+    set(${OUT_MINOR_VAR}
+        "${CMAKE_MATCH_2}"
+        PARENT_SCOPE)
+  else()
+    message(WARNING "wxMINOR_VERSION macro not found in ${HEADER_FILE}")
+  endif()
+endfunction()
+
 # ------------------------------------
 # install script
 # ------------------------------------
 macro(codelite_install_script _script_)
+  set(EXE_PERM
+      OWNER_EXECUTE
+      OWNER_WRITE
+      OWNER_READ
+      GROUP_EXECUTE
+      GROUP_READ
+      WORLD_EXECUTE
+      WORLD_READ)
+  install(
+    FILES ${_script_}
+    DESTINATION ${CL_INSTALL_BIN}
+    PERMISSIONS ${EXE_PERM})
+endmacro()
+
+# ------------------------------------
+# install an executable
+# ------------------------------------
+macro(codelite_install_executable TARGET)
+  if(NOT APPLE)
     set(EXE_PERM
         OWNER_EXECUTE
         OWNER_WRITE
@@ -16,103 +83,87 @@ macro(codelite_install_script _script_)
         GROUP_READ
         WORLD_EXECUTE
         WORLD_READ)
+
     install(
-        FILES ${_script_}
-        DESTINATION ${CL_INSTALL_BIN}
-        PERMISSIONS ${EXE_PERM})
-endmacro()
-
-# ------------------------------------
-# install an executable
-# ------------------------------------
-macro(codelite_install_executable TARGET)
-    if(NOT APPLE)
-        set(EXE_PERM
-            OWNER_EXECUTE
-            OWNER_WRITE
-            OWNER_READ
-            GROUP_EXECUTE
-            GROUP_READ
-            WORLD_EXECUTE
-            WORLD_READ)
-
-        install(
-            TARGETS ${TARGET}
-            DESTINATION ${CL_INSTALL_BIN}
-            PERMISSIONS ${EXE_PERM})
-    endif()
+      TARGETS ${TARGET}
+      DESTINATION ${CL_INSTALL_BIN}
+      PERMISSIONS ${EXE_PERM})
+  endif()
 endmacro()
 
 # ------------------------------------
 # install an executable
 # ------------------------------------
 macro(codelite_install_svgs)
-    install(
-        DIRECTORY ${CL_SRC_ROOT}/svgs/light-theme ${CL_SRC_ROOT}/svgs/dark-theme
-        DESTINATION ${CL_RESOURCES_DIR}/svgs
-        FILES_MATCHING
-        PATTERN "*.svg")
+  install(
+    DIRECTORY ${CL_SRC_ROOT}/svgs/light-theme ${CL_SRC_ROOT}/svgs/dark-theme
+    DESTINATION ${CL_RESOURCES_DIR}/svgs
+    FILES_MATCHING
+    PATTERN "*.svg")
 endmacro()
 
 function(get_distro_name DISTRO_NAME)
-    execute_process(
-        COMMAND /bin/bash "-c" "cat /etc/os-release |grep ^ID=|cut -d = -f 2"
-        OUTPUT_VARIABLE _DISTRO_ID
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
-    execute_process(
-        COMMAND /bin/bash "-c" "cat /etc/os-release |grep VERSION_ID=|cut -d = -f 2"
-        OUTPUT_VARIABLE _VERSION_ID
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
+  execute_process(
+    COMMAND /bin/bash "-c" "cat /etc/os-release |grep ^ID=|cut -d = -f 2"
+    OUTPUT_VARIABLE _DISTRO_ID
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+  execute_process(
+    COMMAND /bin/bash "-c" "cat /etc/os-release |grep VERSION_ID=|cut -d = -f 2"
+    OUTPUT_VARIABLE _VERSION_ID
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-    # clean the output
-    string(REPLACE "\"" "" _DISTRO_ID "${_DISTRO_ID}")
-    string(REPLACE "\"" "" _VERSION_ID "${_VERSION_ID}")
-    string(REPLACE "." "" _DISTRO_ID "${_DISTRO_ID}")
-    string(REPLACE "." "" _VERSION_ID "${_VERSION_ID}")
-    set(${DISTRO_NAME} "")
-    string(FIND ${_DISTRO_ID} "fedora" POS)
-    if(POS GREATER -1)
-        set(${DISTRO_NAME}
-            "fedora_${_VERSION_ID}"
-            PARENT_SCOPE)
-    endif()
+  # clean the output
+  string(REPLACE "\"" "" _DISTRO_ID "${_DISTRO_ID}")
+  string(REPLACE "\"" "" _VERSION_ID "${_VERSION_ID}")
+  string(REPLACE "." "" _DISTRO_ID "${_DISTRO_ID}")
+  string(REPLACE "." "" _VERSION_ID "${_VERSION_ID}")
+  set(${DISTRO_NAME} "")
+  string(FIND ${_DISTRO_ID} "fedora" POS)
+  if(POS GREATER -1)
+    set(${DISTRO_NAME}
+        "fedora_${_VERSION_ID}"
+        PARENT_SCOPE)
+  endif()
 
-    string(FIND ${_DISTRO_ID} "ubuntu" POS)
-    if(POS GREATER -1)
-        set(${DISTRO_NAME}
-            "${_DISTRO_ID}_${_VERSION_ID}"
-            PARENT_SCOPE)
-    endif()
+  string(FIND ${_DISTRO_ID} "ubuntu" POS)
+  if(POS GREATER -1)
+    set(${DISTRO_NAME}
+        "${_DISTRO_ID}_${_VERSION_ID}"
+        PARENT_SCOPE)
+  endif()
 
-    string(FIND ${_DISTRO_ID} "debian" POS)
-    if(POS GREATER -1)
-        set(${DISTRO_NAME}
-            "${_DISTRO_ID}_${_VERSION_ID}"
-            PARENT_SCOPE)
-    endif()
+  string(FIND ${_DISTRO_ID} "debian" POS)
+  if(POS GREATER -1)
+    set(${DISTRO_NAME}
+        "${_DISTRO_ID}_${_VERSION_ID}"
+        PARENT_SCOPE)
+  endif()
 endfunction()
 
 if(MINGW)
-    macro(msys_list_deps __NAME__ OUT_LIST)
-        execute_process(
-            COMMAND sh -c "/usr/bin/cygcheck ${__NAME__} |grep clang64|grep -v -w Found|grep -v ${__NAME__}"
-            OUTPUT_VARIABLE __dep_list
-            OUTPUT_STRIP_TRAILING_WHITESPACE)
-        string(REPLACE "\n" ";" ${OUT_LIST} "${__dep_list}")
-        string(REPLACE " " "" ${OUT_LIST} "${${OUT_LIST}}")
-        string(REPLACE "\\" "/" ${OUT_LIST} "${${OUT_LIST}}")
-    endmacro()
+  macro(msys_list_deps __NAME__ OUT_LIST)
+    execute_process(
+      COMMAND
+        sh -c
+        "/usr/bin/cygcheck ${__NAME__} |grep clang64|grep -v -w Found|grep -v ${__NAME__}"
+      OUTPUT_VARIABLE __dep_list
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+    string(REPLACE "\n" ";" ${OUT_LIST} "${__dep_list}")
+    string(REPLACE " " "" ${OUT_LIST} "${${OUT_LIST}}")
+    string(REPLACE "\\" "/" ${OUT_LIST} "${${OUT_LIST}}")
+  endmacro()
 
-    macro(msys_install_clang64_tool tool_name install_dir)
-        msys_list_deps(${MSYS2_BASE}/clang64/bin/${tool_name} CLANGD_DEPS)
-        # install the tool itself
-        install(FILES "${MSYS2_BASE}/clang64/bin/${tool_name}" DESTINATION ${install_dir})
+  macro(msys_install_clang64_tool tool_name install_dir)
+    msys_list_deps(${MSYS2_BASE}/clang64/bin/${tool_name} CLANGD_DEPS)
+    # install the tool itself
+    install(FILES "${MSYS2_BASE}/clang64/bin/${tool_name}"
+            DESTINATION ${install_dir})
 
-        # and all its dlls
-        foreach(DLL ${CLANGD_DEPS})
-            install(FILES "${DLL}" DESTINATION ${install_dir})
-        endforeach()
-    endmacro()
+    # and all its dlls
+    foreach(DLL ${CLANGD_DEPS})
+      install(FILES "${DLL}" DESTINATION ${install_dir})
+    endforeach()
+  endmacro()
 endif()
 
 set(PCH_HEADERS_LIST
@@ -161,55 +212,57 @@ set(PCH_HEADERS_LIST
     <map>
     <memory>)
 macro(codelite_add_exported_pch _TARGET_)
-    target_precompile_headers(${_TARGET_} PUBLIC ${PCH_HEADERS_LIST})
+  target_precompile_headers(${_TARGET_} PUBLIC ${PCH_HEADERS_LIST})
 endmacro()
 
 macro(codelite_add_pch _TARGET_)
-    target_precompile_headers(${_TARGET_} PRIVATE ${PCH_HEADERS_LIST})
+  target_precompile_headers(${_TARGET_} PRIVATE ${PCH_HEADERS_LIST})
 endmacro()
 
 # ------------------------------------
 # install a library (shared object, but not a plugin)
 # ------------------------------------
 macro(codelite_install_library_target TARGET)
-    if(APPLE)
-        install(TARGETS ${TARGET} DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/MacOS/)
-    elseif(MINGW)
-        # under windows (MinGW) we install libraries under the "bin" folder
-        install(TARGETS ${TARGET} DESTINATION "${CL_INSTALL_BIN}")
-    else()
-        # Under linux, we install libraries in the plugins directory
-        install(TARGETS ${TARGET} DESTINATION ${PLUGINS_DIR})
-    endif()
+  if(APPLE)
+    install(TARGETS ${TARGET}
+            DESTINATION ${CMAKE_BINARY_DIR}/codelite.app/Contents/MacOS/)
+  elseif(MINGW)
+    # under windows (MinGW) we install libraries under the "bin" folder
+    install(TARGETS ${TARGET} DESTINATION "${CL_INSTALL_BIN}")
+  else()
+    # Under linux, we install libraries in the plugins directory
+    install(TARGETS ${TARGET} DESTINATION ${PLUGINS_DIR})
+  endif()
 endmacro()
 
-# Determine if we are running on Windows using MSYS2 shell or using MinGW tools (but not using MSYS)
+# Determine if we are running on Windows using MSYS2 shell or using MinGW tools
+# (but not using MSYS)
 if(UNIX)
-    execute_process(
-        COMMAND /bin/sh "-c" "uname -s|grep MSYS|cut -d_ -f1"
-        OUTPUT_VARIABLE _OS_NAME
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
-    string(FIND "${_OS_NAME}" "MSYS" POS)
-    if(POS GREATER -1)
-        set(MSYS_SHELL 1)
-        set(MSW 1)
-    endif()
+  execute_process(
+    COMMAND /bin/sh "-c" "uname -s|grep MSYS|cut -d_ -f1"
+    OUTPUT_VARIABLE _OS_NAME
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+  string(FIND "${_OS_NAME}" "MSYS" POS)
+  if(POS GREATER -1)
+    set(MSYS_SHELL 1)
+    set(MSW 1)
+  endif()
 elseif(WIN32)
-    execute_process(
-        COMMAND sh "-c" "uname -o|grep -E 'Msys|Cygwin'|cut -d_ -f1"
-        OUTPUT_VARIABLE _OS_NAME
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
+  execute_process(
+    COMMAND sh "-c" "uname -o|grep -E 'Msys|Cygwin'|cut -d_ -f1"
+    OUTPUT_VARIABLE _OS_NAME
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-    if("${_OS_NAME}" MATCHES "Msys|Cygwin")
-        set(MSYS_SHELL 1)
-        set(MSW 1)
-    else()
-        set(WIN_CMD_SHELL 1)
-        set(MSW 1)
-        execute_process(
-            COMMAND sh "-c" "uname -s"
-            OUTPUT_VARIABLE _OS_NAME
-            OUTPUT_STRIP_TRAILING_WHITESPACE)
-        message(FATAL_ERROR "${_OS_NAME}")
-    endif()
+  if("${_OS_NAME}" MATCHES "Msys|Cygwin")
+    set(MSYS_SHELL 1)
+    set(MSW 1)
+  else()
+    set(WIN_CMD_SHELL 1)
+    set(MSW 1)
+    execute_process(
+      COMMAND sh "-c" "uname -s"
+      OUTPUT_VARIABLE _OS_NAME
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+    message(FATAL_ERROR "${_OS_NAME}")
+  endif()
 endif() # UNIX -> WIN32

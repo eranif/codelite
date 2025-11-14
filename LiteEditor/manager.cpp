ea08860fc526cc9f95e5da02ee5e0e5d6ed9e95e
mvohla <hekla@tutamail.com>
2025-11-14 10:30:37 +0100

    Fix code formatting: Whitespace after 'if'

--- LiteEditor/manager.cpp
+++ LiteEditor/manager.cpp
@@ -1688,14 +1688,14 @@ void Manager::ExecuteNoDebug(const wxString& projectName)
 
     m_programProcess = ::CreateAsyncProcess(this, execLine, createProcessFlags, wd, &env_list);
     if (m_programProcess) {
-        if(clConfig::Get().Read(kConfigClearOutputOnLaunch, false)) {
+        if (clConfig::Get().Read(kConfigClearOutputOnLaunch, false)) {
             clGetManager()->ClearOutputTab(kOutputTab_Output);
         }
         clGetManager()->AppendOutputTabText(
             kOutputTab_Output, wxString() << _("Working directory is set to: ") << wd << "\n", false);
         clGetManager()->AppendOutputTabText(
             kOutputTab_Output, wxString() << _("Executing: ") << execLine << "\n", false);
-        if(clConfig::Get().Read(kConfigShowOutputOnLaunch, false)) {
+        if (clConfig::Get().Read(kConfigShowOutputOnLaunch, false)) {
             clGetManager()->ShowOutputPane("Output");
         }
         // Notify about program execution

0c2c2f7db99f156c0383379b80a1a7d1f2c2f1b2
Joris Dauphin <Jarod42@users.noreply.github.com>
2025-11-17 23:36:05 +0100

    [typo] continous -> continuous (#3754)

--- ContinuousBuild/continuousbuildbasepane_continuousbuild_bitmaps.cpp
+++ ContinuousBuild/continuousbuildbasepane_continuousbuild_bitmaps.cpp
@@ -44,6 +44,6 @@ void wxC47E7InitBitmapResources()
         else wxFileSystem::AddHandler(new wxMemoryFSHandlerBase);
     }
 
-    XRC_ADD_FILE(wxT("XRC_resource/continousbuildbasepane_continuousbuild_bitmaps.cpp$C__src_codelite_ContinuousBuild_continousbuildbasepane_continuousbuild_bitmaps.xrc"), xml_res_file_0, xml_res_size_0, wxT("text/xml"));
-    wxXmlResource::Get()->Load(wxT("memory:XRC_resource/continousbuildbasepane_continuousbuild_bitmaps.cpp$C__src_codelite_ContinuousBuild_continousbuildbasepane_continuousbuild_bitmaps.xrc"));
+    XRC_ADD_FILE(wxT("XRC_resource/continuousbuildbasepane_continuousbuild_bitmaps.cpp$C__src_codelite_ContinuousBuild_continuousbuildbasepane_continuousbuild_bitmaps.xrc"), xml_res_file_0, xml_res_size_0, wxT("text/xml"));
+    wxXmlResource::Get()->Load(wxT("memory:XRC_resource/continuousbuildbasepane_continuousbuild_bitmaps.cpp$C__src_codelite_ContinuousBuild_continuousbuildbasepane_continuousbuild_bitmaps.xrc"));
 }

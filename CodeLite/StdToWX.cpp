6b5948483d7bd4e3e7d6f5712898cfbfc4fce4cf
Joris Dauphin <Jarod42@users.noreply.github.com>
2026-02-25 21:37:33 +0100

    [cleanup] Replace some `StdToWX` method by `std::string` methods (#3848)

--- CodeLite/StdToWX.cpp
+++ CodeLite/StdToWX.cpp
@@ -1,39 +1,12 @@
 #include "StdToWX.h"
 
-void StdToWX::Remove(std::string& str, size_t from, size_t len) { str.erase(from, len); }
-
-bool StdToWX::EndsWith(const std::string& str, const std::string& what)
-{
-    if(str.length() >= what.length()) {
-        return (0 == str.compare(str.length() - what.length(), what.length(), what));
-    } else {
-        return false;
-    }
-}
-
-bool StdToWX::StartsWith(const std::string& str, const std::string& what)
-{
-    if(str.length() >= what.length()) {
-        return (0 == str.compare(0, what.length(), what));
-    } else {
-        return false;
-    }
-}
-
 void StdToWX::Trim(std::string& str, bool fromRight)
 {
-    static std::string trimString = " \t\r\n";
+    static const std::string trimString = " \t\r\n";
     if(fromRight) {
         str.erase(str.find_last_not_of(trimString) + 1);
     } else {
         str.erase(0, str.find_first_not_of(trimString));
-    }
-}
-
-void StdToWX::RemoveLast(std::string& str, size_t count)
-{
-    if(str.length() >= count) {
-        str.erase(str.length() - count, count);
     }
 }
 
--- CodeLite/StdToWX.h
+++ CodeLite/StdToWX.h
@@ -37,29 +37,9 @@ class WXDLLIMPEXP_CL StdToWX
 {
 public:
     /**
-     * @brief remove from str string from offset 'from' with len
-     */
-    static void Remove(std::string& str, size_t from, size_t len);
-
-    /**
-     * @brief return true if str starts with what
-     */
-    static bool StartsWith(const std::string& str, const std::string& what);
-
-    /**
-     * @brief return true if str ends with what
-     */
-    static bool EndsWith(const std::string& str, const std::string& what);
-
-    /**
      * @brief
      */
     static void Trim(std::string& str, bool fromRight = true);
-
-    /**
-     * @brief remove last count bytes
-     */
-    static void RemoveLast(std::string& str, size_t count);
 
     /**
      * @brief convert std::vector into wxArrayString

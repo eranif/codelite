a2618f4e259e1f412e8ac76031b8acda1b703265
Jarod42 <Jarod42@users.noreply.github.com>
2026-04-15 21:03:41 +0200

    [cleanup] Remove uncalled methods from `CompletionHelper`

--- CodeLite/CompletionHelper.hpp
+++ CodeLite/CompletionHelper.hpp
@@ -16,12 +16,6 @@ public:
         STRIP_NO_NAME = (1 << 1),           // do not include the function argument name
     };
 
-    enum eTruncateStyle {
-        TRUNCATE_EXACT_POS = (1 << 0),
-        TRUNCATE_COMPLETE_WORDS = (1 << 1),
-        TRUNCATE_COMPLETE_LINES = (1 << 2),
-    };
-
 public:
     CompletionHelper() = default;
     ~CompletionHelper() = default;
@@ -39,29 +33,9 @@ public:
     wxString normalize_function(const TagEntry* tag,
                                 size_t flags = CompletionHelper::STRIP_NO_DEFAULT_VALUES |
                                                CompletionHelper::STRIP_NO_NAME);
-    wxString get_expression(const wxString& file_content, bool for_calltip, wxString* last_word = nullptr) const;
-    wxString truncate_file_to_location(const wxString& file_content, size_t line, size_t column, size_t flags) const;
     std::vector<wxString> split_function_signature(const wxString& signature,
                                                    wxString* return_value,
                                                    size_t flags = CompletionHelper::STRIP_DEFAULT) const;
-    static bool is_cxx_keyword(const wxString& word);
-
-    /**
-     * @brief return list of keywords
-     */
-    void get_cxx_keywords(std::vector<wxString>& keywords);
-
-    /**
-     * @brief format a tag comment
-     * @param tag the tag which owns the comment
-     * @param input_comment raw comment
-     * @return formatted comment
-     */
-    wxString format_comment(TagEntryPtr tag, const wxString& input_comment) const;
-    /**
-     * @brief same as the above, but accepts raw pointer
-     */
-    wxString format_comment(TagEntry* tag, const wxString& input_comment) const;
 
     /**
      * @brief go backward and check if the caret line is an include statement

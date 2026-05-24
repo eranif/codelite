eranif/codelite

commit 336d2d182d9b5c4f194c3fcd950d327336271912

Json nlohmann (#3902)
* Add direct support to `nlohmann` to `JSONItem`
* [cleanup] Use `nlohmann::json` in most places where `JSONItem::createObject()` were used.

CodeLite/JSON.cpp
@@ -98,6 +98,24 @@ JSONItem::JSONItem(std::shared_ptr<nlohmann::ordered_json> root, nlohmann::order
 {
 }
 
+JSONItem::JSONItem(const nlohmann::json& json)
+{
+    if (json.is_discarded()) {
+        return;
+    }
+    m_root = std::make_shared<nlohmann::ordered_json>(json);
+    m_json = m_root.get();
+}
+
+JSONItem::JSONItem(const nlohmann::ordered_json& json)
+{
+    if (json.is_discarded()) {
+        return;
+    }
+    m_root = std::make_shared<nlohmann::ordered_json>(json);
+    m_json = m_root.get();
+}
+
 char* JSONItem::FormatRawString(bool formatted) const
 {
     if (!m_json) {
@@ -375,9 +393,7 @@ JSONItem& JSONItem::addProperty(const wxString& name, bool value)
 }
 
 JSONItem& JSONItem::addProperty(const wxString& name, const wxString& value)
-{
-    return addProperty(name, value.ToStdString(wxConvUTF8));
-}
+{ return addProperty(name, value.ToStdString(wxConvUTF8)); }
 
 JSONItem& JSONItem::addProperty(const wxString& name, const std::string& value)
 {
@@ -388,9 +404,7 @@ JSONItem& JSONItem::addProperty(const wxString& name, const std::string& value)
 }
 
 JSONItem& JSONItem::addProperty(const wxString& name, const wxChar* value)
-{
-    return addProperty(name, wxString(value));
-}
+{ return addProperty(name, wxString(value)); }
 
 JSONItem& JSONItem::addProperty(const wxString& name, long value)
 {
@@ -440,14 +454,10 @@ JSONItem& JSONItem::addProperty(const wxString& name, const wxStringMap_t& strin
 JSONItem& JSONItem::addProperty(const wxString& name, size_t value) { return addProperty(name, (int)value); }
 
 JSONItem& JSONItem::addProperty(const wxString& name, const char* value, const wxMBConv& conv)
-{
-    return addProperty(name, wxString(value, conv));
-}
+{ return addProperty(name, wxString(value, conv)); }
 
 JSONItem& JSONItem::addProperty(const wxString& name, const wxFileName& filename)
-{
-    return addProperty(name, filename.GetFullPath());
-}
+{ return addProperty(name, filename.GetFullPath()); }
 
 JSONItem& JSONItem::addProperty(const wxString& name, JSON&& json)
 {

CodeLite/json_utils.h
@@ -66,6 +66,15 @@ inline wxStringMap_t ToStringMap(const nlohmann::json& json)
 
 inline std::string ToJsonValue(const wxSize& sz) { return std::to_string(sz.x) + "," + std::to_string(sz.y); }
 
+inline nlohmann::json ToJson(const wxArrayString& strings)
+{
+    nlohmann::json json = nlohmann::json::array();
+    for (const auto& s : strings) {
+        json.emplace_back(s.ToStdString(wxConvUTF8));
+    }
+    return json;
+}
+
 inline nlohmann::json ToJson(const wxStringMap_t& map)
 {
     nlohmann::json json;

AutoSave/AutoSaveSettings.cpp
@@ -14,12 +14,7 @@ void AutoSaveSettings::FromJSON(const JSONItem& json)
 }
 
 JSONItem AutoSaveSettings::ToJSON() const
-{
-    JSONItem json = JSONItem::createObject();
-    json.addProperty("m_flags", m_flags);
-    json.addProperty("m_checkInterval", m_checkInterval);
-    return json;
-}
+{ return nlohmann::json{{"m_flags", m_flags}, {"m_checkInterval", m_checkInterval}}; }
 
 AutoSaveSettings AutoSaveSettings::Load()
 {

CodeFormatter/SourceFormatterBase.cpp
@@ -17,14 +17,12 @@ void SourceFormatterBase::FromJSON(const JSONItem& json)
 
 JSONItem SourceFormatterBase::ToJSON() const
 {
-    JSONItem ele = JSONItem::createObject();
-    ele.addProperty("languages", m_languages);
-    ele.addProperty("flags", m_flags);
-    ele.addProperty("config_file", m_configFile);
-    ele.addProperty("name", m_name);
-    ele.addProperty("description", m_description);
-    ele.addProperty("short_description", m_shortDescription);
-    return ele;
+    return nlohmann::json{{"languages", JsonUtils::ToJson(m_languages)},
+                          {"flags", m_flags},
+                          {"config_file", m_configFile.ToStdString(wxConvUTF8)},
+                          {"name", m_name.ToStdString(wxConvUTF8)},
+                          {"description", m_description.ToStdString(wxConvUTF8)},
+                          {"short_description", m_shortDescription.ToStdString(wxConvUTF8)}};
 }
 
 bool SourceFormatterBase::CanHandle(FileExtManager::FileType file_type) const

CodeLite/JSON.h
@@ -27,6 +27,7 @@
 #define ZJSONNODE_H
 
 #include "codelite_exports.h"
+#include "json_utils.h"
 #include "macros.h"
 
 #include <assistant/common/json.hpp>
@@ -52,6 +53,9 @@ class WXDLLIMPEXP_CL JSONItem
 
 public:
     explicit JSONItem(std::nullptr_t) {}
+    JSONItem(const nlohmann::json&);
+    JSONItem(const nlohmann::ordered_json&);
+
     JSONItem() = default;
     virtual ~JSONItem() = default;
 

CodeLite/LSP/InitializedNotification.cpp
@@ -3,7 +3,7 @@
 namespace LSP
 {
 struct InitializedParams : public Params {
-    JSONItem ToJSON() const override { return JSONItem::createObject(); }
+    JSONItem ToJSON() const override { return nlohmann::json::object(); }
 
     void FromJSON(const JSONItem& json) override { wxUnusedVar(json); };
 };

CodeLite/LSP/Message.cpp
@@ -38,12 +38,7 @@ int ReadHeaders(const std::string& message, std::unordered_map<std::string, std:
 
 } // namespace
 
-JSONItem LSP::Message::ToJSON() const
-{
-    JSONItem json = JSONItem::createObject();
-    json.addProperty("jsonrpc", m_jsonrpc);
-    return json;
-}
+JSONItem LSP::Message::ToJSON() const { return nlohmann::json{{"jsonrpc", m_jsonrpc.ToStdString(wxConvUTF8)}}; }
 
 void LSP::Message::FromJSON(const JSONItem& json) { m_jsonrpc = json.namedObject("jsonrpc").toString(); }
 

CodeLite/LSP/WorkspaceSymbolRequest.cpp
@@ -34,12 +34,7 @@ public:
 
     void FromJSON(const JSONItem& json) override { m_query = json["query"].toString(); }
 
-    JSONItem ToJSON() const override
-    {
-        JSONItem json = JSONItem::createObject();
-        json.addProperty("query", m_query);
-        return json;
-    }
+    JSONItem ToJSON() const override { return nlohmann::json{{"query", m_query.ToStdString(wxConvUTF8)}}; }
 
     void SetQuery(const wxString& query) { this->m_query = query; }
     const wxString& GetQuery() const { return this->m_query; }

commit 90bb9702d25df5eb2a88da62829f7938de757ff0

[Fix] Fix bug introduced by 336d2d182d9b5c4f194c3fcd950d327336271912 (Json nlohmann (#3902)) `wxStringMap_t` was serialized as `[{"key": "key1", "value": "value1"}, {"key": "key2", "value": "value2"}]`, and I wrongly changed that `{"key1": "value1", "key2": "value2"}`. I change deserialization to support both.

CodeLite/JSON.cpp
@@ -317,15 +317,22 @@ std::unordered_map<std::string_view, JSONItem> JSONItem::GetAsMap() const
 
 wxStringMap_t JSONItem::toStringMap(const wxStringMap_t& default_map) const
 {
-    if (!m_json || !isArray()) {
+    if (!m_json || (!isArray() && !isObject())) {
         return default_map;
     }
     wxStringMap_t res;
-
-    for (int i = 0; i < arraySize(); ++i) {
-        wxString key = arrayItem(i).namedObject("key").toString();
-        wxString val = arrayItem(i).namedObject("value").toString();
-        res.emplace(key, val);
+    if (isArray()) {
+        for (int i = 0; i < arraySize(); ++i) {
+            wxString key = arrayItem(i).namedObject("key").toString();
+            wxString val = arrayItem(i).namedObject("value").toString();
+            res.emplace(key, val);
+        }
+    } else {
+        for (auto& [key, child] : m_json->items()) {
+            if (child.is_string()) {
+                res.emplace(key, wxString::FromUTF8(child.get<std::string>()));
+            }
+        }
     }
     return res;
 }

CodeLite/json_utils.h
@@ -57,11 +57,25 @@ inline wxSize ToSize(const nlohmann::json& json)
 
 inline wxStringMap_t ToStringMap(const nlohmann::json& json)
 {
-    wxStringMap_t res;
-    for (const auto& [key, item] : json.items()) {
-        res[wxString::FromUTF8(key)] = wxString::FromUTF8(item);
+    if (json.is_object()) {
+        wxStringMap_t res;
+        for (const auto& [key, item] : json.items()) {
+            res[wxString::FromUTF8(key)] = wxString::FromUTF8(item);
+        }
+        return res;
+    } else if (json.is_array()) {
+        wxStringMap_t res;
+        for (const auto& child : json) {
+            if (!child.is_object() || !child.contains("key") || !child.contains("value")) {
+                continue;
+            }
+            res[wxString::FromUTF8(child["key"].get<std::string>())] =
+                wxString::FromUTF8(child["value"].get<std::string>());
+        }
+        return res;
+    } else {
+        return {};
     }
-    return res;
 }
 
 inline std::string ToJsonValue(const wxSize& sz) { return std::to_string(sz.x) + "," + std::to_string(sz.y); }

Tests/CodeliteTest/JsonTests.cpp
@@ -361,6 +361,7 @@ TEST_CASE("toStringMap") {
     root.toElement().addProperty("map", map);
 
     CHECK(map == root.toElement()["map"].toStringMap());
+    CHECK(map == JSONItem{JsonUtils::ToJson(map)}.toStringMap());
 }
 
 TEST_CASE("GetAsVector") {

Tests/CodeliteTest/json_utilsTests.cpp
@@ -12,4 +12,5 @@ TEST_CASE("wxStringMap_t")
 {
     const wxStringMap_t map = {{"hello", "world"}, {"key", "value"}};
     CHECK(map == JsonUtils::ToStringMap(JsonUtils::ToJson(map)));
+    CHECK(map == JsonUtils::ToStringMap(nlohmann::json{nlohmann::json{{"key", "hello"}, {"value", "world"}}, nlohmann::json{{"key", "key"}, {"value", "value"}}}));
 }
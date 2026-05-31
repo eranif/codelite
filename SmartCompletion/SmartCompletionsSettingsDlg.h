c67f7514bf4ba727e1a1e19615c71ec76b5f553f
Jarod42 <Jarod42@users.noreply.github.com>
2026-05-31 08:47:55 +0200

    Remove generated files from SmartCompletion

--- SmartCompletion/SmartCompletionsSettingsDlg.h
+++ SmartCompletion/SmartCompletionsSettingsDlg.h
@@ -1,7 +1,7 @@
 #ifndef SMARTCOMPLETIONSSETTINGSDLG_H
 #define SMARTCOMPLETIONSSETTINGSDLG_H
 #include "SmartCompletionsConfig.h"
-#include "SmartCompletionsUI.h"
+#include "SmartCompletionsUI.hpp"
 
 class SmartCompletionsSettingsDlg : public SmartCompletionsSettingsBaseDlg
 {

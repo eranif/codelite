85018098be08026b0805d2cc48721455e7a26c7b
Joris Dauphin <Jarod42@users.noreply.github.com>
2026-06-21 17:32:40 +0200

    Split `ImportFromwxFB` into utility functions and `Importer` (GUI) class. (#4018)

--- wxcrafter/Importer/import_from_wxFB.h
+++ wxcrafter/Importer/import_from_wxFB.h
@@ -1,38 +1,18 @@
 #ifndef IMPORTFROMFB_H
 #define IMPORTFROMFB_H
 
-#include "import_dlg.h"
 #include "wxc_widget.h"
 #include "wxgui_defs.h"
 
-#include <map>
-
-class wxWindow;
-class NotebookPageWrapper;
-
-class ImportFromwxFB
+namespace ImportFromwxFB
 {
-    static std::map<wxString, wxString> sm_eventMap;
-    wxWindow* m_Parent = nullptr;
+wxString ConvertFBOptionsString(const wxString& content, const wxString& separator = ";");
+void ProcessBitmapProperty(const wxString& value,
+                           wxcWidget* wrapper,
+                           const wxString& property = PROP_BITMAP_PATH,
+                           const wxString& client_hint = "");
+wxString GetEventtypeFromHandlerstub(const wxString& stub);
 
-public:
-    ImportFromwxFB(wxWindow* parent);
-    ~ImportFromwxFB() = default;
-
-    bool ImportProject(ImportDlg::ImportFileData& data, const wxString& sourceFile = "") const;
-    static wxString ConvertFBOptionsString(const wxString& content, const wxString& separator = ";");
-    static void ProcessBitmapProperty(const wxString& value,
-                                      wxcWidget* wrapper,
-                                      const wxString& property = PROP_BITMAP_PATH,
-                                      const wxString& client_hint = "");
-    static wxString GetEventtypeFromHandlerstub(const wxString& stub);
-
-protected:
-    bool ParseFile(wxXmlDocument& doc, wxcWidget::List_t& toplevels) const;
-    wxcWidget* ParseNode(wxXmlNode* node, wxcWidget* parentwrapper, bool& alreadyParented) const;
-    void GetSizeritemContents(const wxXmlNode* node, wxcWidget* wrapper) const;
-    void GetGridBagSizerItem(const wxXmlNode* node, wxcWidget* wrapper) const;
-    void GetBookitemContents(const wxXmlNode* node, NotebookPageWrapper* wrapper, int& depth) const;
-};
+} // namespace ImportFromwxFB
 
 #endif // IMPORTFROMFB_H
---
+++ wxcrafter/Importer/importer_from_wxFB.h
@@ -0,0 +1,35 @@
+#ifndef IMPORTERFROMFB_H
+#define IMPORTERFROMFB_H
+
+#include "import_dlg.h"
+#include "wxc_widget.h"
+#include "wxgui_defs.h"
+
+class wxWindow;
+class NotebookPageWrapper;
+
+namespace ImportFromwxFB
+{
+
+class Importer
+{
+public:
+    Importer(wxWindow* parent);
+    ~Importer() = default;
+
+    bool ImportProject(ImportDlg::ImportFileData& data, const wxString& sourceFile = "") const;
+
+protected:
+    bool ParseFile(wxXmlDocument& doc, wxcWidget::List_t& toplevels) const;
+    wxcWidget* ParseNode(wxXmlNode* node, wxcWidget* parentwrapper, bool& alreadyParented) const;
+    void GetSizeritemContents(const wxXmlNode* node, wxcWidget* wrapper) const;
+    void GetGridBagSizerItem(const wxXmlNode* node, wxcWidget* wrapper) const;
+    void GetBookitemContents(const wxXmlNode* node, NotebookPageWrapper* wrapper, int& depth) const;
+
+private:
+    wxWindow* m_Parent = nullptr;
+};
+
+} // namespace ImportFromwxFB
+
+#endif // IMPORTERFROMFB_H

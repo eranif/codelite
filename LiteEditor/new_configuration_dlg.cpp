233124c1d227247809ba7992b401cf93d033f5d7
Jarod42 <Jarod42@users.noreply.github.com>
2026-07-11 18:53:17 +0200

    Split out non-generated-code from file new_configuration_dlg and add new_configuration_dlg_base for generated-code

--- LiteEditor/new_configuration_dlg.cpp
+++ LiteEditor/new_configuration_dlg.cpp
@@ -22,26 +22,9 @@
 //
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version May  5 2007)
-// http://www.wxformbuilder.org/
-//
-// PLEASE DO "NOT" EDIT THIS FILE!
-///////////////////////////////////////////////////////////////////////////
-
-#ifdef WX_PRECOMP
-
-#include "wx/wxprec.h"
-
-#ifdef __BORLANDC__
-#pragma hdrstop
-#endif //__BORLANDC__
-
-#else
-#include <wx/wx.h>
-#endif // WX_PRECOMP
 
 #include "new_configuration_dlg.h"
+
 #include "manager.h"
 #include "project_settings.h"
 #include "macros.h"
@@ -50,33 +33,14 @@
 
 NewConfigurationDlg::NewConfigurationDlg(wxWindow* parent, const wxString& projectName, int id, wxString title,
                                          wxPoint pos, wxSize size, int style)
-    : wxDialog(parent, id, title, pos, size, style)
+    : NewConfigurationDlgBase(parent, id, title, pos, size, style)
     , m_projectName(projectName)
 {
     this->SetSizeHints(wxDefaultSize, wxDefaultSize);
 
-    wxBoxSizer* bSizer1;
-    bSizer1 = new wxBoxSizer(wxVERTICAL);
-
-    m_panel1 = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL);
-    wxBoxSizer* bSizer3;
-    bSizer3 = new wxBoxSizer(wxVERTICAL);
-
-    m_staticText1 = new wxStaticText(m_panel1, wxID_ANY, _("Configuration Name:"), wxDefaultPosition, wxDefaultSize, 0);
-    bSizer3->Add(m_staticText1, 0, wxALL, 5);
-
     wxTextValidator validator(wxFILTER_ASCII);
-    m_textConfigurationName =
-        new wxTextCtrl(m_panel1, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, validator);
-    bSizer3->Add(m_textConfigurationName, 0, wxALL | wxEXPAND, 5);
+    m_textConfigurationName->SetValidator(validator);
     m_textConfigurationName->SetFocus();
-
-    m_staticText2 = new wxStaticText(m_panel1, wxID_ANY, _("Copy Settings from:"), wxDefaultPosition, wxDefaultSize, 0);
-    bSizer3->Add(m_staticText2, 0, wxALL, 5);
-
-    wxArrayString m_choiceCopyConfigurationsChoices;
-    m_choiceCopyConfigurations =
-        new wxChoice(m_panel1, wxID_ANY, wxDefaultPosition, wxDefaultSize, m_choiceCopyConfigurationsChoices, 0);
 
     // Get all configuration of the project
     m_choiceCopyConfigurations->Append(_("-- None --"));
@@ -90,30 +54,13 @@ NewConfigurationDlg::NewConfigurationDlg(wxWindow* parent, const wxString& proje
         }
     }
     m_choiceCopyConfigurations->SetSelection(0);
-    bSizer3->Add(m_choiceCopyConfigurations, 0, wxALL | wxEXPAND, 5);
 
-    m_panel1->SetSizer(bSizer3);
     m_panel1->Layout();
-    bSizer3->Fit(m_panel1);
-    bSizer1->Add(m_panel1, 1, wxEXPAND | wxALL, 5);
-
-    m_staticline1 = new wxStaticLine(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxLI_HORIZONTAL);
-    bSizer1->Add(m_staticline1, 0, wxEXPAND | wxALL, 5);
-
-    wxBoxSizer* bSizer2;
-    bSizer2 = new wxBoxSizer(wxHORIZONTAL);
-
-    m_buttonOK = new wxButton(this, wxID_ANY, _("&OK"), wxDefaultPosition, wxDefaultSize, 0);
-    bSizer2->Add(m_buttonOK, 0, wxALL, 5);
-
-    m_buttonCancel = new wxButton(this, wxID_CANCEL, _("Cancel"), wxDefaultPosition, wxDefaultSize, 0);
-    bSizer2->Add(m_buttonCancel, 0, wxALL, 5);
-
-    bSizer1->Add(bSizer2, 0, wxALIGN_RIGHT, 5);
+    // bSizer3->Fit(m_panel1);
 
     ConnectButton(m_buttonOK, NewConfigurationDlg::OnButtonOK);
     m_buttonOK->SetDefault();
-    this->SetSizer(bSizer1);
+
     CentreOnScreen();
     GetSizer()->Fit(this);
     CenterOnParent();

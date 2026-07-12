b1b0b7d15de6a1ca6b4e0f6c877d26b29f6b7efe
Joris Dauphin <Jarod42@users.noreply.github.com>
2026-07-12 16:52:00 +0200

    Split out non-generated-code from file new_configuration_dlg and add new_configuration_dlg_base for generated-code (#4057)

--- LiteEditor/new_configuration_dlg.h
+++ LiteEditor/new_configuration_dlg.h
@@ -22,40 +22,19 @@
 //
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version May  5 2007)
-// http://www.wxformbuilder.org/
-//
-// PLEASE DO "NOT" EDIT THIS FILE!
-///////////////////////////////////////////////////////////////////////////
 
 #ifndef __new_configuration_dlg__
 #define __new_configuration_dlg__
 
-#include <wx/wx.h>
-
-#include <wx/choice.h>
-#include <wx/panel.h>
-#include <wx/statline.h>
-#include <wx/button.h>
-
-///////////////////////////////////////////////////////////////////////////
+#include "new_configuration_dlg_base.hpp"
 
 ///////////////////////////////////////////////////////////////////////////////
 /// Class NewConfigurationDlg
 ///////////////////////////////////////////////////////////////////////////////
-class NewConfigurationDlg : public wxDialog
+class NewConfigurationDlg : public NewConfigurationDlgBase
 {
 private:
     wxString m_projectName;
-    wxPanel* m_panel1;
-    wxStaticText* m_staticText1;
-    wxTextCtrl* m_textConfigurationName;
-    wxStaticText* m_staticText2;
-    wxChoice* m_choiceCopyConfigurations;
-    wxStaticLine* m_staticline1;
-    wxButton* m_buttonOK;
-    wxButton* m_buttonCancel;
 
 protected:
     void OnButtonOK(wxCommandEvent& event);

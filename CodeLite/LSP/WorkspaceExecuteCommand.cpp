3399095473bbc05677884657b67d8ab6fcac8f99
Joris Dauphin <Jarod42@users.noreply.github.com>
2026-09-13 19:01:45 +0200

    Show dialogBox when LSP cannot fix a suggestion (as renaming namespace for clangd) (#4132)

--- CodeLite/LSP/WorkspaceExecuteCommand.cpp
+++ CodeLite/LSP/WorkspaceExecuteCommand.cpp
@@ -1,9 +1,16 @@
 #include "WorkspaceExecuteCommand.hpp"
 
+#include "LSP/ResponseError.h"
+
+#include <wx/msgdlg.h>
+
 namespace LSP
 {
-WorkspaceExecuteCommand::WorkspaceExecuteCommand(const wxString& filepath, const LSP::Command& command)
-    : m_filepath(filepath)
+WorkspaceExecuteCommand::WorkspaceExecuteCommand(const wxString& languageServerName,
+                                                 const wxString& filepath,
+                                                 const LSP::Command& command)
+    : m_languageServerName(languageServerName)
+    , m_filepath(filepath)
 {
     SetMethod("workspace/executeCommand");
     m_params.reset(new ExecuteCommandParams(command.GetCommand(), command.GetArguments()));
@@ -17,5 +24,18 @@ std::optional<LSPEvent> WorkspaceExecuteCommand::OnResponse(const LSP::ResponseM
     LSP_DEBUG() << "WorkspaceExecuteCommand::OnResponse()" << endl;
     LSP_DEBUG() << response.ToString() << endl;
     return std::nullopt;
+}
+
+void WorkspaceExecuteCommand::HandleError(const LSP::ResponseMessage& response,
+                                          [[maybe_unused]] wxEvtHandler* owner) /* override */
+{
+    // an example for such an error:
+    // {"error":{"code":-32001,
+    //  "message":"Cannot rename symbol: symbol is not a supported kind (e.g. namespace, macro)"},
+    //  "id":42,"jsonrpc":"2.0"}
+    LSP::ResponseError errMsg(response.ToString());
+    wxMessageBox(wxString::Format(_("Error from language server %s:\n%s"), m_languageServerName, errMsg.GetMessage()),
+                 m_languageServerName,
+                 wxICON_ERROR | wxCENTER);
 }
 } // namespace LSP
--- CodeLite/LSP/WorkspaceExecuteCommand.hpp
+++ CodeLite/LSP/WorkspaceExecuteCommand.hpp
@@ -7,13 +7,16 @@ namespace LSP
 {
 class WXDLLIMPEXP_CL WorkspaceExecuteCommand : public LSP::Request
 {
-    wxString m_filepath;
-
 public:
-    WorkspaceExecuteCommand(const wxString& filepath, const LSP::Command& command);
-    virtual ~WorkspaceExecuteCommand() = default;
+    WorkspaceExecuteCommand(const wxString& languageServerName, const wxString& filepath, const LSP::Command& command);
+    ~WorkspaceExecuteCommand() override = default;
 
     std::optional<LSPEvent> OnResponse(const LSP::ResponseMessage& response, wxEvtHandler* owner) override;
+    void HandleError(const LSP::ResponseMessage& response, wxEvtHandler* owner) override;
+
+private:
+    wxString m_languageServerName;
+    wxString m_filepath;
 };
 } // namespace LSP
 
